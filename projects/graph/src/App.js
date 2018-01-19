f68b9090f0b62b69b0a1455b29cfd047585e298c
analogy <actweed47@gmail.com>
2018-01-19 06:13:25 -0500

    modified app.js and graph.js

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 800;
+const canvasHeight = 600;
 
 /**
  * GraphView
@@ -30,26 +30,65 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+
     // Clear it
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // also can use canvas.width = canvas.width;
 
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
-    // draw vert values (labels)
+    // draw vert values (label)
+
+    //Edges
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#CCCC00';
+
+    // Let V be the set of vertices in G
+    for (let v of this.props.graph.vertexes) {
+      // Let E the set of edges in G (vertex)
+      for (let e of v.edges) {
+        ctx.beginPath();
+        ctx.moveTo(v.pos.x, v.pos.y); //Point1 of edge
+        const v2 = e.destination; //
+        ctx.lineTo(v2.pos.x, v2.pos.y); // connect 2 points with a line
+        ctx.stroke(); // fill the line in
+      }
+    }
+
+    // Vertexes
+    ctx.fillStyle = 'purple';
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'orange';
+
+    // Let V be the set of vertices in G
+    for (let v of this.props.graph.vertexes) {
+      ctx.beginPath();
+      ctx.arc(v.pos.x, v.pos.y, 22, 0, 2 * Math.PI, false);
+      ctx.fill();
+      ctx.stroke();
+    }
+
+    // Labels
+    ctx.fillStyle = 'black';
+    ctx.font = 'italic 12pt Calibri';
+    ctx.textAlign = 'center';
+
+    for (let v of this.props.graph.vertexes) {
+      ctx.beginPath();
+      ctx.fillText(v.value, v.pos.x, v.pos.y);
+    }
   }
-  
+
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
-
 
 /**
  * App
@@ -64,12 +103,14 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    this.state.graph.randomize(5, 3, 150, 0.6);
+    this.state.graph.bfs(this.state.graph.vertexes[0]);
   }
 
   render() {
     return (
       <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
+        <GraphView graph={this.state.graph} />
       </div>
     );
   }
