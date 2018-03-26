754320a9a0687c63287ebd3357d406d2eb75e097
atiffany <abby.tiffany@gmail.com>
2018-03-26 15:58:11 -0700

    initial drawings

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 /**
  * GraphView
@@ -31,9 +31,52 @@ class GraphView extends Component {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
     
-    // Clear it
-    ctx.fillStyle = 'white';
+    // sky
+    ctx.fillStyle = 'blue';
+    var gradient = ctx.createLinearGradient(0, 120, 0, 400);
+    gradient.addColorStop(0, "cyan");
+    gradient.addColorStop(1, "blue");
+    ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // beach
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 400, 500, 100);
+
+    // sun
+    ctx.beginPath();
+    ctx.arc(20, 20, 90, 0, 2*Math.PI);
+    ctx.fillStyle = "orange";
+    ctx.fill();
+
+    // sun rays
+    ctx.strokeStyle = "orange";
+    ctx.moveTo(140, 15);
+    ctx.lineTo(210, 17);
+    ctx.moveTo(130, 70);
+    ctx.lineTo(190, 100);
+    ctx.moveTo(80, 110);
+    ctx.lineTo(130, 180);
+    ctx.moveTo(30, 135);
+    ctx.lineTo(45, 200);
+    ctx.stroke();
+    
+    // sand (don't tell Anakin)
+    for(var i = 0; i < 300; i++) {
+      ctx.strokeStyle = 'salmon';
+      ctx.moveTo(4 * i, 400);
+      ctx.lineTo(5.75 * i, 500);
+      ctx.stroke();
+    }
+
+    // seafoam
+    ctx.strokeStyle = 'rgba(255, 255, 255, .5)';
+    for (var j = 0; j < 400; j++) {
+      ctx.moveTo(500, 390);
+      ctx.lineTo(4 * j, 400);
+    }
+    ctx.stroke();
+
 
     // !!! IMPLEMENT ME
     // compute connected components
