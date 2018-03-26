c7005b5a1794f9c6546ebc4ef325fbb31e83852b
etisdew <26851121+etisdew@users.noreply.github.com>
2018-03-26 16:01:31 -0700

    test

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = window.innerWidth;
+const canvasHeight = (window.innerWidth * .80 | 0);
 
 /**
  * GraphView
@@ -23,17 +23,50 @@ class GraphView extends Component {
   componentDidUpdate() {
     this.updateCanvas();
   }
+  
+  randomHEX() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  randomRGB() {
+    return `rgb(${((Math.random() * 255) + 1) | 0}, ${((Math.random() * 255) + 1) | 0}, ${((Math.random() * 255) + 1) | 0}`;
+  }
+
+  randomRGBA(r, g, b, a) {
+    return `rgba(${!!r ? r : ((Math.random() * 255) + 1) | 0}, ${!!g ? g : ((Math.random() * 255) + 1) | 0}, ${!!b ? b : ((Math.random() * 255) + 1) | 0}, ${!!a ? a : Math.random().toFixed(2) }`;
+  }
+
 
   /**
    * Render the canvas
    */
   updateCanvas() {
     let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
+    let c = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    c.fillStyle = '#000000';
+    c.fillRect(0, 0, canvasWidth, canvasHeight);
+    let i = 0;
+    
+    // console.log(c.strokeStyle);
+    
+    
+    let stroke = (() => {
+      do {
+        c.beginPath();
+        c.moveTo(i * 2, i / 2);
+        c.strokeStyle = this.randomRGBA(i, i, i, 0.1);
+        c.lineTo(i < canvasWidth ? canvasWidth / 2: 0, i < canvasHeight ? canvasHeight / 2 : i / 2);//, (Math.random() * canvasHeight) + 1)|0;
+        c.stroke();
+        i += 1;
+      } while (i <=  127);
+      })();
+
+      stroke;
+      stroke;
+
+
 
     // !!! IMPLEMENT ME
     // compute connected components
---
+++ projects/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './graph/src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: './index.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env', 'react']
+      }
+    },
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      loader: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.png$/,
+      exclude: /node_modules/,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            optipng: { optimizationLevel: 7 },
+            pngquant: { quality: "65-90", speed: 4 }
+          }
+        }]
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './graph/src/index.html' })]
+};
