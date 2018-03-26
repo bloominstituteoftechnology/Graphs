c886df32c98b3a4093719e4c8a83221229136268
Moises Dobarganes <designerexpert@gmail.com>
2018-03-26 17:29:59 -0400

    Messing around drawing on canvas some procedural stuff

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1280;
+const canvasHeight = 720;
 
 /**
  * GraphView
@@ -30,10 +30,42 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // Draw Pretty Picture
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    let centerX = canvasWidth / 2;
+    let centerY = canvasHeight / 2;
+    let radius = -540;
+    let radius2 = 0;
+    for (let i = 0.25; i < 1080; i = i + 0.25) {
+      let strokeColor = getRandomColor();
+      let x = centerX + (radius + i) * Math.cos(-i * Math.PI / 180) * -1;
+      let y = centerY + (radius + i) * Math.sin(-i * Math.PI / 180) * -1;
+      let lineX = centerX + (radius + i) * Math.cos(-i * Math.PI / 180) * -3;
+      let lineY = centerY + (radius + i) * Math.sin(-i * Math.PI / 180) * -3;
+      ctx.strokeStyle = `${strokeColor}55`;
+      ctx.beginPath();
+      ctx.fillStyle = `${strokeColor}33`;
+      ctx.arc(x, y, 9, 0.01, 0.01 * Math.PI);
+      ctx.stroke();
+      ctx.lineTo(lineX, lineY);
+      ctx.stroke();
+      ctx.fill();
+    }
+
+    // End Draw Pretty Picture
 
     // !!! IMPLEMENT ME
     // compute connected components
@@ -41,7 +73,7 @@ class GraphView extends Component {
     // draw verts
     // draw vert values (labels)
   }
-  
+
   /**
    * Render
    */
