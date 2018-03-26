d4b7b3e7ccc82156dc500864ff46c9c65b80cb13
JasonCorp84 <csababalogh84@gmail.com>
2018-03-26 15:59:53 -0700

    Csaba Balogh

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 700;
+const canvasHeight = 700;
 
 /**
  * GraphView
@@ -30,10 +30,45 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+    let radius = 15;
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    
+
+    ctx.beginPath();
+    ctx.arc(350,200, 30,0,2*Math.PI);
+    ctx.stroke();
+    ctx.moveTo(350,230);
+    ctx.lineTo(200,370);
+    ctx.stroke();
+
+    ctx.moveTo(350,230);
+    ctx.lineTo(300,370);
+    ctx.stroke();
+
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.strokeText("1",345,207);
+
+    ctx.beginPath();
+    ctx.arc(200,400, 30,0,2*Math.PI);
+    ctx.stroke();
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.strokeText("2",195,395);
+
+    ctx.beginPath();
+    ctx.arc(300,400, 30,0,2*Math.PI);
+    ctx.stroke();
+    ctx.font = "30px Arial";
+    ctx.textAlign = "center";
+    ctx.strokeText("3",295,395);
+    
+
+    
+   
 
     // !!! IMPLEMENT ME
     // compute connected components
