fcf8227c296af6177b5d2fcda81f3dde0908ed85
Ting Wang <felix.wang.1026@gmail.com>
2018-03-26 15:57:00 -0400

    coded the vertex, edge, bfs, and getConnectedComponents

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -2,77 +2,77 @@ import React, { Component } from 'react';
 import { Graph } from './graph';
 import './App.css';
 
-// !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
 
 /**
  * GraphView
  */
 class GraphView extends Component {
-  /**
-   * On mount
-   */
-  componentDidMount() {
-    this.updateCanvas();
-  }
+    /**
+     * On mount
+     */
+    componentDidMount() {
+        this.updateCanvas();
+    }
 
-  /**
-   * On state update
-   */
-  componentDidUpdate() {
-    this.updateCanvas();
-  }
+    /**
+     * On state update
+     */
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
 
-  /**
-   * Render the canvas
-   */
-  updateCanvas() {
-    let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
-    // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    /**
+     * Render the canvas
+     */
+    updateCanvas() {
+        let canvas = this.refs.canvas;
+        let ctx = canvas.getContext('2d');
 
-    // !!! IMPLEMENT ME
-    // compute connected components
-    // draw edges
-    // draw verts
-    // draw vert values (labels)
-  }
-  
-  /**
-   * Render
-   */
-  render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
-  }
+        // Clear it
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        // !!! IMPLEMENT ME
+        // compute connected components
+        // draw edges
+        // draw verts
+        // draw vert values (labels)
+    }
+
+    /**
+     * Render
+     */
+    render() {
+        return (
+            <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />
+        );
+    }
 }
-
 
 /**
  * App
  */
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      graph: new Graph()
-    };
+        this.state = {
+            graph: new Graph()
+        };
 
-    // !!! IMPLEMENT ME
-    // use the graph randomize() method
-  }
+        // !!! IMPLEMENT ME
+        // use the graph randomize() method
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <GraphView graph={this.state.graph} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- projects/graph/src/graph.js
+++ projects/graph/src/graph.js
@@ -2,121 +2,171 @@
  * Edge
  */
 export class Edge {
-  // !!! IMPLEMENT ME
+    constructor(destination, weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
 }
 
 /**
  * Vertex
  */
 export class Vertex {
-  // !!! IMPLEMENT ME
+    constructor(value) {
+        this.edges = [];
+        this.pos = {};
+        this.value = value;
+    }
 }
 
 /**
  * Graph
  */
 export class Graph {
-  constructor() {
-    this.vertexes = [];
-  }
-
-  /**
-   * Create a random graph
-   */
-  randomize(width, height, pxBox, probability=0.6) {
-    // Helper function to set up two-way edges
-    function connectVerts(v0, v1) {
-      v0.edges.push(new Edge(v1));
-      v1.edges.push(new Edge(v0));
+    constructor() {
+        this.vertexes = [];
     }
 
-    let count = 0;
-
-    // Build a grid of verts
-    let grid = [];
-    for (let y = 0; y < height; y++) {
-      let row = [];
-      for (let x = 0; x < width; x++) {
-        let v = new Vertex();
-        //v.value = 'v' + x + ',' + y;
-        v.value = 'v' + count++;
-        row.push(v);
-      }
-      grid.push(row);
-    }
-
-    // Go through the grid randomly hooking up edges
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        // Connect down
-        if (y < height - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y+1][x]);
-          }
+    /**
+     * Create a random graph
+     */
+    randomize(width, height, pxBox, probability = 0.6) {
+        // Helper function to set up two-way edges
+        function connectVerts(v0, v1) {
+            v0.edges.push(new Edge(v1));
+            v1.edges.push(new Edge(v0));
         }
 
-        // Connect right
-        if (x < width - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y][x+1]);
-          }
+        let count = 0;
+
+        // Build a grid of verts
+        let grid = [];
+        for (let y = 0; y < height; y++) {
+            let row = [];
+            for (let x = 0; x < width; x++) {
+                let v = new Vertex();
+                //v.value = 'v' + x + ',' + y;
+                v.value = 'v' + count++;
+                row.push(v);
+            }
+            grid.push(row);
         }
-      }
+
+        // Go through the grid randomly hooking up edges
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                // Connect down
+                if (y < height - 1) {
+                    if (Math.random() < probability) {
+                        connectVerts(grid[y][x], grid[y + 1][x]);
+                    }
+                }
+
+                // Connect right
+                if (x < width - 1) {
+                    if (Math.random() < probability) {
+                        connectVerts(grid[y][x], grid[y][x + 1]);
+                    }
+                }
+            }
+        }
+
+        // Last pass, set the x and y coordinates for drawing
+        const boxBuffer = 0.8;
+        const boxInner = pxBox * boxBuffer;
+        const boxInnerOffset = (pxBox - boxInner) / 2;
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                grid[y][x].pos = {
+                    x:
+                        (x * pxBox +
+                            boxInnerOffset +
+                            Math.random() * boxInner) |
+                        0,
+                    y:
+                        (y * pxBox +
+                            boxInnerOffset +
+                            Math.random() * boxInner) |
+                        0
+                };
+            }
+        }
+
+        // Finally, add everything in our grid to the vertexes in this Graph
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.vertexes.push(grid[y][x]);
+            }
+        }
     }
 
-    // Last pass, set the x and y coordinates for drawing
-    const boxBuffer = 0.8;
-    const boxInner = pxBox * boxBuffer;
-    const boxInnerOffset = (pxBox - boxInner) / 2;
+    /**
+     * Dump graph data to the console
+     */
+    dump() {
+        let s;
 
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        grid[y][x].pos = {
-          'x': (x * pxBox + boxInnerOffset + Math.random() * boxInner) | 0,
-          'y': (y * pxBox + boxInnerOffset + Math.random() * boxInner) | 0
-        };
-      }
+        for (let v of this.vertexes) {
+            if (v.pos) {
+                s = v.value + ' (' + v.pos.x + ',' + v.pos.y + '):';
+            } else {
+                s = v.value + ':';
+            }
+
+            for (let e of v.edges) {
+                s += ` ${e.destination.value}`;
+            }
+            console.log(s);
+        }
     }
 
-    // Finally, add everything in our grid to the vertexes in this Graph
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        this.vertexes.push(grid[y][x]);
-      }
+    /**
+     * BFS
+     */
+    bfs(start) {
+        const queue = [];
+        const connectedComponents = [];
+
+        for (let v of this.vertexes) {
+            v.color = 'white';
+        }
+
+        start.color = 'gray';
+        queue.push(start);
+
+        while (!queue.length) {
+            let nextVertex = queue[0];
+            for (let e of nextVertex.edges) {
+                let v = e.destination;
+                if (v.color === 'white') {
+                    v.color = 'gray';
+                    queue.push(v);
+                }
+            }
+            queue.shift();
+            nextVertex.color = 'black';
+
+            connectedComponents.push(nextVertex);
+        }
+        return connectedComponents;
     }
-  }
 
-  /**
-   * Dump graph data to the console
-   */
-  dump() {
-    let s;
+    /**
+     * Get the connected components
+     */
+    getConnectedComponents() {
+        const connectedComponents = [];
+        for (let v of this.vertexes) {
+            v.color = 'white';
+        }
 
-    for (let v of this.vertexes) {
-      if (v.pos) {
-        s = v.value + ' (' + v.pos.x + ',' + v.pos.y + '):';
-      } else {
-        s = v.value + ':';
-      }
-
-      for (let e of v.edges) {
-        s += ` ${e.destination.value}`;
-      }
-      console.log(s);
+        for (let v of this.vertexes) {
+            if (v.color === 'white') {
+                let component = this.bfs(v);
+                connectedComponents.push(component);
+            }
+        }
+        return connectedComponents;
     }
-  }
-
-  /**
-   * BFS
-   */
-  bfs(start) {
-    // !!! IMPLEMENT ME
-  }
-
-  /**
-   * Get the connected components
-   */
-  getConnectedComponents() {
-    // !!! IMPLEMENT ME
-  }
 }
