c1d6815d8fea57807b1c39582353cb65abd33f9f
Michael Dennis <michaeladennis@yahoo.com>
2018-03-26 15:59:34 -0700

    adding some stuffs

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import { Graph } from './graph';
 import './App.css';
 
-// !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 400;
+const canvasHeight = 400;
 
 /**
  * GraphView
@@ -30,11 +29,21 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+    const getRndColor = () => {
+      return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    } 
     // Clear it
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for(let i = 0; i < 1000; i++)
+    {    
+      ctx.moveTo(Math.floor( Math.random() * canvas.width ) / 3, Math.floor( Math.random() * canvas.height ) / 3);
+      ctx.beginPath();
+      ctx.fillStyle = getRndColor();
+      ctx.arc( (i % 2 === 0) ? canvas.width/2 + i * Math.random() : canvas.width/2 - i* Math.random(), (i % 2 === 0) ? canvas.height/2 + i * Math.random(): canvas.height/2 - i* Math.random(), Math.floor(Math.random() * 25), 0, Math.PI * 2, false); // Outer circle
+      ctx.stroke();
+    }
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
