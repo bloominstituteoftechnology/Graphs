d03b188c9ea28146a6f029247828d90965d29e1d
Aaron Wilder <element1028@gmail.com>
2018-03-26 15:59:23 -0700

    drawing stuff

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 640;
+const canvasHeight = 480;
 
 /**
  * GraphView
@@ -32,8 +32,29 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // ctx.moveTo(80,80);
+    for (var i = 0; i < 100; i++) {
+      for (var j = 0; j < 100; j++) {
+        for (var p = 0; p < 100; p++) {
+        ctx.fillStyle = `rgb(${Math.floor(255 - 10.5 * i)}, ${Math.floor(255 - 10.5 * j)}, ${Math.floor(255 - 10.5 * p)})`;
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
+      }
+    }
+    for (var i = 0; i < 20; i++) {
+      if (ctx.lineWidth > 10) {
+        ctx.lineWidth = 1;
+      } else {
+        ctx.lineWidth = 1 + i;
+      }
+      ctx.beginPath();
+      ctx.moveTo(1 + i + 20, i + 1);
+      ctx.lineTo(1 + i * 80, 500);
+      ctx.stroke();
+    }
+    
+  
+
 
     // !!! IMPLEMENT ME
     // compute connected components
