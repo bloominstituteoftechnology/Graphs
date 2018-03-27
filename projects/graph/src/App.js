16654006dad982f5be8d0ab773f0ee8e200fda4b
Moises Dobarganes <designerexpert@gmail.com>
2018-03-27 15:23:05 -0400

    Rough Draft for Activating the selected Vertices on Graph and changing their fill color to navy blue

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -5,13 +5,12 @@ import './App.css';
 // !!! IMPLEMENT ME
 const canvasWidth = 750;
 const canvasHeight = 600;
-// Styling the Vertices
-const xpadding = 8;
-const ypadding = 4;
 // Sizing the Graph
 const xsize = 5;
 const ysize = 4;
 const jitter = 150;
+// Sizing the Vertices
+const vRadius = 20;
 
 /**
  * GraphView
@@ -55,7 +54,7 @@ class GraphView extends Component {
     // --- accessible through 'this.props.graph'
 
     const connectedComponents = this.props.graph.getConnectedComponents();
-    console.log('Connected Components:', connectedComponents);
+    // console.log('Connected Components:', connectedComponents);
 
     // draw edges
     // For each vertex inside the connected components array
@@ -90,7 +89,7 @@ class GraphView extends Component {
       ctx.lineWidth = 2;
       ctx.moveTo(vertex.pos.x, vertex.pos.y);
       ctx.strokeStyle = 'black';
-      ctx.arc(vertex.pos.x, vertex.pos.y, 20, 0, 2 * Math.PI, true);
+      ctx.arc(vertex.pos.x, vertex.pos.y, vRadius, 0, 2 * Math.PI, true);
       ctx.stroke();
       ctx.closePath();
       // Fill them In White
@@ -105,13 +104,12 @@ class GraphView extends Component {
       connected.forEach(connectedVertex => {
         ctx.beginPath();
         ctx.moveTo(connectedVertex.pos.x, connectedVertex.pos.y);
-        ctx.arc(connectedVertex.pos.x, connectedVertex.pos.y, 20, 0, 2 * Math.PI, true);
+        ctx.arc(connectedVertex.pos.x, connectedVertex.pos.y, vRadius, 0, 2 * Math.PI, true);
         ctx.fill();
       });
     })
     this.props.graph.vertexes.forEach(vertex => {
       // Write each Vertex Values
-      
       ctx.fillStyle = 'black';
       ctx.font = '14px serif';
       ctx.beginPath();
@@ -128,12 +126,68 @@ class GraphView extends Component {
       ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
     });
   }
+  /* WHEN CANVAS IS CLICKED!*/
+  graphClicked = (event) => {
+    // function for figuring out where our canvas starts in the DOM x and y
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    let canvasPosition = offset(event.target);
+    let localXPos = event.clientX - canvasPosition.left;
+    let localYPos = event.clientY - canvasPosition.top;
+
+    //console.log("top: ", canvasPosition.top, "left: ", canvasPosition.left);
+    //console.log("Mouse x: ", localXPos, "Mouse y: ", localYPos);
+    this.props.graph.vertexes.forEach(vertex => {
+      // if localXPos > vertex.pos.x subtract localXPos - vertex.pos.x
+      // if localXPos < vertex.pos.x subtract vertex.pos.x - localXPos
+      let xDif = localXPos > vertex.pos.x ? localXPos - vertex.pos.x : vertex.pos.x - localXPos;
+      let yDif = localYPos > vertex.pos.y ? localYPos - vertex.pos.y : vertex.pos.y - localYPos;
+      if (yDif < vRadius && xDif < vRadius) {
+        // Redraw Vert with New Colors
+        let ctx = event.target.getContext('2d'); // Get our canvas to use
+        // Draw Our Outline
+        ctx.beginPath();
+        ctx.lineWidth = 2;
+        ctx.moveTo(vertex.pos.x, vertex.pos.y);
+        ctx.strokeStyle = 'black';
+        ctx.arc(vertex.pos.x, vertex.pos.y, vRadius, 0, 2 * Math.PI, true);
+        ctx.stroke();
+        ctx.closePath();
+        // Draw our Fill
+        ctx.fillStyle = `rgb(0, 75, 136)`;
+        ctx.beginPath();
+        ctx.moveTo(vertex.pos.x, vertex.pos.y);
+        ctx.arc(vertex.pos.x, vertex.pos.y, vRadius, 0, 2 * Math.PI, true);
+        ctx.fill();
+        // Draw Vertex Values
+        ctx.fillStyle = 'black';
+        ctx.font = '14px serif';
+        ctx.beginPath();
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.moveTo(vertex.pos.x, vertex.pos.y);
+        // Stroke and Fill the Text In
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'white'
+        ctx.strokeText(vertex.value, vertex.pos.x, vertex.pos.y);
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'black'
+        ctx.strokeText(vertex.value, vertex.pos.x, vertex.pos.y);
+        ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
+        console.log(`${vertex.value} was clicked!`);
+      }
+    })
+  }
 
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} onClick={this.graphClicked}></canvas>;
   }
 }
 
