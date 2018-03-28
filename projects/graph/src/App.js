ce57ed6715797eb08e550941d8230ac5d93b679e
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-03-28 15:15:28 -0500

    bugs fixed

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,9 +3,9 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-const canvasWidth = 700;
-const canvasHeight = 480;
-
+const canvasWidth = 750;
+const canvasHeight = 600;
+ 
 const vertexRadius = 10;
 
 /**
@@ -32,44 +32,70 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-
+    
     // Clear it
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = 'lightgray';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    console.log("Updating Canvas");
+    ctx.strokeStyle = 'black';
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    
+    console.log("updating canvasa");
     console.log(this.props.graph.vertexes);
 
-    console.log("edge", this.props.graoh.vertexes[0], edges[0]);
+    console.log('edge ', this.props.graph.vertexes[0].edges[0]);
+
+    //REMEMBER: Draw lines first!
+    // let parentVert = this.props.graph.vertexes[0];
+    // let debugEdge = this.props.graph.vertexes[0].edges[0];
+
+    for (let parentVert of this.props.graph.vertexes) {
+      for (let debugEdge of parentVert.edges) {
+        ctx.moveTo(parentVert.pos.x, parentVert.pos.y);
+        ctx.lineTo(debugEdge.destination.pos.x, debugEdge.destination.pos.y);
+        ctx.stroke();
+      }  
+    }
+    // we know our data is here :D
+    // lets draw it!
+
+    //let debugNode = this.props.graph.vertexes[0];
+    //console.log(debugNode.pos.x);
+
+    for (let vertex of this.props.graph.vertexes) {
+      
+      // draw node first, so make a circle
+      ctx.moveTo(vertex.pos.x, vertex.pos.y);
+      ctx.beginPath();
+      ctx.arc(vertex.pos.x, vertex.pos.y, vertexRadius, 0, Math.PI * 2);
+      ctx.stroke();
+
+      //draw the fill
+      ctx.fillStyle = 'white';
+      ctx.fill();
+
+      //draw the label or text
+      ctx.fillStyle = 'black';
+      ctx.textAlign = 'center';
+      ctx.textBaseLine = 'middle';
+      ctx.font = '10px Arial'; //TODO: Do we want stroke text or fill text?
+      ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
+    }
+    
 
 
-    // remember to draw lines
-    for (let parentVert of this.props.graph.vertexes)
-    {
-    for (let debugEdge of parentVert.edges)
-    {
-    ctx.moveTo(debugNode.pos.x, debugNode.pos.y);
-    ctx.beginPath();
-    ctx.arc(debugNode.pos.x, debugNode.pos.y, vertexRadius, 0, Math.PI * 2);
-    ctx.stroke();
-    // !!! IMPLEMENT ME;
+
+
+
+
+
+
+    // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
-    //draw the fill
-    ctx.fillStyle = "white";
-    ctx.fill();
-
-    //Draw the label
-    ctx.fillstyle = "black";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.font = "10px Arial";
-    ctx.fillText(debugNode.value, debugNode.pos.x, debugNode.pos.y);
   }
-    
+  
   /**
    * Render
    */
@@ -92,6 +118,9 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    // since our eventual goal is to implement randomiz here
+    // this is probably a good place to try our test function and see
+    // what happens and figure out how it works
     this.state.graph.randomize(5, 4, 150, 0.6);
   }
 
@@ -105,3 +134,4 @@ class App extends Component {
 }
 
 export default App;
+
--- projects/graph/src/graph.js
+++ projects/graph/src/graph.js
@@ -3,22 +3,26 @@
  */
 export class Edge {
   // !!! IMPLEMENT ME
-  constructor(destination, weight = 1){
-    this.destination = destination;
+  // step 1: need to know start and end
+  // step 2: start is already taken care of, because the edge will belong to a vertex
+  // step 3: So all we need to know is destination
+  // Edge has weight, and destination
+  constructor(destination, weight = 1) {
     this.weight = weight;
+    this.destination = destination;
   }
 }
 
 /**
  * Vertex
  */
-
 export class Vertex {
   // !!! IMPLEMENT ME
-  constructor(value= 'vertex', pos = { x: -1, y: -1}){
+  // vertex's have value, pos, and edges
+  constructor(value = '', pos = {x: -1, y: -1}) {
     this.value = value;
     this.edges = [];
-    this.pos = [];
+    this.pos = pos;
   }
 }
 
@@ -29,115 +33,140 @@ export class Graph {
   constructor() {
     this.vertexes = [];
   }
-debugCreateTestData(){
-let debugVert1 = new Vertex('dV1', {x: 10, y: 20});
-let debugVert2 = new Vertex('dv2', {x: 100, y:100});
+  
+  debugCreateTestData() {
+    console.log("Debug: Ran debugCreateTestData()");
+    let debugVert1 = new Vertex('dv1', { x: 10, y: 20 });
+    let debugVert2 = new Vertex("dv2", { x: 100, y: 100 });
 
-let edge1 = new Edge(debugVert2);
-let edge2 = new Edge(debugVert4)
-debugVert1.edges.push(edge1);
+    // to test our edges let's make a debug edge arbitrarily
+    // that connects node 1 and node 2
+    // to do that, we need to create an edge that belongs to debugVert1
+    // and has a destination of debugVert2, which by our definition of our
+    // edge class, is all we need to say that you go from 1 to 2
 
+    let edge1 = new Edge(debugVert2);
+    debugVert2.edges.push(edge1);
+    //console.log check>>>
 
-this.vertexes.push(debugVert1);
-this.vertexes.push(debugVert2);
+    this.vertexes.push(debugVert1); // TODO: Can I do comma's here?
+    this.vertexes.push(debugVert2);
+  }
+
+/**
+* Create a random graph
+*/
+randomize(width, height, pxBox, probability = 0.6) {
+  // Helper function to set up two-way edges
+  function connectVerts(v0, v1) {
+    v0.edges.push(new Edge(v1));
+    v1.edges.push(new Edge(v0));
+  }
+
+  let count = 0;
+
+  // Build a grid of verts
+  let grid = [];
+  for (let y = 0; y < height; y++) {
+    let row = [];
+    for (let x = 0; x < width; x++) {
+      let v = new Vertex();
+      //v.value = 'v' + x + ',' + y;
+      v.value = "v" + count++; // v1, v2
+      row.push(v);
+    }
+    grid.push(row);
+  }
+
+  // Go through the grid randomly hooking up edges
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      // Connect down
+      if (y < height - 1) {
+        if (Math.random() < probability) {
+          connectVerts(grid[y][x], grid[y + 1][x]);
+        }
+      }
+
+      // Connect right
+      if (x < width - 1) {
+        if (Math.random() < probability) {
+          connectVerts(grid[y][x], grid[y][x + 1]);
+        }
+      }
+    }
+  }
+
+  // Last pass, set the x and y coordinates for drawing
+  const boxBuffer = 0.8;
+  const boxInner = pxBox * boxBuffer;
+  const boxInnerOffset = (pxBox - boxInner) / 2;
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      grid[y][x].pos = { x: (x * pxBox + boxInnerOffset + Math.random() * boxInner) | 0, y: (y * pxBox + boxInnerOffset + Math.random() * boxInner) | 0 };
+    }
+  }
+
+  // Finally, add everything in our grid to the vertexes in this Graph
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      this.vertexes.push(grid[y][x]);
+    }
+  }
 }
-  /**
-   * Create a random graph
-   */
-  randomize(width, height, pxBox, probability=0.6) {
-    // Helper function to set up two-way edges
-    function connectVerts(v0, v1) {
-      v0.edges.push(new Edge(v1));
-      v1.edges.push(new Edge(v0));
+
+/**
+* Dump graph data to the console
+*/
+dump() {
+  let s;
+
+  for (let v of this.vertexes) {
+    if (v.pos) {
+      s = v.value + " (" + v.pos.x + "," + v.pos.y + "):";
+    } else {
+      s = v.value + ":";
     }
 
-    let count = 0;
-
-    // Build a grid of verts
-    let grid = [];
-    for (let y = 0; y < height; y++) {
-      let row = [];
-      for (let x = 0; x < width; x++) {
-        let v = new Vertex();
-        //v.value = 'v' + x + ',' + y;
-        v.value = 'v' + count++;
-        row.push(v);
-      }
-      grid.push(row);
+    for (let e of v.edges) {
+      s += ` ${e.destination.value}`;
     }
-
-    // Go through the grid randomly hooking up edges
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        // Connect down
-        if (y < height - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y+1][x]);
-          }
-        }
-
-        // Connect right
-        if (x < width - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y][x+1]);
-          }
-        }
-      }
-    }
-
-    // Last pass, set the x and y coordinates for drawing
-    const boxBuffer = 0.8;
-    const boxInner = pxBox * boxBuffer;
-    const boxInnerOffset = (pxBox - boxInner) / 2;
-
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        grid[y][x].pos = {
-          'x': (x * pxBox + boxInnerOffset + Math.random() * boxInner) | 0,
-          'y': (y * pxBox + boxInnerOffset + Math.random() * boxInner) | 0
-        };
-      }
-    }
-
-    // Finally, add everything in our grid to the vertexes in this Graph
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        this.vertexes.push(grid[y][x]);
-      }
-    }
+    console.log(s);
   }
+}
 
-  /**
-   * Dump graph data to the console
-   */
-  dump() {
-    let s;
+/**
+* BFS
+*/
+bfs(start) {
+  // !!! IMPLEMENT ME
+  const returnArray = [];
 
-    for (let v of this.vertexes) {
-      if (v.pos) {
-        s = v.value + ' (' + v.pos.x + ',' + v.pos.y + '):';
-      } else {
-        s = v.value + ':';
-      }
-
-      for (let e of v.edges) {
-        s += ` ${e.destination.value}`;
-      }
-      console.log(s);
-    }
+let loop_extract = vertex => {
+// While the edges array of the vertex passed in is not empty
+let { edges } = vertex;
+while (edges.length !== 0) {
+edges.forEach(edge => {
+  // If the Destination is not already included in our return array
+  // Ensuring we do not have loops
+  if (!returnArray.includes(edge.destination)) {
+    returnArray.push(edge.destination); // Vertex is Pushed in the Array
+    console.log(`${edge.value} <----> ${edge.destination.value}`);
+    loop_extract(edge.destination);
   }
+});
+}
+};
 
-  /**
-   * BFS
-   */
-  bfs(start) {
-    // !!! IMPLEMENT ME
-  }
+loop_extract(start);
 
-  /**
-   * Get the connected components
-   */
-  getConnectedComponents() {
-    // !!! IMPLEMENT ME
+return returnArray;
+}
+/**
+* Get the connected components
+*/
+getConnectedComponents() {
+  // !!! IMPLEMENT ME
   }
 }
