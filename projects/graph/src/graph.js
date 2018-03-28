21406b08bef715d691f065ed58d84abc8e6665b2
AustenHowes <austie702@gmail.com>
2018-03-28 16:48:24 -0500

    Writing comments line by line to ensure I understand what I've stolen

--- projects/graph/src/graph.js
+++ projects/graph/src/graph.js
@@ -14,9 +14,10 @@ export class Edge {
  */
 export class Vertex {
   // !!! IMPLEMENT ME
-  constructor() {
+  constructor(value='vertex') {
+    this.value = value;
     this.edges = [];
-    this.value = null;
+    this.color = 'white';
   }
 }
 
@@ -34,12 +35,12 @@ export class Graph {
   randomize(width, height, pxBox, probability=0.6) {
     // Helper function to set up two-way edges
     function connectVerts(v0, v1) {
-      console.log(v0, v1);
       v0.edges.push(new Edge(v1));
       v1.edges.push(new Edge(v0));
     }
 
     let count = 0;
+    this.vertexes.length = 0;
 
     // Build a grid of verts
     let grid = [];
@@ -118,14 +119,84 @@ export class Graph {
   /**
    * BFS
    */
-  bfs(start) {
+  // We begin each BFS by 
+  bfs(start, reset=true) {
     // !!! IMPLEMENT ME
+
+    // Create a queue to store components (WHAT ARE THESE COMPONENTS?????????????????????????????????????????????)
+    const component = [];
+
+    // Create a queue to store the verts still in review
+    const queue = [];
+
+    // Every time we run the BFS, it resets for a new search
+    // Since each new BFS run has reset = true, we are essentially saying "always do the following upon a new BFS"
+    if (reset) {
+      // Go through the vertexes array...
+      for (let v of this.vertexes) {
+        // ...and reset all vert colors to white
+        v.color = 'white';
+      }
+    }
+
+    // Each time we look to a node for review, we will set its color to gray
+    start.color = 'gray';
+
+    // This pushes the list of vertices from the queue into the vertex array "start" is pointing to
+    queue.push(start);
+
+    // As long as the queue isn't empty...
+    while (queue.length > 0) {
+      // We set the first item in queue to equal the variable "u"
+      const u = queue[0];
+
+      // ... we will search every item at the front of the queue for its edges...
+      for (let e of u.edges) {
+        // ...and for said edges' destinations
+        const v = e.destination;
+        
+        // If the color of the queued vertex is white...
+        if (v.color === 'white') {
+
+          // ...we will set its color to gray while it is under review
+          v.color = 'gray';
+
+          // If we find edges,then we add those edges' connected verticies (v) to the queue
+          queue.push(v);
+        }
+      }
+
+      // Once the vertex is no longer under review, we will take it out of the queue...
+      queue.shift(); // de-queue
+
+      // ...and change its color to black
+      u.color = 'black';
+
+      // This pushes the first item in the queue into the component array
+      component.push(u);
+    }
+
+    // Returns the component array
+    return component;
   }
 
   /**
    * Get the connected components
    */
   getConnectedComponents() {
-    // !!! IMPLEMENT ME
+    const componentsList = [];
+
+    let needReset = true;
+
+    for (let v of this.vertexes) {
+      if (needReset || v.color === 'white') {
+        const component = this.bfs(v, needReset);
+        needReset = false;
+
+        componentsList.push(component);
+      }
+    }
+
+    return componentsList;
   }
 }
