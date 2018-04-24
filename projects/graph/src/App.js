8ed495c8c13a1518df7aea7105398aa4dd43a879
groov1234 <c.groover@outlook.com>
2018-04-24 09:54:29 -0400

    ported matrix canvas animation into react from vanilla js

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 // !!! IMPLEMENT ME
 // const canvasWidth = 
 // const canvasHeight = 
-
+const canvasWidth = window.innerHeight;
+const canvasHeight = window.innerWidth;
 /**
  * GraphView
  */
@@ -36,6 +37,41 @@ class GraphView extends Component {
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // !!! IMPLEMENT ME
+    let matrix = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%";
+
+
+matrix = matrix.split("");
+
+
+let font_size = 10;
+let columns = canvasWidth/font_size; 
+let drops = [];
+
+
+for( let x = 0; x < columns; x++)
+    drops[x] = 1;
+
+
+function draw() {
+ctx.fillStyle = "rgba(0, 0, 0, 0.04)"; 
+ctx.fillRect(0, 0, canvasWidth, canvasHeight); 
+
+ctx.fillStyle = "#0F0"; 
+ctx.font = font_size + "px arial";
+
+
+for(let i = 0; i < drops.length; i++) {
+let text = matrix[Math.floor(Math.random()*matrix.length)]; 
+ctx.fillText(text, i*font_size, drops[i]*font_size); 
+
+
+if(drops[i]*font_size > canvasHeight && Math.random() > 0.975)
+    drops[i] = 0;
+drops[i]++; 
+    }
+}
+
+setInterval(draw, 35);
     // compute connected components
     // draw edges
     // draw verts
