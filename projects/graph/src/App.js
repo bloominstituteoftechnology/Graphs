25e3e9197b783cb6ca35bb4908bd1dac2574c2f6
Elly <ellyhahn@gmail.com>
2018-04-26 16:19:51 -0700

    modify app.js and graph.js

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, version } from 'react';
 import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasHeight = 700;
+const canvasWidth = 1000;
 
 /**
  * GraphView
@@ -24,6 +24,13 @@ class GraphView extends Component {
     this.updateCanvas();
   }
 
+  randomizeColor(colorCode = "") {
+    if (colorCode.length === 6) return "#" + colorCode;
+    const randomColor = ((Math.random() * 240) | 0).toString(16);
+    colorCode += randomColor.length === 1 ? "0" + randomColor : randomColor;
+    return this.randomizeColor(colorCode);
+  }
+  
   /**
    * Render the canvas
    */
@@ -32,14 +39,34 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = '#80CBC4';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    // !!! IMPLEMENT ME
-    // compute connected components
-    // draw edges
-    // draw verts
-    // draw vert values (labels)
+    const vertexes = this.props.graph.vertexes;
+    const radius = 20;
+
+    for (let vertex of vertexes) {
+      for (let edge of vertex.edges) {
+        ctx.beginPath();
+        ctx.moveTo(vertex.pos.x, vertex.pos.y);
+        ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+        ctx.strokeStyle = this.randomizeColor();
+        ctx.stroke();
+      }
+    }
+
+    for (let vertex of vertexes) {
+      ctx.beginPath();
+      ctx.arc(vertex.pos.x, vertex.pos.y, radius, 0, 2*Math.PI);
+      ctx.fillStyle = this.randomizeColor();
+      ctx.fill();
+      ctx.stroke();
+      ctx.font = "12px Verdana";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.fillStyle = "white";
+      ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
+    }
   }
   
   /**
@@ -64,12 +91,22 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    this.state.graph.randomize(5, 7, 111, 0.6);
+    this.state.graph.bfs(0);
   }
+
+  onHandleClick = () => {
+    const graph = new Graph();
+    graph.randomize(5, 7, 111, 0.6);
+    this.setState({graph});
+  }
+
 
   render() {
     return (
       <div className="App">
         <GraphView graph={this.state.graph}></GraphView>
+        <button onClick={this.onHandleClick}>Generate New Graph</button>
       </div>
     );
   }
