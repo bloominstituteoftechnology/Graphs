ab37e0933c84b2cf775abd15cfd6aaa52fbd74a0
Res Yeg <shaqk702@gmail.com>
2018-05-07 15:46:57 -0700

    still playing with shapes and lines

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 400;
+const canvasHeight = 400;
 
 /**
  * GraphView
@@ -32,21 +32,102 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // ctx.fillStyle = 'white';
+    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
+
+    for (let i = 0; i < 3; i++) {
+      const max = 320;
+      const min = 80;
+      const x = Math.floor(Math.random() * (max - min + 1)) + min;
+      const y = Math.floor(Math.random() * (max - min + 1)) + min;
+      // const gradient=ctx.createLinearGradient(0,0,0,170);
+      // gradient.addColorStop(0,"black");
+      // gradient.addColorStop(1,"white");
+      // ctx.fillStyle = gradient;
+      ctx.beginPath();
+      ctx.arc(x, y, 40, 0, 2*Math.PI);
+      ctx.stroke();
+    }
+
+    for (let i = 0; i < 3; i++) {
+      ctx.globalAlpha=0.2;
+      const max = 320;
+      const min = 80;
+      const x = Math.floor(Math.random() * (max - min + 1)) + min;
+      const y = Math.floor(Math.random() * (max - min + 1)) + min;
+      // const gradient=ctx.createLinearGradient(0,0,0,170);
+      // gradient.addColorStop(0,"black");
+      // gradient.addColorStop(1,"white");
+      // ctx.fillStyle = gradient;
+      ctx.fillStyle = 'red';
+      ctx.fillRect(x, y, 80, 80);
+    }
+
+    for (let i = 0; i < 10; i++) {
+      const x = Math.floor(Math.random() * 400);
+      const y = Math.floor(Math.random() * 400);
+      ctx.moveTo(200, 200);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+
+    // for (let i = 0; i < 2; i++) {
+      const max = 320;
+      const min = 80;
+      const x = Math.floor(Math.random() * (max - min + 1)) + min;
+      const y = Math.floor(Math.random() * (max - min + 1)) + min;
+      // the triangle
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(100, 300);
+      ctx.lineTo(300, 300);
+      ctx.closePath();
+      
+      // the outline
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = '#666666';
+      ctx.stroke();
+      
+      // the fill color
+      ctx.fillStyle = "#FFCC00";
+      ctx.fill();
+    // }
+
+    for (let i = 0; i < 2; i++) {
+      const max = 320;
+      const min = 80;
+      const x = Math.floor(Math.random() * (max - min + 1)) + min;
+      const y = Math.floor(Math.random() * (max - min + 1)) + min;
+      // the rectangle
+      ctx.rect(x,y,150,100);
+      ctx.stroke();
+      
+      // the outline
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = '#666666';
+      ctx.stroke();
+      
+      // the fill color
+      ctx.fillStyle = "#FFCC00";
+      ctx.fill();
+    }
+
   }
   
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    const borderStyles = {
+        border: '10px solid gray',
+    };
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} style={borderStyles}></canvas>;
   }
 }
 
