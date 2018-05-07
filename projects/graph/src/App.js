3a12535ffb7720bcf7fe93045d2f863f56424c53
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-05-07 18:51:57 -0400

    Twin peaks alphabet soup

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -32,7 +32,7 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
 
     // Clear it
-    ctx.fillStyle = 'grey';
+    ctx.fillStyle = 'rgb(10, 10, 10)';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // !!! IMPLEMENT ME
@@ -40,20 +40,110 @@ class GraphView extends Component {
     // draw edges
     // draw verts
     // draw vert values (labels)
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(1000, 700);
-    ctx.strokeStyle="#EE0000";
-    ctx.stroke();
-    ctx.closePath();
 
-    ctx.beginPath();
-    ctx.moveTo(1000, 0);
-    ctx.lineTo(0, 700);
-    ctx.strokeStyle="#00EE00";
-    ctx.stroke();
-    ctx.closePath();
+    let w = 1000;
+    let h = 700;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w -= 10;
+    }
+
+    w = 0;
+    h = 700;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(500, 0);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w += 10;
+    }
+
+    w = 0;
+    h = 700;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(1000, 0);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w += 10;
+    }
+
+    w = 0;
+    h = 0;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, 700);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w += 10;
+    }
+
+    w = 0;
+    h = 0;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(1000, 700);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w += 10;
+    }
+
+    w = 0;
+    h = 0;
+    for (let i = 0; i < 100; i++) {
+      ctx.beginPath();
+      ctx.moveTo(500, 700);
+      ctx.lineTo(w, h);
+      ctx.strokeStyle = '#AA0000';
+      ctx.stroke();
+      ctx.closePath();
+      w += 10;
+    }
+
+    function random(min, max) {
+      let int = Math.random() * (max - min) + min;
+      return Math.floor(int);
+    }
+
+    for (let i = 0; i < 10000; i++) {
+      ctx.beginPath();
+      ctx.fillStyle = `rgba(10, 10, 10, ${Math.random()})`;
+      ctx.fillRect(random(10, 1000), random(10, 700), random(1, 10), random(1, 10));
+      ctx.closePath();
+    }
+
+    // for (let i = 0; i < 200; i++) {
+    //   ctx.beginPath();
+    //   ctx.arc(random(100, 900), random(100, 600), random(1, 30), 0, 360);
+    //   ctx.strokeStyle = `rgba(21, 0, 0, ${Math.random() * (0.2) + 0.1})`;
+    //   ctx.lineWidth = 4;
+    //   ctx.stroke();
+    //   ctx.closePath();
+    // }
+
+    for (let i = 0; i < 1000; i++) {
+      ctx.beginPath();
+      ctx.font='30px sans-serif';
+      ctx.strokeStyle = `rgba(0, 250, 0, ${Math.random()})`;
+      ctx.strokeText(`${String.fromCharCode(random(65, 85))}`, random(100, 900), random(100, 700));
+      ctx.rotate(random(0, 360)*Math.PI/180);
+      ctx.closePath();
+    }
   }
+
 
   /**
    * Render
