9e0619eafbf9fb803f2091a39634bad06aa68652
unknown <davsoudry@gmail.com>
2018-05-07 18:02:48 -0500

    added trippy canvas styling

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 400;
+const canvasHeight = 300;
 
 /**
  * GraphView
@@ -32,9 +32,29 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'lightgreen';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(30, 30, 90, 90);
 
+    ctx.lineTo(100, 300);
+    ctx.lineTo(40,50);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(300, 50, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.moveTo(200, 200);
+
+    ctx.strokeStyle = 'blue';
+
+    for(let i = 0; i < 200; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0,Math.cos(i) * 600);
+      ctx.arc(350, 150, 40, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
