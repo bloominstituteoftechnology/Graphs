7771c71233905b3f3e06005d1666d48e6ac60cd5
Christopher Beards <beardscm@gmail.com>
2018-05-08 16:30:03 -0400

    draw edges; implemented-unconfirmed

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -33,45 +33,63 @@ class GraphView extends Component {
     
     // Clear it
     
-    // ctx.fillStyle = 'white';
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'white';
 
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+//=============================================================
     // ---------------------
     //        DRAWING
     // ---------------------
-    ctx.fillStyle = '#00F0F0';
-    ctx.font = 'italic 40pt serif';
-    ctx.fillText('Circle', 190, 325);
+    // ctx.fillStyle = '#00F0F0';
+    // ctx.font = 'italic 40pt serif';
+    // ctx.fillText('Circle', 190, 325);
 
     // Translating changes the orgin of the drawing surface
-    ctx.translate(150, 30);
+    // ctx.translate(150, 30);
 
-    ctx.moveTo(108, 0.0);
-    ctx.lineTo(141, 70);
-    ctx.lineTo(218, 78.3);
-    ctx.lineTo(162, 131);
-    ctx.lineTo(175, 205);
-    ctx.lineTo(108, 170);
-    ctx.lineTo(108, 0);
-    ctx.lineTo(75, 68);
-    ctx.lineTo(1, 78);
-    ctx.lineTo(55, 131);
-    ctx.lineTo(41.2, 205);
+    // ctx.moveTo(108, 0.0);
+    // ctx.lineTo(141, 70);
+    // ctx.lineTo(218, 78.3);
+    // ctx.lineTo(162, 131);
+    // ctx.lineTo(175, 205);
+    // ctx.lineTo(108, 170);
+    // ctx.lineTo(108, 0);
+    // ctx.lineTo(75, 68);
+    // ctx.lineTo(1, 78);
+    // ctx.lineTo(55, 131);
+    // ctx.lineTo(41.2, 205);
+
+    //-Complete Star Code-
 
     // ctx.lineTo(41.2, 205);
     // ctx.lineTo(55, 131);
     // ctx.lineTo(1, 78);
     // ctx.lineTo(75, 68);
     // ctx.lineTo(108, 0);
-    ctx.fill();
+    // ctx.fill();
+//=============================================================
  
 
     // !!! IMPLEMENT ME
     // compute connected components
+
+
+
     // draw edges
+    for (let v of this.props.graph.vertexes) {
+      for (let e of v.edges) {
+        ctx.moveTo(e.pos.x, e.pos.y);
+        ctx.lineTo(e.pos.destination.pos.x, e.pos.destination.pos.y);
+        ctx.stroke(); //draws the path defined w/ moveTo() & lineTo()
+      }
+    }
+
+
     // draw verts
+
+
+
     // draw vert values (labels)
   }
   
