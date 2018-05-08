5c872d3e09a352924375bfa0cda8a95ec81028b4
Res Yeg <shaqk702@gmail.com>
2018-05-07 21:05:14 -0700

    grid

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -30,10 +30,12 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+
     // Clear it
-    // ctx.fillStyle = 'white';
-    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0 + 1, 0 + 1, canvasWidth - 2, canvasHeight - 2);
+    ctx.strokeRect(0 + 2, 0 + 2, canvasWidth - 4, canvasHeight - 4);
 
     // !!! IMPLEMENT ME
     // compute connected components
@@ -41,82 +43,111 @@ class GraphView extends Component {
     // draw verts
     // draw vert values (labels)
 
-    for (let i = 0; i < 3; i++) {
-      const max = 320;
-      const min = 80;
-      const x = Math.floor(Math.random() * (max - min + 1)) + min;
-      const y = Math.floor(Math.random() * (max - min + 1)) + min;
-      // const gradient=ctx.createLinearGradient(0,0,0,170);
-      // gradient.addColorStop(0,"black");
-      // gradient.addColorStop(1,"white");
-      // ctx.fillStyle = gradient;
+    for (var i = 0; i < 400; i++) {
       ctx.beginPath();
-      ctx.arc(x, y, 40, 0, 2*Math.PI);
+      ctx.moveTo(i * 10, 0);
+      ctx.lineTo(i * 10, 400);
       ctx.stroke();
-    }
 
-    for (let i = 0; i < 3; i++) {
-      ctx.globalAlpha=0.2;
-      const max = 320;
-      const min = 80;
-      const x = Math.floor(Math.random() * (max - min + 1)) + min;
-      const y = Math.floor(Math.random() * (max - min + 1)) + min;
-      // const gradient=ctx.createLinearGradient(0,0,0,170);
-      // gradient.addColorStop(0,"black");
-      // gradient.addColorStop(1,"white");
-      // ctx.fillStyle = gradient;
-      ctx.fillStyle = 'red';
-      ctx.fillRect(x, y, 80, 80);
-    }
-
-    for (let i = 0; i < 10; i++) {
-      const x = Math.floor(Math.random() * 400);
-      const y = Math.floor(Math.random() * 400);
-      ctx.moveTo(200, 200);
-      ctx.lineTo(x, y);
-      ctx.stroke();
-    }
-
-    // for (let i = 0; i < 2; i++) {
-      const max = 320;
-      const min = 80;
-      const x = Math.floor(Math.random() * (max - min + 1)) + min;
-      const y = Math.floor(Math.random() * (max - min + 1)) + min;
-      // the triangle
       ctx.beginPath();
-      ctx.moveTo(x, y);
-      ctx.lineTo(100, 300);
-      ctx.lineTo(300, 300);
-      ctx.closePath();
-      
-      // the outline
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = '#666666';
+      ctx.moveTo(0, i * 10);
+      ctx.lineTo(400, i * 10);
       ctx.stroke();
-      
-      // the fill color
-      ctx.fillStyle = "#FFCC00";
-      ctx.fill();
+    }
+
+    // let radius = canvas.height / 2;
+    // ctx.translate(radius, radius);
+    // radius = radius * 0.90;
+    // setInterval(drawClock, 1000);
+    
+    // function drawClock(ctx, radius) {
+    //   let now = new Date();
+    //   let hour = now.getHours();
+    //   let minute = now.getMinutes();
+    //   let second = now.getSeconds();
+
+    //   hour=hour%12;
+    //   hour=(hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+
+    //   minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    //   second=(second*Math.PI/30);
+    //   console.log(hour, minute, second);
     // }
 
-    for (let i = 0; i < 2; i++) {
-      const max = 320;
-      const min = 80;
-      const x = Math.floor(Math.random() * (max - min + 1)) + min;
-      const y = Math.floor(Math.random() * (max - min + 1)) + min;
-      // the rectangle
-      ctx.rect(x,y,150,100);
-      ctx.stroke();
+    // for (let i = 0; i < 3; i++) {
+    //   const max = 320;
+    //   const min = 80;
+    //   const x = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   const y = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   ctx.beginPath();
+    //   ctx.arc(x, y, 40, 0, 2*Math.PI);
+    //   ctx.stroke();
+    // }
+
+    // for (let i = 0; i < 3; i++) {
+    //   ctx.globalAlpha=0.2;
+    //   const max = 320;
+    //   const min = 80;
+    //   const x = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   const y = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   // const gradient=ctx.createLinearGradient(0,0,0,170);
+    //   // gradient.addColorStop(0,"black");
+    //   // gradient.addColorStop(1,"white");
+    //   // ctx.fillStyle = gradient;
+    //   ctx.fillStyle = 'red';
+    //   ctx.fillRect(x, y, 80, 80);
+    // }
+
+    // for (let i = 0; i < 2; i++) {
+    //   const x = Math.floor(Math.random() * 400);
+    //   const y = Math.floor(Math.random() * 400);
+    //   ctx.moveTo(200, 200);
+    //   ctx.lineTo(x, y);
+
+    //   ctx.lineWidth = 10;
+    //   ctx.strokeStyle = '#666666';
+
+    //   ctx.stroke();
+    // }
+
+      // const max = 320;
+      // const min = 80;
+      // const x = Math.floor(Math.random() * (max - min + 1)) + min;
+      // const y = Math.floor(Math.random() * (max - min + 1)) + min;
+      // // the triangle
+      // ctx.beginPath();
+      // ctx.moveTo(x, y);
+      // ctx.lineTo(100, 300);
+      // ctx.lineTo(300, 300);
+      // ctx.closePath();
       
-      // the outline
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = '#666666';
-      ctx.stroke();
+      // // the outline
+      // ctx.lineWidth = 5;
+      // ctx.strokeStyle = '#666666';
+      // ctx.stroke();
       
-      // the fill color
-      ctx.fillStyle = "#FFCC00";
-      ctx.fill();
-    }
+      // // the fill color
+      // ctx.fillStyle = "#FFCC00";
+      // ctx.fill();
+
+    // for (let i = 0; i < 2; i++) {
+    //   const max = 320;
+    //   const min = 80;
+    //   const x = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   const y = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   // the rectangle
+    //   ctx.rect(x,y,150,100);
+    //   ctx.stroke();
+      
+    //   // the outline
+    //   ctx.lineWidth = 5;
+    //   ctx.strokeStyle = '#666666';
+    //   ctx.stroke();
+      
+    //   // the fill color
+    //   ctx.fillStyle = "#FFCC00";
+    //   ctx.fill();
+    // }
 
   }
   
@@ -124,10 +155,10 @@ class GraphView extends Component {
    * Render
    */
   render() {
-    const borderStyles = {
-        border: '10px solid gray',
-    };
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} style={borderStyles}></canvas>;
+    // const borderStyles = {
+    //     border: '10px solid gray',
+    // };
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
   }
 }
 
