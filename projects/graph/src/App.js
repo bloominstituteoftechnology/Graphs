e6294f610bfe23f397dee2b6a096299da091aae6
Dixie Korley <dixiekorley@gmail.com>
2018-05-08 17:45:05 -0400

    Facing errors, typo situation

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
@@ -36,34 +35,38 @@ class GraphView extends Component {
     ctx.fillStyle = 'green';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+    for (let vertex of this.props.graph.vertexes) {
+      ctx.arc(vertex.pos.x, vertex.pos.y, 10, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
-    ctx.fillStyle = '#3BB9FF';
-    ctx.fillRect(0, 0, 500, 200);
+    // ctx.fillStyle = '#3BB9FF';
+    // ctx.fillRect(0, 0, 500, 200);
 
     // const grd = ctx.createLinearGradient(0, 0, 500, 0)
     // grd.addColorStop(0, "purple");
     // grd.addColorStop(1, "pink");
 
-    ctx.fillStyle = 'yellow';
-    ctx.arc(800 / 2, 150 / 2, radius, 0, 2 * Math.PI, false); 
-    ctx.fill();
+    // ctx.fillStyle = 'yellow';
+    // ctx.arc(800 / 2, 150 / 2, radius, 0, 2 * Math.PI, false); 
+    // ctx.fill();
 
 
-    ctx.fillStyle = 'ivory';
-    ctx.fillRect(80, 150, 200, 200);
+    // ctx.fillStyle = 'ivory';
+    // ctx.fillRect(80, 150, 200, 200);
     
-    ctx.fillStyle = 'brown';
-    ctx.fillRect(100, 170, 50, 50);
+    // ctx.fillStyle = 'brown';
+    // ctx.fillRect(100, 170, 50, 50);
     
-    ctx.fillStyle = 'brown';
-    ctx.fillRect(210, 170, 50, 50);
+    // ctx.fillStyle = 'brown';
+    // ctx.fillRect(210, 170, 50, 50);
 
-    ctx.fillStyle = 'gray';
-    ctx.fillRect(153, 250, 50, 100);
+    // ctx.fillStyle = 'gray';
+    // ctx.fillRect(153, 250, 50, 100);
 
     
     // let sWidth = 500;
