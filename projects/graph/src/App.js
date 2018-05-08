1fb36aff8b2ef58162f8bd8eafa436ebe67e0583
Amanda Phillips <amanda@phillipsdev.org>
2018-05-08 15:22:40 -0500

    randomize implemented

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,11 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-const canvasWidth = 400;
-const canvasHeight = 300;
+const randomizeHeight = 4;
+const randomizeWidth = 5;
+const boxSize = 150;
+const canvasWidth = boxSize * randomizeWidth;
+const canvasHeight = boxSize * randomizeHeight;
 
 const vertexRadius = 10;
 /**
@@ -35,12 +38,12 @@ class GraphView extends Component {
     // Clear it
     ctx.fillStyle = '#e8ebef';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    // var grd = ctx.createLinearGradient(0, 500, 0, 0);
-    // grd.addColorStop(0, '#000000');
-    // grd.addColorStop(1, '#028187');
+    var grd = ctx.createLinearGradient(0, 500, 0, 0);
+    grd.addColorStop(0, '#000000');
+    grd.addColorStop(1, '#028187');
 
-    // ctx.fillStyle = grd;
-    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // for (let i = 0; i < 50; i++) {
     //   ctx.beginPath();
@@ -66,6 +69,7 @@ class GraphView extends Component {
         ctx.beginPath();
         ctx.moveTo(vertex.pos.x, vertex.pos.y);
         ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+        ctx.strokeStyle = 'white';
         ctx.stroke();
       }
     }
@@ -78,7 +82,7 @@ class GraphView extends Component {
       ctx.fill();
       ctx.stroke();
 
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = 'white';
       ctx.font = '10px Arial';
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
@@ -113,7 +117,8 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
-    this.state.graph.debugCreateTestData();
+    //this.state.graph.debugCreateTestData();
+    this.state.graph.randomize(randomizeWidth, randomizeHeight, boxSize);
   }
 
   render() {
