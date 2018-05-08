369dff2858ae66b4a855fcdb55410c76528cb92c
Mike Streltsoff <MStreltsoff@gmail.com>
2018-05-08 11:27:50 -0400

    MStreltsoff Graphs

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1250;
+const canvasHeight = 300;
 
 /**
  * GraphView
@@ -32,8 +32,28 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    
+    ctx.fillStyle = 'tan' 
+    ctx.fillRect(0, 0, 480, 450)
+    ctx.fillStyle = 'grey'
+    ctx.fillRect(10, 10, 460, 275)
+
+    const X = 85, W = 60, G = 30
+    ctx.lineWidth = 10
+    const colors = ['blue', 'black', 'red', 'yellow', 'green']
+    const args = [
+        [X,X,W],
+        [X+W+W+G,X,W],
+        [X+W+W+G+W+W+G,X,W],
+        [X+W+G/2,X+W,W],
+        [X+W+G/2+W+W+G,X+W,W]]
+
+    while (colors.length > 0) {
+        ctx.strokeStyle = colors.shift()
+        ctx.beginPath()
+        ctx.arc.apply(ctx, args.shift().concat([0,Math.PI*2,true]))
+        ctx.stroke()
+    }
 
     // !!! IMPLEMENT ME
     // compute connected components
