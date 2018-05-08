09fbc0cb515f9e0df2d0462ebd9cb6f9020ed341
Ronnie Miksch <fakepreferences@gmail.com>
2018-05-07 20:35:11 -0700

    Added sweet fibonacci spiral,a lso with pixel shader ish logic

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -15,6 +15,7 @@ class GraphView extends Component {
    */
   componentDidMount() {
     this.updateCanvas();
+    // window.requestAnimationFrame(this.updateCanvas);
   }
 
   /**
@@ -35,76 +36,109 @@ class GraphView extends Component {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
+    // ------------- Retro C64 looking trippy thing ---------------
+    // for(let x = 0; x < canvasWidth; x += 5) {
+    //   for(let y = 0; y < canvasHeight; y += 5) {
+    //       const r = (Math.sqrt(y * (x/20))) % 255;
+    //       const g = (Math.cos(x*y + Math.sin(y)) * 100) % 255;
+    //       const b = (r*(y/(Math.PI*r)) + g) % 255;
+    //       ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    //       ctx.fillRect(x, y, 5, 5);
+    //   }
+    // }
+    // -------------------------------------------
+
     // ------------- Happy Accident ---------------
     // for(let x = 0; x < canvasWidth; x++) {
     //   for(let y = 0; y < canvasHeight; y++) {
     //       const r = (Math.sqrt(y * (x/20))) % 255;
     //       const g = (Math.cos(x*y + Math.sin(y)) * 100) % 255;
     //       const b = (r*(y/(Math.PI*r)) + g) % 255;
-          // ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-          // ctx.fillRect(x, y, 1, 1);
+    //       ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    //       ctx.fillRect(x, y, 1, 1);
     //   }
     // }
     // -------------------------------------------
 
+    // --------------- FIB Spiral ----------------------
 
-    // ------------ Space thing ---------------------------------------------
-    ctx.beginPath();
-    for(let x = 0; x < canvasWidth; x++) {
-      for(let y = 0; y < canvasHeight; y++) {
-        const r = Math.cos((Math.sqrt(y)*Math.tan(x)*-x)) % 255;
-        const b = Math.tan((Math.sqrt(y)*Math.tan(x)*x)) % 255;
-        const g = (b + (r + (Math.PI/2))) % 255;
-
+    for(let y = 0; y < canvasHeight; y++) {
+      for(let x = 0; x < canvasWidth; x++) {
+        const py = (y - canvasHeight/2) / canvasHeight ;
+        const px = (x - canvasWidth/2) / canvasHeight ;
+        const lp = Math.sqrt(Math.pow(py, 2) + Math.pow(px, 2));
+        const spiralTension = 0.618;
+        const theta = Math.atan(px/py);
+        const spiral = ((Math.log(lp)/spiralTension) + theta);
+        const r = ( Math.sin(10 * spiral) * 150 ) * Math.atan(x/y) % 255;
+        const g = ( Math.sin(10 * spiral) * 150 ) % 255;
+        const b = ( Math.sin(10 * spiral) * 150 ) + Math.sqrt(y) * 2 % 255;
         ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
         ctx.fillRect(x, y, 1, 1);
       }
     }
+    // window.requestAnimationFrame(this.updateCanvas);
 
-    ctx.globalAlpha = 0.01;
-    ctx.lineWidth=1;
-    // for(let i = 0; i < 50; i++) {
-    //   // ctx.beginPath();
-    //   // ctx.fillStyle = 'black';
-    //   ctx.strokeStyle = 'rgb(200, 50, 100)';
-    //   ctx.arc((Math.cos(i)*canvasWidth), (i)*10, Math.sqrt(i), 0, Math.PI * 2);
-    //   // ctx.fill();
-    //   ctx.stroke();
-    //   ctx.closePath();
+    // -----------------------------------------------
+
+
+    // ------------ Space thing ---------------------------------------------
+    // ctx.beginPath();
+    // for(let x = 0; x < canvasWidth; x++) {
+    //   for(let y = 0; y < canvasHeight; y++) {
+    //     const r = Math.cos((Math.sqrt(y)*Math.tan(x)*-x)) % 255;
+    //     const b = Math.tan((Math.sqrt(y)*Math.tan(x)*x)) % 255;
+    //     const g = (b + (r + (Math.PI/2))) % 255;
+
+    //     ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    //     ctx.fillRect(x, y, 1, 1);
+    //   }
     // }
+
+    // ctx.globalAlpha = 0.01;
+    // ctx.lineWidth=1;
+    // // for(let i = 0; i < 50; i++) {
+    // //   // ctx.beginPath();
+    // //   // ctx.fillStyle = 'black';
+    // //   ctx.strokeStyle = 'rgb(200, 50, 100)';
+    // //   ctx.arc((Math.cos(i)*canvasWidth), (i)*10, Math.sqrt(i), 0, Math.PI * 2);
+    // //   // ctx.fill();
+    // //   ctx.stroke();
+    // //   ctx.closePath();
+    // // }
+    // // ctx.beginPath();
+    // // for(let i = 0; i < 50; i++) {
+    // //   // ctx.fillStyle = 'black';
+    // //   ctx.strokeStyle = 'rgb(200, 50, 100)';
+    // //   ctx.arc((i*i)/2, i*10, Math.sqrt(i), 0, Math.PI * 2);
+    // //   // ctx.fill();
+    // //   ctx.stroke();
+    // //   ctx.closePath();
+    // // }
+
     // ctx.beginPath();
     // for(let i = 0; i < 50; i++) {
     //   // ctx.fillStyle = 'black';
     //   ctx.strokeStyle = 'rgb(200, 50, 100)';
-    //   ctx.arc((i*i)/2, i*10, Math.sqrt(i), 0, Math.PI * 2);
+    //   ctx.arc(Math.cos(i)*canvasWidth, i*10, Math.sqrt(i), 0, Math.PI * 2);
     //   // ctx.fill();
     //   ctx.stroke();
     //   ctx.closePath();
     // }
-
-    ctx.beginPath();
-    for(let i = 0; i < 50; i++) {
-      // ctx.fillStyle = 'black';
-      ctx.strokeStyle = 'rgb(200, 50, 100)';
-      ctx.arc(Math.cos(i)*canvasWidth, i*10, Math.sqrt(i), 0, Math.PI * 2);
-      // ctx.fill();
-      ctx.stroke();
-      ctx.closePath();
-    }
-    ctx.globalAlpha = 1;
-    ctx.lineWidth=0.5;
-    ctx.beginPath();
-    // ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'white';
-    const radgrad = ctx.createRadialGradient(canvasWidth/2, canvasHeight/2, 100, canvasWidth/2, canvasWidth/2, 1);
-    radgrad.addColorStop(0, 'rgba(0,0,0,0)');
-    radgrad.addColorStop(1, '#70002C');
-    // radgrad.addColorStop(1, 'white');
-    ctx.fillStyle = radgrad;
-    ctx.arc(canvasWidth/2, canvasHeight/2, 70, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+    // ctx.globalAlpha = 1;
+    // ctx.lineWidth=0.5;
+    // ctx.beginPath();
+    // // ctx.fillStyle = 'white';
+    // ctx.strokeStyle = 'white';
+    // const radgrad = ctx.createRadialGradient(canvasWidth/2, canvasHeight/2, 100, canvasWidth/2, canvasWidth/2, 1);
+    // radgrad.addColorStop(0, 'rgba(0,0,0,0)');
+    // radgrad.addColorStop(1, '#70002C');
+    // // radgrad.addColorStop(1, 'white');
+    // ctx.fillStyle = radgrad;
+    // ctx.arc(canvasWidth/2, canvasHeight/2, 70, 0, Math.PI * 2);
+    // ctx.fill();
+    // ctx.stroke();
+    // ctx.closePath();
 
     // -------------------------------------------------------
 
@@ -115,7 +149,7 @@ class GraphView extends Component {
     // draw vert values (labels)
   }
 
-  
+
   /**
    * Render
    */
