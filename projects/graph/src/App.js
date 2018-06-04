d3c0d144fc05f8c464b337c36efb4ddb8a8c505e
AJ Genung <DragonRydrz@me.com>
2018-06-04 18:47:46 -0400

    making a tunnel.

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -35,11 +35,21 @@ class GraphView extends Component {
     var grd = ctx.createLinearGradient(0, 0, canvasWidth, canvasHeight);
     grd.addColorStop(0, 'black');
     grd.addColorStop(1, 'white');
-    ctx.fillStyle = grd;
+    ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    for (let i = 0; i < canvasWidth; i) {}
+    for (let i = 0; i <= canvasWidth; i = i + 50) {
+      ctx.moveTo(i, 0);
+      ctx.lineTo(canvasWidth - i, canvasHeight);
+    }
+    for (let i = 50; i < canvasHeight; i = i + 50) {
+      ctx.moveTo(0, i);
+      ctx.lineTo(canvasWidth, canvasHeight - i);
+    }
 
+    for (let i = 0; i < 0.5; i = i + (0.6 - i) * 0.15) {}
+
+    ctx.stroke();
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
