41f9ed88d0a73b60c7e0782c2187c9c6e4032a22
kait-schorr <kaitlynschorr@gmail.com>
2018-06-04 17:47:02 -0500

    I made animated bubbles!

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
-import { Graph } from './graph';
-import './App.css';
+import React, { Component } from "react";
+import { Graph } from "./graph";
+import "./App.css";
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 900;
+const canvasHeight = 700;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
 /**
  * GraphView
@@ -24,32 +30,61 @@ class GraphView extends Component {
     this.updateCanvas();
   }
 
+  generateBubbles(num) {
+    let bubbles = [];
+    for (let i = 0; i < num; i++) {
+      let bubble = {};
+      bubble.color = `rgba(${getRandomInt(1, 100)}, ${getRandomInt(
+        1,
+        255
+      )}, ${getRandomInt(200, 255)}, ${Math.random()})`;
+      bubble.x = getRandomInt(1, canvasWidth);
+      bubble.y = getRandomInt(1, canvasHeight);
+      bubble.r = getRandomInt(1, 20);
+      bubble.speed = 1 / (bubble.r / 20);
+      bubbles.push(bubble);
+    }
+    return bubbles;
+  }
   /**
    * Render the canvas
    */
   updateCanvas() {
     let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
-    // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    let ctx = canvas.getContext("2d");
 
+    let bubbles = this.generateBubbles(1000);
+    // Clear it
+    setInterval(() => {
+      ctx.fillStyle = "#035E8C";
+      ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+      for (let i = 0; i < bubbles.length; i++) {
+        ctx.fillStyle = `${bubbles[i].color}`;
+        ctx.beginPath();
+        ctx.arc(bubbles[i].x, bubbles[i].y, bubbles[i].r, 0, 2 * Math.PI);
+        ctx.fill();
+        // ctx.stroke();
+        bubbles[i].y -= bubbles[i].speed;
+        if (bubbles[i].y < 0) bubbles[i].y = canvasHeight;
+      }
+    }, 50);
+
+    // ctx.
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
   }
-  
+
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
-
 
 /**
  * App
@@ -69,7 +104,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
+        <GraphView graph={this.state.graph} />
       </div>
     );
   }
