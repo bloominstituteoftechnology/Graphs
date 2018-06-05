b3b2abc419329e7f6cd8e9bdf28b8fd638def965
Edward Manda <lilkups@yahoo.com>
2018-06-05 08:38:46 -0500

    experimentation

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 600;
+const canvasHeight = 400;
 
 /**
  * GraphView
@@ -23,17 +23,46 @@ class GraphView extends Component {
   componentDidUpdate() {
     this.updateCanvas();
   }
+  // START FO MY CUSTOM FUNCTIONS
 
+  draw(canvas, ctx) {
+    ctx.fillStyle = 'lightblue';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // // Create a gradient
+    const rGrd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+    rGrd.addColorStop(0, 'orange');
+    rGrd.addColorStop(0.5, 'yellow');
+    rGrd.addColorStop(1, 'lightblue');
+
+    // // Fill with gradient
+    ctx.fillStyle = rGrd;
+    ctx.fillRect(0, 0, 200, 150);
+
+    // Create gradient
+    const lGrd = ctx.createLinearGradient(0, 0, canvasWidth, canvasHeight);
+    lGrd.addColorStop(0, 'blue');
+    lGrd.addColorStop(0.5, 'yellow');
+    lGrd.addColorStop(1, 'brown');
+
+    // Fill with gradient
+    ctx.fillStyle = lGrd;
+    ctx.fillRect(0, 200, canvasWidth, canvasHeight);
+  }
+
+  // END OF MY CUSTOM FUNCTIONS
   /**
    * Render the canvas
    */
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+
     // Clear it
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    this.draw(canvas, ctx);
 
     // !!! IMPLEMENT ME
     // compute connected components
@@ -41,15 +70,14 @@ class GraphView extends Component {
     // draw verts
     // draw vert values (labels)
   }
-  
+
   /**
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight} />;
   }
 }
-
 
 /**
  * App
@@ -59,7 +87,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      graph: new Graph()
+      graph: new Graph(),
     };
 
     // !!! IMPLEMENT ME
@@ -69,7 +97,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
+        <GraphView graph={this.state.graph} />
       </div>
     );
   }
