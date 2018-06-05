5c972e1365521dd7651106ed6e88b15936332d5d
kait-schorr <kaitlynschorr@gmail.com>
2018-06-05 16:04:12 -0500

    Got edges working

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,9 @@ import { Graph } from "./graph";
 import "./App.css";
 
 // !!! IMPLEMENT ME
-const canvasWidth = 900;
-const canvasHeight = 700;
+const canvasWidth = 800;
+const canvasHeight = 600;
+const circleSize = 15;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -37,7 +38,7 @@ class GraphView extends Component {
       bubble.color = `rgba(${getRandomInt(1, 100)}, ${getRandomInt(
         1,
         255
-      )}, ${getRandomInt(200, 255)}, ${Math.random()})`;
+      )}, ${getRandomInt(200, 255)}, ${Math.random() / 1.2})`;
       bubble.x = getRandomInt(1, canvasWidth);
       bubble.y = getRandomInt(1, canvasHeight);
       bubble.r = getRandomInt(1, 20);
@@ -53,22 +54,24 @@ class GraphView extends Component {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext("2d");
 
-    let bubbles = this.generateBubbles(1000);
-    // Clear it
-    setInterval(() => {
-      ctx.fillStyle = "#035E8C";
-      ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    console.log("In update canvas", this.props);
+    // let bubbles = this.generateBubbles(1000);
+    // // Clear it
+    // setInterval(() => {
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fill();
 
-      for (let i = 0; i < bubbles.length; i++) {
-        ctx.fillStyle = `${bubbles[i].color}`;
-        ctx.beginPath();
-        ctx.arc(bubbles[i].x, bubbles[i].y, bubbles[i].r, 0, 2 * Math.PI);
-        ctx.fill();
-        // ctx.stroke();
-        bubbles[i].y -= bubbles[i].speed;
-        if (bubbles[i].y < 0) bubbles[i].y = canvasHeight;
-      }
-    }, 50);
+    //   for (let i = 0; i < bubbles.length; i++) {
+    //     ctx.fillStyle = `${bubbles[i].color}`;
+    //     ctx.beginPath();
+    //     ctx.arc(bubbles[i].x, bubbles[i].y, bubbles[i].r, 0, 2 * Math.PI);
+    //     ctx.fill();
+    //     // ctx.stroke();
+    //     bubbles[i].y -= bubbles[i].speed;
+    //     if (bubbles[i].y < 0) bubbles[i].y = canvasHeight;
+    //   }
+    // }, 50);
 
     // ctx.
     // !!! IMPLEMENT ME
@@ -76,6 +79,27 @@ class GraphView extends Component {
     // draw edges
     // draw verts
     // draw vert values (labels)
+
+    for (let vertex of this.props.graph.vertexes) {
+      ctx.beginPath();
+      ctx.arc(vertex.pos.x, vertex.pos.y, circleSize, 0, 2 * Math.PI);
+      ctx.fillStyle = "white";
+      ctx.fill();
+      ctx.stroke();
+
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+      ctx.font = "16px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
+      for (let edge of vertex.edges) {
+        console.log("Edge: ", edge);
+        ctx.fillStyle = "black";
+        ctx.moveTo(vertex.pos.x, vertex.pos.y);
+        ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+        ctx.stroke();
+      }
+    }
   }
 
   /**
@@ -102,6 +126,7 @@ class App extends Component {
   }
 
   render() {
+    this.state.graph.debugCreateTestData();
     return (
       <div className="App">
         <GraphView graph={this.state.graph} />
