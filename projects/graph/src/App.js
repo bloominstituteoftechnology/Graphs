fce346f81383a910a0cf9548402fe4ef5244456c
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-07-09 17:25:53 -0400

    finished canvas art

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -32,65 +32,43 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
 
     roundedRect(ctx, 150, 80, 800, 500, 9);
-    roundedRect(ctx, 220, 180, 100, 70, 10);
-    roundedRect(ctx, 53, 119, 49, 16, 6);
-    roundedRect(ctx, 135, 53, 49, 33, 10);
-    roundedRect(ctx, 135, 119, 25, 49, 10);
+    roundedRect(ctx, 220, 180, 120, 70, 10);
+    roundedRect(ctx, 220, 420, 180, 50, 6);
+    roundedRect(ctx, 750, 180, 140, 33, 10);
+    roundedRect(ctx, 750, 400, 70, 120, 10);
 
+    ctx.fillStyle = 'yellow';
     ctx.beginPath();
-    ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
-    ctx.lineTo(31, 37);
+    ctx.arc(200, 120, 20, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(200, 120);
     ctx.fill();
 
-    for (var i = 0; i < 8; i++) {
-      ctx.fillRect(51 + i * 16, 35, 4, 4);
-    }
-
-    for (i = 0; i < 6; i++) {
-      ctx.fillRect(115, 51 + i * 16, 4, 4);
-    }
-
-    for (i = 0; i < 8; i++) {
-      ctx.fillRect(51 + i * 16, 99, 4, 4);
-    }
-
     ctx.beginPath();
-    ctx.moveTo(83, 116);
-    ctx.lineTo(83, 102);
-    ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
-    ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
-    ctx.lineTo(111, 116);
-    ctx.lineTo(106.333, 111.333);
-    ctx.lineTo(101.666, 116);
-    ctx.lineTo(97, 111.333);
-    ctx.lineTo(92.333, 116);
-    ctx.lineTo(87.666, 111.333);
-    ctx.lineTo(83, 116);
+    ctx.arc(605, 320, 20, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(605, 320);
     ctx.fill();
-    
-    
-    ctx.fillStyle = 'white';
+
     ctx.beginPath();
-    ctx.moveTo(91, 96);
-    ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
-    ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
-    ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
-    ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
-    ctx.moveTo(103, 96);
-    ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
-    ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
-    ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
-    ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+    ctx.arc(324, 520, 20, Math.PI / 7, -Math.PI / 7, false);
+    ctx.lineTo(324, 520);
     ctx.fill();
 
     ctx.fillStyle = 'black';
-    ctx.beginPath();
-    ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
-    ctx.fill();
+    for (var i = 0; i < 18; i++) {
+      ctx.fillRect(220 + i * 40, 118, 4, 4);
+    }
 
-    ctx.beginPath();
-    ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
-    ctx.fill();
+    for (i = 0; i < 12; i++) {
+      ctx.fillRect(520, 100 + i * 40, 4, 4);
+    }
+
+    for (i = 0; i < 18; i++) {
+      ctx.fillRect(220 + i * 40, 318, 4, 4);
+    }
+
+    for (i = 0; i < 8; i++) {
+      ctx.fillRect(220 + i * 40, 518, 4, 4);
+    }
 
     // !!! IMPLEMENT ME
     // compute connected components
