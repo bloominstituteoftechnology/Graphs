a338529e47f86c69e39fa05d96c4ee7391d9d0af
Anthony <anthony@anthonys-mbp.lan>
2018-07-09 18:41:14 -0400

    Canvas Layous:
    Learned how to utilize canvas and ctx functionality within react. Designed a layout that displays the ROY G BIV color spectrum across the app.

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+let canvasWidth = 500;
+let canvasHeight = 500;
 
 /**
  * GraphView
@@ -28,12 +28,130 @@ class GraphView extends Component {
    * Render the canvas
    */
   updateCanvas() {
-    let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
     
-    // Clear it
+    // Base Layer
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(0, 0, 500, 500);
+
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillRect(80, 80, 340, 340);
+
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(150, 150, 200, 200);
+
+    // Top Left
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 0, 70, 70);
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(0, 0, 60, 60);
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0, 0, 50, 50);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 0, 40, 40);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 30, 30);
+
+    ctx.fillStyle = 'indigo';
+    ctx.fillRect(0, 0, 20, 20);
+
+    ctx.fillStyle = 'violet';
+    ctx.fillRect(0, 0, 10, 10);
+
+    // Top Right
+    ctx.fillStyle = 'red';
+    ctx.fillRect(430, 0, 70, 70);
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(440, 0, 60, 60);
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(450, 0, 50, 50);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(460, 0, 40, 40);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(470, 0, 30, 30);
+
+    ctx.fillStyle = 'indigo';
+    ctx.fillRect(480, 0, 20, 20);
+
+    ctx.fillStyle = 'violet';
+    ctx.fillRect(490, 0, 10, 10);
+    
+    // Bottom Left
+    ctx.fillStyle = 'red';
+    ctx.fillRect(0, 430, 70, 70);
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(0, 440, 60, 60);
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(0, 450, 50, 50);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(0, 460, 40, 40);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 470, 30, 30);
+
+    ctx.fillStyle = 'indigo';
+    ctx.fillRect(0, 480, 20, 20);
+
+    ctx.fillStyle = 'violet';
+    ctx.fillRect(0, 490, 10, 10);
+
+    // Bottom Right
+    ctx.fillStyle = 'red';
+    ctx.fillRect(430, 430, 70, 70);
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(440, 440, 60, 60);
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(450, 450, 50, 50);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(460, 460, 40, 40);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(470, 470, 30, 30);
+
+    ctx.fillStyle = 'indigo';
+    ctx.fillRect(480, 480, 20, 20);
+
+    ctx.fillStyle = 'violet';
+    ctx.fillRect(490, 490, 10, 10);
+    
+    // Center
+    ctx.fillStyle = 'red';
+    ctx.fillRect(215, 215, 70, 70);
+
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(220, 220, 60, 60);
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(225, 225, 50, 50);
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(230, 230, 40, 40);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(235, 235, 30, 30);
+
+    ctx.fillStyle = 'indigo';
+    ctx.fillRect(240, 240, 20, 20);
+
+    ctx.fillStyle = 'violet';
+    ctx.fillRect(245, 245, 10, 10);
+
+
 
     // !!! IMPLEMENT ME
     // compute connected components
