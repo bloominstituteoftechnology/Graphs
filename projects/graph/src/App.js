8e05e88ee6e6076a9e75d525f13b62fd4af69c53
Abraham Bueno <abueno@localhost.localdomain>
2018-07-09 17:42:27 -0500

    Working progress.

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 800;
+const canvasHeight = 800;
 
 /**
  * GraphView
@@ -30,18 +30,77 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
-    
+
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'red';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
 
     // !!! IMPLEMENT ME
     // compute connected components
+
     // draw edges
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(100, 500);
+    ctx.stroke();
     // draw verts
+
     // draw vert values (labels)
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(250, 160, 30, 0, 2 * Math.PI);
+    // cts.
+    ctx.fill();
+    //<<< >>> <<< >>> <<< >>>
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 1200);
+    ctx.stroke();
+    //<<< >>> <<< >>> <<< >>>
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(250, 700);
+    ctx.stroke();
+    //<<< >>> <<< >>> <<< >>>
+
+    ctx.font = "30px Arial";
+    ctx.strokeText("Hello Canvas", canvas.width / 3 + 60, canvas.height / 3);
+    //<<< >>> <<< >>> <<< >>>
+    ctx.font = "30px Comic Sans MS";
+    ctx.fillStyle = "yellow";
+    ctx.textAlign = "center";
+    ctx.fillText("Hello Canvas", canvas.width / 2, canvas.height / 2);
+    //<<< >>> <<< >>> <<< >>>
+    for (var i = 0; i < 6; i++) {
+      for (var j = 0; j < 6; j++) {
+        ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' +
+          Math.floor(255 - 42.5 * j) + ',0)';
+        ctx.fillRect(j * 25, i * 25, 50, 50);
+      }
+    }
+    //<<< >>> <<< >>> <<< >>>
+    var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 700);
+    grd.addColorStop(0, "yellow");
+    grd.addColorStop(1, "white");
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(300, 10, 360, 80);
+
+
+    //<<< >>> <<< >>> <<< >>>
+    ctx.beginPath();
+    ctx.moveTo(100, 20);           // Create a starting point
+    ctx.lineTo(200, 20);          // Create a horizontal line
+    ctx.arcTo(250, 20, 250, 70, 50); // Create an arc
+    ctx.lineTo(250, 120);         // Continue with vertical line
+    ctx.stroke();
   }
-  
+
   /**
    * Render
    */
@@ -64,6 +123,8 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    const g = new Graph();
+    g.randomize(5, 4, 150, 0.6);
   }
 
   render() {
