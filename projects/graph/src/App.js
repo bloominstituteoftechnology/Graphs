35046b24c1a472aac00cf7376fe782f07ddc1ac6
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-07-09 15:41:41 -0700

    messing with canvas

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 /**
  * GraphView
@@ -28,19 +28,162 @@ class GraphView extends Component {
    * Render the canvas
    */
   updateCanvas() {
-    let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
     
-    // Clear it
-    ctx.fillStyle = 'white';
+    
+    var gradient = ctx.createLinearGradient(50, 400, 50, 0);
+        gradient.addColorStop(0, 'blue');
+        gradient.addColorStop(.3, 'skyblue');
+    ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    // !!! IMPLEMENT ME
-    // compute connected components
-    // draw edges
-    // draw verts
-    // draw vert values (labels)
-  }
+    ctx.strokeStyle = 'yellow';
+    for (let i = 0; i < 200; i++) {
+      ctx.beginPath();
+      ctx.moveTo(Math.cos(i) * 0, Math.sin(i) * 0);
+      ctx.lineTo(Math.sin(i) * 200, Math.cos(i) * 200);
+      ctx.stroke();
+    }
+
+    ctx.beginPath();
+    ctx.arc(30, 20, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'yellow';
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.ellipse(350, 525, 250, 150, 1 * Math.PI/180, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#D4C6AC';
+    ctx.fillStyle = '#D4C6AC';
+    ctx.fill();
+    
+
+    ctx.beginPath();
+    ctx.arc(300, 35, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(315, 30, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(330, 25, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(345, 25, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(360, 30, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(375, 35, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(315, 40, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(330, 45, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(345, 45, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(360, 40, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.fillStyle = 'white';
+    ctx.fill();    
+
+
+    ctx.beginPath();
+    ctx.arc(210, 55, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(225, 50, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(240, 45, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(255, 45, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(270, 50, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(285, 55, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(225, 60, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(240, 65, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(255, 65, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+    ctx.beginPath();
+    ctx.arc(270, 60, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.strokeStyle = '#f7f7f7';
+    ctx.fillStyle = '#f7f7f7';
+    ctx.fill();    
+}
   
   /**
    * Render
