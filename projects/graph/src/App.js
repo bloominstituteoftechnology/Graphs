387ed1e4ad7c3d3b40689f93ecb73cd923c04c91
Natalie <natalieschennum@gmail.com>
2018-07-09 15:03:40 -0700

    Recs and mouse controlled ball

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,9 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1400
+const canvasHeight = 800
+
 
 /**
  * GraphView
@@ -29,19 +30,88 @@ class GraphView extends Component {
    */
   updateCanvas() {
     let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
+    let ctx = canvas.getContext("2d");
+    let raf;
+    let running = false;
+
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = "#626D71";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillStyle = "#CDCDC0";
+    ctx.fillRect(100, 100, canvasWidth, canvasHeight);
+    ctx.fillStyle = "#DDBC95";
+    ctx.fillRect(200, 200, canvasWidth, canvasHeight);
+    ctx.fillStyle = "#B38867";
+    ctx.fillRect(300, 300, canvasWidth, canvasHeight);
+    var ball = {
+      x: 100,
+      y: 100,
+      vx: 5,
+      vy: 1,
+      radius: 10,
+      color: 'dark-brown',
+      draw: function() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fill();
+      }
+    };
+    
+    function clear() {
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+      // ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+    
+    function draw() {
+       clear();
+      ball.draw();
+      ball.x += ball.vx;
+      ball.y += ball.vy;
+    
+      if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+        ball.vy = -ball.vy;
+      }
+      if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+        ball.vx = -ball.vx;
+      }
+    
+      raf = window.requestAnimationFrame(draw);
+    }
+    
+    canvas.addEventListener('mousemove', function(e) {
+      if (!running) {
+        clear();
+        ball.x = e.clientX;
+        ball.y = e.clientY;
+        ball.draw();
+      }
+    });
+    
+    canvas.addEventListener('click', function(e) {
+      if (!running) {
+        raf = window.requestAnimationFrame(draw);
+        running = true;
+      }
+    });
+    
+    canvas.addEventListener('mouseout', function(e) {
+      window.cancelAnimationFrame(raf);
+      running = false;
+    });
+    
+    ball.draw();
+  
+  }
 
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
-  }
   
+
   /**
    * Render
    */
@@ -69,7 +139,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
+      <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Canvas API</h1>
+        </header>
+        <GraphView graph={this.state.graph}>Canvas API</GraphView>
       </div>
     );
   }
