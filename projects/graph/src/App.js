4194ae3c1cdbe8ed0ec4103c3ef7d31a02c593ce
Harrison Brock <harrison@harrisonbrock.com>
2018-07-09 16:25:32 -0400

    CODE DRAW BLUE CANVAS

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -1,53 +1,54 @@
-import React, { Component } from 'react';
-import { Graph } from './graph';
+import React, {Component} from 'react';
+import {Graph} from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1000;
+const canvasHeight = 900;
 
 /**
  * GraphView
  */
 class GraphView extends Component {
-  /**
-   * On mount
-   */
-  componentDidMount() {
-    this.updateCanvas();
-  }
+    /**
+     * On mount
+     */
+    componentDidMount() {
+        this.updateCanvas();
+    }
 
-  /**
-   * On state update
-   */
-  componentDidUpdate() {
-    this.updateCanvas();
-  }
+    /**
+     * On state update
+     */
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
 
-  /**
-   * Render the canvas
-   */
-  updateCanvas() {
-    let canvas = this.refs.canvas;
-    let ctx = canvas.getContext('2d');
-    
-    // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    /**
+     * Render the canvas
+     */
+    updateCanvas() {
+        let canvas = this.refs.canvas;
+        let ctx = canvas.getContext('2d');
 
-    // !!! IMPLEMENT ME
-    // compute connected components
-    // draw edges
-    // draw verts
-    // draw vert values (labels)
-  }
-  
-  /**
-   * Render
-   */
-  render() {
-    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
-  }
+        // Clear it
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        // !!! IMPLEMENT ME
+        // compute connected components
+
+        // draw edges
+        // draw verts
+        // draw vert values (labels)
+    }
+
+    /**
+     * Render
+     */
+    render() {
+        return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
+    }
 }
 
 
@@ -55,24 +56,24 @@ class GraphView extends Component {
  * App
  */
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      graph: new Graph()
-    };
+        this.state = {
+            graph: new Graph()
+        };
 
-    // !!! IMPLEMENT ME
-    // use the graph randomize() method
-  }
+        // !!! IMPLEMENT ME
+        // use the graph randomize() method
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <GraphView graph={this.state.graph}></GraphView>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <GraphView graph={this.state.graph}></GraphView>
+            </div>
+        );
+    }
 }
 
 export default App;
