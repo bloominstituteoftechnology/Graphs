f79cf0c4372352ec89a54d4b9a5957bdbcf1fb0b
Henry K <mykim1891@hotmail.com>
2018-07-09 16:25:01 -0700

    Final poster

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -33,14 +33,17 @@ class GraphView extends Component {
 
     // Clear it
     // grey canvas
-    ctx.fillStyle = "#aaacb5";
+    let gradient_canvas = ctx.createLinearGradient(345, 582, 700, 50);
+    gradient_canvas.addColorStop(0, "#c4beb0"); // darker
+    gradient_canvas.addColorStop(0.25, "#dbd5c7"); // lighter
+    ctx.fillStyle = gradient_canvas;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // red line
 
     let gradient0 = ctx.createLinearGradient(345, 582, 700, 50);
     gradient0.addColorStop(0, "#cc2037"); // darker
-    gradient0.addColorStop(0.20, "#f2485f"); // lighter
+    gradient0.addColorStop(0.25, "#f2485f"); // lighter
     ctx.fillStyle = gradient0;
     ctx.lineWidth = 1;
     ctx.beginPath();
@@ -50,47 +53,74 @@ class GraphView extends Component {
     ctx.lineTo(700, 100); // top right: end
     ctx.closePath();
     ctx.fill();
-    
+
     let gradient1 = ctx.createLinearGradient(0, 850, 300, 549);
-    gradient1.addColorStop(0.80, "#f2485f"); // lighter
+    gradient1.addColorStop(0.75, "#f2485f"); // lighter
     gradient1.addColorStop(1, "#cc2037"); // darker
     ctx.fillStyle = gradient1;
     ctx.beginPath();
-    ctx.lineTo(301, 549); // middle
+    ctx.moveTo(301, 549); // middle
     ctx.lineTo(0, 660); // lower left
     ctx.lineTo(0, 850); // bottom left: canvas edge
     ctx.lineTo(100, 850); // bottom right
     ctx.lineTo(346, 581); // middle again
     ctx.closePath();
     ctx.fill();
-    // ctx.stroke();
 
     // cyan line
-    ctx.fillStyle = "#2cb786";
+    let gradient2 = ctx.createLinearGradient(345, 580, 700, 100);
+    gradient2.addColorStop(0, "#2cb786"); // darker
+    gradient2.addColorStop(0.30, "#49d8a6"); // lighter
+    ctx.fillStyle = gradient2;
     ctx.lineWidth = 1;
     ctx.beginPath();
     ctx.moveTo(700, 100); // top right
     ctx.lineTo(345, 582); // middle
-    ctx.lineTo(100, 850); // bottom left
-    ctx.lineTo(350, 850); // bottom right
     ctx.lineTo(392, 613); // middle again
     ctx.lineTo(700, 155); // top right: end
     ctx.closePath();
     ctx.fill();
-    // ctx.stroke();
+
+    let gradient3 = ctx.createLinearGradient(100, 850, 345, 582);
+    gradient3.addColorStop(0.70, "#49d8a6"); // lighter
+    gradient3.addColorStop(1, "#2cb786"); // darker
+    ctx.fillStyle = gradient3;
+    ctx.beginPath();
+    ctx.moveTo(345, 582); // middle
+    ctx.lineTo(100, 850); // bottom left
+    ctx.lineTo(350, 850); // bottom right
+    ctx.lineTo(392, 613); // middle again
+    ctx.closePath();
+    ctx.fill();
+
 
     // yellow line
-    ctx.fillStyle = "#f9bb34";
-    ctx.lineWidth = 1;
-
+    let gradient4 = ctx.createLinearGradient(392, 613, 700, 155);
+    gradient4.addColorStop(0, "#f9bb34"); // darker
+    gradient4.addColorStop(0.20, "#ffcd60"); // lighter
+    ctx.fillStyle = gradient4;
+    
+    // ctx.fillStyle = "blue";
     ctx.beginPath();
     ctx.moveTo(700, 155); // top right
     ctx.lineTo(392, 613); // middle
-    ctx.lineTo(350, 850); // bottom left
-    // ctx.lineTo(700, 850); // bottom right: canvas edge
-    ctx.lineTo(525, 850); // bottom right
     ctx.lineTo(445, 640); // middle again
     ctx.lineTo(700, 220); // top right: end
+    ctx.closePath();
+    ctx.fill();
+    // ctx.stroke();
+
+
+    let gradient5 = ctx.createLinearGradient(350, 850, 392, 613);
+    gradient5.addColorStop(0.80, "#ffcd60"); // lighter
+    gradient5.addColorStop(1, "#f9bb34"); // darker
+    // ctx.fillStyle = "blue";
+    ctx.fillStyle = gradient5;
+    ctx.beginPath();
+    ctx.moveTo(392, 613); // middle
+    ctx.lineTo(350, 850); // bottom left
+    ctx.lineTo(525, 850); // bottom right
+    ctx.lineTo(445, 640); // middle again
     ctx.closePath();
     ctx.fill();
     // ctx.stroke();
@@ -99,7 +129,7 @@ class GraphView extends Component {
     ctx.lineWidth = 1;
     ctx.beginPath();
     ctx.moveTo(0, 343);
-    ctx.lineTo(700, 828);
+    ctx.lineTo(700, 810);
     // ctx.stroke();
 
     // text
