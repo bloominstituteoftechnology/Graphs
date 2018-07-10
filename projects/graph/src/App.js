8c541542320733ea816cc72c5138fc625411cbd5
totsuya <supertatortots@gmail.com>
2018-07-10 17:12:24 -0400

    incraesed values of canvas

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-const canvasWidth = 100;
-const canvasHeight = 100;
+const canvasWidth = 900;
+const canvasHeight = 1100;
 
 /**
  * GraphView
@@ -32,7 +32,7 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // !!! IMPLEMENT ME
