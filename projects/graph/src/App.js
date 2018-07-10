002137d09b0f366259b4604364d5bb6e6bcadbd6
Anthony <anthony@anthonys-mbp.lan>
2018-07-10 17:32:22 -0400

    Phase 1 of Graphs:
    Managed to display circluar arcs to the canvas.
    Moving to Phase 3.

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 // !!! IMPLEMENT ME
 let canvasWidth = 500;
 let canvasHeight = 500;
+const circleRadius = 15;
 
 /**
  * GraphView
@@ -31,125 +32,155 @@ class GraphView extends Component {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext('2d');
     
+    console.log('this.props.graph: ', this.props.graph);
+    // call the dummy graph function to test
+    this.props.graph.createDummyGraph();
+    console.log('called dummy graph'); 
+
     // Base Layer
     ctx.fillStyle = 'grey';
     ctx.fillRect(0, 0, 500, 500);
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(80, 80, 340, 340);
+    ctx.font = '13px Arial';
+    ctx.textAlign = 'center'; 
+    ctx.textBaseline = 'middle'; 
+    // ---- Canvas Practice Phase 1 day 2 -----
+    // draw dummy vertex 1
+    this.props.graph.vertexes.forEach((v) => {
+      ctx.beginPath();
+      ctx.fillStyle = 'white';
+      ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
+      ctx.fill();
+      ctx.stroke();
+      
+      // fill in the text
+      ctx.fillStyle = 'black';
+      ctx.fillText(v.value, v.pos.x, v.pos.y);
+    })
 
-    ctx.fillStyle = 'grey';
-    ctx.fillRect(150, 150, 200, 200);
+    // draw dummy graph:
+    // ctx.arc(10, 10, 10, 0, 2 * Math.PI); // define an arc path
+    // ctx.stroke(); // follow the path and draw it
+    // ctx.beginPath(); // begin new path
+    // ctx.arc(100, 100, 10, 0, 2 * Math.PI); // define an arc path
+    // ctx.stroke();
+
+    // ---- Canvas Practice Phase 2 day 1 -----
+    // ctx.fillStyle = 'white';
+    // ctx.fillRect(80, 80, 340, 340);
+
+    // ctx.fillStyle = 'grey';
+    // ctx.fillRect(150, 150, 200, 200);
 
     // Top Left
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, 70, 70);
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(0, 0, 70, 70);
 
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(0, 0, 60, 60);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(0, 0, 60, 60);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(0, 0, 50, 50);
+    // ctx.fillStyle = 'yellow';
+    // ctx.fillRect(0, 0, 50, 50);
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 0, 40, 40);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(0, 0, 40, 40);
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(0, 0, 30, 30);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(0, 0, 30, 30);
 
-    ctx.fillStyle = 'indigo';
-    ctx.fillRect(0, 0, 20, 20);
+    // ctx.fillStyle = 'indigo';
+    // ctx.fillRect(0, 0, 20, 20);
 
-    ctx.fillStyle = 'violet';
-    ctx.fillRect(0, 0, 10, 10);
+    // ctx.fillStyle = 'violet';
+    // ctx.fillRect(0, 0, 10, 10);
 
     // Top Right
-    ctx.fillStyle = 'red';
-    ctx.fillRect(430, 0, 70, 70);
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(430, 0, 70, 70);
 
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(440, 0, 60, 60);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(440, 0, 60, 60);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(450, 0, 50, 50);
+    // ctx.fillStyle = 'yellow';
+    // ctx.fillRect(450, 0, 50, 50);
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(460, 0, 40, 40);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(460, 0, 40, 40);
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(470, 0, 30, 30);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(470, 0, 30, 30);
 
-    ctx.fillStyle = 'indigo';
-    ctx.fillRect(480, 0, 20, 20);
+    // ctx.fillStyle = 'indigo';
+    // ctx.fillRect(480, 0, 20, 20);
 
-    ctx.fillStyle = 'violet';
-    ctx.fillRect(490, 0, 10, 10);
+    // ctx.fillStyle = 'violet';
+    // ctx.fillRect(490, 0, 10, 10);
     
-    // Bottom Left
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 430, 70, 70);
+    // // Bottom Left
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(0, 430, 70, 70);
 
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(0, 440, 60, 60);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(0, 440, 60, 60);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(0, 450, 50, 50);
+    // ctx.fillStyle = 'yellow';
+    // ctx.fillRect(0, 450, 50, 50);
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(0, 460, 40, 40);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(0, 460, 40, 40);
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(0, 470, 30, 30);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(0, 470, 30, 30);
 
-    ctx.fillStyle = 'indigo';
-    ctx.fillRect(0, 480, 20, 20);
+    // ctx.fillStyle = 'indigo';
+    // ctx.fillRect(0, 480, 20, 20);
 
-    ctx.fillStyle = 'violet';
-    ctx.fillRect(0, 490, 10, 10);
+    // ctx.fillStyle = 'violet';
+    // ctx.fillRect(0, 490, 10, 10);
 
-    // Bottom Right
-    ctx.fillStyle = 'red';
-    ctx.fillRect(430, 430, 70, 70);
+    // // Bottom Right
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(430, 430, 70, 70);
 
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(440, 440, 60, 60);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(440, 440, 60, 60);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(450, 450, 50, 50);
+    // ctx.fillStyle = 'yellow';
+    // ctx.fillRect(450, 450, 50, 50);
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(460, 460, 40, 40);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(460, 460, 40, 40);
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(470, 470, 30, 30);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(470, 470, 30, 30);
 
-    ctx.fillStyle = 'indigo';
-    ctx.fillRect(480, 480, 20, 20);
+    // ctx.fillStyle = 'indigo';
+    // ctx.fillRect(480, 480, 20, 20);
 
-    ctx.fillStyle = 'violet';
-    ctx.fillRect(490, 490, 10, 10);
+    // ctx.fillStyle = 'violet';
+    // ctx.fillRect(490, 490, 10, 10);
     
-    // Center
-    ctx.fillStyle = 'red';
-    ctx.fillRect(215, 215, 70, 70);
+    // // Center
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(215, 215, 70, 70);
 
-    ctx.fillStyle = 'orange';
-    ctx.fillRect(220, 220, 60, 60);
+    // ctx.fillStyle = 'orange';
+    // ctx.fillRect(220, 220, 60, 60);
 
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(225, 225, 50, 50);
+    // ctx.fillStyle = 'yellow';
+    // ctx.fillRect(225, 225, 50, 50);
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect(230, 230, 40, 40);
+    // ctx.fillStyle = 'green';
+    // ctx.fillRect(230, 230, 40, 40);
 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(235, 235, 30, 30);
+    // ctx.fillStyle = 'blue';
+    // ctx.fillRect(235, 235, 30, 30);
 
-    ctx.fillStyle = 'indigo';
-    ctx.fillRect(240, 240, 20, 20);
+    // ctx.fillStyle = 'indigo';
+    // ctx.fillRect(240, 240, 20, 20);
 
-    ctx.fillStyle = 'violet';
-    ctx.fillRect(245, 245, 10, 10);
+    // ctx.fillStyle = 'violet';
+    // ctx.fillRect(245, 245, 10, 10);
 
 
 
