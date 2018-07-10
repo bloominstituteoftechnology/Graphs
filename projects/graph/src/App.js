4fccfe1943f5e1dc2df4b1e950a6f5aa65f9a831
Natalie <natalieschennum@gmail.com>
2018-07-10 12:45:40 -0700

    Lines Drawn and Random Graph Rendering

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-const canvasWidth = 1400
-const canvasHeight = 800
+const canvasWidth = 750
+const canvasHeight = 600
 
 const circleRadius = 15
 
@@ -34,41 +34,68 @@ class GraphView extends Component {
     let canvas = this.refs.canvas; 
     let ctx = canvas.getContext('2d'); 
 
-    console.log('this.props.graph: ', this.props.graph); 
-    //call dummy funciton
-    this.props.graph.createDummyGraph(); 
-    console.log('called createDummyGraph'); 
+    // console.log('this.props.graph: ', this.props.graph); 
+    // //call dummy funciton
+    // this.props.graph.createDummyGraph(); 
+    // console.log('called createDummyGraph'); 
 
     //clear it
     ctx.fillStyle= 'grey'; 
     ctx.fillRect(0, 0, canvasWidth, canvasHeight); 
-    ctx.font = '13px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+//     ctx.font = '13px Arial';
+//     ctx.textAlign = 'center';
+//     ctx.textBaseline = 'middle';
     
-    // draw our dummy vertexes
-    this.props.graph.vertexes.forEach((v) => {
-      ctx.beginPath();
-      ctx.fillStyle = 'white';
-      ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
-      ctx.fill();
-      ctx.stroke();
+//     // draw our dummy vertexes
+//     this.props.graph.vertexes.forEach((v) => {
+//       ctx.beginPath();
+//       ctx.fillStyle = 'white';
+//       ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
+//       ctx.fill();
+//       ctx.stroke();
 
-      // fill in the text
-      ctx.fillStyle = 'black';
-      ctx.fillText(v.value, v.pos.x, v.pos.y);
-});
+//       // fill in the text
+//       ctx.fillStyle = 'black';
+//       ctx.fillText(v.value, v.pos.x, v.pos.y);
+// });
 
    
-   
-  }
+  
 
   // !!! IMPLEMENT ME
   // compute connected components
   // draw edges
   // draw verts
   // draw vert values (labels)
+  for (let vertex of this.props.graph.vertexes) {
+    const posX = vertex.pos.x;
+    const posY = vertex.pos.y;
 
+    for (let edge of vertex.edges) {
+      ctx.beginPath();
+      ctx.moveTo(posX, posY);
+      ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+      ctx.stroke();
+    }
+  }
+
+  for (let vertex of this.props.graph.vertexes) {
+    const posX = vertex.pos.x;
+    const posY = vertex.pos.y;
+
+    ctx.beginPath();
+    ctx.arc(posX, posY, circleRadius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = 'black';
+    ctx.fill();
+
+    ctx.fillStyle = 'white';
+    ctx.font = '12px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(vertex.value, posX, posY);
+  }
+}
 
   /**
    * Render
@@ -92,6 +119,7 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    this.state.graph.randomize(5, 4, 150, 0.6); 
   }
 
   render() {
