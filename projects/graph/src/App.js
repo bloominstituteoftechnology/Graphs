614eb97396fb7352c680a6846064586f74689bed
sagdish <forsi1@yahoo.com>
2018-07-10 13:34:03 -0700

    phase 1 and phase 2 done

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 // !!! IMPLEMENT ME
 const canvasWidth = 900;
 const canvasHeight = 800;
+const circleRadius = 15;
 
 /**
  * GraphView
@@ -30,97 +31,35 @@ class GraphView extends Component {
   updateCanvas() {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
+
+    console.log('this.props.graph: ', this.props.graph)
+
+    this.props.graph.createDummyGraph();
+    console.log('called createDummyGraph');
     
     // Clear it
     ctx.fillStyle = '#c0dfe8';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-    console.log(ctx);
-    
 
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(10, 10, 880, 780);
+    ctx.font = '14px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
 
-    ctx.fillStyle = '#bf9ebe';
+    // draw dummy vertexes
 
-    // ctx.beginPath();
-    // ctx.arc(110, 180, 90, 0, Math.PI)
-    // ctx.stroke();
-
-    // ctx.beginPath();
-    // ctx.arc(290, 180, 90, 1, Math.PI, true)
-    // ctx.fill();
-    function strokeStar(x, y, r, n, inset) {
-      ctx.save();
+    this.props.graph.vertexes.forEach(v => {
       ctx.beginPath();
-      ctx.translate(x, y);
-      ctx.moveTo(0,0-r);
-      for (var i = 0; i < n; i++) {
-          ctx.rotate(Math.PI / n);
-          ctx.lineTo(0, 0 - (r*inset));
-          ctx.rotate(Math.PI / n);
-          ctx.lineTo(0, 0 - r);
-      }
-      let rand = Math.random() * 2;
-      ctx.fillStyle = rand > 1 ? "red" : "blue";
-      ctx.closePath();
+      ctx.fillStyle = 'white';
+      ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, (Math.PI * 2));
       ctx.fill();
-      ctx.restore();
-  }
-  let stars = strokeStar(200, 350, 30, 5, 3);
+      ctx.stroke();
 
-    // ctx.beginPath();
-    // ctx.moveTo(300, 250);
-    
-    // ctx.stroke();
+      // fill the text
 
-    const star = new Image();
-    let rand = Math.random() * 2;
-    const url1 = 'https://vectr.com/tmp/aMt2sqUiO/b1ox4deAmE.svg?width=640&height=640&select=b1ox4deAmEpage0';
-    const url2 = 'https://vectr.com/tmp/aMt2sqUiO/ft9txtpky.svg?width=640&height=640&select=ft9txtpkypage0';
-    
-
-    function start() {
-      //ball.src = "https://mdn.mozillademos.org/files/1456/Canvas_sun.png";
-      
-      window.requestAnimationFrame(game);
-    }
-
-    function game() {
-      //ctx.globalCompositeOperation = "destination-over";
-      // ctx.save();
-      // ctx.translate(250, 250);
-      
-
-      const time = new Date();
-
-      ctx.save();
-      ctx.rotate(2 * time.getSeconds() + 2 * time.getMilliseconds());
-      //ctx.translate(30, 40);
-      star.src = rand > 1 ? url1 : url2;
-      ctx.drawImage(star, (Math.random() * 1000), (Math.random() * 1000));
-
-      //ctx.restore();
-      // ctx.fillStyle = '#fff263'
-      // ctx.beginPath();
-      // //ctx.arc(30, 40, 300, 0, Math.PI);
-      // ctx.fill();
-
-      window.requestAnimationFrame(game);
-    }
-
-    start();
-
-
-    
-
-
-
-
-
-
-    
-
-
+      ctx.fillStyle = 'black';
+      ctx.fillText(v.value, v.pos.x, v.pos.y);
+    });
+   
 
     // !!! IMPLEMENT ME
     // compute connected components
