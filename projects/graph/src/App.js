847679841939c4bc1c0121570236847ac4fb71ff
Justin M <jekm321@gmail.com>
2018-07-10 19:40:07 -0400

    colors and circles match

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -34,7 +34,6 @@ class GraphView extends Component {
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
     // this.props.graph.createDummyGraph();
-    this.props.graph.randomize(8, 6, 150);
     this.props.graph.dump();
 
     // Clear it
@@ -42,41 +41,74 @@ class GraphView extends Component {
     ctx.fillRect(canvasStartX, canvasStartY, canvasWidth, canvasHeight);
     // ctx.fillRect(100, 100, canvas.width, canvas.height);
 
-    ctx.font = '13px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+    this.props.graph.randomize(8, 6, 149);
+    ctx.font = '13px Arial';  //font and size of text
+    ctx.textAlign = 'center';  //location of text on x axis 
+    ctx.textBaseline = 'middle';  //location of text on y axis
 
     // !!! IMPLEMENT ME
     // compute connected components
-    const checked = [];
+    const checked = {};
     this.props.graph.vertexes.forEach(v => {
-      const connected = [];
-
+      checked[v.value] = false;
     })
 
-    this.props.graph.vertexes.forEach(v => {
-      ctx.strokeStyle = v.color;
-      for (let j = 0; j < v.edges.length; j++) {
-        ctx.moveTo(v.pos.x + canvasStartX, v.pos.y + canvasStartY);
-        // draw edges
-        ctx.lineTo(v.edges[j].destination.pos.x + canvasStartX, v.edges[j].destination.pos.y + canvasStartY);
-        ctx.stroke();
+    const unchecked = [];
+    for (let i = 0; i < this.props.graph.vertexes.length; i++) {
+      let v = this.props.graph.vertexes[i];
+      if (!checked[v.value]) {
+        let color = v.color
+        let current = v;
+        unchecked.push(current);
+        checked[v.value] = true;
+        let count = 0;
+        while (unchecked.length > 0) {
+          for (let j = 0; j < current.edges.length; j++) {
+            if (!checked[current.edges[j].destination.value]) {
+              checked[current.edges[j].destination.value] = true;
+              unchecked.push(current.edges[j].destination);
+            }
+          }
+          let vertex = unchecked.shift();
+          ctx.fillStyle = color;  //sets color for the circle
+          ctx.strokeStyle = color;  //sets color for the circle's edge
+          for (let j = 0; j < vertex.edges.length; j++) {
+            ctx.moveTo(vertex.pos.x + canvasStartX, vertex.pos.y + canvasStartY);
+            // draw edges
+            ctx.lineTo(vertex.edges[j].destination.pos.x + canvasStartX, vertex.edges[j].destination.pos.y + canvasStartY);
+            ctx.stroke();
+          }
+
+          ctx.beginPath();
+          // draw verts
+          ctx.arc(vertex.pos.x + canvasStartX, vertex.pos.y + canvasStartY, circleRadius, 0, 2 * Math.PI);  //(x,y) center of cicle, radius, arc of circle (in radians)
+          ctx.fill();  //fills in the circle
+          ctx.stroke();  //draws the circle
+
+          // draw vert values (labels)
+          // ctx.beginPath();
+          ctx.fillStyle = 'black';  //sets color for the text
+          ctx.fillText(vertex.value, vertex.pos.x + canvasStartX, vertex.pos.y + canvasStartY);  //fill in the text of v.value @ (x,y) of (v.pos.x, v.pos.y);
+          current = unchecked[0];
+        }
       }
-    })
+    }
 
-    this.props.graph.vertexes.forEach(v => {
-      ctx.beginPath();
-      ctx.fillStyle = v.color;  //sets color for the circle
-      ctx.strokeStyle = v.color;  //sets color for the circle's edge
-      // draw verts
-      ctx.arc(v.pos.x + canvasStartX, v.pos.y + canvasStartY, circleRadius, 0, 2 * Math.PI);  //(x,y) center of cicle, radius, arc of circle (in radians)
-      ctx.fill();  //fills in the circle
-      ctx.stroke();  //draws the circle
 
-      // draw vert values (labels)
-      ctx.fillStyle = 'black';  //sets color for the text
-      ctx.fillText(v.value, v.pos.x + canvasStartX, v.pos.y + canvasStartY);  //fill in the text of v.value @ (x,y) of (v.pos.x, v.pos.y);
-    })
+
+    // this.props.graph.vertexes.forEach(v => {
+    //   ctx.beginPath();
+    //   ctx.fillStyle = v.color;  //sets color for the circle
+    //   ctx.strokeStyle = v.color;  //sets color for the circle's edge
+    //   // draw verts
+    //   ctx.arc(v.pos.x + canvasStartX, v.pos.y + canvasStartY, circleRadius, 0, 2 * Math.PI);  //(x,y) center of cicle, radius, arc of circle (in radians)
+    //   ctx.fill();  //fills in the circle
+    //   ctx.stroke();  //draws the circle
+
+    //   // draw vert values (labels)
+    //   ctx.fillStyle = 'black';  //sets color for the text
+    //   ctx.fillText(v.value, v.pos.x + canvasStartX, v.pos.y + canvasStartY);  //fill in the text of v.value @ (x,y) of (v.pos.x, v.pos.y);
+    // })
 
   }
 
