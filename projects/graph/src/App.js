396bada12d6b5ee5d983c352ef924999fcf1746c
Justin Rigler <justinmrigler@gmail.com>
2018-07-10 19:28:22 -0400

    starting hard coding

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 1000;
+const canvasHeight = 900;
 
 /**
  * GraphView
