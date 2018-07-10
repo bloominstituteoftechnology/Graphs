d239d0cf1c0d9eab147dc40dc917aa13beabf211
Devon Bacon <devon.bacon@gmail.com>
2018-07-10 12:46:10 -0700

    App.js - edited random parameters to align with instruction

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
-    this.state.graph.randomize(5, 5, 90);
+    this.state.graph.randomize(5, 4, 150);
   }
 
   render() {
