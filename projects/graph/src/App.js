ffdb0b7fd7f0019e03fb8989b5c6ca4c366254ca
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-07-11 12:27:24 -0700

    App.js - Applied Background Color

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,9 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-const canvasWidth = 500;
-const canvasHeight = 500;
+const canvasWidth = 800;
+const canvasHeight = 600;
+// const circleRadius = 15;
 
 /**
  * GraphView
@@ -31,28 +32,111 @@ class GraphView extends Component {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext('2d');
     
+    // // Vertex click event
+    // let start;
+    // let end;
+
+    // canvas.addEventListener(
+    //   'click',
+    //   e => {
+    //     const x = e.pageX - canvas.offsetLeft;
+    //     const y = e.pageY - canvas.offsetTop;
+    //     let vertClick;
+
+    //     for (let vertex of this.props.graph.vertexes) {
+    //       if (
+    //         Math.abs(vertex.pos.x - x) <= circleRadius &&
+    //         Math.abs(vertex.pos.y - y) <= circleRadius
+    //       ) {
+    //         vertClick = vertex;
+    //         if (!start) {
+    //           start = vertClick;
+
+    //           ctx.textAlign = 'center';
+    //           ctx.textBaseline = 'middle';
+    //           ctx.font = '8px Arial';
+    //           ctx.fillStyle = 'black';
+
+    //           ctx.fillText('START', vertex.pos.x, vertex.pos.y + 20);
+    //         } else if (!end) {
+    //           end = vertClick;
+    //           ctx.textAlign = 'center';
+    //           ctx.textBaseline = 'middle';
+    //           ctx.font = '8px Arial';
+    //           ctx.fillStyle = 'black';
+
+    //           ctx.fillText('END', vertex.pos.x, vertex.pos.y + 20);
+    //           console.log(`Start at: ${start.value} and End at: ${end.value}`);
+    //         }
+    //       }
+    //     }
+    //   },
+    //   false
+    // );
+
     // Clear it
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'rgb(58, 49, 79)';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // ctx.font = '10px Arial';
+    // ctx.textAlign = 'center';
+    // ctx.textBaseline = 'middle';
+
+    // for (let vertex of this.props.graph.vertexes) {
+    //   for (let edge of vertex.edges) {
+    //     ctx.beginPath();
+    //     ctx.moveTo(vertex.pos.x, vertex.pos.y);
+    //     ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+    //     ctx.stroke();
+    //     ctx.fillText(edge.weight, (vertex.pos.x + edge.destination.pos.x) / 2, (vertex.pos.y + edge.destination.pos.y) / 2);
+      
+        // Add edge weight.
+        // const xCenter = (vertex.pos.x + edge.destination.pos.x) / 2;
+        // const yCenter = (vertex.pos.y + edge.destination.pos.y) / 2;
+        // ctx.font = '12px Arial';
+        // ctx.fillStyle = 'purple';
+        // if (edge.drawWeight === false) {
+        //   ctx.fillText(edge.weight, xCenter + 8, yCenter + 8);
+        // } else {
+        //   continue;
+        // }
+    //   }
+    // }
+
+    // Draw our dummy vertex
+    // this.props.graph.vertexes.forEach((v) => {
+    //   ctx.beginPath();
+    //   ctx.fillStyle = '#cd5360';
+    //   ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
+    //   // ctx.lineCap="square";
+    //   // // ctx.moveTo(200, 20);
+    //   // ctx.lineTo(300, 20);
+    //   ctx.fill();
+    //   ctx.stroke();
+
+    //   // fill in the text
+    //   ctx.fillStyle = '#ffffff';
+    //   ctx.fillText(v.value, v.pos.x, v.pos.y);
+    // });
 
     // Set up the gradient
-    let grd = ctx.createLinearGradient(400, 50, 50, 90, 60, 100);
-    grd.addColorStop(0, '#932fa3');
-    grd.addColorStop(1, '#259ec6');
+    // let grd = ctx.createLinearGradient(400, 50, 50, 90, 60, 100);
+    // grd.addColorStop(0, '#932fa3');
+    // grd.addColorStop(1, '#259ec6');
 
     // Fill with gradient
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // ctx.fillStyle = grd;
+    // ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // Create circles in a loop
-    ctx.strokeStyle = '#dce5e8';
-    for (let i = 0; i < canvas.width; i += 18) {
-      for (let j = 0; j < canvas.height; j += 10) {
-        ctx.beginPath();
-        ctx.arc(i, j, 40, 0, 2 * Math.PI);
-        ctx.stroke();
-      }
-    }
+    // ctx.strokeStyle = '#dce5e8';
+    // for (let i = 0; i < canvas.width; i += 18) {
+      // for (let j = 0; j < canvas.height; j += 10) {
+        // ctx.beginPath();
+        // ctx.arc(i, j, 40, 0, 2 * Math.PI);
+        // ctx.stroke();
+    //   }
+    // }
 
     // !!! IMPLEMENT ME
     // compute connected components
@@ -82,12 +166,26 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
+    // this.state.graph.randomize(5, 4, 150, 0.6);
+    // this.state.graph.getConnectedComponents();
   }
+
+  // handleClick() {
+  //   const newGraph = { graph: new Graph() };
+  //   newGraph.graph.randomize(5, 4, 150, 0.6);
+  //   newGraph.graph.getConnectedComponents();
+  //   this.setState(newGraph);
+  // }
 
   render() {
     return (
       <div className="App">
         <GraphView graph={this.state.graph}></GraphView>
+        {/* <div className="btnDiv">
+          <div className="Button" onClick={() => this.handleClick()}>
+            Random Graph
+          </div>
+        </div> */}
       </div>
     );
   }
