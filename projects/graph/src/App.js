7cd2860c4fb48ce21526a405d8d0c1de194e8eba
Natalie <natalieschennum@gmail.com>
2018-07-11 14:28:09 -0700

    Some Shaddow

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -31,8 +31,8 @@ class GraphView extends Component {
    * Render the canvas
    */
   updateCanvas() {
-    let canvas = this.refs.canvas; 
-    let ctx = canvas.getContext('2d'); 
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
 
     // console.log('this.props.graph: ', this.props.graph); 
     // //call dummy funciton
@@ -40,65 +40,69 @@ class GraphView extends Component {
     // console.log('called createDummyGraph'); 
 
     //clear it
-    ctx.fillStyle= 'grey'; 
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight); 
-//     ctx.font = '13px Arial';
-//     ctx.textAlign = 'center';
-//     ctx.textBaseline = 'middle';
-    
-//     // draw our dummy vertexes
-//     this.props.graph.vertexes.forEach((v) => {
-//       ctx.beginPath();
-//       ctx.fillStyle = 'white';
-//       ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
-//       ctx.fill();
-//       ctx.stroke();
+    ctx.fillStyle = 'lightBlue';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    //     ctx.font = '13px Arial';
+    //     ctx.textAlign = 'center';
+    //     ctx.textBaseline = 'middle';
 
-//       // fill in the text
-//       ctx.fillStyle = 'black';
-//       ctx.fillText(v.value, v.pos.x, v.pos.y);
-// });
+    //     // draw our dummy vertexes
+    //     this.props.graph.vertexes.forEach((v) => {
+    //       ctx.beginPath();
+    //       ctx.fillStyle = 'white';
+    //       ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
+    //       ctx.fill();
+    //       ctx.stroke();
 
-   
-  
+    //       // fill in the text
+    //       ctx.fillStyle = 'black';
+    //       ctx.fillText(v.value, v.pos.x, v.pos.y);
+    // });
 
-  // !!! IMPLEMENT ME
-  // compute connected components
-  // draw edges
-  // draw verts
-  // draw vert values (labels)
-  this.props.graph.getConnectedComponents(); //to access those that are connected
 
-  for (let vertex of this.props.graph.vertexes) {
-    const posX = vertex.pos.x;
-    const posY = vertex.pos.y;
 
-    for (let edge of vertex.edges) {
+
+    // !!! IMPLEMENT ME
+    // compute connected components
+    // draw edges
+    // draw verts
+    // draw vert values (labels)
+    this.props.graph.getConnectedComponents(); //to access those that are connected
+
+    for (let vertex of this.props.graph.vertexes) {
+      const posX = vertex.pos.x;
+      const posY = vertex.pos.y;
+
+      for (let edge of vertex.edges) {
+        ctx.beginPath();
+        ctx.moveTo(posX, posY);
+        ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
+        ctx.strokeStyle = vertex.color;
+        ctx.stroke();
+      }
+    }
+
+    for (let vertex of this.props.graph.vertexes) {
+      const posX = vertex.pos.x;
+      const posY = vertex.pos.y;
+
       ctx.beginPath();
-      ctx.moveTo(posX, posY);
-      ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
-      ctx.strokeStyle = vertex.color; 
+      ctx.arc(posX, posY, circleRadius, 0, 2 * Math.PI);
       ctx.stroke();
+      ctx.fillStyle = vertex.color;
+      ctx.fill();
+      ctx.shadowColor = 'black';
+      ctx.shadowBlur = 10;
+
+
+
+      ctx.fillStyle = 'white';
+      ctx.font = '12px Arial';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(vertex.value, posX, posY);
     }
   }
-
-  for (let vertex of this.props.graph.vertexes) {
-    const posX = vertex.pos.x;
-    const posY = vertex.pos.y;
-
-    ctx.beginPath();
-    ctx.arc(posX, posY, circleRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = vertex.color;
-    ctx.fill();
-
-    ctx.fillStyle = 'white';
-    ctx.font = '12px Arial';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(vertex.value, posX, posY);
-  }
-}
 
   /**
    * Render
@@ -122,7 +126,7 @@ class App extends Component {
 
     // !!! IMPLEMENT ME
     // use the graph randomize() method
-    this.state.graph.randomize(5, 4, 150, 0.6); 
+    this.state.graph.randomize(5, 4, 150, 0.6);
   }
 
   render() {
