d552f85f7e0d035adf8321fdc847b98b497fd284
Yasin Shuman <email@yasin.io>
2018-07-12 16:24:00 -0700

    complete w/o stretch

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -36,16 +36,22 @@ class GraphView extends Component {
     ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    this.drawVertexes(ctx);
-  }
-
-  drawVertexes(ctx) {
     ctx.font = "13px Arial";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
+    // set containing all of the connected components
+    const components = this.props.graph.getConnectedComponents();
+    // loop through all of the components, passing each to `drawVertexes`
+    components.forEach(component => {
+      this.drawVertexes(ctx, component, this.generateRandomColor());
+    });
+  }
 
+  drawVertexes(ctx, vertexes, color) {
     // draw the lines between vertexes
-    for (let vertex of this.props.graph.vertexes) {
+    ctx.strokeStyle = color;
+
+    for (let vertex of vertexes) {
       for (let edge of vertex.edges) {
         ctx.beginPath();
         ctx.moveTo(vertex.pos.x, vertex.pos.y);
@@ -53,10 +59,10 @@ class GraphView extends Component {
         ctx.stroke();
       }
     }
-
-    for (let v of this.props.graph.vertexes) {
+    // drawing the circles
+    for (let v of vertexes) {
       ctx.beginPath();
-      ctx.fillStyle = "white";
+      ctx.fillStyle = color;
       ctx.arc(v.pos.x, v.pos.y, circleRadius, 0, 2 * Math.PI);
       ctx.fill();
       ctx.stroke();
@@ -65,6 +71,15 @@ class GraphView extends Component {
       ctx.fillStyle = "black";
       ctx.fillText(v.value, v.pos.x, v.pos.y);
     }
+  }
+
+  generateRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 
   /**
@@ -89,12 +104,21 @@ class App extends Component {
     // !!! IMPLEMENT ME
     // use the graph randomize() method
     this.state.graph.randomize(5, 4, 150, 0.6);
-    this.state.graph.getConnectedComponents();
   }
+
+  randomize = () => {
+    const state = {
+      graph: new Graph()
+    };
+
+    state.graph.randomize(5, 4, 150, 0.6);
+    this.setState(state);
+  };
 
   render() {
     return (
       <div className="App">
+        <button onClick={this.randomize}>Randomize</button>
         <GraphView graph={this.state.graph} />
       </div>
     );
