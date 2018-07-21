6833ce2b0c239361b8ed43dfe8d40ad5041c7b44
soycode <soycode@users.noreply.github.com>
2018-07-21 12:33:20 -0700

    flake8 (style fixes)

--- guided-demo/graphs-1/src/graph_matrix.py
+++ guided-demo/graphs-1/src/graph_matrix.py
@@ -1,11 +1,19 @@
+"""Graph representation using adjacency matrix."""
+
 class Vertex:
-    def __init__(self, value):
-        self.value = value
+    """Vertices just need a label, the matrix will track edges."""
+    # Using simple classes for illustrative purposes
+    # pylint: disable=too-few-public-methods
+    def __init__(self, label):
+        self.label = label
 
 
 class Graph:
-    def __init__(self, rows, cols):
-        self.matrix = [[0] * cols] * rows
+    """The graph is a matrix of 0s/1s indicating existence of edges."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self, num_vertices):
+        self.matrix = [[0] * num_vertices] * num_vertices
 
-    def connect_vertex(self, a, b):
-        self.matrix[a][b] = 1
+    def connect_vertex(self, row, col):
+        """Add an edge between vertices indicated by row/col of matrix."""
+        self.matrix[row][col] = 1
