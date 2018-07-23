4e6a9660aa3775abf10fba5ad7de26ceb47c367c
Obo Agboghidi <oagboghi2@gmail.com>
2018-07-23 16:12:49 -0700

    graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,3 +9,17 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+    def add_vortex(self, v):
+        if v not in self.vertices:
+            self.vertices[v] = set()
+        else:
+            raise ValueError("")
+
+    def add_edge(self, start, end, biderectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("")
+        else:
+            self.vertices[start].add(end)
+            if biderectional = True:
+                self.vertices[end].add(start)
