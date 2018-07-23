dbc9429bf66d64b6a25aba8b8394a3dcb7af7f2a
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-07-23 14:17:44 -0700

    feat: define vertices

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,4 +8,6 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+
+    
