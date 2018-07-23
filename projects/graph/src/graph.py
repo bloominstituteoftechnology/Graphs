279835130a752e1611469039b4017a6c9f19037c
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-23 15:35:29 -0400

    -add_edge method -add_vertex method added

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,8 +4,23 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+g = {
+    "0": {"1", "3"},
+    "1": {"0"},
+    "2": set(),
+    "3": {"0"}
+}
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        pass
+
+
+    def add_vertex(self, vertex):
+        pass
+    
+
