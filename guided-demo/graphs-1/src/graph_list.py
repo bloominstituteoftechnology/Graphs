cedbf6fb9cc7e3d35cd5fb414c575818d8073f96
Christina Kopecky <kopecky12112@gmail.com>
2018-07-23 12:40:43 -0700

    copied info from lecture
    added add_vertex method Graph

--- guided-demo/graphs-1/src/graph_list.py
+++ guided-demo/graphs-1/src/graph_list.py
@@ -1,23 +1,28 @@
 """Graph representation using adjacency list."""
-
-class Edge:
-    """Edges in the adjacency list are just a destination."""
-    # Using simple classes for illustrative purposes
-    # pylint: disable=too-few-public-methods
-    def __init__(self, destination):
-        self.destination = destination
-
-
 class Vertex:
     """Vertices have a label and a set of edges."""
-    # pylint: disable=too-few-public-methods
     def __init__(self, label):
         self.label = label
         self.edges = set()
 
+    def __repr__(self):
+        return str(self.label)
 
-class Graph:
-    """The graph itself is simply a set of vertices."""
-    # pylint: disable=too-few-public-methods
+
+class ListGraph:
+    """Adjacency list graph."""
     def __init__(self):
         self.vertices = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        """Add an edge from start to end."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error - vertices not in graph!')
+        start.edges.add(end)
+        if bidirectional:
+            end.edges.add(start)
+
+    def add_vertex(self, vertex):
+        if not hasattr(vertex, 'label'):
+            raise Exception('This is not a vertex!')
+        self.vertices.add(vertex)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,4 +8,9 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+        
+    def add_vertex(self, vertex):
+        if not hasattr(vertex, 'label'):
+            raise Exception('This is not a vertex!')
+        self.vertices.add(vertex)
