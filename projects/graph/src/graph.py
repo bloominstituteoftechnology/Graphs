133b364e1b850ef43aea566e6d55d0e5f5686cac
AmyShackles <amyshackles@gmail.com>
2018-07-23 13:18:41 -0700

    Finished writing out the starter code for the Graph class.
    -  I imported defaultdict from collections and am using that to contain self.vertices at the moment.
    -  As the code currently stands, the code raises an exception if a person attempts to add edges when the start vertex and/or the end vertex is not already present in the graph.
    -  There's an if statement taking care of the bidirectionality and bidirectionality is set to True by default so if a unidirectional edge were to be added, it's important to remember to provide that False flag.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,49 @@
+<<<<<<< HEAD
+=======
+#!/usr/bin/python
+
+from collections import defaultdict
+
+>>>>>>> 2a3a1aa... Finished writing out the starter code for the Graph class.
 """
 Simple graph implementation compatible with BokehGraph class.
 """
 
 
+# class Vertex:
+#     """Vertices have a label and a set of edges."""
+
+#     def __init__(self, label):
+#         self.label = label
+#         self.edges = set()
+
+#     def __repr__(self):
+#         return str(self.label)
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = defaultdict(set)
+
+    def add_edge(self, start, end, bidirectional=True):
+        """Add an edge from start to end."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("Error - vertices not in graph!")
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+
+graph = Graph()
+graph.add_vertex("0")
+graph.add_vertex("1")
+graph.add_vertex("2")
+graph.add_vertex("3")
+graph.add_edge("0", "1")
+graph.add_edge("0", "3")
+print(graph.vertices)
