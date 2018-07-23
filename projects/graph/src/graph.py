727869e7e0c17ff8ab0b0323fde061cfcce0101e
unknown <sam.park.email@gmail.com>
2018-07-23 16:49:10 -0400

    init commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,7 +5,38 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    """Vertices have a label and a set of edges."""
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
+
+    def __repr__(self):
+        return str(self.label)
+
+
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """Represent a graph as a dictionary of vertices mapping labels to edges"""
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        """Add an edge from start to end."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error - vertices not in graph!')
+        start.vertices.add(end)
+        if bidirectional:
+            end.vertices.add(start)
+
+    def add_vertex(self, vertex):
+        self.vertices.add(vertex)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
