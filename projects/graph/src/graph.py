b76f6fd2c05a9f2e740d0d2126a20ee6eddc9630
Lavell Burton <taylormade800@gmail.com>
2018-07-23 15:18:49 -0700

    initial commit forgot to submit at beginning of day

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,5 +7,13 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
+    def __init__(self, vertices):
+        self.matrix = [[0] * vertices] * vertices
+
+    def add_edge(self, start, end, bidirectional=True):
+        self.matrix[start][end] = 1
+        if bidirectional:
+            self.matrix[end][start] = 1
+        
+
         pass  # TODO
