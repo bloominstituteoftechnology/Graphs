14a5800564cefbe5600b7d611f88cb3bbdfed811
bbenefield89 <bsquared18@gmail.com>
2018-07-23 16:54:11 -0400

    ADD `add_vertex` METHOD
    - in graph.py
    adds new vertice to graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,4 +8,17 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.graph = {}
+
+    def add_vertex(self, vertice):
+        self.graph[ vertice ] = set()
+
+    def add_edge(graph):
+        edges = []
+        
+
+graph = Graph()
+
+graph.add_vertex('1')
+graph.add_vertex('2')
+print(graph.graph)
