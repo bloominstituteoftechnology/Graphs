ff2c1688bc085589ae97822cc4cc74fa5bb71970
Christina Kopecky <kopecky12112@gmail.com>
2018-07-23 12:56:12 -0700

    added addedge method

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,8 +9,15 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = set()
-        
+
     def add_vertex(self, vertex):
         if not hasattr(vertex, 'label'):
             raise Exception('This is not a vertex!')
         self.vertices.add(vertex)
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("supplied vertex not in graph!!")
+        start.edges.add(end)
+        if bidirectional:
+            end.edges.add(start)
+
