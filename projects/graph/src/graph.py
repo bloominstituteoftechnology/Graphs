5e1a398770d2bf4fb71478efb0cbdac0cdb66340
Mister-Corn <code@ronaldlibago.com>
2018-07-23 12:59:13 -0700

    Basic class `graph` set up.
    * Next: Implement `Vertex` class and have `Graph` work with `Vertex` instances

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,8 +4,35 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Vertex:
+    """Object representation of Vertex"""
+    def __init__(self, label=None):
+        self.label = label
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+    
+    def add_edge(self, start, end, bidirectional=True):
+        self.vertices[start].add(end)
+
+        if bidirectional:
+            self.vertices[end].add(start)
+    
+
+def main():
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    print(graph.vertices)
+
+if __name__ == '__main__':
+    main()
