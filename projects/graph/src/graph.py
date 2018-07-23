1a8031ada6d9873a8f5f2bed02646144a3b6f1cd
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-23 13:30:54 -0700

    graph class initialized and add_vertex method added

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,4 +8,7 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
