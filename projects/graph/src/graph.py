882fea1d8a8cc2c7359c44e3879c5320b8c550f5
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-23 14:31:42 -0700

    add_edge method added

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/pyth
 
 """
 Simple graph implementation compatible with BokehGraph class.
@@ -12,3 +12,10 @@ class Graph:
 
     def add_vertex(self, vertex):
         self.vertices[vertex] = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        self.vertices[start].add(end)
+
+        if bidirectional:
+            self.vertices[end].add(start)
+    
