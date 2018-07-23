feebd8693485dc52ac0004ca97d0ec0dbe70c40e
kelly <kelfro@fastmail.com>
2018-07-23 13:57:44 -0700

    Stretch complete raise errors

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -11,12 +11,18 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self,vertex):
-        self.vertices[vertex] = set()
+        if vertex in self.vertices:
+            raise ValueError('oops, vertex already there') 
+        else:
+            self.vertices[vertex] = set()
 
     def add_edge(self, start_index, end_index, bidirectional=True) :
-        self.vertices[start_index].add(end_index)
-        if bidirectional:
-            self.vertices[end_index].add(start_index)
+        if start_index in self.vertices or end_index in self.vertices:
+            self.vertices[start_index].add(end_index)
+            if bidirectional:
+                self.vertices[end_index].add(start_index)
+        else:
+            raise KeyError('error')
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
@@ -25,4 +31,5 @@ graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
+#graph.add_edge('0', '4')
 print(graph.vertices)
