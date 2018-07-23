1c03f11806d17282af89b7d1afeedc2e6e06806a
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-23 17:48:25 -0400

    Created a Vertex and Graph class to add vertices and edges.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,8 +4,28 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+ class Vertex:
+    def __init__(self, label):
+      self.label = label
+      self.edges = set()
+
+    def __repr__(self):
+      return str(self.label)
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+   def __init__(self):
+      self.vertices = set() 
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+          raise Exception('Error - vertices not in graph!')
+        start.edges.add(end)
+        self.vertices[start] = 1
+        if bidirectional: 
+          end.edges.add(start)
+
+    def add_vertex(self, vertex):
+        if not hasattr(vertex, 'label'):
+            raise Exception('This is not a vertex!')
+        self.vertices.add(vertex)
