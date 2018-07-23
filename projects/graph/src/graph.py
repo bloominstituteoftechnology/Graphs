8d089c327fe7040af682f8dd5fd50a3fe868e2fe
David <AquilaVirtual>
2018-07-23 15:50:08 -0700

    implemented Graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python
 
 """
-Simple graph implementation compatible with BokehGraph class.
-"""
+Simple graph implementation compatible with BokehGraph class."""
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, v):
+        if v not in self.vertices:
+            self.vertices[v] = set()
+        else:
+            raise ValueError("vertex already exists in the Graph")
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("%s or %s does not exist" %(start, end))
+        else:
+            self.vertices[start].add(end)
+            if bidirectional == True:
+                self.vertices[end].add(start)
+
+
+
+
+
