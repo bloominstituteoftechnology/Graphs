33946483f9c277caedfbb793aaa6d3076baf75cb
Zach Campbell <beazlebubbles@gmail.com>
2018-07-23 15:23:25 -0600

    Added basic graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,4 +9,15 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, label):
+        self.vertices[label] = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start in self.vertices and end in self.vertices:
+            self.vertices[start].add(end)
+        else:
+            raise ValueError("Please provide start and end vertices that exist.")
+        if bidirectional:
+            self.vertices[end].add(start)
