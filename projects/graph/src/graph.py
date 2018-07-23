05db3485503ad59deae82ba1268b8b8bb19357f9
Danny Vail <danny_vail@outlook.com>
2018-07-23 17:49:00 -0400

    Implemented Graph class - Represents graph as a dictionary of vertices and edges - add_vertex method adds free standing vertex to graph - add_edge method connects two existing vertices and throws exception if either do not exist - ignore vs code

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,6 +6,29 @@ Simple graph implementation compatible with BokehGraph class.
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """
+    Represent a graph as a dictionary of vertices mapping labels to edges.
+    """
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start in self.vertices and end in self.vertices:
+            self.vertices[start].add(end)
+            if bidirectional:
+                self.vertices[end].add(start)
+        else:
+            raise Exception('Vertex does not exist')
+
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
