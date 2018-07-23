2f94ff369740d28f387c096888592e0a66e45ac9
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-23 18:48:54 -0400

    Phase 1 completed.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python
-
 """
 Simple graph implementation compatible with BokehGraph class.
 """
 
- class Vertex:
-    def __init__(self, label):
-      self.label = label
-      self.edges = set()
-
-    def __repr__(self):
-      return str(self.label)
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-   def __init__(self):
-      self.vertices = set() 
+
+    def __init__(self):
+        self.vertices = {}
+
+    def __repr__(self):
+        return str(self.label)
 
     def add_edge(self, start, end, bidirectional=True):
-        if start not in self.vertices or end not in self.vertices:
-          raise Exception('Error - vertices not in graph!')
-        start.edges.add(end)
-        self.vertices[start] = 1
-        if bidirectional: 
-          end.edges.add(start)
+
+        if start not in self.vertices:
+            raise Exception('Error: The start vertice is not in the graph.')
+        elif end not in self.vertices:
+            raise Exception('Error: The end vertice is not in the graph.')
+
+        self.vertices[start].add(end)
+
+        if bidirectional:
+            self.vertices[end].add(start)
 
     def add_vertex(self, vertex):
-        if not hasattr(vertex, 'label'):
-            raise Exception('This is not a vertex!')
-        self.vertices.add(vertex)
+        self.vertices[vertex] = set()
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
