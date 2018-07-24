d43599a7884b7f82c49a01cffc157fc1b0b01340
Anne Courtney <annecourtney_1@yahoo.com>
2018-07-24 15:48:14 -0500

    got the draw to work but have no idea why

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,7 +3,7 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+from draw import BokehGraph
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -28,10 +28,14 @@ graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
-graph.add_vertex('l3')
+graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 print(graph.vertices)
+
+b = BokehGraph(graph)
+b.show()
+
 
 
 
