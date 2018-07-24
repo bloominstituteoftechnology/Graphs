d8559bc31d94f5323a5a172a1b73143f2c424ea2
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-07-23 21:39:12 -0500

    modify add_vertex to add w/o instantiating Vertex

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -11,9 +11,8 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self, vertex):
-        if not hasattr(vertex, 'label'):
-            raise Exception('This is not a vertex!')
-        self.vertices[vertex] = set()
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
 
     def add_edge(self, start, end, bidirectional=True):
         if start not in self.vertices or end not in self.vertices:
@@ -24,17 +23,15 @@ class Graph:
             self.vertices[end].add(start)
 
 graph = Graph()  # Instantiate your graph
-v0 = Vertex(0)
-v1 = Vertex(1)
-v2 = Vertex(2)
-v3 = Vertex(3)
-graph.add_vertex(v0)
-graph.add_vertex(v1)
-graph.add_vertex(v2)
-graph.add_vertex(v3)
-graph.add_vertex('5')
-graph.add_edge(v0, v1)
-graph.add_edge(v0, v3)
-graph.add_edge(v0, '5')
+
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('3')
+graph.add_vertex('0')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('0', '5')
 print(graph.vertices)
 
