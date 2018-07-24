27abd668328393a732f7301399ef03fe9f88fdef
NandoTheessen <nando.theessen@gmail.com>
2018-07-24 22:32:06 +0200

    working rendering of graph, working randomized graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,8 +4,17 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    def add_vert(self, vertex, edges=()):
+        if not set(edges).issubset(self.vertices):
+            raise Exception("can't have edge to non-existant vertex")
+        self.vertices[vertex] = set(edges)
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('u stoopid?')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
