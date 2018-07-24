3a829aa16fbb2f98bc55092f6a8215bd67671d70
Marco A Guzman <marcoguzman16@gmail.com>
2018-07-24 12:34:15 -0700

    working on draw.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,9 +7,9 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self, label):
+    def __init__(self):
         self.vertices = {}
-        self.label = label
+        
 
     def add_edge(self,start,end, bidirectional=True):
         if start not in self.vertices or end not in self.vertices:
@@ -19,16 +19,18 @@ class Graph:
             end.edges.add(start)
 
     def add_vertex(self, vertex):
-        if not prop(vertex, 'label'):
+        if not hasattr(vertex, 'label'):
             raise Exception('NOT a vertex!!')
-        self.vertices.add(vertex)
+        self.vertices[vertex] = set()
 
+"""
 graph = Graph()  # Instantiate your graph (from repo)
-graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
+graph.add_vertex('4')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 print(graph.vertices)
+"""
 
