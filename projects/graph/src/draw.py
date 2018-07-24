63aae77d9bacd13500a03e1d75ff0e53fc69bd4c
april <martinezapril56@yahoo.com>
2018-07-24 15:39:59 -0400

    typing code

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -4,11 +4,19 @@ General drawing methods for graphs using Bokeh.
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import (GraphRenderer, StaticLayoutProvider,
+Circle, LabelSe, ColumnDataSource)
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
+    def __init__(self, graph, title='Graph', width=10, height=10,
+                show_axis=False, show_grid=False, circle_size=)
+
+    if not graph.vertices:
+            raise Expception('graph should contain vertices!')
+    self.graph = graph
+
+    #Setup plot 
+                   
         pass  # TODO
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -14,21 +14,20 @@ class Graph:
         self.vertices[vertex] = set()
 
     def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices and end in self.verices:
+            raise Exception('Error - Node not in graph')
+
         self.vertices[start].add(end)
-
-if bidirectional:
-self.vertices[end.add(start)]
-
-def main():
+        if bidirectional:
+            self.vertices[end.add(start)]
 
 
-    graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
-    print(graph.vertices)
-if __name__ == "__main__":
-    main()
+
+        graph = Graph()  # Instantiate your graph
+        graph.add_vertex('0')
+        graph.add_vertex('1')
+        graph.add_vertex('2')
+        graph.add_vertex('3')
+        graph.add_edge('0', '1')
+        graph.add_edge('0', '3')
+        print(graph.vertices)
