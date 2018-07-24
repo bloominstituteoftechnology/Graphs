47717d3c111f7f50d48d21e8863a66fed1e30b3e
FireInjun <mk@FireInjun.com>
2018-07-24 18:01:21 -0400

    slight mods to the graph.py file.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
+from collections import defaultdict
 
 """
 Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    def __init__(self):
+        pass
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         pass  # TODO
