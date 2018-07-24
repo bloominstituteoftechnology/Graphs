d533fcf0820312aa78bbf7d79aaab963e5d5a476
Jacob Henry <jacobiswinning@gmail.com>
2018-07-24 16:57:30 -0600

    added Dictionary graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,24 +5,41 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
-class Vertex:
+class Vert:
     def __init__(self, label):
         self.label = label
+        self.edges = set()
 
     def __repr__(self):
         return str(self.label)
 
 
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+# class Graph:
+#     """Represent a graph as a matrix of vertices mapping labels to edges."""
 
-    def __init__(self, num_vertices):
-        self.matrix = [[0 for _ in range(num_vertices)] for _ in range(num_vertices)]
-        self.vertices = [Vertex(str(i)) for i in range(num_vertices)]
+#     def __init__(self, num_vertices):
+#         self.matrix = [[0 for _ in range(num_vertices)] for _ in range(num_vertices)]
+#         self.vertices = [Vert(str(i)) for i in range(num_vertices)]
+
+#     def add_edge(self, x_pos, y_pos, is_bidirectional=True):
+#         """adds edges to graph"""
+#         self.matrix[x_pos][y_pos] = 1
+#         if is_bidirectional:
+#             self.matrix[y_pos][x_pos] = 1
+
+
+# todo
+# Get current working graph class to represent it in a list.
+class DictGraph:
+    """Represent a graph as a dictionary of verts mapped to edges"""
+
+    def __init__(self):
+        self.verts = set()
 
     def add_edge(self, x_pos, y_pos, is_bidirectional=True):
-        """adds edges to graph"""
-        self.matrix[x_pos][y_pos] = 1
+        x_pos.edges.add(y_pos)
         if is_bidirectional:
-            self.matrix[y_pos][x_pos] = 1
+            y_pos.edges.add(x_pos)
 
+    def add_vert(self, vert):
+        self.verts.add(vert)
