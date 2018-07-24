b64ec5d9aa423e1094586397d4a97db3b41638cf
april <martinezapril56@yahoo.com>
2018-07-24 16:12:21 -0400

    applying todays lesson

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -18,5 +18,60 @@ class BokehGraph:
     self.graph = graph
 
     #Setup plot 
-                   
-        pass  # TODO
+    self.width = width
+    self.height = height
+    self.pos = {}  # dict to map vertices to X, Y positions
+    self.plot = figures(title=title, x_range=(0, width),
+y_range=(0, height))
+        self.plot.axis.visible = show_axis
+        self.plot.grid.visible = show_grid
+        self._setup_graph_renderer(circle_size)
+
+
+    def _setup_graph_renderer(self, circle_size):
+            graph_renderer = GraphRenderer()
+
+            graph_renderer.node_renderer.data_source.add(
+                list(self.graph.vertices.keys()), 'index')
+            graph_renderer.node_renderer.data_source.add(
+                self._get_random_colors(), 'color')
+            graph_renderer.node_renderer.glyph =
+    Circle(size=circle_size,
+           fill_color='color')
+        graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
+        self.randomize()
+        graph_renderer.layout_provider = StaticLayoutProvider(
+            graph_layout=self.pos)
+        self.plot.renderers.append(graph_renderer)
+
+    def _get_random_colors(self):
+        colors = []
+        for _ in range(len(self.graph.vertices)):
+            color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
+            colors.append(color)
+        return colors
+
+    def _get_edge_indexes(self):
+        start_indices = []
+        end_indices = []
+        checked = set()
+
+        for vertex, edges in self.graph.vertices.items():
+            if vertex not in checked:
+                for destination in edges:
+                    start_indices.append(vertex)
+                    end_indices.append(destination)
+                checked.add(vertex)
+
+        return dict(start=start_indices, end=end_indices)
+
+    def show(self, output_path='./graph.html'):
+        output_file(output_path)
+        show(self.plot)
+
+    def randomize(self):
+        """Randomize vertex positions."""
+        for vertex in self.graph.vertices:
+            # TODO make bounds and random draws less hacky
+            self.pos[vertex] = (1 + random() * (self.width - 2),
+                                1 + random() * (self.height - 2))
