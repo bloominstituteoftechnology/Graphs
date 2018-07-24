0e1b56f0ff288281512160174f4d55a1e294292b
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-24 15:58:49 -0700

    graph displayed, label dont work

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,8 @@
-#!/usr/bin/pyth
+#!/usr/bin/python
 
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -18,4 +17,17 @@ class Graph:
 
         if bidirectional:
             self.vertices[end].add(start)
-    
+
+def main():
+    graph = Graph()  
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('2', '3', False)
+    graph.add_edge('2', '1')    
+    print(graph.vertices)
+
+if __name__ == '__main__':
+    main()
