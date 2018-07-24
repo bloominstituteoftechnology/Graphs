c72c40c3e6bcb8f31bf93ec8ad654a8e974d5d3d
Bradiowave <divebombwalker@gmail.com>
2018-07-24 14:02:34 -0700

    basic graph working

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -20,14 +20,3 @@ class Graph:
         self.vertices[start].add(end)
         if bidirectional:
             self.vertices[end].add(start)
-        
-
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '4')
-print(graph.vertices)
