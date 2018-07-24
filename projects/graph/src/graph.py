d8f0b84e97294e4688474e39a2d698f07aca3e35
Jacob Henry <jacobiswinning@gmail.com>
2018-07-24 03:17:45 -0600

    added a working matrix graph, need to convert it to a dict representation

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,7 +5,24 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+
+    def __repr__(self):
+        return str(self.label)
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+
+    def __init__(self, num_vertices):
+        self.matrix = [[0 for _ in range(num_vertices)] for _ in range(num_vertices)]
+        self.vertices = [Vertex(str(i)) for i in range(num_vertices)]
+
+    def add_edge(self, x_pos, y_pos, is_bidirectional=True):
+        """adds edges to graph"""
+        self.matrix[x_pos][y_pos] = 1
+        if is_bidirectional:
+            self.matrix[y_pos][x_pos] = 1
+
