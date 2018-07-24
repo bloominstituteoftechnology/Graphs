0990e460cc49f74475cb605a197cd6f5955260d4
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-24 18:16:38 -0400

    draw render vertex and edges

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -12,10 +12,9 @@ from graph import *
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self, graph, width=800, height:600, circle_size=30):
+    def __init__( self, graph, width=800, height=600, circle_size=30 ):
         if not graph.vertices:
             raise Exception("Graph should have vertices")
-        
         self.graph = graph
         self.width = width 
         self.height = height
@@ -29,5 +28,7 @@ class BokehGraph:
         graph_render.node_render.data_sorce.add(list(self.graph.vertices.keys()), "index")
         graph_render.node_render.data_sorce.add(Spectral8, 'color')
         graph_render.node_render.glyph = Circle(size=circle_size, fill_color='color')
-
-    
+        graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
+        self.randomize()
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.pos)
+        self.plot.renderers.append(graph_renderer)
