6f256b6929adbc0925d6cfaad90bf94e10d492ea
Brandon Hopper <brandonhopper0@gmail.com>
2018-07-24 17:16:17 -0400

    -graph defined and started setup renderer method

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -6,9 +6,28 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
+from bokeh.palettes import Spectral8
+from graph import *
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph, width=800, height:600, circle_size=30):
+        if not graph.vertices:
+            raise Exception("Graph should have vertices")
+        
+        self.graph = graph
+        self.width = width 
+        self.height = height
+        self.pos = {}
+        self.plot = figure(x_range=(0, width), y_range=(0, height))
+        self._setup_graph_renderer(circle_size)
+    
+    def _setup_graph_renderer(self, circle_size):
+        graph_render = GraphRenderer()
+
+        graph_render.node_render.data_sorce.add(list(self.graph.vertices.keys()), "index")
+        graph_render.node_render.data_sorce.add(Spectral8, 'color')
+        graph_render.node_render.glyph = Circle(size=circle_size, fill_color='color')
+
+    
