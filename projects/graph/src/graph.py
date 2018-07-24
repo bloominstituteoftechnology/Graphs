deae441ce9159abd39544b99859da4963080eba1
Steve <shalomlandsman@gmail.com>
2018-07-24 15:41:44 -0400

    basic bokeh graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,29 +5,26 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 class Graph:
-    """Adjacency list graph."""
     def __init__(self):
         self.vertices = dict()
 
     def add_edge(self, start, end, bidirectional=True):
-        # """Add an edge from start to end."""
         if start not in self.vertices or end not in self.vertices:
             raise Exception('Error - vertices not in graph!')
         self.vertices[start].add(end)
         if bidirectional:
             self.vertices[end].add(start)
 
-    def add_vertex(self, vertex):
-        # if not hasattr(vertex, 'label'):
-        #     raise Exception('This is not a vertex!')
-        self.vertices[vertex] = set()
+    def add_vertex(self, vertex, edges=()):
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error no edges to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
 
 graph = Graph()
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 print(graph.vertices)
