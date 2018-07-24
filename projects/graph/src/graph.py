f35350a0233c363a6b3e819394bb96a27a1d3a56
samarv <samarvir1996@gmail.com>
2018-07-25 04:23:05 +0530

    working with randoms

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,52 +3,66 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
-
-"""Graph representation using adjacency list."""
-
-class Edge:
-    """Edges in the adjacency list are just a destination."""
-    # Using simple classes for illustrative purposes
-    # pylint: disable=too-few-public-methods
-    def __init__(self, destination):
-        self.destination = destination
+from random import random, randint
 
 class Vertex:
-    """Vertices have a label and a set of edges."""
-    def __init__(self, label):
+    """Object representation of Vertex"""
+    def __init__(self, label=None):
         self.label = label
-        self.edges = set()
-
+        self.coords = None
+    
     def __repr__(self):
         return str(self.label)
+    
+    def __hash__(self):
+        return hash(str(self.label))
+    
+    def __eq__(self, other):
+        return self.label == str(other)
 
 
 class Graph:
-    """Adjacency list graph."""
-    def __init__(self):
-        self.Vertex = {}
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self, num_of_vertices=0, chance=0.6):
+        self.vertices = {}
 
+        if num_of_vertices > 0:
+            for i in range(num_of_vertices - 1):
+                self.vertices[i] = set()
+        
+            for i in range(num_of_vertices - 1):
+                p = randint(0,num_of_vertices - 1)
+                if random() <= chance:
+                    self.vertices[i].add(p)
+
+    def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
+    
     def add_edge(self, start, end, bidirectional=True):
-        """Add an edge from start to end."""
-        if start not in self.Vertex or end not in self.Vertex:
-            raise Exception('Error - vertices not in graph!')
-        self.Vertex[start].edges.add(end)
-        print(start)
-        print(self.Vertex[start].edges)
+        """Add an edge (default bidirectional) between two vertices."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+        self.vertices[start].add(end)
         if bidirectional:
-            self.Vertex[end].edges.add(start)
+            self.vertices[end].add(start)
 
-    def add_vertex(self, vertex):
-        self.Vertex[vertex] = Vertex(vertex)
+        
 
+def main():
+    graph = Graph()  # Instantiate your graph
 
+    graph.add_vertex(Vertex('0'))
+    graph.add_vertex(Vertex('1'))
+    graph.add_vertex(Vertex('2'))
+    graph.add_vertex(Vertex('3'))
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    print(graph.vertices)
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.Vertex)
+if __name__ == '__main__':
+    main()
---
+++ projects/graph/src/show.py
@@ -0,0 +1,34 @@
+from graph import Graph
+from draw import BokehGraph
+
+2 +  3
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_vertex('8')
+graph.add_vertex('9')
+graph.add_vertex('10')
+graph.add_edge('0', '1')
+graph.add_edge('1', '8')
+graph.add_edge('1', '2')
+graph.add_edge('2', '4')
+graph.add_edge('2', '5')
+graph.add_edge('3', '6')
+graph.add_edge('3', '5')
+graph.add_edge('3', '7')
+graph.add_edge('4', '5')
+graph.add_edge('1', '9')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+bokeh = BokehGraph(graph)
+
+bokeh.randomize()
+
+bokeh.show()
