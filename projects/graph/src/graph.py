27b4354efff5188b90bbe5da6b2f8b56cc005e7a
JSNavey <jsnavey@gmail.com>
2018-07-24 17:24:45 -0400

    implement draw.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -33,6 +33,28 @@ class Graph:
         if bidirectional:
             self.vertices[end].add(start)
 
+"""
+# solution
+
+class Graph:
+    # Represent a graph as a dictionary of vertices mapping labels to edges.
+    def __init__(self):
+        self.vertices = {}  # represent as a dict
+
+    def add_vertex(self, vertex, edges=()):
+        if vertex in self.vertices:
+            raise Exception('Error - adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error - cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges) 
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error - vertices to connect not in graph')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
+"""
 
 
 graph = Graph()  # Instantiate your graph
