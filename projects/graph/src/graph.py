272f49bc855c99cc9d3e91d121b436e7efcd6dc8
FireInjun <mk@FireInjun.com>
2018-07-25 16:39:20 -0400

    Finished with lots of help, the environment was really complicated to setup

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
-from collections import defaultdict
+from draw import BokehGraph
 
-"""
-Simple graph implementation compatible with BokehGraph class.
-"""
+# """
+# Simple graph implementation compatible with BokehGraph class.
+# """
 
-# After seeing examples and reading docs, the following is not needed.
+
 # class Vertex:
-#   """Vertices have a 'label' and a set of edges"""
+#     """Vertices have a label and a set of edges."""
+
 #     def __init__(self, label):
 #         self.label = label
 #         self.edges = set()
 
 #     def __repr__(self):
-#       retun str(self.label)
+#         return str(self.label)
 
 
 class Graph:
@@ -23,20 +24,37 @@ class Graph:
         self.vertices = {}
 
     def add_edge(self, start, end, bidirectional=True):
-        """Add a 'line' that runs from point to point"""
+        """Add an edge from start to end."""
         if start not in self.vertices or end not in self.vertices:
-            raise Exception("ERROR!")
+            raise Exception("Error - vertices not in graph!")
         self.vertices[start].add(end)
-        if bidirectional
+        if bidirectional:
             self.vertices[end].add(start)
 
     def add_vertex(self, vertex, edges=()):
-        """Add a 'point' that the 'edge/line' will run between"""
-        pass
+        if not set(edges).issubset(self.vertices):
+            raise Exception("Error: Cannot have edge to nonexistent vertices")
+        if vertex in self.vertices:
+            raise Exception("Error: Vertex already exists.")
+        self.vertices[vertex] = set(edges)
 
 
 def main():
-    pass
+    graph = Graph()
+    graph.add_vertex("0")
+    graph.add_vertex("1")
+    graph.add_vertex("2")
+    graph.add_vertex("3")
+    graph.add_vertex("4")
+    graph.add_vertex("5")
+    graph.add_vertex("6")
+    graph.add_vertex("7")
+    graph.add_edge("0", "1")
+    graph.add_edge("0", "3")
+    graph.add_edge("7", "5", False)
+
+    b = BokehGraph(graph)
+    b.show()
 
 
 if __name__ == "__main__":
