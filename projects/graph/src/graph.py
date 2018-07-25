1a3d8b702dc3f017755582497deddf49a26ec5e7
JSNavey <jsnavey@gmail.com>
2018-07-25 18:37:17 -0400

    implement BFS.
    not sure this's correct way??

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import random
+
 """
 Simple graph implementation compatible with BokehGraph class.
 """
@@ -35,7 +37,7 @@ class ListGraph:
 """
 
 """
-# solution
+# solution for Part 1
 
 class Graph:
     # Represent a graph as a dictionary of vertices mapping labels to edges.
@@ -74,7 +76,26 @@ class Graph:
         if bidirectional:
             self.vertices[end].add(start)
 
+    """ function to print a BFS of graph"""
+    def BFS(self, start):
+        visited = [False] * (len(self.vertices))  # mark all the vertices as not visited
+        queue = [] # crate a queue for BFS
+        queue.append(start) #mark the vertices as visited and enqueue it
+        visited[int(start)] = True
 
+        while queue:
+            start = queue.pop(0)
+            print (start, end=" ")
+
+            for i in self.vertices[start]:
+                if visited[int(i)] == False:
+                    queue.append(i)
+                    visited[int(i)] = True
+
+
+
+# need this in order to show the set print in the terminal for part 1
+# but once the graph is drawn in draw.py and show in the browser, this section can delete.
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
@@ -83,8 +104,11 @@ graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
+print('Following is BFS start from')
+graph.BFS('0')
 # graph.add_edge('0', '4')
 # graph.add_edge('a', '0')
 # graph.add_edge('x', 'y')
 print(graph.vertices)
+
 
