5e43fd184b12609a77c1936dd061a8fd925f78ea
bbenefield89 <bsquared18@gmail.com>
2018-07-25 16:38:54 -0400

    ADD NEW METHOD `breadth_first_search`
    - in graph.py
    use BFS to pass through every node in graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -16,6 +16,28 @@ class Graph:
     def add_edge(self, node, connect_node):
         self.graph[ node ].add(connect_node)
 
+    def breadth_first_search(self, node_value):
+        if node_value not in self.graph:
+            print(f"Graph.breadth_first_search() expects a single value passed as an argument")
+            return None
+        
+        queue = []
+        queue_set = set()
+        queue.append(node_value)
+        queue_set.add(node_value)
+        
+        i = 0
+
+        while i < len(queue):
+            current = self.graph[ queue[ i ] ]
+
+            for edge in current:
+                if edge not in queue_set:
+                    queue.append(edge)
+                    queue_set.add(edge)
+
+            i += 1
+
 graph = Graph()
 
 graph.add_vertex('0')
@@ -28,3 +50,5 @@ graph.add_edge('0', '3')
 graph.add_edge('1', '2')
 graph.add_edge('2', '3')
 graph.add_edge('3', '1')
+
+graph.breadth_first_search(1) # {'0': {'1', '3'}, '1': {'2'}, '2': {'3'}, '3': {'1'}}
