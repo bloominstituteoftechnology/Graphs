c54acc13b3c824b80454e81571656de852f6f44c
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-07-26 05:41:47 +0800

    Did the following:
    1. Created and implemented random graph in graph_demo.py.

--- sample-codes/random_graph.py
+++ sample-codes/random_graph.py
@@ -1,26 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 """
 Demonstration of Graph and BokehGraph functionality.
 """
 
-from sys import argv
 from random import sample
+from sys import argv
 from draw import BokehGraph
 from graph import Graph
 
-def main(num_vertices=12, num_edges=12):
+
+def main(num_vertices=8, num_edges=8):
+    """Build and show random graph."""
     graph = Graph()
-    
+    # Add appropriate number of vertices
     for num in range(num_vertices):
         graph.add_vertex(str(num))
-    
+
+    # Add random edges between vertices
     for _ in range(num_edges):
         vertices = sample(graph.vertices.keys(), 2)
+        # TODO check if edge already exists
         graph.add_edge(vertices[0], vertices[1])
-    
+
     bokeh_graph = BokehGraph(graph)
     bokeh_graph.show()
+
 
 if __name__ == '__main__':
     if len(argv) == 3:
@@ -28,4 +33,4 @@ if __name__ == '__main__':
         NUM_EDGES = int(argv[2])
         main(NUM_VERTICES, NUM_EDGES)
     else:
-        main()
+        main()  # accept defaults
