ed95eeb835927932e95747d9f31e316e92781058
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-25 19:26:59 -0400

    Recoded the Graph class to raise exceptions if a vertice already exists and when the edges do not match any of the existing vertices. Began coding the methods for the BokehGraph class.

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -4,11 +4,36 @@ General drawing methods for graphs using Bokeh.
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle,
+                          LabelSet, ColumnDataSource)
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+
+    def __init__(self,
+                 graph,
+                 title='Graph',
+                 width=10,
+                 height=10,
+                 show_axis=False,
+                 show_grid: False,
+                 circle_size=35):
+        if not graph.vertices:
+            raise Exception('Graph should contain vertices')
+        self.graph = graph
+
+        # Setup plot
+        self.width = width
+        self.height = height
+        self.pos = {}  # dict to map vertices to x, y positions
+        self.plot = figure(title=title, x_range=(0, width), y_range(0, height))
+
+        self.plot.axis.visible = show_axis
+        self.plot.axis.visible = show_grid
+
+    def _setup_graph_renderer(self, circle):
+        pass
+
+    def show(self):
+        pass
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,18 +3,22 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
 
-    def __repr__(self):
-        return str(self.label)
+    def add_vertex(self, edges=()):
+        # Add a new vertex, optionally with edges to other vertices. 
+        if vertex in self.vertices: 
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices): 
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-
+        # Add a edge (default bidirectional) between two vertices. 
         if start not in self.vertices:
             raise Exception('Error: The start vertice is not in the graph.')
         elif end not in self.vertices:
@@ -24,16 +28,3 @@ class Graph:
 
         if bidirectional:
             self.vertices[end].add(start)
-
-    def add_vertex(self, vertex):
-        self.vertices[vertex] = set()
-
-
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
