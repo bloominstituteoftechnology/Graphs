61cd07bcb3ce2ceb2fb5ba1b41dc4791eb7219fb
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-25 14:20:55 -0700

    labels displayed on verticies

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -61,6 +61,7 @@ class BokehGraph:
 
         graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.pos)
         self.plot.renderers.append(graph_renderer)
+        self._setup_labels()
 
     def _get_random_colors(self):
         colors = []
@@ -82,6 +83,19 @@ class BokehGraph:
                 checked.add(vertex)
 
         return dict(start=start_indices, end=end_indices)
+    
+    def _setup_labels(self):
+        label_data = {'x': [], 'y': [], 'names': []}
+        for vertex, position in self.pos.items():
+            label_data['x'].append(position[0])
+            label_data['y'].append(position[1])
+            label_data['names'].append(str(vertex))
+        label_source = ColumnDataSource(label_data)
+        labels = LabelSet(x='x', y='y', text='names', level='glyph',
+                          text_align='center', text_baseline='middle',
+                          source=label_source, render_mode='canvas')
+        self.plot.add_layout(labels)
+
 
     def show(self, output_path='./graph.html'):
         output_file(output_path)
