1efc6d57cbf05553866cf17b8e2e15cecb0c26b9
Bee C <b.choeung@gmail.com>
2018-07-25 19:00:41 -0400

    trying to implement bfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import random
 """
 Simple graph implementation compatible with BokehGraph class.
 """
@@ -8,6 +8,8 @@ class Vertex:
     """Object representation of Vertex"""
     def __init__(self, label):
         self.label = label
+        self.edges = set()
+        self.pos = pos
         self.edges = set()
 
 class Graph:
@@ -43,4 +45,39 @@ def main(): #instantiate your graph
 
     if __name__ == '_main_':
         main()
+
+def bfs(self, start):
+    random_color = '#' + \
+        ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+    queue = []
+    found = []
+    queue.append(start)
+    found.append(start)
+
+    start.color = random_color
+
+    while (len(queue) > 0):
+        v = queue[0]
+        for edge in v.edges:
+            if edge not in found:
+                found.append(edge)
+                queue.append(edge)
+                edge.color = random_color
+
+        queue.pop(0)  # TODo look at collections.dequeue
+    return found
+
+# Get the connected components
+def get_connected_components(self):
+    # Connected Components
+    # Go to the next unfound vertex in graph vertices and call BFS on it
+    # Go to step 1 until we get to the end of the array(loop)
+
+    searched = []
+
+    for index, vertex in self.vertices.items():
+        if vertex not in searched:
+            searched.append(self.bfs(vertex))
+
+    return searched
 
