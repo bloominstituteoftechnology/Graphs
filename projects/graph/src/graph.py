867932a9c191ced455a43bc1c95fbf906b430274
Steve <shalomlandsman@gmail.com>
2018-07-25 18:06:56 -0400

    add labels

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -20,11 +20,24 @@ class Graph:
             raise Exception('Error no edges to nonexistent vertices')
         self.vertices[vertex] = set(edges)
 
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
+    # def depth_first_search(self, start):
+    #     visited, stack = set(), [start]
+    #     while stack:
+    #         vertex = stack.pop()
+    #         if vertex not in visited:
+    #             visited.add(vertex)
+    #     current = start
+    #     if self.vertices[current].keys() is search_value:
+
+def main():
+    graph = Graph()
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    print(graph.vertices)
+
+if __name__ == "__main__":
+    main()
