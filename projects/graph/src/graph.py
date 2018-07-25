86ff2df47ca214dbf9f5b3509103f3ce8d310a32
jesuarva <jesuarva.developer@gmail.com>
2018-07-25 02:23:46 +0200

    IMPLEMENT BFS

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,12 +6,16 @@ Simple graph implementation compatible with BokehGraph class.
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """
+    Represent a graph as a dictionary of vertices mapping labels to edges.
+    """
 
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, label):
+        if label in self.vertices:
+            raise Exception("Vertex al ready in the Graph")
         vertex = Vertex(label)
         self.vertices[str(label)] = vertex.edges
 
@@ -32,6 +36,39 @@ class Graph:
             else:
                 self.vertices[str(start_vertex)].add(str(end_vertex))
 
+    def bfs(self, starting_vertex=0, to_search=None):
+        '''
+        Search Breath first. Argunments:
+        'starting_vertex': vertex from which start the search.
+        to_search: Node to search
+        '''
+        deep = 0
+        control = set()
+        bf = [str(starting_vertex)]
+
+        while bf:
+            print(f'''BF{bf}\n{control}''')
+            deep += 1
+            current_vertex = bf[0]
+
+            if str(to_search) == current_vertex:
+                bf += [*self.vertices[current_vertex]]
+                return {'Found value': current_vertex, 'deep': deep, 'Connected Nodes': control}
+            elif current_vertex in control:
+                bf.remove(current_vertex)
+            else:
+                # ADD to 'bf' list the current_vertex`s edges
+                bf += [*self.vertices[current_vertex]]
+
+                # Remove the curent_vertex from 'bf', and add it to 'control'
+                control.add(bf.pop(0))
+
+        return {
+            'Searched value': f'''{to_search} No found''',
+            'deep': deep,
+            'Minimum Spanning Tree': control
+        }
+
 
 class Vertex:
     def __init__(self, label):
@@ -39,12 +76,18 @@ class Vertex:
         self.edges = set()
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
-graph.add_edge('0', '4')
+_graph = Graph()  # Instantiate your graph
+_graph.add_vertex('0')
+_graph.add_vertex('1')
+_graph.add_vertex('2')
+_graph.add_vertex('3')
+_graph.add_vertex('4')
+_graph.add_vertex('5')
+_graph.add_vertex('6')
+_graph.add_vertex('7')
+_graph.add_edge('0', '1')
+_graph.add_edge('0', '3')
+print('\n', _graph.vertices)
+print('\n', _graph.bfs(0, 3))
+print('\n', _graph.bfs(0, 100))
+# graph.add_edge('0', '4')
