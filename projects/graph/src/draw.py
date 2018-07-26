8fb91f3f68d1f4379d11712e1225e4c448ab6dd6
bbenefield89 <bsquared18@gmail.com>
2018-07-26 16:14:15 -0400

    ADD RANDOM COLOR TO VERTICE
    in draw.py
    for each vertice add a random color

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -3,32 +3,49 @@ General drawing methods for graphs using Bokeh.
 """
 
 from bokeh.io import show, output_file
-from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource)
+from bokeh.plotting import figure
 from bokeh.palettes import Spectral8
+from random import (choice, randint)
 
-from random import randint
-
+# custom class
 from graph import (graph, Graph)
 
 # class BokehGraph:
 #     """Class that takes a graph and exposes drawing methods."""
-#     def __init__(self, width=400, height=400):
-#         pass
+#     def __init__(self, x_range, y_range):
+#         self.x_range = x_range
+#         self.y_range = y_range
+#         self.plot = figure(x_range=self.x_range, y_range=self.y_range)
 
-plot = figure(x_range=(0, 10), y_range=(0, 10))
+#     def render_graph(self):
+#         graph_renderer = GraphRenderer()
+#         start = []
+#         end = []
+#         pos = {}
+#         colors = []
+
+plot           = figure(x_range=(0, 10), y_range=(0, 10))
 graph_renderer = GraphRenderer()
-start = []
-end = []
-pos = {}
-color = ['black', 'red', 'blue', 'green']
+start          = []
+end            = []
+pos            = {}
+color          = []
 
 for vert in graph.graph:
+    random_color   = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
+    
     pos[ int(vert) ] = [ randint(1, len(graph.graph)), randint(1, len(graph.graph)) ]
+    color.append(random_color)
 
     for edge in graph.graph[ vert ]:
         start.append(int(vert))
         end.append(int(edge))
+
+print(pos)
+print(start)
+
+print(color)
 
 # add data to `data_source`
 graph_renderer.node_renderer.data_source.add([ key for key in graph.graph ], 'index')
