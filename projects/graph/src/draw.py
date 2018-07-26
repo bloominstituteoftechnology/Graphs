817d5a39dbe6e535050c23f618a967a92d59bf97
07sebascode <sebastiannorena@gmail.com>
2018-07-25 21:23:48 -0400

    layout changed to not random

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -25,6 +25,7 @@ class BokehGraph:
         self.plot.axis.visible = show_axis
         self.plot.grid.visible = show_grid
         self._setup_graph_renderer(circle_size)
+        self._setup_labels()
 
 
     def _setup_graph_renderer(self, circle_size):
@@ -69,8 +70,29 @@ class BokehGraph:
     def randomize(self):
         """Randomize vertex positions."""
         counter=0
+        even=False
         for vertex in self.graph.vertices:
             # TODO make bounds and random draws less hacky
-            self.pos[vertex] = (1 + random() * (self.width - 2),
-                                1 + random() * (self.height - 2))
-            counter +=0.1
+            if counter>=10:
+                counter=0
+            if even:
+                self.pos[vertex] = (1 + counter * (self.width - 2),
+                                1.5 + counter%1 * (self.height - 2))
+            else:
+                self.pos[vertex] = (1 + counter * (self.width - 2),
+                                (counter%1)-1.5 * (self.height - 2))
+            
+            counter +=0.2
+            even= not even
+
+    def _setup_labels(self):
+        label_data = {'x': [], 'y': [], 'names': []}
+        for vertex, position in self.pos.items():
+            label_data['x'].append(position[0])
+            label_data['y'].append(position[1])
+            label_data['names'].append(str(vertex))
+        label_source = ColumnDataSource(label_data)
+        labels = LabelSet(x='x', y='y', text='names', level='glyph',
+                          text_align='center', text_baseline='middle',
+                          source=label_source, render_mode='canvas')
+        self.plot.add_layout(labels)
