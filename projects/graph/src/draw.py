78fae1770b3814b1dd98d03f86b73c8100e13662
Jacob Henry <jacobiswinning@gmail.com>
2018-07-26 14:36:59 -0600

    added bokehGraph class, defined vars, started render fnc

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -13,8 +13,55 @@ from bokeh.models import (
 )
 
 ## get bokeh up and running...
+# declare constants
+TITLE="Graph",
+WIDTH=10,
+HEIGHT=10,
+ALLOW_AXIS=False,
+ALLOW_GRID=False,
+VERT_SIZE=20,
+
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
 
-    def __init__(self):
+    def __init__(
+        self,
+        graph,
+        title,
+        width,
+        height,
+        allow_grid,
+        allow_axis,
+        vert_size,
+        color,
+    ):
+        if graph.vertices:
+            raise Exception('No verts have been defined!')
+        self.graph = graph
+
+        self.render_graph(vert_size)
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.plot = figure(title = TITLE, x_range(0,width), y_range(0, height))
+        self.plot.grid.visible = ALLOW_GRID
+        self.plot.axis.visible = ALLOW_AXIS
+        self.pos = {}
+        
+    def render_graph(self, vert_size):
+        display_graph = GraphRenderer(kwargs)
+
+        display_graph.node__renderer.data_src.data = self.get_edges()
+        
+
         pass  # TODO
+
+
+## exercise
+# connected_components = []
+# visited = set()
+
+# for v in graph.verts:
+#     if v not in visited:
+#         visited.add(v)
+#         components = bfs(v)
+#         connected_components.push(components)
