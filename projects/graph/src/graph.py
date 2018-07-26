5a046e05168adf1cc41e77ab8ec1ef24ea317f13
april <martinezapril56@yahoo.com>
2018-07-26 18:36:15 -0400

    making changes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,153 +1,199 @@
 #!/usr/bin/python
-import random
+
+"""
+Simple graph implementation compatible with BokehGraph class.
+"""
 
 
 class Vertex:
-    def __init__(self, label, color="gray", **pos):
-        self.label = label
-        self.color = color
-        self.pos = pos
-        self.edges = set()
+    """Represent a vertex with a label and possible connected component."""
 
-    '''
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
     def __repr__(self):
-        return str(self.label)
-    '''
-
-    # Use the str method instead
-    def __str__(self):
-        if not self.pos:
-            pos = dict(x=None, y=None)
-        else:
-            pos = self.pos
-        return "Vartex is {}, position at ({}, {}), color is {} and has edges  {}".format(self.label, pos['x'], pos['y'], self.color, self.edges)
+        return 'Vertex: ' + self.label
 
 
 class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+        self.components = 0
 
-    # we can see what properties are on the Graph
-    def __str__(self):
-        return str(self.vertices)
+    def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        """ And an edge (default bidirectional) between two vertices"""
-
-        # Change this so that if not in vertices, just add it
-        '''
+        """Add a edge (default bidirectional) between two vertices."""
         if start not in self.vertices or end not in self.vertices:
-            raise Exception("Vertices to connect not in graph!")
+            raise Exception('Vertices to connect not in graph!')
         self.vertices[start].add(end)
-        '''
-        # using the key, if we are passed an object just get the key
-
-        if isinstance(start, Vertex):
-            start = start.label
-
-        if isinstance(end, Vertex):
-            end = end.label
-
-        # add start if not in vertices
-        if start not in list(self.vertices.keys()):
-            self.add_vertex(Vertex(start))
-
-        # add end if not in vertices
-        if end not in list(self.vertices.keys()):
-            self.add_vertex(Vertex(end))
-
-        self.vertices[start].edges.add(self.vertices[end])
         if bidirectional:
-            self.vertices[end].edges.add(self.vertices[start])
+            self.vertices[end].add(start)
 
-    def add_vertex(self, vertex):
+    def search(self, start, target=None, method='dfs'):
+        """Search the graph using BFS or DFS."""
+        quack = [start]  # Queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
 
-        if not isinstance(vertex, Vertex):
-            vertex = Vertex(vertex)
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
 
-        if vertex.label in self.vertices:
-            return False  # ignores it
+        return visited
 
-        self.vertices[vertex.label] = vertex
-        return True  # added
+    def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
 
-    # Create a random graph
-    def create_vertices_and_edges(self, n_verts):
-        # create verts and put them in a grid
-        grid = []
-        for i in range(n_verts):
-            grid.append(Vertex(str(i)))
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
 
-        # randomly loop through verts and randomly connect it to the next one, passing in the random bidirection
-        for i in range(n_verts - 1):
-            if (random.randrange(n_verts) < n_verts // 2):
-                if(random.randrange(n_verts) < n_verts // 2):
-                    self.add_edge(grid[i].label, grid[i+1].label, False)
-                self.add_edge(grid[i].label, grid[i+1].label)
+# import random
 
-        for vert in grid:
-            self.add_vertex(vert)
 
-    # for debugging only
+# class Vertex:
+#     def __init__(self, label, color="gray", **pos):
+#         self.label = label
+#         self.color = color
+#         self.pos = pos
+#         self.edges = set()
 
-    def debug_create_test_data(self):
+#     '''
+#     def __repr__(self):
+#         return str(self.label)
+#     '''
 
-        v0 = Vertex("0")
-        self.add_vertex(v0)
-        self.add_vertex("1")
-        self.add_vertex("2")
+#     # Use the str method instead
+#     def __str__(self):
+#         if not self.pos:
+#             pos = dict(x=None, y=None)
+#         else:
+#             pos = self.pos
+#         return "Vartex is {}, position at ({}, {}), color is {} and has edges  {}".format(self.label, pos['x'], pos['y'], self.color, self.edges)
 
-        self.add_vertex("3")
-        self.add_vertex("4")
-        self.add_vertex("5")
-        self.add_vertex("6")
 
-        v7 = Vertex("7")
-        v8 = Vertex("8")
-        v9 = Vertex("9")
-        self.add_vertex(v7)
-        self.add_vertex(v8)
-        self.add_vertex(v9)
-        self.add_vertex("10")
+# class Graph:
+#     def __init__(self):
+#         self.vertices = {}
 
-        self.add_edge("0", "1")
-        self.add_edge("5", "1")
-        self.add_edge("7", "5", False)
+#     # we can see what properties are on the Graph
+#     def __str__(self):
+#         return str(self.vertices)
 
-    def bfs(self, start):
-        random_color = '#' + \
-            ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-        queue = []
-        found = []
-        queue.append(start)
-        found.append(start)
+#     def add_edge(self, start, end, bidirectional=True):
+#         """ And an edge (default bidirectional) between two vertices"""
 
-        start.color = random_color
+#         # Change this so that if not in vertices, just add it
+#         '''
+#         if start not in self.vertices or end not in self.vertices:
+#             raise Exception("Vertices to connect not in graph!")
+#         self.vertices[start].add(end)
+#         '''
+#         # using the key, if we are passed an object just get the key
 
-        while (len(queue) > 0):
-            v = queue[0]
-            for edge in v.edges:
-                if edge not in found:
-                    found.append(edge)
-                    queue.append(edge)
-                    edge.color = random_color
+#         if isinstance(start, Vertex):
+#             start = start.label
 
-            queue.pop(0)  # TODo look at collections.dequeue
-        return found
+#         if isinstance(end, Vertex):
+#             end = end.label
 
-    # Get the connected components
-    def get_connected_components(self):
-        # Connected Components
-        # Go to the next unfound vertex in graph vertices and call BFS on it
-        # Go to step 1 until we get to the end of the array(loop)
+#         # add start if not in vertices
+#         if start not in list(self.vertices.keys()):
+#             self.add_vertex(Vertex(start))
 
-        searched = []
+#         # add end if not in vertices
+#         if end not in list(self.vertices.keys()):
+#             self.add_vertex(Vertex(end))
 
-        for index, vertex in self.vertices.items():
-            if vertex not in searched:
-                searched.append(self.bfs(vertex))
+#         self.vertices[start].edges.add(self.vertices[end])
+#         if bidirectional:
+#             self.vertices[end].edges.add(self.vertices[start])
 
-        return searched
+#     def add_vertex(self, vertex):
+
+#         if not isinstance(vertex, Vertex):
+#             vertex = Vertex(vertex)
+
+#         if vertex.label in self.vertices:
+#             return False  # ignores it
+
+#         self.vertices[vertex.label] = vertex
+#         return True  # added
+
+#     # Create a random graph
+#     def create_vertices_and_edges(self, n_verts):
+#         # create verts and put them in a grid
+#         grid = []
+#         for i in range(n_verts):
+#             grid.append(Vertex(str(i)))
+
+#         # randomly loop through verts and randomly connect it to the next one, passing in the random bidirection
+#         for i in range(n_verts - 1):
+#             if (random.randrange(n_verts) < n_verts // 2):
+#                 if(random.randrange(n_verts) < n_verts // 2):
+#                     self.add_edge(grid[i].label, grid[i+1].label, False)
+#                 self.add_edge(grid[i].label, grid[i+1].label)
+
+#         for vert in grid:
+#             self.add_vertex(vert)
+
+    
+
+#     def bfs(self, start):
+#         random_color = '#' + \
+#             ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+#         queue = []
+#         found = []
+#         queue.append(start)
+#         found.append(start)
+
+#         start.color = random_color
+
+#         while (len(queue) > 0):
+#             v = queue[0]
+#             for edge in v.edges:
+#                 if edge not in found:
+#                     found.append(edge)
+#                     queue.append(edge)
+#                     edge.color = random_color
+
+#             queue.pop(0)  # TODo look at collections.dequeue
+#         return found
+
+#     # Get the connected components
+#     def get_connected_components(self):
+#         # Connected Components
+#         # Go to the next unfound vertex in graph vertices and call BFS on it
+#         # Go to step 1 until we get to the end of the array(loop)
+
+#         searched = []
+
+#         for index, vertex in self.vertices.items():
+#             if vertex not in searched:
+#                 searched.append(self.bfs(vertex))
+
+#         return searched
 
 
 # import random
