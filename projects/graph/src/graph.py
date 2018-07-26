0f8800c67756c84dc1ab0d0c355eca7a87ab7f89
Herbert Hodgson <herberthodgson@hotmail.com>
2018-07-26 18:01:45 -0400

    Part 5: Connected components

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,11 +4,20 @@ Simple graph implementation compatible with BokehGraph class.
 from draw import BokehGraph
 
 
+class Vertex:
+    """Represent a vertex with a label and possible connected component."""
+
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
+        self.components = 0
 
     def add_vertex(self, vertex):
         self.vertices[vertex] = set()
@@ -25,17 +34,31 @@ class Graph:
         """Search the graph using BFS or DFS."""
         quack = [start]  # Queue or stack, depending on method
         pop_index = 0 if method == "bfs" else -1
-        visited = set()
+        visited = set(start)
 
         while quack:
             current = quack.pop(pop_index)
             if current == target:
                 break
-            visited.add(current)
+            visited.update(self.vertices[current])
             # Add possible (unvisited) vertices to queue
             quack.extend(self.vertices[current] - visited)
 
         return visited
+
+    def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
+
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
 
 
 def main():
