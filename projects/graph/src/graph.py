71633ba919c5e81e952bbad08102ccc666502d1a
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-07-27 05:17:58 +0800

    Did the following:
    1. Refactored add_vertex method.
    2. Refactored add_edge method.
    3. Added search method.
    4. Created find_components method.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,66 +6,72 @@ import random
 
 class Vertex:
     """ Vertices always have a label and a set of edges. """
-    def __init__(self, label, color="gray", **pos):
-        self.label = label
-        self.color = color
-        self.pos = pos
-        self.edges = set()
+    """ Represent a vertex with a label and possible connected component. """
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+        # self.color = color
+        # self.pos = pos
+        # self.edges = set()
 
     def __repr__(self):
-        return str(self.label)
+        return "Vertex: " + self.label
+        # return str(self.label)
 
 class Graph:    
     """ Represent a graph as a dictionary of vertices mapping labels to edges. """
     """ Type of graph: adjacency list """
     def __init__(self):
         self.vertices = {}
+        self.components = 0
+
+    def add_vertex(self, vertex, edges=()):
+        """ Add a new vertex, optionally with edges to other vertices. """
+        if vertex in self.vertices:
+            raise Exception("Error: adding vertex that already exists.")
+
+        if not set(edges).issubset(self.verticles):
+            raise Exception("Error: cannot have edge to nonexistent vertices.")
+
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
         """ Adding an edge from start to end. """
         if start not in self.vertices or end not in self.vertices:
-            raise Exception("%s or %s does not exist" %(start, end))
-        else:
-            self.vertices[start].add(end)
-            if bidirectional == True:
-                self.vertices[end].add(start)
+            raise Exception("Vertices to connect not in graph!")
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
 
-    def add_vertex(self, vertex):
-        if vertex not in self.vertices:
-            self.vertices[vertex] = set()
-        else:
-            raise ValueError("That vertex already exists in the graph.")
+    def search(self, start, target=None, method='dfs'):
+        """Search the graph using BFS or DFS."""
+        quack = [start]  # Queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
 
-    def bfs(self, start):
-        random_color = '#' + \
-            ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-        queue = []
-        found = []
-        queue.append(start)
-        found.append(start)
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
 
-        start.color = random_color
+        return visited
 
-        while (len(queue) > 0):
-            v = queue[0]
-            for edge in v.edges:
-                if edge not in found:
-                    found.append(edge)
-                    queue.append(edge)
-                    edge.color = random_color
+    def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
 
-            queue.pop(0)
-        return found
-
-    def get_connected_components(self):
-        
-        searched = []
-
-        for index, vertex in self.vertices.items():
-            if vertex not in searched:
-                searched.append(self.bfs(vertex))
-
-        return searched
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
 
 # Testing Graph implementation:
 
