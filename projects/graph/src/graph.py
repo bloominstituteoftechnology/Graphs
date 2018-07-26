e63b5cefa48ede40ffd05f6a62bd5c4f3e49d90e
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-26 14:37:58 -0700

    search method added

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -11,14 +11,15 @@ class Vertex:
 
     def __repr__(self):
         return 'Vertex: ' + self.label
-        
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+        self.components = 0
 
-    def add_vertex(self, vertex):
-        self.vertices[vertex] = set()
+    def add_vertex(self, vertex, edges=()):
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
         self.vertices[start].add(end)
@@ -26,7 +27,19 @@ class Graph:
         if bidirectional:
             self.vertices[end].add(start)
 
-    
+    def search(self, start, target=None, method='dfs'):
+        quack = [start]
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
+
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            quack.extend(self.vertices[current] - visited)
+
+        return visited
 
 def main():
     graph = Graph()  
@@ -34,11 +47,11 @@ def main():
     graph.add_vertex('1')
     graph.add_vertex('2')
     graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('2', '3', False)
+    graph.add_vertex('4')    
+    graph.add_edge('0', '1', False)
+    graph.add_edge('2', '3')
     graph.add_edge('2', '1')    
     print(graph.vertices)
-    print(graph.BFS('3'))
 
 if __name__ == '__main__':
     main()
