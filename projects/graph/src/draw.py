687587f65e2e1be5525c6f73d78fbc2ef7f8b53f
april <martinezapril56@yahoo.com>
2018-07-26 17:47:16 -0400

    working towards mvp

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,5 +1,7 @@
+"""
+General drawing methods for graphs using Bokeh.
+"""
 
-from graph import Graph
 from random import choice, random
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
@@ -9,8 +11,9 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
+
     def __init__(self, graph, title='Graph', width=10, height=10,
-                show_axis=False, show_grid=False, circle_size=35):
+                 show_axis=False, show_grid=False, circle_size=35):
         if not graph.vertices:
             raise Exception('Graph should contain vertices!')
         self.graph = graph
@@ -19,12 +22,12 @@ class BokehGraph:
         self.width = width
         self.height = height
         self.pos = {}  # dict to map vertices to x, y positions
-        self.plot = figure(title=title, x_range=(0, width), y_range=(0, height))
+        self.plot = figure(title=title, x_range=(
+            0, width), y_range=(0, height))
         self.plot.axis.visible = show_axis
         self.plot.grid.visible = show_grid
         self._setup_graph_renderer(circle_size)
-        self._setup_labels()
-
+        self._get_labels()
 
     def _setup_graph_renderer(self, circle_size):
         graph_renderer = GraphRenderer()
@@ -35,28 +38,18 @@ class BokehGraph:
             self._get_random_colors(), 'color')
         graph_renderer.node_renderer.glyph = Circle(size=circle_size,
                                                     fill_color='color')
-
-
-        graph_renderer = GraphRenderer()
-
-        graph_renderer.node_renderer.data_source.add(
-            list(self.graph.vertices.keys()), 'index')
-        graph_renderer.node_renderer.data_source.add(
-            self._get_random_colors(), 'color')
-        graph_renderer.node_renderer.glyph = Circle(size=circle_size,
-                                                    fill_color='color')                                    
-
-
         graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
         self.randomize()
-        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.pos)
+        graph_renderer.layout_provider = StaticLayoutProvider(
+            graph_layout=self.pos)
         self.plot.renderers.append(graph_renderer)
 
     def _get_random_colors(self):
         colors = []
-        for _ in range(len(self.graph.vertices)):
-            color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
-            colors.append(color)
+        for i in range(len(self.graph.vertices)):
+            # color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
+            # colors.append(color)
+            colors.append(self.graph.vertices[str(i)].color)
         return colors
 
     def _get_edge_indexes(self):
@@ -64,42 +57,14 @@ class BokehGraph:
         end_indices = []
         checked = set()
 
-        for vertex, edges in self.graph.vertices.items():
+        for index, vertex in self.graph.vertices.items():
             if vertex not in checked:
-                for destination in edges:
+                for destination in vertex.edges:
                     start_indices.append(vertex.label)
                     end_indices.append(destination.label)
                 checked.add(vertex)
 
         return dict(start=start_indices, end=end_indices)
-
-        def show(self, output_path='./graph.htm'):
-            output_file(output_path)
-            show(self.plot)
-
-    def _setup_labels(self):
-
-        label_data = {'x': [], 'y': [], 'names': []}
-
-        for vertex, position in self.pos.items():
-
-            label_data['x'].append(position[0])
-
-            label_data['y'].append(position[1])
-
-            label_data['names'].append(str(vertex))
-
-        label_source = ColumnDataSource(label_data)
-
-        labels = LabelSet(x='x', y='y', text='names', level='glyph',
-
-                        text_align='center', text_baseline='middle',
-
-                        source=label_source, render_mode='canvas')
-
-        self.plot.add_layout(labels)
-
-    
 
     def show(self, output_path='./graph.html'):
         output_file(output_path)
@@ -112,35 +77,163 @@ class BokehGraph:
             self.pos[vertex] = (1 + random() * (self.width - 2),
                                 1 + random() * (self.height - 2))
 
-def main():
-    graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_vertex('4')
-    graph.add_vertex('5')
-    graph.add_vertex('6')
-    graph.add_vertex('7')
-    graph.add_vertex('8')
-    graph.add_vertex('9')
-    graph.add_vertex('10')
+    def _get_labels(self):
+        label_data = {"x": [], "y": [], "name": []}
+        for vertex, edges in self.pos.items():
+            label_data["x"].append(edges[0])
+            label_data["y"].append(edges[1])
+            label_data["name"].append(vertex)
+        label_source = ColumnDataSource(label_data)
 
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '2')
-    graph.add_edge('1', '3')
-    graph.add_edge('1', '4')
-    graph.add_edge('2', '5')
-    graph.add_edge('2', '6')
-    graph.add_edge('3', '1')
-    graph.add_edge('4', '8')
-    graph.add_edge('5', '9')
-    graph.add_edge('9', '10')
+        labels = LabelSet(
+            x="x",
+            y="y",
+            text="name",
+            text_color="white",
+            level="glyph",
+            text_align="center",
+            text_baseline="middle",
+            text_line_height=1,
+            source=label_source,
+            render_mode="canvas",
+        )
+        self.plot.add_layout(labels)
 
-    bg = BokehGraph(graph)
-    bg.show()
+# from graph import Graph
+# from random import choice, random
+# from bokeh.io import show, output_file
+# from bokeh.plotting import figure
+# from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+#                           ColumnDataSource)
 
 
-if __name__ == '__main__':
-    main()
-print(__name__)
+# class BokehGraph:
+#     """Class that takes a graph and exposes drawing methods."""
+#     def __init__(self, graph, title='Graph', width=10, height=10,
+#                 show_axis=False, show_grid=False, circle_size=35):
+#         if not graph.vertices:
+#             raise Exception('Graph should contain vertices!')
+#         self.graph = graph
+
+#         # Setup plot
+#         self.width = width
+#         self.height = height
+#         self.pos = {}  # dict to map vertices to x, y positions
+#         self.plot = figure(title=title, x_range=(0, width), y_range=(0, height))
+#         self.plot.axis.visible = show_axis
+#         self.plot.grid.visible = show_grid
+#         self._setup_graph_renderer(circle_size)
+#         self._setup_labels()
+
+#     def _setup_graph_renderer(self, circle_size):
+#         graph_renderer = GraphRenderer()
+
+#         graph_renderer.node_renderer.data_source.add(
+#             list(self.graph.vertices.keys()), 'index')
+#         graph_renderer.node_renderer.data_source.add(
+#             self._get_random_colors(), 'color')
+#         graph_renderer.node_renderer.glyph = Circle(size=circle_size,
+#                                                     fill_color='color')
+#         graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
+#         self.randomize()
+#         graph_renderer.layout_provider = StaticLayoutProvider(
+#             graph_layout=self.pos)
+#         self.plot.renderers.append(graph_renderer)
+
+#     def _get_random_colors(self):
+#         colors = []
+#         for _ in range(len(self.graph.vertices)):
+#             color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
+#             colors.append(color)
+#         return colors
+
+#     def _get_edge_indexes(self):
+#         start_indices = []
+#         end_indices = []
+#         checked = set()
+
+#         for index, vertex in self.graph.vertices.items():
+#             if vertex not in checked:
+#                 for destination in vertex.edges:
+#                     start_indices.append(vertex.label)
+#                     end_indices.append(destination.label)
+#                 checked.add(vertex)
+
+#         return dict(start=start_indices, end=end_indices)
+
+#     def show(self, output_path='./graph.htm'):
+#         output_file(output_path)
+#         show(self.plot)
+
+#     def _setup_labels(self):
+
+#         label_data = {'x': [], 'y': [], 'names': []}
+
+#         for vertex, position in self.pos.items():
+
+#             label_data['x'].append(position[0])
+
+#             label_data['y'].append(position[1])
+
+#             label_data['names'].append(str(vertex))
+
+#         label_source = ColumnDataSource(label_data)
+
+#         labels = LabelSet(
+#             x='x',
+#             y='y',
+#             text='name',
+#             level='glyph',
+#             text_align='center',
+#             text_baseline='middle',
+#             source=label_source,
+#             render_mode='canvas'
+#             )
+
+#         self.plot.add_layout(labels)
+
+    
+
+#     def show(self, output_path='./graph.html'):
+#         output_file(output_path)
+#         show(self.plot)
+
+#     def randomize(self):
+#         """Randomize vertex positions."""
+#         for vertex in self.graph.vertices:
+#             # TODO make bounds and random draws less hacky
+#             self.pos[vertex] = (1 + random() * (self.width - 2),
+#                                 1 + random() * (self.height - 2))
+
+# def main():
+#     graph = Graph()  # Instantiate your graph
+#     graph.add_vertex('0')
+#     graph.add_vertex('1')
+#     graph.add_vertex('2')
+#     graph.add_vertex('3')
+#     graph.add_vertex('4')
+#     graph.add_vertex('5')
+#     graph.add_vertex('6')
+#     graph.add_vertex('7')
+#     graph.add_vertex('8')
+#     graph.add_vertex('9')
+#     graph.add_vertex('10')
+
+#     graph.add_edge('0', '1')
+#     graph.add_edge('0', '2')
+#     graph.add_edge('1', '3')
+#     graph.add_edge('1', '4')
+#     graph.add_edge('2', '5')
+#     graph.add_edge('2', '6')
+#     graph.add_edge('3', '1')
+#     graph.add_edge('4', '8')
+#     graph.add_edge('5', '9')
+#     graph.add_edge('9', '10')
+
+#     bg = BokehGraph(graph)
+#     bg.show()
+
+
+# if __name__ == '__main__':
+#     main()
+# print(__name__)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,37 +1,120 @@
 #!/usr/bin/python
 import random
 
+
 class Vertex:
-    """Object representation of Vertex"""
-    def __init__(self, label ):
+    def __init__(self, label, color="gray", **pos):
         self.label = label
-        self.edges = set()
-        self.pos = pos
         self.color = color
+        self.pos = pos
+        self.edges = set()
+
+    '''
+    def __repr__(self):
+        return str(self.label)
+    '''
+
+    # Use the str method instead
+    def __str__(self):
+        if not self.pos:
+            pos = dict(x=None, y=None)
+        else:
+            pos = self.pos
+        return "Vartex is {}, position at ({}, {}), color is {} and has edges  {}".format(self.label, pos['x'], pos['y'], self.color, self.edges)
+
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex, edges=()):
-        """Add anew vertex, optionally with edges to other vehicles."""
-        if vertex in self.vertices:
-            raise Exception('Error: adding vertex that already exists')
-        if not set(edges).issubset(self.vertices):
-            raise Exception('Error: cannot have edge to nonexistent vertices')
-        self.vertices[vertex] = set(edges)
+    # we can see what properties are on the Graph
+    def __str__(self):
+        return str(self.vertices)
 
-    def add_edge(self, start, end, bidirectional = True):
-        """Add a edge (default bidrectional) between two vertices."""
+    def add_edge(self, start, end, bidirectional=True):
+        """ And an edge (default bidirectional) between two vertices"""
+
+        # Change this so that if not in vertices, just add it
+        '''
         if start not in self.vertices or end not in self.vertices:
-            raise Exception('Vertices to connect not in graph!')    
+            raise Exception("Vertices to connect not in graph!")
         self.vertices[start].add(end)
+        '''
+        # using the key, if we are passed an object just get the key
+
+        if isinstance(start, Vertex):
+            start = start.label
+
+        if isinstance(end, Vertex):
+            end = end.label
+
+        # add start if not in vertices
+        if start not in list(self.vertices.keys()):
+            self.add_vertex(Vertex(start))
+
+        # add end if not in vertices
+        if end not in list(self.vertices.keys()):
+            self.add_vertex(Vertex(end))
+
+        self.vertices[start].edges.add(self.vertices[end])
         if bidirectional:
-            self.vertices[end].add(start)
+            self.vertices[end].edges.add(self.vertices[start])
+
+    def add_vertex(self, vertex):
+
+        if not isinstance(vertex, Vertex):
+            vertex = Vertex(vertex)
+
+        if vertex.label in self.vertices:
+            return False  # ignores it
+
+        self.vertices[vertex.label] = vertex
+        return True  # added
+
+    # Create a random graph
+    def create_vertices_and_edges(self, n_verts):
+        # create verts and put them in a grid
+        grid = []
+        for i in range(n_verts):
+            grid.append(Vertex(str(i)))
+
+        # randomly loop through verts and randomly connect it to the next one, passing in the random bidirection
+        for i in range(n_verts - 1):
+            if (random.randrange(n_verts) < n_verts // 2):
+                if(random.randrange(n_verts) < n_verts // 2):
+                    self.add_edge(grid[i].label, grid[i+1].label, False)
+                self.add_edge(grid[i].label, grid[i+1].label)
+
+        for vert in grid:
+            self.add_vertex(vert)
+
+    # for debugging only
+
+    def debug_create_test_data(self):
+
+        v0 = Vertex("0")
+        self.add_vertex(v0)
+        self.add_vertex("1")
+        self.add_vertex("2")
+
+        self.add_vertex("3")
+        self.add_vertex("4")
+        self.add_vertex("5")
+        self.add_vertex("6")
+
+        v7 = Vertex("7")
+        v8 = Vertex("8")
+        v9 = Vertex("9")
+        self.add_vertex(v7)
+        self.add_vertex(v8)
+        self.add_vertex(v9)
+        self.add_vertex("10")
+
+        self.add_edge("0", "1")
+        self.add_edge("5", "1")
+        self.add_edge("7", "5", False)
 
     def bfs(self, start):
-        #he making thats hexadecimal
         random_color = '#' + \
             ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
         queue = []
@@ -46,30 +129,98 @@ class Graph:
             for edge in v.edges:
                 if edge not in found:
                     found.append(edge)
-                    queue.color = random_color
-            queue.pop(0)
+                    queue.append(edge)
+                    edge.color = random_color
+
+            queue.pop(0)  # TODo look at collections.dequeue
         return found
-            
+
+    # Get the connected components
     def get_connected_components(self):
-        
+        # Connected Components
+        # Go to the next unfound vertex in graph vertices and call BFS on it
+        # Go to step 1 until we get to the end of the array(loop)
+
         searched = []
 
         for index, vertex in self.vertices.items():
             if vertex not in searched:
                 searched.append(self.bfs(vertex))
 
-            return searched
+        return searched
+
+
+# import random
+
+# class Vertex:
+#     """Object representation of Vertex"""
+#     def __init__(self, label ):
+#         self.label = label
+#         self.edges = set()
+#         self.pos = pos
+#         self.color = color
+
+# class Graph:
+#     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+#     def __init__(self):
+#         self.vertices = {}
+
+#     def add_vertex(self, vertex, edges=()):
+#         """Add anew vertex, optionally with edges to other vehicles."""
+#         if vertex in self.vertices:
+#             raise Exception('Error: adding vertex that already exists')
+#         if not set(edges).issubset(self.vertices):
+#             raise Exception('Error: cannot have edge to nonexistent vertices')
+#         self.vertices[vertex] = set(edges)
+
+#     def add_edge(self, start, end, bidirectional = True):
+#         """Add a edge (default bidrectional) between two vertices."""
+#         if start not in self.vertices or end not in self.vertices:
+#             raise Exception('Vertices to connect not in graph!')    
+#         self.vertices[start].add(end)
+#         if bidirectional:
+#             self.vertices[end].add(start)
+
+#     def bfs(self, start):
+#         #he making thats hexadecimal
+#         random_color = '#' + \
+#             ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+#         start.color = random_color
+#         queue = [start]
+#         found = set()
+    
+#         while queue:
+#             current_vertex = queue.pop(0)
+#             found.add(current_vertex)
+#             for edge in current_vertex .edges:
+#                 if edge not in found:
+#                     found.append(edge)
+#                     queue.append(edge)
+#                     edge.color = random
+
+        
+#         return found
+            
+#     def get_connected_components(self):
+        
+#         searched = []
+
+#         for index, vertex in self.vertices.items():
+#             if vertex not in searched:
+#                 searched.append(self.bfs(vertex))
+
+#             return searched
         
 
-def main(): #instantiate your graph
-    graph = Graph()
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
-    print(graph.vertices)
+# def main(): #instantiate your graph
+#     graph = Graph()
+#     graph.add_vertex('0')
+#     graph.add_vertex('1')
+#     graph.add_vertex('2')
+#     graph.add_vertex('3')
+#     graph.add_edge('0', '1')
+#     graph.add_edge('0', '3')
+#     print(graph.vertices)
 
-    if __name__ == '_main_':
-        main()
+#     if __name__ == '_main_':
+#         main()
