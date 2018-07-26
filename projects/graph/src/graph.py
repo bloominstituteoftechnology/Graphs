204602277b7d05c170baa0683e0a3145edb56d10
maxwashington <mnwashington18@gmail.com>
2018-07-26 17:50:01 -0500

    Hmmm its works? I read over like a million blogs and this was the result. I havent connect bokuh graphs but maybe another time.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -140,46 +140,129 @@
 
 #         return searched
 
-from collections import defaultdict
+from decimal import Decimal
 
+class Node:
+    def __init__(self, label):
+        self.label = label
+
+class Edge:
+    def __init__(self, to_node, length):
+        self.to_node = to_node
+        self.length = length
 
 class Graph:
-    def init(self):
+    def __init__(self):
         self.nodes = set()
-        self.edges = defaultdict(list)
-        self.distances = {}
+        self.edges = dict()
 
-    def add_node(self, value):
-        self.nodes.add(value)
+    def add_node(self, node):
+        self.nodes.add(node)
 
-    def add_edge(self, from_node, to_node, distance):
-        self.edges[from_node].append(to_node)
-        self.distances[(from_node, to_node)] = distance
+    def add_edge(self, from_node, to_node, length):
+        edge = Edge(to_node, length)
+        if from_node.label in self.edges:
+            from_node_edges = self.edges[from_node.label]
+        else:
+            self.edges[from_node.label] = dict()
+            from_node_edges = self.edges[from_node.label]
+        from_node_edges[to_node.label] = edge
 
-    def dijkstra(graph, initial):
-        visited = {initial: 0}
-        path = defaultdict(list)
-        nodes = set(graph.nodes)
+def min_dist(q, dist):
+    """
+    Returns the node with the smallest distance in q.
+    Implemented to keep the main algorithm clean.
+    """
+    min_node = None
+    for node in q:
+        if min_node == None:
+            min_node = node
+        elif dist[node] < dist[min_node]:
+            min_node = node
 
-        while nodes:
-            min_node = None
-            for node in nodes:
-                if node in visited:
-                    if min_node is None:
-                        min_node = node
-                    elif visited[node] < visited[min_node]:
-                        min_node = node
+    return min_node
 
-            if min_node is None:
-                break
+AnswerToTheUniverse = Decimal('Infinity')
 
-            nodes.remove(min_node)
-            current_weight = visited[min_node]
+def dijkstra(graph, source):
+    q = set()
+    dist = {}
+    prev = {}
 
-            for edge in graph.edges[min_node]:
-                weight = current_weight + graph.distances[(min_node, edge)]
-                if edge not in visited or weight < visited[edge]:
-                    visited[edge] = weight
-                    path[edge].append(min_node)
+    for v in graph.nodes:                   # initialization
+        dist[v] = AnswerToTheUniverse       # unknown distance from source to v
+        prev[v] = AnswerToTheUniverse       # previous node in optimal path from source
+        q.add(v)                            # all nodes initially in q (unvisited nodes)
 
-        return path
+    # distance from source to source
+    dist[source] = 0
+
+    while q:
+        # node with the least distance selected first
+        u = min_dist(q, dist)
+
+        q.remove(u)
+
+        if u.label in graph.edges:
+            for _, v in graph.edges[u.label].items():
+                alt = dist[u] + v.length
+                if alt < dist[v.to_node]:
+                    # a shorter path to v has been found
+                    dist[v.to_node] = alt
+                    prev[v.to_node] = u
+
+    return dist, prev
+
+
+def to_array(prev, from_node):
+    """Creates an ordered list of labels as a route."""
+    previous_node = prev[from_node]
+    route = [from_node.label]
+    while previous_node != AnswerToTheUniverse:
+        route.append(previous_node.label)
+        temp = previous_node
+        previous_node = prev[temp]
+
+    route.reverse()
+    return route
+
+graph = Graph()
+node_a = Node("A")
+graph.add_node(node_a)
+node_b = Node("B")
+graph.add_node(node_b)
+node_c = Node("C")
+graph.add_node(node_c)
+node_d = Node("D")
+graph.add_node(node_d)
+node_e = Node("E")
+graph.add_node(node_e)
+node_f = Node("F")
+graph.add_node(node_f)
+node_g = Node("G")
+graph.add_node(node_g)
+
+graph.add_edge(node_a, node_b, 4)
+graph.add_edge(node_a, node_c, 3)
+graph.add_edge(node_a, node_e, 7)
+graph.add_edge(node_b, node_c, 6)
+graph.add_edge(node_b, node_d, 5)
+graph.add_edge(node_c, node_d, 11)
+graph.add_edge(node_c, node_e, 8)
+graph.add_edge(node_d, node_e, 2)
+graph.add_edge(node_d, node_f, 2)
+graph.add_edge(node_d, node_g, 10)
+graph.add_edge(node_e, node_g, 5)
+graph.add_edge(node_f, node_g, 3)
+
+dist, prev = dijkstra(graph, node_a)
+
+print("The quickest path from {} to {} is [{}] with a distance of {}".format(
+    node_a.label,
+    node_f.label,
+    " -> ".join(to_array(prev, node_f)),
+    str(dist[node_f])
+    )
+)
+
+#Thank to https://gist.github.com/econchick . I would not have understand half of this or even what a shortest path is without his gists and explanation. 
