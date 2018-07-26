ada70a94c46df1343e2e0305ce8d807daadf31fe
Mason Morrow <A99@99s-MacBook-Pro.local>
2018-07-26 23:56:36 +0200

    Add random graph generator

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,12 +5,32 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from random import sample
+from draw import BokehGraph
+from graph import Graph
 
 
-def main():
-    pass  # TODO
+def main(vertices=10, edges=10):
+    '''Build a random Graph'''
+    graph = Graph()
+    for num in range(vertices):
+        graph.add_vertex(num)
+
+    for _ in range(edges):
+        vertices = sample(graph.vertices.keys(), 2)
+        graph.add_edge(vertices[0], vertices[1])
+
+    bokeh_graph = BokehGraph(graph, title="Random Graph")
+    bokeh_graph.show()
 
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+    if len(argv) == 3:
+        VERTICES = int(argv[1])
+        EDGES = int(argv[2])
+        if EDGES > (VERTICES * (VERTICES - 1))/2:
+            print('Too many edges. Choose a lower number')
+        else:
+            main(VERTICES, EDGES)
+    else:
+        print('Expected arguments: Vertices(int) Edges(int)')
