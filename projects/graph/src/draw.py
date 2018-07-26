dc25552c2534b3c5fd86ae0f7baebe2132b39686
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2018-07-26 15:05:45 -0700

    adds connected components

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,7 +7,7 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
-from graph import Graph
+from graph import Graph, Vertex
 
 # Random Number Generators:
 def RNG_vert():
@@ -20,7 +20,8 @@ class BokehGraph:
     Class that takes a graph and exposes drawing methods.
     """
     def __init__(self, graph, title='Graph', width=10, height=10,
-                 show_axis=False, show_grid=False, circle_size=35):
+                 show_axis=False, show_grid=False, circle_size=35,
+                 draw_components=False):
         if not graph.vertices:
             raise Exception('Graph should contain vertices')
         self.graph = graph
@@ -36,33 +37,43 @@ class BokehGraph:
         self.plot.grid.visible = show_grid
 
         # render the vertice as a circle
-        self._setup_graph_renderer(circle_size)
+        self._setup_graph_renderer(circle_size, draw_components)
 
         # render the labels
         self._setup_labels()
 
-    def _setup_graph_renderer(self, circle_size):
+    def _setup_graph_renderer(self, circle_size, draw_components):
+        # The renderer will have the actual logic for drawing
         graph_renderer = GraphRenderer()
-
-        # vertices:
+        # Saving vertices in an arbitrary but persistent order
+        self.vertex_keys = list(self.graph.vertices.keys())
+        # Add the vertex data as instructions for drawing nodes
         graph_renderer.node_renderer.data_source.add(
-            list(self.graph.vertices.keys()), 'index')
-        # vertice color
-        graph_renderer.node_renderer.data_source.add(
-            self.color_generator(), 'color')
+            [vertex.label for vertex in self.vertex_keys], 'index')
+        # Connected Components will be the same color
+        # island components will be random colors
+        colors = (self._color_connected_components() if draw_components
+                  else self._RCG())
+        graph_renderer.node_renderer.data_source.add(colors, 'color')
         # give the vertice shape
         graph_renderer.node_renderer.glyph = Circle(size=circle_size,
                                                     fill_color='color')
-        # connect the vertices
+        # Add the edge [start, end] indices as instructions for drawing edges
         graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
-
-        # randomize position:
+        # Randomize vertex coordinates, and set as layout
         self.randomize()
-        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.position)
+        # destructure this a little to squish a long line
+        _layout = StaticLayoutProvider(graph_layout=self.position)
+        graph_renderer.layout_provider = _layout
+        # Attach the prepared renderer to the plot so it can be shown
         self.plot.renderers.append(graph_renderer)
 
-    def color_generator(self):
+    def _RCG(self, num_colors=None):
+        """
+        Random Color Generator
+        """
         colors = []
+        num_colors = num_colors or len(self.graph.vertices)
         for _ in range(len(self.graph.vertices)):
             color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
             colors.append(color)
@@ -72,23 +83,21 @@ class BokehGraph:
         start_indices = []
         end_indices = []
         checked = set()
-
         for vertex, edges in self.graph.vertices.items():
             if vertex not in checked:
                 for destination in edges:
-                    start_indices.append(vertex)
-                    end_indices.append(destination)
+                    start_indices.append(vertex.label)
+                    end_indices.append(destination.label)
                 checked.add(vertex)
-
         return dict(start=start_indices, end=end_indices)
 
     def randomize(self):
         """
         Randomize Vertex Positions
         """
-        for vertex in self.graph.vertices:
-            self.position[vertex] = (1 + random() * (self.width - 2),
-                                     1 + random() * (self.height - 2))
+        for vertex in self.vertex_keys:
+            self.position[vertex.label] = (1 + random() * (self.width - 2),
+                                           1 + random() * (self.height - 2))
     
     def _setup_labels(self):
         label_data = {'x': [], 'y': [], 'names': []}
@@ -102,26 +111,74 @@ class BokehGraph:
                           source=label_source, render_mode='canvas')
         self.plot.add_layout(labels)
 
+    def _color_connected_components(self):
+        """
+        Return same-color for vertices in connected components
+        """
+        self.graph.find_components()
+        component_colors = self._RCG(self.graph.components)
+        vertex_colors = []
+        for vertex in self.vertex_keys:
+            vertex_colors.append(component_colors[vertex.component])
+        return vertex_colors
+
     def show(self, output_path='./graph.html'):
         output_file(output_path)
         show(self.plot)
 
 
-def main(num_vertices=RNG_vert(), num_edges=RNG_edge()):
-    """
-    Random Graph
-    """
-    graph = Graph()
-    # Add appropriate num of vertices
-    for num in range(num_vertices):
-        graph.add_vertex(str(num))
-    # Add random edges between vertices
-    for _ in range(num_edges):
-        vertices = sample(graph.vertices.keys(), 2)
-        # TODO check if edges already exists
-        graph.add_edge(vertices[0], vertices[1])
-    bokeh_graph = BokehGraph(graph)
-    bokeh_graph.show()
+
+# def main(num_vertices=8, num_edges=8, draw_components=True):
+#     """
+#     Connected Component Graph
+#     """
+#     graph = Graph()
+#     # Add appropriate num of vertices
+#     for num in range(num_vertices):
+#         graph.add_vertex(Vertex(label=str(num)))
+#     # Add random edges between vertices
+#     for _ in range(num_edges):
+#         vertices = sample(graph.vertices.keys(), 2)
+#         # TODO check if edges already exists
+#         graph.add_edge(vertices[0], vertices[1])
+#     bokeh_graph = BokehGraph(graph, draw_components=draw_components)
+#     bokeh_graph.show()
+
+# if __name__ == '__main__':
+#     if len(argv) == 4:
+#         NUM_VERTICES = int(argv[1])
+#         NUM_EDGES = int(argv[2])
+#         DRAW_COMPONENTS = bool(int(argv[3]))
+#         main(NUM_VERTICES, NUM_EDGES, DRAW_COMPONENTS)
+#     else:
+#         print('Expected arguments: num_vertices num_edges draw_components')
+#         print('Both numbers should be integers, draw_components should be 0/1')
+
+
+# def main(num_vertices=RNG_vert(), num_edges=RNG_edge()):
+#     """
+#     Random Graph
+#     """
+#     graph = Graph()
+#     # Add appropriate num of vertices
+#     for num in range(num_vertices):
+#         graph.add_vertex(Vertex(str(num)))
+#     # Add random edges between vertices
+#     for _ in range(num_edges):
+#         vertices = sample(graph.vertices.keys(), 2)
+#         # TODO check if edges already exists
+#         graph.add_edge(vertices[0], vertices[1])
+#     bokeh_graph = BokehGraph(graph)
+#     bokeh_graph.show()
+
+# if __name__ == '__main__':
+#     if len(argv) == 3:
+#         NUM_VERTICES = int(argv[1])
+#         NUM_EDGES = int(argv[2])
+#         main(NUM_VERTICES, NUM_EDGES)
+#     else:
+#         main()
+
 
 # def main():
 #     """
@@ -129,36 +186,30 @@ def main(num_vertices=RNG_vert(), num_edges=RNG_edge()):
 #     """
 #     graph = Graph()
 
-#     graph.add_vertex('0')
-#     graph.add_vertex('1')
-#     graph.add_vertex('2')
-#     graph.add_vertex('3')
+#     # v0 = Vertex(0)
+#     # v1 = Vertex(1)
+#     # v2 = Vertex(2)
+#     # v3 = Vertex(3)
 
-#     graph.add_edge('0', '1')
-#     graph.add_edge('0', '2')
-#     graph.add_edge('1', '2')
-#     graph.add_edge('2', '0')
-#     graph.add_edge('2', '3')
-#     graph.add_edge('2', '3')
+#     graph.add_vertex(0)
+#     graph.add_vertex(1)
+#     graph.add_vertex(2)
+#     graph.add_vertex(3)
+
+#     graph.add_edge(0, 1)
+#     graph.add_edge(0, 2)
+#     graph.add_edge(1, 2)
+#     graph.add_edge(2, 0)
+#     graph.add_edge(2, 3)
 
 #     print("\n")
 #     print("The Graph: ", graph.vertices)
+#     graph.BFS(0)
+#     graph.DFS(3)
 #     print("\n")
-#     print("Following is Breadth First Traversal"
-#                     " (starting from vertex 2)")
-#     graph.BFS('0')
-#     print("\n")
-#     print("Following is Depth First Traversal"
-#                     " (starting from vertex 2)")
-#     graph.DFS('3')
-#     print("\n")
-#     bgraph = BokehGraph(graph)
-#     bgraph.show()
 
-if __name__ == '__main__':
-    if len(argv) == 3:
-        NUM_VERTICES = int(argv[1])
-        NUM_EDGES = int(argv[2])
-        main(NUM_VERTICES, NUM_EDGES)
-    else:
-        main()
+#     # bokeh_graph = BokehGraph(graph)
+#     # bokeh_graph.show()
+
+# if __name__ == '__main__':
+#     main()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,12 +4,24 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Vertex:
+    """
+    Represent a vertex with a label and possible connected component.
+    """
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
+    def __repr__(self):
+        return 'v' + self.label
+
 class Graph:
     """
     Represent a graph as a dictionary of vertices mapping labels to edges.
     """
     def __init__(self):
         self.vertices = {}
+        self.components = 0
 
     def add_vertex(self, vertex, edges=()):
         """
@@ -36,18 +48,22 @@ class Graph:
         Recursive helper function for Depth-First Search
         """
         # mark current node visited
-        visited[int(root)] = True
+        visited[root] = True
         print("Current Node:", root)
-        print("Visited Status Change: ", visited)
+        print("Changes when visited: ", visited)
         # recur over all vertices adjacent to the current node
         for adjacents in self.vertices[root]:
-            if visited[int(adjacents)] == False:
+            print("Check adjacent nodes: ", adjacents)
+            if visited[adjacents] == False:
                 self._dfs_helper(adjacents, visited)
 
     def DFS(self, vertex):
         """
         Depth-first Search (Recursive)
         """
+        print("\n")
+        print("Depth-First Search ")
+        print("starting from vertex 3:")
         # mark all vertices as not visited
         visited = [False] * (len(self.vertices))
         print("Initially, all nodes are False: ", visited)
@@ -55,10 +71,14 @@ class Graph:
         # print out DFS traversal
         self._dfs_helper(vertex, visited)
 
+
     def BFS(self, root):
         """
         Breadth-first Search (Boolean Method)
         """
+        print("\n")
+        print("Breadth-First Search ")
+        print("starting from vertex 0:")
         # mark all vertices as not visited
         visited = [False] * (len(self.vertices))
         print("Initially, all nodes are False: ", visited)
@@ -68,28 +88,67 @@ class Graph:
         q.append(root)
         print("Queue with root value appended: ", q)
         # mark root node as visited
-        visited[int(root)] = True
+        visited[root] = True
         print("Root has been visited: ", visited)
         while q:
             # dequeue a vertex from the queue
             # and then print it
+            print("Before Dequeued Root: ", root)
+            print("Queue: ", q)
             root = q.pop(0)
             print("Dequeued Root: ", root)
             print("Queue after the Root's gone: ", q)
-            print("Root still visited -> ", visited)
+            print("Changes when visited: ", visited)
             # get all adjacent vertices of dequeued root
             # if adjacent vertexes have no been visted:
             # mark them as visted and enqueue them
             for adjacents in self.vertices[root]: 
-                if visited[int(adjacents)] == False:
+                if visited[adjacents] == False:
                     q.append(adjacents)
-                    visited[int(adjacents)] = True
+                    visited[adjacents] = True
 
-        # def connected_component(self, component):
-        #     connected_components = []
-        #     visited = set()
+    def search(self, start, target=None, method='dfs'):
+        """
+        Search Graph with either DFS(stack) or BFS
+        """
+        print("\n")
+        print("Search: ")
+        quack = [start] # Queue or Stack, depending on method
+        print("Initial Queue/Stack: ", quack)
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
+        print("Initial Visited: ", visited)
 
-        #     for v in graph.vertices:
-        #         if v not in visited:
-        #             visited.update(component)
-        #             connected_components.append(component)
+        while quack:
+            current = quack.pop(pop_index)
+            print("Current Position: ", current)
+            if current == target:
+                break
+            visited.add(current)
+            # add unvisited vertices to Queue/Stack(quack)
+            print("Visited: ", visited)
+            quack.extend(self.vertices[current] - visited)
+            print("Quack: ", quack)
+        
+        return visited
+
+    def find_components(self):
+        """
+        Identify components and update vertex component IDs.
+        """
+        visited = set()
+        current_component = 0
+
+        for vertex in self.vertices:
+            if vertex not in visited:
+                """
+                Choose a Search Method:
+                """
+                # reachable = self.DFS(vertex)
+                # reachable = self.BFS(vertex)
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
