44a2d505d96fd27b4db2fdd7f70214bd5a0e7f49
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-07-26 02:14:04 -0400

    Completed phase 2.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,22 +3,23 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, edges=()):
-        # Add a new vertex, optionally with edges to other vertices. 
-        if vertex in self.vertices: 
+    def add_vertex(self, vertex, edges=()):
+        # Add a new vertex, optionally with edges to other vertices.
+        if vertex in self.vertices:
             raise Exception('Error: adding vertex that already exists')
-        if not set(edges).issubset(self.vertices): 
+        if not set(edges).issubset(self.vertices):
             raise Exception('Error: cannot have edge to nonexistent vertices')
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        # Add a edge (default bidirectional) between two vertices. 
+        # Add a edge (default bidirectional) between two vertices.
         if start not in self.vertices:
             raise Exception('Error: The start vertice is not in the graph.')
         elif end not in self.vertices:
