f8742279facc2c42132c7b55efe8a8eacbb18db6
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-26 15:48:47 -0700

    search method returns the visited list

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -12,7 +12,7 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self, graph, title='Graph', width=10, height=10,
-                show_axis=False, show_grid=False, circle_size=45):
+                show_axis=False, show_grid=False, circle_size=45, draw_components=False):
         if not graph.vertices:
             raise Exception('Graph should contain vertices!')
         self.graph = graph
@@ -24,10 +24,10 @@ class BokehGraph:
         self.plot = figure(title=title, x_range=(0, width), y_range=(0, height))
         self.plot.axis.visible = show_axis
         self.plot.grid.visible = show_grid
-        self._setup_graph_renderer(circle_size)
+        self._setup_graph_renderer(circle_size, draw_components)
 
 
-    def _setup_graph_renderer(self, circle_size):
+    def _setup_graph_renderer(self, circle_size,draw_components):
         graph_renderer = GraphRenderer()
 
         graph_renderer.node_renderer.data_source.add(
@@ -63,7 +63,7 @@ class BokehGraph:
         self.plot.renderers.append(graph_renderer)
         self._setup_labels()
 
-    def _get_random_colors(self):
+    def _get_random_colors(self, num_colors=None):
         colors = []
         for _ in range(len(self.graph.vertices)):
             color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
@@ -114,13 +114,17 @@ graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('1', '2')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_edge('0', '1', False)
+graph.add_edge('0', '3', False)
+graph.add_edge('1', '2', False)
+graph.add_edge('0', '2', False)
 graph.add_edge('0', '2')
-graph.add_edge('0', '2')
+graph.add_edge('4', '5')
+print(graph.search('1', '3'))
 
-print(graph.vertices)
+# print(graph.search)
 
-bg = BokehGraph(graph)
+bg = BokehGraph(graph, draw_components=False)
 bg.show()
