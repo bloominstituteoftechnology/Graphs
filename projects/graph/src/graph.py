fc06d6a1ad42389947976180954737bcd054ece9
Lavell Burton <taylormade800@gmail.com>
2018-07-26 15:52:50 -0700

    finally got file to run in server now need to tweak and optimize

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-import numpy as np
+# import numpy as np
+import random
 from random import choice, random
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
@@ -11,213 +12,150 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    """Represent a vertex with a label and possible connected component."""
+
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
+    def __repr__(self):
+        return 'Vertex: ' + self.label
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
+        self.components = 0
+
+    def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        """Add an edge from start to end."""
+        """Add a edge (default bidirectional) between two vertices."""
         if start not in self.vertices or end not in self.vertices:
-            raise Exception("Error - vertices not in graph!")
+            raise Exception('Vertices to connect not in graph!')
         self.vertices[start].add(end)
         if bidirectional:
             self.vertices[end].add(start)
 
-    def add_vertex(self, vertex):
-        if not hasattr(vertex, "label"):
-            raise Exception("This is not a vertex!")
-        self.vertices.add(vertex)
+    def search(self, start, target=None, method='dfs'):
+        """Search the graph using BFS or DFS."""
+        quack = [start]  # Queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
 
-    def _get_labels(self):
-        label_data = {"x": [], "y": [], "name": []}
-        for vertex, edges in self.pos.items():
-            label_data["x"].append(edges[0])
-            label_data["y"].append(edges[1])
-            label_data["name"].append(vertex)
-        print("label", label_data)
-        label_source = ColumnDataSource(label_data)
-        print("label source:", label_source)
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
 
-        labels = LabelSet(
-            x="x",
-            y="y",
-            text="name",
-            text_color="white",
-            level="glyph",
-            text_align="center",
-            text_baseline="middle",
-            text_line_height=1,
-            source=label_source,
-            render_mode="canvas",
-        )
-        self.plot.add_layout(labels)
+        return visited
 
+    def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
 
-class Vertex:
-    """Vertices have a label and a set of edges."""
-
-    def __init__(self, label):
-        self.label = label
-        self.edges = set()
-
-    def __repr__(self):
-        return str(self.label)
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
 
 
-class ListGraph:
-    """Adjacency list graph."""
+# class ListGraph:
+#     """Adjacency list graph."""
 
-    def __init__(self):
-        self.vertices = set()
+#     def __init__(self):
+#         self.vertices = set()
 
-    def add_edge(self, start, end, bidirectional=True):
-        """Add an edge from start to end."""
-        if start not in self.vertices or end not in self.vertices:
-            raise Exception("Error - vertices not in graph!")
-        start.edges.add(end)
-        if bidirectional:
-            end.edges.add(start)
+#     def add_edge(self, start, end, bidirectional=True):
+#         """Add an edge from start to end."""
+#         if start not in self.vertices or end not in self.vertices:
+#             raise Exception("Error - vertices not in graph!")
+#         start.edges.add(end)
+#         if bidirectional:
+#             end.edges.add(start)
 
-    def add_vertex(self, vertex):
-        if not hasattr(vertex, "label"):
-            raise Exception("This is not a vertex!")
-        self.vertices.add(vertex)
-   
-    def dfs(start, reset=true) {
-        const component = [];
-        const stack = [];
+#     def add_vertex(self, vertex):
+#         if not hasattr(vertex, "label"):
+#             raise Exception("This is not a vertex!")
+#         self.vertices.add(vertex)
 
-    if (reset) {
-      for (let v of this.vertexes) {
-        v.color = 'white';}
-    }
+#     def bfs(self, start):
+#         random_color = '#' + \
+#             ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+#         queue = []
+#         found = []
+#         queue.append(start)
+#         found.append(start)
 
-    stack.push(start);
+#         start.color = random_color
 
-    while (stack.length > 0) {
-      const u = stack.pop();
-      if (u.color === 'white') {
-        u.color = 'gray';
-        for (let e of u.edges) {
-          stack.push(e.destination);}
-      }
+#         while (len(queue) > 0):
+#             v = queue[0]
+#             for edge in v.edges:
+#                 if edge not in found:
+#                     found.append(edge)
+#                     queue.append(edge)
+#                     edge.color = random_color
 
-      stack.unshift(); // de-stack
-      u.color = 'black';
+#             queue.pop(0)  # TODo look at collections.stack
+#         return found
 
-      component.push(u);}
+#     def dfs(self, start):
+#         random_color = '#' + \
+#             ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+#         stack = []
+#         found = []
+#         stack.append(start)
+#         found.append(start)
 
-    return component;}
+#         start.color = random_color
 
-  /**
-   * Get the connected components
-   */
-  getConnectedComponents() {
-    const componentsList = [];
+#         while (len(stack) > 0):
+#             v = stack[len(stack) - 1]
+#             for edge in v.edges:
+#                 if edge not in found:
+#                     found.append(edge)
+#                     stack.append(edge)
+#                     edge.color = random_color
 
-    let needReset = true;
+#             stack.pop()  # TODo look at collections.stack
+#         return found
 
-    for (let v of this.vertexes) {
-      if (needReset || v.color === 'white') {
-        const component = this.dfs(v, needReset);
-        needReset = false;
+#     # Get the connected components
+#     def get_connected_components(self):
+#         # Connected Components
+#         # Go to the next unfound vertex in graph vertices and call BFS on it
+#         # Go to step 1 until we get to the end of the array(loop)
 
-        componentsList.push(component);
-      }
-    }
+#         searched = []
 
-    return componentsList;
-  }
-"""
-General drawing methods for graphs using Bokeh.
-"""
+#         for index, vertex in self.vertices.items():
+#             if vertex not in searched:
+#                 searched.append(self.bfs(vertex))  # using bfs
 
-
-class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-
-    def __init__(self, graph, title='Graph', width=10, height=10,
-                 show_axis=False, show_grid=False, circle_size=35):
-        if not graph.vertices:
-            raise Exception('Graph should contain vertices!')
-        self.graph = graph
-
-        # Setup plot
-        self.width = width
-        self.height = height
-        self.pos = {}  # dict to map vertices to x, y positions
-        self.plot = figure(title=title, x_range=(
-            0, width), y_range=(0, height))
-        self.plot.axis.visible = show_axis
-        self.plot.grid.visible = show_grid
-        self._setup_graph_renderer(circle_size)
-
-    def _setup_graph_renderer(self, circle_size):
-        graph_renderer = GraphRenderer()
-
-        graph_renderer.node_renderer.data_source.add(
-            list(self.graph.vertices.keys()), 'index')
-        graph_renderer.node_renderer.data_source.add(
-            self._get_random_colors(), 'color')
-        graph_renderer.node_renderer.glyph = Circle(size=circle_size,
-                                                    fill_color='color')
-        graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
-        self.randomize()
-        graph_renderer.layout_provider = StaticLayoutProvider(
-            graph_layout=self.pos)
-        self.plot.renderers.append(graph_renderer)
-
-    def _get_random_colors(self):
-        colors = []
-        for _ in range(len(self.graph.vertices)):
-            color = '#'+''.join([choice('0123456789ABCDEF') for j in range(6)])
-            colors.append(color)
-        return colors
-
-    def _get_edge_indexes(self):
-        start_indices = []
-        end_indices = []
-        checked = set()
-
-        for vertex, edges in self.graph.vertices.items():
-            if vertex not in checked:
-                for destination in edges:
-                    start_indices.append(vertex)
-                    end_indices.append(destination)
-                checked.add(vertex)
-
-        return dict(start=start_indices, end=end_indices)
-
-    def show(self, output_path='./graph.html'):
-        output_file(output_path)
-        show(self.plot)
-
-    def randomize(self):
-        """Randomize vertex positions."""
-        for vertex in self.graph.vertices:
-            # TODO make bounds and random draws less hacky
-            self.pos[vertex] = (1 + random() * (self.width - 2),
-                                1 + random() * (self.height - 2))
+#         return searched
 
 
-def main():
-    graph = Graph()
-    graph.add_vertex("0")
-    graph.add_vertex("1")
-    graph.add_vertex("2")
-    graph.add_vertex("3")
-    graph.add_vertex("4")
-    graph.add_vertex("5")
-    graph.add_vertex("6")
-    graph.add_vertex("7")
-    graph.add_edge("0", "1")
-    graph.add_edge("0", "3")
-    graph.add_edge("7", "5", False)
-
-
-
-# Random Graph
-adj = np.random.randint(0,2,(n,n))
-n = np.random.randint(1, N+1)
+# """
+# General drawing methods for graphs using Bokeh.
+# """
+# # Random Graph
+# # adj = np.random.randint(0,2,(n,n))
+# # n = np.random.randint(1, N+1)
