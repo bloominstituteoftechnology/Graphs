f02963964fd3429e1e421a0be90455b8852aca32
Navjot Singh <navjotts@gmail.com>
2018-07-26 13:41:00 -0700

    part4

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,6 +2,12 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+from enum import Enum
+
+class SearchMethod(Enum):
+  BFS = 0
+  DFS = 1
+
 class Graph:
   """Represent a graph as a dictionary of vertices mapping labels to edges."""
   def __init__(self):
@@ -16,3 +22,16 @@ class Graph:
       raise Exception("Please make sure vertices are in the graph, before creating an edge between them!")
     self.vertices[start].add(end)
     self.vertices[end].add(start)
+
+  def search(self, root, method=SearchMethod.BFS):
+    collection = [root]
+    marked = set(root)
+    index = 0 if method == 'bfs' else -1
+
+    while len(collection) > 0:
+      v = collection.pop(index)
+      print(v)
+      for each in self.vertices[v]:
+        if each not in marked:
+          collection.append(each)
+          marked.add(each)
