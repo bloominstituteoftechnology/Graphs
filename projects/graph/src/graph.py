0fd156a66e521d71fb6a50b598bdef7b23b16eb2
darthMinimus <irishseanvaldivia@gmail.com>
2018-08-29 01:36:58 -0500

    Struggling

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,13 +1,23 @@
-"""
-Simple graph implementation compatible with BokehGraph class.
-"""
+"""Graph representation using adjacency list."""
+
+class Edge:
+    """Edges in the adjacency list are just a destination."""
+    # Using simple classes for illustrative purposes
+    # pylint: disable=too-few-public-methods
+    def __init__(self, destination):
+        self.destination = destination
+
+
+class Vertex:
+    """Vertices have a label and a set of edges."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices/nodes mapping labels to edges."""
-    def __init__(self, num_vertices):
-        self.matrix = {}
-
-    def connect_vertex(self, row, col):
-
-
+    """The graph itself is simply a set of vertices."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.vertices = set()
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -1,4 +1,32 @@
 #!/usr/bin/python
+# copy pasta!
+"""Graph representation using adjacency list."""
+
+class Edge:
+    """Edges in the adjacency list are just a destination."""
+    # Using simple classes for illustrative purposes
+    # pylint: disable=too-few-public-methods
+    def __init__(self, destination):
+        self.destination = destination
+
+
+class Vertex:
+    """Vertices have a label and a set of edges."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
+
+
+class Graph:
+    """The graph itself is simply a set of vertices."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.vertices = set()
+
+
+
+
 
 """
 Demonstration of Graph and BokehGraph functionality.
@@ -6,9 +34,12 @@ Demonstration of Graph and BokehGraph functionality.
 
 from sys import argv
 
+ def draw_random_graph(graph, vertices, edges):
+     pass
 
 def main():
-    pass  # TODO
+   
+        
 
 
 if __name__ == '__main__':
