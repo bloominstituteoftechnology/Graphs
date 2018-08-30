da8a50a96569c802498a0729b02712d5a309d2ec
Thuy Pham <tpham045@hotmail.com>
2018-08-29 18:36:59 -0700

    Able to draw some vertices with edge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,27 +3,24 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+
+
 class Vertex:
     def __init__(self, label):
         self.label = label
         self.edges = set()
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        self.vertices = set()
-    
+        self.vertices = {}
+
     def add_vertex(self, item):
-        self.vertices.add(item)
-    
-    def add_edge(self, start, end, is_directed = False):
-        start.edges.add(end)
+        self.vertices.update({item.label: item.edges})
+
+    def add_edge(self, start, end, is_directed=False):
+        start.edges.add(end.label)
         if not is_directed:
-            end.edges.add(start)
-
-my_gr = Graph()
-v1 = Vertex('A')
-v2 = Vertex('B')
-
-my_gr.add_vertex(v1)
-my_gr.add_vertex(v2)
+            end.edges.add({start.label})
