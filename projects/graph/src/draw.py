c7e72403795d1a185597fd4e832bc1488642f9fd
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-10 14:21:47 -0700

    BokehGraph initialized

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,7 +1,9 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-
+from graph import Graph, Vertex
+from sys import argv
+from random import choice, random, sample
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
@@ -10,5 +12,18 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph, title='Graph', width=10, height=10,
+                show_axis=False, show_grid=False, circle_size=45, 
+                draw_components=False):
+        if not graph.vertices:
+            raise Exception('Graph should contain vertices!')
+        self.graph = graph
+
+        # Setup plot
+        self.width = width
+        self.height = height
+        self.pos = {}  # dict to map vertices to x, y positions
+        self.plot = figure(title=title, x_range=(0, width), y_range=(0, height))
+        self.plot.axis.visible = show_axis
+        self.plot.grid.visible = show_grid
+        self._setup_graph_renderer(circle_size, draw_components)
