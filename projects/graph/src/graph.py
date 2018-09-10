fb94ecdfed0c415a9e4c26687501acbc60613e95
Nicky Chen <nicky.chen0@gmail.com>
2018-09-10 14:31:09 -0700

    part 1 completed

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -11,21 +11,24 @@ class Graph:
 
     def add_vertex(self, vertex):
         if vertex not in self.vertices:
-            self.vertices[vertex] = []
+            self.vertices[vertex] = set()
+        else:
+            raise ValueError(f'There is already a vertex {vertex} here')
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 not in self.vertices or vertex2 not in self.vertices:
+            raise valueError(f"There's not vertex there to add an edge to!")
+        else:
+            self.vertices[vertex1].add(vertex2)
+            self.vertices[vertex2].add(vertex1)
 
 
-class Node:
-    def __init__(self):
-        self.neighbors = []
-    # O(1)
-
-    def addNeighbor(self, neighbor_node):
-        self.neighbors.append(neighbor_node)
-    # O(1)
-
-    def getNeighbors(self):
-        return self.neighbors
-    # O(n)
-
-    def isNeighbor(self, node):
-        return node in self.neighbors
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+# graph.add_edge('0', '4')
+print(graph.vertices)
