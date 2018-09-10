f3192ef2f15afac7a2ded6226bb39d982a693184
Simon <Simon_Freelance@hotmail.com>
2018-09-10 19:11:57 -0400

    Made self.vertices use sets instead of arrays

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,7 +12,7 @@ class Graph:
         if vertex in self.vertices:
             raise Exception(f'vertex {vertex} already exists')
         
-        self.vertices[vertex] = []
+        self.vertices[vertex] = set()
     
     def add_edge(self, vertex1, vertex2):
         if not vertex1 in self.vertices:
@@ -21,5 +21,5 @@ class Graph:
         if not vertex2 in self.vertices:
             raise Exception(f'vertex2: {vertex2} does not exist')
 
-        self.vertices[vertex1].append(vertex2)
-        self.vertices[vertex2].append(vertex1)
+        self.vertices[vertex1].add(vertex2)
+        self.vertices[vertex2].add(vertex1)
