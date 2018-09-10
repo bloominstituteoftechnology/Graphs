790dc64be263f61f92928bab61e4aa988899f2b2
Marco Guzman <marcoguzman16@gmail.com>
2018-09-10 15:58:12 -0700

    slight change vertex size

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -14,7 +14,7 @@ class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
 
     def __init__(self, graph, title='Graph', width=10, height=10,
-                 show_axis=False, show_grid=False, circle_size=35,
+                 show_axis=True, show_grid=True, circle_size=40,
                  draw_components=False):
         if not graph.vertices:
             raise Exception('Graph should contain vertices!')
