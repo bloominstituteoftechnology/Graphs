b6e32413666604593bb635232b67a13f639bb2e3
Vu Cao <cdhvu@yahoo.com>
2018-09-10 12:47:20 -0700

    Added add_edge method for Graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -14,3 +14,10 @@ class Graph:
             raise ValueError(f"Duplicate vertex '{label}' found")
         #create new vertex object with the label passed to function
         self.vertices[label] = set()
+
+    def add_edge(self, label, destination):
+        if label not in self.vertices:
+            raise ValueError(f"Vertex '{label}' not found")
+        if destination not in self.vertices:
+            raise ValueError(f"Vertex '{destination}' not found")
+        self.vertices[label].add(destination)
