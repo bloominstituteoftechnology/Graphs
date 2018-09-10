5868b57a1b9991e31b11d968411951bf2c91732e
sean chavez <seanchavez@seanchavezsMBP.home>
2018-09-10 13:35:34 -0700

    Sean Chavez--Graphs Implement Vertex class Implement Graph class wip

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,8 +2,22 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Vertex:
+    def __init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+  
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+        self.numVertices = 0
+
+    def add_vertex(self, key):
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(key)
+        self.vertices[key] = newVertex
+        return newVertex
+
+    
