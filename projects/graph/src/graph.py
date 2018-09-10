64489954c20f57db308d7ce51896f31c8f0c28bb
Alec <afroenygam@gmail.com>
2018-09-10 16:45:45 -0400

    initial commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,13 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+class Node:
+    def __init__(self):
+        self.neighbors = []
+    def addNeighbor(self, neighbor_node):
+        self.neighbors.append(neighbor_node)
+    def getNeighbors(self):
+        return self.neighbors
+    def isNeighbor(self, node):
+        return node in self.neighbors
