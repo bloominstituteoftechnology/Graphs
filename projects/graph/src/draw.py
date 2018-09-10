c86dd0a0c2a97e8e03053e391c9e3b35ead77dee
Erin Meredith <ekmeredith@att.net>
2018-09-10 19:28:40 -0400

    working/stuck on Bokeh

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -6,9 +6,18 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
+from graph import Graph
+from graph import graph
 
-
-class BokehGraph:
+class BokehGraph(Graph):
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  # TODO
+        Graph.__init__(self)
+
+    def show(self):
+        output_file("graph.html")
+        p = figure(title="Graphs Part 2")
+        p.line(graph, line_width=2)
+        show(p)
+
+BokehGraph.show(graph)
