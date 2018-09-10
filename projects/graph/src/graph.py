5972f9c9cdf8936c6d23d070dd75c365ff5e8cee
Vu Cao <cdhvu@yahoo.com>
2018-09-10 12:40:47 -0700

    Added add_vertex method to Graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,10 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+
+    def add_vertex(self, label):
+        #check if an object with label already exists
+        if label in self.vertices:
+            raise ValueError(f"Duplicate vertex '{label}' found")
+        #create new vertex object with the label passed to function
+        self.vertices[label] = set()
