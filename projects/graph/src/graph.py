232ca422c18e4486cb290a60913d99037ed62fd0
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-10 17:28:59 -0500

    Graph, Vertex, Edge Classes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,9 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    def add_vertex(self, vertex, edges=None):
+        self.vertices[vertex] = set(edges)
+    def add_edge(self, head, tail):
+        self.vertices[head].add(tail)
+        self.vertices[tail].add(head)
