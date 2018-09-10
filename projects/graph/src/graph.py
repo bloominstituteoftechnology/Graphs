2c7f0faa357f08b7804ed8ab0ab15df391facf07
Alec <afroenygam@gmail.com>
2018-09-10 17:21:25 -0400

    Graph class updated with add_vertex and add_edge methods

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,13 +1,6 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
-
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
-
 class Node:
     def __init__(self):
         self.neighbors = []
@@ -17,3 +10,42 @@ class Node:
         return self.neighbors
     def isNeighbor(self, node):
         return node in self.neighbors
+
+class Vertex:
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
+    def __repr__(self):
+        return 'Vertex: ' + self.label
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+        self.components = 0
+
+    def add_vertex(self, vertex, edges=()):
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot set edges to non-existent vertices')
+        self.vertices[vertex] = set(edges)
+
+    def add_edge(self, start, end, bidirectional=True):
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error: Verticies to connect not in graph')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
+
+graph = Graph()
+print(graph)
+print(graph.vertices)
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_edge('1', '2')
+graph.add_vertex('3')
+graph.add_edge('1', '3')
+graph.add_edge('2','3', False)
+print(graph.vertices)
