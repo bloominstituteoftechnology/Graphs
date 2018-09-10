60e005ffc9f2ae0e02a9f6c316cbe84e57e1571e
DirupT <iamcooled@gmail.com>
2018-09-10 15:04:29 -0500

    finished stretch for part 1

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,4 +12,16 @@ class Graph:
         self.vertices[vertex] = set()
 
     def add_edge(self, key, value):
+        if key not in self.vertices:
+            raise Exception(f'No {key} vertex')
         self.vertices[key].add(value)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
