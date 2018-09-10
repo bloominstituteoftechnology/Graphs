4beee247eadda9926355697c19001752085f7d29
JJ Ashcraft <jj@jjashcraft.com>
2018-09-10 15:35:08 -0400

    graph.py stretch completed, added vertice checking

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,12 +1,7 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-vertice_list = {
-    '0': {'1', '3'},
-    '1': {'0'},
-    '2': set(),
-    '3': {'0'}
-}
+
 
 
 class Graph:
@@ -16,9 +11,13 @@ class Graph:
     def add_vertice(self, vertice):
       self.vertices[vertice] = set()
     def add_edge(self, vertice1, vertice2):
-      self.vertices[vertice1].add(vertice2)
-      self.vertices[vertice2].add(vertice1)
-
+      if not vertice1 in self.vertices:
+        print(f'Vertice {vertice1} does not exist, create missing vertice prior to creating edge.')
+      elif not vertice2 in self.vertices:
+        print(f'Vertice {vertice2} does not exist, create missing vertice prior to creating edge.')
+      else:
+        self.vertices[vertice1].add(vertice2)
+        self.vertices[vertice2].add(vertice1)
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertice('0')
@@ -27,5 +26,6 @@ graph.add_vertice('2')
 graph.add_vertice('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
+graph.add_edge('0', '7')
 print(graph.vertices)
 
