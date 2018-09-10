a5836a20455ef56f1baabe374860605590a0cbf8
Nicky Chen <nicky.chen0@gmail.com>
2018-09-10 13:30:55 -0700

    initial commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,27 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = []
+
+
+class Node:
+    def __init__(self):
+        self.neighbors = []
+    # O(1)
+
+    def addNeighbor(self, neighbor_node):
+        self.neighbors.append(neighbor_node)
+    # O(1)
+
+    def getNeighbors(self):
+        return self.neighbors
+    # O(n)
+
+    def isNeighbor(self, node):
+        return node in self.neighbors
