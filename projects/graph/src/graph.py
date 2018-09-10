463b8e1582bc1b808f438e4b0badf3d1e24f3ce9
Tyson Gushiken <tysonism@gmail.com>
2018-09-10 14:04:33 -0700

    Add exceptions to Graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,11 +12,22 @@ class Graph:
         if vertex not in self.vertices:
             self.vertices[vertex] = set()
         else:
-            return False
+            raise Exception("This vertex has already been added")
 
     def add_edge(self, start, end):
         if start in self.vertices and end in self.vertices:
             self.vertices[start].add(end)
             self.vertices[end].add(start)
         else:
-            return False
+            raise Exception("One of your vertices doesn't exist")
+
+# Test
+graph = Graph()  
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_vertex('3')
+print(graph.vertices)
