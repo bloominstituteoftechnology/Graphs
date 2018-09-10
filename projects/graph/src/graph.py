ddffd61f8e3f537fee9317816a3723f5ac94605c
sean chavez <seanchavez@seanchavezsMBP.home>
2018-09-10 15:20:21 -0700

    Complete Part 1

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,7 +6,9 @@ class Vertex:
     def __init__(self, key):
         self.id = key
         self.connectedTo = {}
-  
+    
+    def add_neighbor(self, nbr, weight = 0):
+        self.connectedTo[nbr] = weight
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -20,4 +22,19 @@ class Graph:
         self.vertices[key] = newVertex
         return newVertex
 
-    
+    def add_edge(self, f, t, cost = 0):
+        if f not in self.vertices:
+            nv = self.add_vertex(f)
+        if t not in self.vertices:
+            nv = self.add_vertex(t)
+        self.vertices[f].add_neighbor(self.vertices[t], cost)  
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
