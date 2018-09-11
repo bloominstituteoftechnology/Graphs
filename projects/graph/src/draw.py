3f696e7b80420c9d72c42c73fcdafd20348f4e05
Erin Meredith <ekmeredith@att.net>
2018-09-11 16:13:49 -0400

    completed part 2

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -2,22 +2,50 @@
 General drawing methods for graphs using Bokeh.
 """
 
+import math
+
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
-from graph import Graph
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle
+from bokeh.palettes import Spectral8
 from graph import graph
 
-class BokehGraph(Graph):
+class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        Graph.__init__(self)
+    def __init__(self, graph):
+        self.graph = graph
 
     def show(self):
-        output_file("graph.html")
-        p = figure(title="Graphs Part 2")
-        p.line(graph, line_width=2)
-        show(p)
+        node_indices = list(self.graph.vertices.keys())
+        x_value = [x for (x, y) in self.graph.vertices.items()]
+        y_value = [list(y) for (x, y) in self.graph.vertices.items()]
 
-BokehGraph.show(graph)
+        plot = figure(title="Graph Layout", x_range=(0, 4),
+                      y_range=(0, 4), tools="", toolbar_location=None)
+
+        graph = GraphRenderer()
+
+        graph.node_renderer.data_source.add(x_value, 'index')
+        graph.node_renderer.data_source.add(['red', 'blue', 'green', 'orange'], 'color')
+        graph.node_renderer.glyph = Circle(radius=0.1, fill_color='color')
+
+        print("\n x value: ", x_value)
+        print("\n y value: ", y_value)
+
+        graph.edge_renderer.data_source.data = dict(
+            start=x_value, end=y_value)
+
+        circ = [int(i) for i in x_value]
+        x = [i for i in circ]
+        y = [math.sin(i) for i in circ]
+
+        graph_layout = dict(zip(x_value, zip(x, y)))
+        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        plot.renderers.append(graph)
+
+        output_file('graph.html')
+        show(plot)
+
+bg = BokehGraph(graph)
+bg.show()
