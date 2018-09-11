fd9b32706a20b2a46ce713cf5a466047548ffca3
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-11 09:12:02 -0700

    node init

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,18 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+class Node:
+    def __init__(self):
+        self.neighbors = []
+
+    def addNeighbor(self, newNode):
+        self.neighbors.append(newNode)
+
+    def getNeighbors(self):
+        return self.neighbors
+
+    def isNeighbor(self, node):
+        return node in self.neighbors
+
+    
