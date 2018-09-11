4517e206d12a3d05e27a3e9c2f0103a168e9eaae
DirupT <iamcooled@gmail.com>
2018-09-11 18:08:06 -0500

    added dfs to graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -20,5 +20,26 @@ class Graph:
         if bidirectional:
             self.vertices[value].add(key)
 
+    def dfs(self, start):
+        stack = [start]
+        visited = set()
 
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.extend(self.vertices[vertex] - visited)
+
+        return visited
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+
+BokehGraph(graph).show()
+print(graph.dfs('0'))
 
