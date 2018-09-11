f6a421a904e9b992fd08db9fa623fcb57942c8fe
Adrian Adames <adrianadames@gmail.com>
2018-09-10 21:55:13 -0400

    Attempted implementing a graph using an adjency. Graph has very little functionality so far.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,17 +6,55 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+
+    def count(self):
+        #Return the number of vertices in the graph
+        return len(self.vertices)
+
+    def add_vertex(self, label):
+        v = self.Vertex(label)
+        # self.vertices[v] = set().append(v)
+        return v
+    
+    def add_edge(self, origin, destination):
+        # Insert and return new Edge from origin to destination 
+        e = self.Edge(origin, destination)
+        origin.edges.add_vertex(destination)
+        destination.edges.add_vertex(origin)
+
+
+    #Vertex Class
+    class Vertex:
+        def __init__(self, label):
+            self.label= label
+            self.edges = set()
+        
+        def __str__(self):
+            return "Vertex " + str(self.label)
+
+    #Edge Class
+    class Edge:
+        def __init__(self, origin, destination):
+            self.origin = origin
+            self.destination = destination
+        
+        def endpoints(self):
+            return (self.origin, self.destination)
 
 
 
 
-#need a way to store edges and store vertices values
 
-#one way is through objects and pointers (should be familiar with this because we've worked with linked lists)
-    # * each node is a pointer to an object and each edge is a pointer (like a linked list, each element POINTS to the next element in the linked list). "You're storing a list of pointers (pointers another way of saying edges, i guess.)"
-
-
+# We want something of the form 
+# {
+#     '0': {'1', '3'},
+#     '1': {'0'},
+#     '2': set(),
+#     '3': {'0'}
+# }
+# where the keys are the vertices and the associated values
+# (contained in a dictionary) are the edges
 
 
 
@@ -26,3 +64,15 @@ class Graph:
 # -The ColumnDataSource associated with the edge sub-renderer has two 
 # required columns: "start" and "end". These columns contain the node
 # indices of for the start and end of the edges.
+
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+
