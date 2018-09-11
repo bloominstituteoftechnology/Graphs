70a0ac21e49b8430895913dbe49d3027a1ebf045
Simon <Simon_Freelance@hotmail.com>
2018-09-11 16:14:43 -0400

    Implemented edges to connect to the right nodes

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -29,14 +29,19 @@ class BokehGraph:
         graph.node_renderer.data_source.add(['black'] * N, 'color')
         graph.node_renderer.glyph = Circle(radius=0.1, fill_color='color')
 
+      
+        # Draws the edges to neighboring nodes
+        start_indices = []
+        end_indices = []
+        for vertex in self.graph.vertices:
+            for edge_end in self.graph.vertices[vertex]:
+                start_indices.append(vertex.id)
+                end_indices.append(edge_end.id)
         graph.edge_renderer.data_source.data = dict(
-            start=[0]*N,
-            end=node_indices)
-
-        ### start of layout code
-        # circ = [i*2*math.pi/8 for i in node_indices]
-        # x = [math.cos(i) for i in circ]
-        # y = [math.sin(i) for i in circ]
+            start=start_indices,
+            end=end_indices)
+        
+        # Plots the node locations
         x = [vertex.x for vertex in self.graph.vertices]
         y = [vertex.y for vertex in self.graph.vertices]
         graph_layout = dict(zip(node_indices, zip(x, y)))
@@ -44,21 +49,32 @@ class BokehGraph:
 
         plot.renderers.append(graph)
 
+
+
         output_file('graph.html')
         show(plot)
 
 
 if __name__ == '__main__':
     graph = Graph()
+    # Middle Node
     node0 = Node(0,0, .2)
+    # Bottom Left
     node1 = Node(-0.5,-0.5, .2)
+    # Top Left
     node2 = Node(-0.5, 0.5, .2)
+    # Top Right
     node3 = Node(0.5,0.5, .2)
+    # Bottom Right
     node4 = Node(0.5,-0.5, .2)
     graph.add_vertex(node0)
     graph.add_vertex(node1)
     graph.add_vertex(node2)
     graph.add_vertex(node3)
     graph.add_vertex(node4)
+    # Middle to Top Right
+    graph.add_edge(node0, node1)
+    # Middle to Bottom Right
+    graph.add_edge(node0, node2)
     bokeh_graph = BokehGraph(graph)
     bokeh_graph.show()
--- projects/graph/src/node.py
+++ projects/graph/src/node.py
@@ -1,5 +1,9 @@
 class Node:
+    id = 0
     def __init__(self, x, y, radius):
         self.x = x
         self.y = y
         self.radius = radius
+        self.radius = radius
+        self.id = Node.id
+        Node.id += 1
