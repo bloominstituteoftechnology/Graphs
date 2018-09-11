5acc7b1d6c41cc2fedac5ab7f5497b6f3dd6e971
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-11 15:23:29 -0700

    makes a graph, does BFS

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -25,6 +25,36 @@ class Graph:
             self.vertices[vertex1].add(vertex2)
             self.vertices[vertex2].add(vertex1)
 
+    def BFS(self, start, target=None):
+        """Search the graph using BFS"""
+        queue = [start]
+        # if we make visited a set, can do set operations with the vertices
+        visited = set([start])
+
+        while queue:
+            current = queue.pop(0)
+            if current == target:
+                break
+            visited.add(current)
+            # Add to visit vertices to queue
+            queue.extend(self.vertices[current] - visited)
+            visited.update(self.vertices[current])
+
+        return visited
+
+
+class Vertex:
+    """Represent a vertex with an id, value, color, edges"""
+
+    def __init__(self, vertex_id, value, color="white"):
+        self.vertex_id = str(vertex_id)
+        self.value = value
+        self.color = color
+        self.edges = []
+
+    def __repr__(self):
+        return 'Vertex: ' + self.vertex_id
+
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
@@ -33,4 +63,5 @@ graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
-print(graph.vertices)
+# print(graph.vertices)
+print(graph.BFS('0'))
