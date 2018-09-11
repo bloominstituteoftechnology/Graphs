77b9db4c8e04f1220e698f86027d9e82076b9b9b
Mark Hong <m4rkh0ng@gmail.com>
2018-09-11 17:23:58 -0400

    made a bunch of nodes and edges for part 2

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,70 +1,174 @@
-import math
-
 """
 General drawing methods for graphs using Bokeh.
 """
+import math
 
 from bokeh.io import show, output_file
-from bokeh.plotting import figure, output_file, show
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource, Oval)
+from bokeh.plotting import figure
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource, Label, LabelSet
 from bokeh.palettes import Spectral8
-# from bokeh.charts import Scatter, output_file, show
+from graph import Graph
 
-N = 8
-node_indices = list(range(N))
 
-# dataframe=pandas.DataFrame(columns=["X", "Y"])
-# dataframe["X"]=[1,2,3,4,5]
-# dataframe["Y"]=[5,6,4,5,3]
+class BokehGraph:
+    """Class that takes a graph and exposes drawing methods."""
+    def __init__(self):
+        self.graph = Graph()
 
-# p=Scatter(dataframe, x="X", y="Y", title="Temperature Observations", xlabel="Day of observations", ylabel="Temperature")
+myGraph = BokehGraph()  # Instantiate your graph
+myGraph.graph.add_vertex('0')
+myGraph.graph.add_vertex('1')
+myGraph.graph.add_vertex('2')
+myGraph.graph.add_vertex('3')
+myGraph.graph.add_vertex('4')
+myGraph.graph.add_vertex('5')
+myGraph.graph.add_vertex('6')
+myGraph.graph.add_vertex('7')
+myGraph.graph.add_vertex('8')
+myGraph.graph.add_vertex('9')
+myGraph.graph.add_vertex('10')
+myGraph.graph.add_vertex('11')
+myGraph.graph.add_vertex('12')
+myGraph.graph.add_vertex('13')
+myGraph.graph.add_vertex('14')
+myGraph.graph.add_vertex('15')
+myGraph.graph.add_vertex('16')
+myGraph.graph.add_vertex('17')
+myGraph.graph.add_vertex('18')
+myGraph.graph.add_vertex('19')
+myGraph.graph.add_vertex('20')
+myGraph.graph.add_vertex('21')
+myGraph.graph.add_vertex('22')
+myGraph.graph.add_vertex('23')
+myGraph.graph.add_vertex('24')
+myGraph.graph.add_vertex('25')
+myGraph.graph.add_vertex('26')
+myGraph.graph.add_vertex('27')
+myGraph.graph.add_edge('0', '1')
+myGraph.graph.add_edge('0', '5')
+myGraph.graph.add_edge('2', '4')
+myGraph.graph.add_edge('3', '7')
+myGraph.graph.add_edge('2', '6')
+myGraph.graph.add_edge('6', '9')
+myGraph.graph.add_edge('5', '8')
+myGraph.graph.add_edge('7', '11')
+myGraph.graph.add_edge('8', '13')
+myGraph.graph.add_edge('10', '11')
+myGraph.graph.add_edge('10', '13')
+myGraph.graph.add_edge('11', '14')
+myGraph.graph.add_edge('12', '16')
+myGraph.graph.add_edge('13', '17')
+myGraph.graph.add_edge('14', '15')
+myGraph.graph.add_edge('15', '19')
+myGraph.graph.add_edge('16', '17')
+myGraph.graph.add_edge('17', '18')
+myGraph.graph.add_edge('17', '20')
+myGraph.graph.add_edge('17', '22')
+myGraph.graph.add_edge('18', '21')
+myGraph.graph.add_edge('20', '27')
+myGraph.graph.add_edge('21', '24')
+myGraph.graph.add_edge('22', '27')
+myGraph.graph.add_edge('23', '26')
+myGraph.graph.add_edge('24', '25')
+myGraph.graph.add_edge('25', '26')
+print(f"Hey, these are the vertices: {myGraph.graph.vertices}")
 
-# output_file("Scatter_charts.html")
+N = len(myGraph.graph.vertices)
+node_indices = list(myGraph.graph.vertices)
 
-# show(p)
 
-# class BokehGraph:
-#     """Class that takes a graph and exposes drawing methods."""
-#     def __init__(self):
-#         graph = GraphRenderer()
+plot = figure(title="graph layout demo", x_range=(-3,40), y_range=(-3,20), tools='', toolbar_location=None, plot_width=1450, plot_height=500)
 
-# p=figure(plot_width=500,plot_height=400,title="This is the title, now")
-p=figure(
-    plot_width=500,
-    plot_height=400,
-    title="Learning to Graph using Bokeh",
-    tools='',
-    toolbar_location=None
-)
+graph_renderer = GraphRenderer()
 
-graph = GraphRenderer()
+graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+graph_renderer.node_renderer.data_source.add(['teal', 'green', 'crimson', 'blue', 'cyan', 'orange', 'yellow', 'olive', 'gold', 'pink', 'red', 'white', 'magenta', 'aquamarine', 'turquoise', 'gray', 'salmon', 'lemonchiffon', 'khaki', 'chartreuse', 'mediumspringgreen', 'seagreen', 'darkcyan', 'hotpink', 'blueviolet', 'skyblue', 'navy', 'lightcoral'], 'color')
+# graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
 
-graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(['red'] * N, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+start_indices = []
+end_indices = []
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices
+for vertex in myGraph.graph.vertices:
+    for edge_end in myGraph.graph.vertices[vertex]:
+        start_indices.append(vertex)
+        end_indices.append(edge_end)
+print(start_indices)
+print(end_indices)
+
+graph_renderer.edge_renderer.data_source.data = dict(
+    start=start_indices,
+    end=end_indices
 )
 
 # layout
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+# circ = [i*2*math.pi/8 for i in node_indices]
+# x = [math.cos(i) for i in circ]
+# y = [math.sin(i) for i in circ]
+grid = [int(v) for v in myGraph.graph.vertices]
+x = [6 * (i // 4) for i in grid]
+y = [6 * (i % 4) for i in grid]
+# x = [i for i in grid]
+# y = [i ** 2 for i in grid]
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
-graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-plot = figure(title="graph layout demo", x_range=(-1.1,1.1), y_range=(-1.1,1.1), tools='', toolbar_location=None)
+# graph_layout = dict(zip(node_indices, zip(x, y)))
+# graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-plot.renderers.append(graph)
+plot.renderers.append(graph_renderer)
+
+labelSource = ColumnDataSource(data=dict(x=x, y=y, names=grid))
+labels = LabelSet(x='x', y='y', text= 'names', level='glyph', text_align='center', text_baseline='middle', source= labelSource, render_mode='canvas')
 
 # p.circle([1,2,3,4,5],[5,6,5,5,3],size=5,color="red")
 # p.triangle([1,2,3,4,5],[5,6,5,5,3],size=12,color="red")
-p.circle([1,2,3,4,5],[5,6,5,5,3],size=[25,50,12,6,100],color="red")
+# p.triangle([1,2,3,4,5],[5,6,5,5,3],size=[10,10,10,10,10],color="red")
 
-output_file("Scatter_plotting.html")
-show(p)
+plot.add_layout(labels)
+
+output_file("Day 2 Testing Graphs.html")
 show(plot)
+
+################## earlier attempts below
+# show(p)
+
+# N = len(graph.vertices)
+# node_indices = list(graph.vertices)
+
+# start_indices = []
+# end_indices = []
+
+# for vertex in graph.vertices:
+#     for edge in graph.vertices[vertex]:
+#         start_indices.append(vertex)
+#         end_indices.append(edge_end)
+
+# graph_renderer.layout_provider = StaticLayoutProvider(graph_layout)
+
+# labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center', text_baseline='middle', source=labelSource )
+
+
+# N = 8
+# node_indices = list(range(N))
+
+# # dataframe=pandas.DataFrame(columns=["X", "Y"])
+# # dataframe["X"]=[1,2,3,4,5]
+# # dataframe["Y"]=[5,6,4,5,3]
+
+# # p=Scatter(dataframe, x="X", y="Y", title="Temperature Observations", xlabel="Day of observations", ylabel="Temperature")
+
+# # output_file("Scatter_charts.html")
+
+# # show(p)
+
+
+# p=figure(plot_width=500,plot_height=400,title="This is the title, now")
+# p=figure(
+#     plot_width=500,
+#     plot_height=400,
+#     title="Learning to Graph using Bokeh",
+#     tools='',
+#     toolbar_location=None
+# )
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,24 +9,60 @@ Simple graph implementation compatible with BokehGraph class.
 # }     
 
 
-class Edge:
-    """Edges in the adjacency list are just a destination."""
-    # Using simple classes for illustrative purposes
-    # pylint: disable=too-few-public-methods
-    def __init__(self, destination):
-        self.destination = destination
+# class Edge:
+#     """Edges in the adjacency list are just a destination."""
+#     # Using simple classes for illustrative purposes
+#     # pylint: disable=too-few-public-methods
+#     def __init__(self, destination):
+#         self.destination = destination
 
 
-class Vertex:
-    """Vertices have a label and a set of edges."""
-    # pylint: disable=too-few-public-methods
-    def __init__(self, label):
-        self.label = label
-        self.edges = set()
+# class Vertex:
+#     """Vertices have a label and a set of edges."""
+#     # pylint: disable=too-few-public-methods
+#     def __init__(self, label):
+#         self.label = label
+#         self.edges = set()
 
+
+# class Graph:
+#     """The graph itself is simply a set of vertices."""
+#     # pylint: disable=too-few-public-methods
+#     def __init__(self):
+#         self.vertices = set()
 
 class Graph:
-    """The graph itself is simply a set of vertices."""
-    # pylint: disable=too-few-public-methods
-    def __init__(self):
-        self.vertices = set()
+  def __init__(self):
+    self.vertices = {}
+  def add_vertex(self, vertex_id):
+    self.vertices[vertex_id] = set()
+  def add_edge(self, v1, v2):
+    if v1 in self.vertices and v2 in self.vertices:
+      self.vertices[v1].add(v2)
+      self.vertices[v2].add(v1)
+    else:
+      raise IndexError("That vertex does not exist!")
+  def add_directed_edge(self, v1, v2):
+    if v1 in self.vertices and v2 in self.vertices:
+      self.vertices[v1].add(v2)
+    else:
+      raise IndexError("That vertex does not exist!")
+
+# class Graph2:
+#     def __init__(self):
+#         self.vertices = {}
+
+#     def add_vertex(self, vertex):
+#         if vertex not in self.vertices:
+#             self.vertices[vertex] = set()
+#         else:
+#             sys.exit("Vertex {} already exists.".format(vertex))
+
+#     def add_edge(self, vertex1, vertex2):
+#         if vertex1 not in self.vertices:
+#             sys.exit("No '{}' vertex!".format(vertex1))
+#         elif vertex2 not in self.vertices:
+#             sys.exit("No '{}' vertex!".format(vertex2))
+#         else:
+#             self.vertices[vertex1].add(vertex2)
+#             self.vertices[vertex2].add(vertex1)
