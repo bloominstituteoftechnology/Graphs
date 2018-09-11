33a3dd03a8f567b58b42a3ed62ed6d1ab545f87e
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-11 16:24:47 -0500

    BokehGraph.show()

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,3 +1,4 @@
+import math
 """
 General drawing methods for graphs using Bokeh.
 """
@@ -7,8 +8,51 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+from bokeh.palettes import Spectral8
+from graph import graph
+
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def show(self):
+        N = len(graph.vertices)
+        node_indices = list(graph.vertices)
+
+        plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10.1), y_range=(-1.1,10.1),
+                    tools='', toolbar_location=None)
+
+        graph_renderer = GraphRenderer()
+
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.data_source.add(Spectral8, 'color')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
+
+        start_indices = []
+        end_indices = []
+
+        for vertex in graph.vertices:
+            for edge_tail in graph.vertices[vertex]:
+                start_indices.append(vertex)
+                end_indices.append(edge_tail)
+
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=start_indices,
+            end=end_indices)
+
+        ### start of layout code
+        grid = [int(v) for v in graph.vertices]
+        x = [2 * (i//3) for i in grid]
+        y = [2 * (i%2) for i in grid]
+
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        plot.renderers.append(graph_renderer)
+
+        output_file('graph.html')
+        show(plot)
+
+bokeh_graph = BokehGraph(graph)
+bokeh_graph.show()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,8 +7,20 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    def add_vertex(self, vertex, edges=None):
+    def add_vertex(self, vertex, edges=tuple()):
         self.vertices[vertex] = set(edges)
     def add_edge(self, head, tail):
-        self.vertices[head].add(tail)
-        self.vertices[tail].add(head)
+        if head in self.vertices and tail in self.vertices:
+            self.vertices[head].add(tail)
+            self.vertices[tail].add(head)
+        else:
+            raise IndexError("One or more of these vertices does not exist!")
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
