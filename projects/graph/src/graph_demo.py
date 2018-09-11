2cb148cebb6c8f7e88a29a0c47ff2c085c05e4f4
JJ Ashcraft <jj@jjashcraft.com>
2018-09-11 18:23:39 -0400

    added random numbers to defaults

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -10,8 +10,21 @@ from draw import BokehGraph
 from graph import Graph
 from random import randint
 
-number_vertices = int(sys.argv[1])
-number_edges = int(sys.argv[2])
+input_cmd = list(sys.argv)
+
+print(len(input_cmd))
+print((input_cmd))
+
+if len(input_cmd) > 1:
+    number_vertices = int(sys.argv[1])
+else:
+    number_vertices = randint(4, 8)
+
+if len(input_cmd) > 2:
+    number_edges = int(sys.argv[2])
+else:
+    number_edges = randint(2, 4)
+
 
 if number_vertices < 0 or number_edges < 0:
     raise ValueError("number must be greater than zero")
@@ -31,10 +44,11 @@ unique_edges = set()
 while(len(unique_edges) < number_edges):
     v1 = randint(0, number_vertices)
     v2 = randint(0, number_vertices)
+    if v1 == v2:
+        continue
     if (v2, v1) not in unique_edges:
         unique_edges.add((v1, v2))
         graph.add_directed_edge(v1, v2)
-
 
 print('unique edges', len(unique_edges))
 print('unique edges', len(set(unique_edges)))
