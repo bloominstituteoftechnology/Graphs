f440fa9b3cf93342850184331fb6337de4002b1a
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-09-11 14:53:52 -0600

    Finshed drawing graph with BokehGraph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -16,12 +16,12 @@ class Graph:
         else:
             print('Those vertices do not exist')
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('4', '3')
-print(graph.vertices)
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# graph.add_edge('4', '3')
+# print(graph.vertices)
