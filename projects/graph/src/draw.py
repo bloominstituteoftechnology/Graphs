9cd7e6ba320792d21b6278b80d40bd5064bf9d56
JJ Ashcraft <jj@jjashcraft.com>
2018-09-11 16:12:19 -0400

    draw.py completed for part 2

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,36 +1,66 @@
-# """
-# General drawing methods for graphs using Bokeh.
-# """
 
-# from bokeh.io import show, output_file
-# from bokeh.plotting import figure
-# from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-#                           ColumnDataSource)
-
-
-# class BokehGraph:
-#     """Class that takes a graph and exposes drawing methods."""
-#     def __init__(self):
-#         pass  # TODO
 
 import math
+
+from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, Oval
-from bokeh.palettes import Spectral8
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+# from bokeh.palettes import Spectral8
+from graph import Graph
 
-N = 8
-node_indices = list(range(N))
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
 
-plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
-              tools="", toolbar_location=None)
 
-graph = GraphRenderer()
 
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color="fill_color")
-graph.node_renderer.data_source.data = dict(
-    index=node_indices,
-    fill_color=Spectral8)
+node_indices = list(graph.vertices)
+l = len(graph.vertices)
+print(node_indices)
+plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10), y_range=(-1.1,10),
+              tools='', toolbar_location=None)
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+graph_renderer = GraphRenderer()  
+
+graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+graph_renderer.node_renderer.data_source.add(['red']*l, 'color')
+graph_renderer.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+
+start_i = []
+end_i = []
+
+for vertex in graph.vertices:
+  for end_point in graph.vertices[vertex]:
+    start_i.append(vertex)
+    end_i.append(end_point)
+
+print(start_i)
+print(end_i)
+
+
+graph_renderer.edge_renderer.data_source.data = dict(
+    start=start_i,
+    end=end_i)
+
+### start of layout code
+square = [int(v) for v in graph.vertices]
+x = [2 * (i//3) for i in square]
+# x = [i for i in circ]
+# y = [i for i in circ]
+y = [2 * (i % 3) for i in square]
+
+print(x, y)
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+print(graph_layout)
+graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot.renderers.append(graph_renderer)
+
+output_file('graph.html')
+show(plot)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,20 +12,30 @@ class Graph:
       self.vertices[vertex] = set()
     def add_edge(self, vertex1, vertex2):
       if not vertex1 in self.vertices:
-        print(f'vertex {vertex1} does not exist, create missing vertex prior to creating edge.')
+        raise IndexError(f'vertex {vertex1} does not exist, create missing vertex prior to creating edge.')
       elif not vertex2 in self.vertices:
-        print(f'vertex {vertex2} does not exist, create missing vertex prior to creating edge.')
+        raise IndexError(f'vertex {vertex2} does not exist, create missing vertex prior to creating edge.')
       else:
         self.vertices[vertex1].add(vertex2)
         self.vertices[vertex2].add(vertex1)
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '7')
-print(graph.vertices)
+
+
+
+
+
+
+        
+
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# graph.add_edge('0', '7')
+# print(graph.vertices)
+
+
 
