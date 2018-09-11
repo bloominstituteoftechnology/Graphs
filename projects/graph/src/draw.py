2f0a6c548415207a736cad169705f8dd0911e43b
Austin Merando <ajmerando1@yahoo.com>
2018-09-11 15:41:35 -0700

    bokeh graph implemented, dft and bft started

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,72 +7,65 @@ from bokeh.palettes import Spectral8
 from graph import Graph
 
 
+class BokehGraph:
+    def __init__(self):
+        self.graph = Graph()
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_vertex('4')
-graph.add_vertex('5')
-graph.add_vertex('6')
-graph.add_vertex('7')
-graph.add_vertex('8')
-graph.add_vertex('9')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
+    def show(self):
+        node_indices = list(self.graph.vertices)
+        plot = figure(title='Graph Layout Demonstration', x_range=(-1.1, 10.1), y_range=(-1.1, 10.1),
+                      tools='', toolbar_location=None)
 
-N = len(graph.vertices)
-node_indices = list(graph.vertices)
+        graph_renderer = GraphRenderer()
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.data_source.add(
+        Spectral8, 'color')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
 
+        start_indices = []
+        end_indices = []
 
+        for vertex in self.graph.vertices:
+            for edge_end in self.graph.vertices[vertex]:
+                start_indices.append(vertex)
+                end_indices.append(edge_end)
+                graph_renderer.edge_renderer.data_source.data = dict(
+        start=start_indices,
+        end=end_indices)
 
-
-
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10.1), y_range=(-1.1,10.1),
-              tools='', toolbar_location=None)
-
-graph_renderer = GraphRenderer()
-
-graph_renderer.node_renderer.data_source.add(node_indices, 'index')
-graph_renderer.node_renderer.data_source.add(['red', 'blue', 'green', 'orange'], 'color')
-graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
-
-start_indices = []
-end_indices = []
-
-for vertex in graph.vertices:
-    for edge_end in graph.vertices[vertex]:
-        start_indices.append(vertex)
-        end_indices.append(edge_end)
-print(start_indices)
-print(end_indices)
+        # start of layout code
+        grid = [int(v) for v in self.graph.vertices]
+        x = [2 * (i // 3) for i in grid]
+        y = [2 * (i % 3) for i in grid]
 
 
-graph_renderer.edge_renderer.data_source.data = dict(
-    start=start_indices,
-    end=end_indices)
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(
+            graph_layout=graph_layout)
 
-### start of layout code
-grid = [int(v) for v in graph.vertices]
-x = [2 * (i // 3) for i in grid]
-y = [2 * (i % 3) for i in grid]
-# x = [i for i in grid]
-# y = [i ** 2 for i in grid]
+        plot.renderers.append(graph_renderer)
 
-graph_layout = dict(zip(node_indices, zip(x, y)))
-graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+        labelSource = ColumnDataSource(data=dict(x=x, y=y, names=grid))
+        labels = LabelSet(x='x', y='y', text='names', level='glyph',
+                        text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas')
 
-plot.renderers.append(graph_renderer)
+        plot.add_layout(labels)
 
-labelSource = ColumnDataSource(data=dict(x=x, y=y, names=grid))
-labels = LabelSet(x='x', y='y', text='names', level='glyph',
-             text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas')
+        output_file('graph.html')
+        show(plot)
 
 
-plot.add_layout(labels)
-
-
-output_file('graph.html')
-show(plot)
+bg = BokehGraph()  # Instantiate your graph
+bg.graph.add_vertex('0')
+bg.graph.add_vertex('1')
+bg.graph.add_vertex('2')
+bg.graph.add_vertex('3')
+bg.graph.add_vertex('4')
+bg.graph.add_vertex('5')
+bg.graph.add_vertex('6')
+bg.graph.add_vertex('7')
+bg.graph.add_vertex('8')
+bg.graph.add_vertex('9')
+bg.graph.add_edge('0', '1')
+bg.graph.add_edge('0', '3')
+bg.show()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,20 +1,42 @@
-"""
-Simple graph implementation compatible with BokehGraph class.
-"""
-
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = set()
+    def add_vertex(self, node):
+        self.vertices[node] = set()
+        return self.vertices
 
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-            self.vertices[v2].add(v1)
+    def add_edge(self, from_node, to_node):
+        self.vertices[from_node].add(to_node)
+        self.vertices[to_node].add(from_node)
+        return
+
+    def add_directed_edge(self, ver1, ver2):
+        if ver1 in self.vertices:
+            self.vertices[ver1].add(ver2)
         else:
-            raise IndexError("That vertex does not exist!")
+            raise IndexError(
+                'That vertex value is not available. please add it first')
+
+    def dft(adjList, node_id, visited):
+        visited.append(node_id)
+        for child_node in adjList[node_id]:
+            if child_node not in visited:
+                dft(adjList, child_node, visited)
+
+    def bft(adjList, node_id):
+        frontier = []
+        frontier.append(node_id)
+        visited = []
+        while len(frontier > 0):
+            n = frontier.pop(0)
+            if n not in visited:
+                print(n)
+                visited.append(n)
+                for next_node in adjList[n]:
+                    frontier.append(next_node)
+
+    def __str__(self):
+        return f"{self.vertices}"
