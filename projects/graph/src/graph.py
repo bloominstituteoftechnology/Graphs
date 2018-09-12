409063ffd0e089aac89a67bbe400a4e0e81e8a04
Vu Cao <cdhvu@yahoo.com>
2018-09-12 09:40:31 -0700

    Complete part 3 and 4

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,12 +5,10 @@ import random
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self, vertices=4, edges=6, directed=False, probability = None):
+    def __init__(self, vertices, probability):
         self.vertices = {}
-        self.N = vertices
-        self.edges = edges
-        self.directed = directed
-        self.edge_prob = probability
+        self.total_v = int(vertices)
+        self.prob = float(probability)
 
     def add_vertex(self, label):
         #check if an object with label already exists
@@ -38,19 +36,61 @@ class Graph:
         self.vertices[end].add(start)
 
     def generate_vertices(self):
-        for i in range(self.N):
+        for i in range(self.total_v):
             label = 'v'+ str(i)
             self.add_vertex(label)
     
     def generate_edges(self):
-        count = 0
+        count_chance = 0
+        count_here = 0
         vertices = list(self.vertices.keys())
-
         for v in vertices:
             for k in [i for i in vertices if i != v]:
                 chance = random.random()
-                if chance < 0.5:
-                    self.add_undirected_edge(v, k)
-                    count+=2
-                    if(count==self.edges):
-                        return
+                print(chance, self.prob)
+                count_chance += 1 if chance <= self.prob else 0
+                if chance < self.prob and v not in self.vertices[k]:
+                    count_here += 1
+                    self.add_undirected_edge(v, k) 
+        print(count_chance, count_here)
+        # edges = []
+        # while True:
+        #     start = vertices[random.randint(0, len(vertices)-1)]
+        #     end = vertices[random.randint(0, len(vertices)-1)]
+        #     chance = random.random()
+        #     if start!=end and [start, end] not in edges and [end, start] not in edges and chance <= self.prob:
+        #         edges.append([start, end])
+        #         if len(edges) == self.total_e:
+        #             break
+
+        # for edge in edges:
+        #     if not self.directed:
+        #         self.add_undirected_edge(edge[0], edge[1])
+
+    def breadth_first_for_each(self, start):
+        queue = [] # setup a que to check nodes
+        queue.append(start) # first/root node
+        visited = []
+        while len(queue) > 0:
+            current = queue.pop(0) # first in que pulled out
+            visited.append(current) #push into visited
+            for edge in self.vertices[current]:  #look at connections,  
+                if edge not in visited and edge not in queue:    # if the connected node isn't visited,
+                    queue.append(edge)   # then enqueue it
+        print(visited)
+
+    
+    def depth_first_for_each(self, start):
+        stack = [] # setup a que to check nodes
+        stack.append(start) # first/root node
+        visited = []
+        while len(stack) > 0:
+            current = stack.pop() # first in que pulled out
+            visited.append(current) #push into visited
+            for edge in self.vertices[current]:  #look at connections,  
+                if edge not in visited and edge not in stack:    # if the connected node isn't visited,
+                    stack.append(edge)   # then enqueue it
+        print(visited)
+                
+            
+        
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -6,21 +6,17 @@ Demonstration of Graph and BokehGraph functionality.
 
 from sys import argv
 from graph import Graph
-# from draw import BokehGraph
+from draw import BokehGraph
 
 
-def main(vertexes = 4, edges = 6, directed=False, layout = 'circle', colors='orange', output=None, edge_prob=None):
-    print('vertexes: ', vertexes)
-    print('edges: ', edges)
-    print('directed: ', directed)
-    print('layout: ', layout)
-    print('colors: ', colors)
-    print('output: ', output)
-    print('edge_prob: ', edge_prob)
+def main(vertices = 4, edge_prob=0.5, layout = 'circle', colors='orange', output=None):
+    graph = Graph(vertices, edge_prob)
+    graph.generate_vertices()
+    graph.generate_edges()
 
+    bokeh_graph = BokehGraph(graph)
+    bokeh_graph.show()
 
 if __name__ == '__main__':
     # TODO - parse argv
-
-    print(*argv[1:])
     main(*argv[1:])
