814c58356a6d37bd4fd476a9f7fbb891af3c0c81
Simon <Simon_Freelance@hotmail.com>
2018-09-11 20:16:50 -0400

    Directional arrows have been implemented

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -8,7 +8,8 @@ from graph import Graph
 from node import Node
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+
+from bokeh.models import (GraphRenderer, Arrow, OpenHead, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
 
@@ -27,22 +28,30 @@ class BokehGraph:
 
         graph = GraphRenderer()
         graph.node_renderer.data_source.add(node_indices, 'index')
-        r = lambda: random.randint(0,255)
+        r = lambda: random.randint(50,255)
         graph.node_renderer.data_source.add(['#%02X%02X%02X' % (r(),r(),r()) for i in range(N)], 'color')
         graph.node_renderer.glyph = Circle(radius=0.1, fill_color='color')
 
-      
+
         # Draws the edges to neighboring nodes
         start_indices = []
         end_indices = []
+        make_arrow = []
         for vertex in self.graph.vertices:
             for edge_end in self.graph.vertices[vertex]:
-                start_indices.append(vertex.id)
-                end_indices.append(edge_end.id)
+                # Bi-directional
+                if vertex in self.graph.vertices[edge_end]:
+                    start_indices.append(vertex.id)
+                    end_indices.append(edge_end.id)
+                else:
+                    # Directional Edges
+                    plot.add_layout(Arrow(end=OpenHead(line_color="firebrick", line_width=4),x_start=vertex.x, y_start=vertex.y, x_end=edge_end.x, y_end=edge_end.y))
+        
+        # Renders the Bi-directional edges
         graph.edge_renderer.data_source.data = dict(
             start=start_indices,
             end=end_indices)
-        
+
         # Plots the node locations
         x = [vertex.x for vertex in self.graph.vertices]
         y = [vertex.y for vertex in self.graph.vertices]
@@ -51,7 +60,7 @@ class BokehGraph:
 
         identities = [vertex.id for vertex in self.graph.vertices]
         labelSource = ColumnDataSource(data=dict(x=x, y=y, names=identities))
-        labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas')
+        labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas', text_color='black')
         plot.renderers.append(graph)
 
         plot.add_layout(labels)
@@ -82,7 +91,7 @@ if __name__ == '__main__':
     graph.add_vertex(node4)
     graph.add_edge(node1, node2)
     graph.add_edge(node2, node3)
-    graph.add_edge(node3, node4)
-    graph.add_edge(node4, node1)
+    graph.add_directional_edge(node3, node4)
+    graph.add_directional_edge(node4, node1)
     bokeh_graph = BokehGraph(graph)
     bokeh_graph.show()
