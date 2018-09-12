ef9eb6e42f56ba1ff19f7f22d81a81f38084fd33
MacBook <LonnieDavis@MacBookPros-MacBook-Pro.local>
2018-09-12 17:31:20 -0500

    Adding BokehGraph Class

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -8,76 +8,52 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, Label, La
                           ColumnDataSource, GraphRenderer, Oval)
 from bokeh.palettes import Spectral8
 
-class BokehGraph():
-    def __init__(self):
+class BokehGraph:
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        N = len(graph.vertices)
+        node_indices = list(graph.vertices)
 
-graph = Graph()  
-graph.add_vertex(0)
-graph.add_vertex(1)
-graph.add_vertex(2)
-graph.add_vertex(3)
-graph.add_vertex(4)
-graph.add_vertex(5)
-graph.add_vertex(6)
-graph.add_vertex(7)
-graph.add_vertex(8)
-graph.add_vertex(9)
-graph.add_edge(0, 1)
-graph.add_edge(0, 3)
-graph.add_edge(1, 2)
-graph.add_edge(2, 5)
-graph.add_edge(2, 4)
-graph.add_edge(4, 9)
-graph.add_edge(3, 7)
-graph.add_edge(3, 6)
+        plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10.1), y_range=(-1.1,10.1),
+                    tools='', toolbar_location=None)
 
+        graph_renderer = GraphRenderer()
 
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.data_source.add([graph.vertices[vertex_id].color for vertex_id in graph.vertices], 'color')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
 
+        start_indices = []
+        end_indices = []
 
+        for vertex_id in graph.vertices:
+            for edge_end in graph.vertices[vertex_id].edges:
+                start_indices.append(vertex_id)
+                end_indices.append(edge_end)
 
 
 
-N = len(graph.vertices)
-node_indices = list(graph.vertices)
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=start_indices,
+            end=end_indices)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10.1), y_range=(-1.1,10.1),
-              tools='', toolbar_location=None)
+        ### start of layout code
 
-graph_renderer = GraphRenderer()
+        x = [graph.vertices[vertex_id].x for vertex_id in graph.vertices]
+        y = [graph.vertices[vertex_id].y for vertex_id in graph.vertices]
 
-graph_renderer.node_renderer.data_source.add(node_indices, 'index')
-graph_renderer.node_renderer.data_source.add([graph.vertices[vertex_id].color for vertex_id in graph.vertices], 'color')
-graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-start_indices = []
-end_indices = []
+        plot.renderers.append(graph_renderer)
 
-for vertex_id in graph.vertices:
-    for edge_end in graph.vertices[vertex_id].edges:
-        start_indices.append(vertex_id)
-        end_indices.append(edge_end)
+        labelSource = ColumnDataSource(data=dict(x=x, y=y, names=[graph.vertices[vertex_id].value for vertex_id in graph.vertices]))
+        labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center',
+        text_baseline='middle', source=labelSource, render_mode='canvas')
 
+        plot.add_layout(labels)
 
-
-graph_renderer.edge_renderer.data_source.data = dict(
-    start=start_indices,
-    end=end_indices)
-
-### start of layout code
-
-x = [graph.vertices[vertex_id].x for vertex_id in graph.vertices]
-y = [graph.vertices[vertex_id].y for vertex_id in graph.vertices]
-
-graph_layout = dict(zip(node_indices, zip(x, y)))
-graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
-
-plot.renderers.append(graph_renderer)
-
-labelSource = ColumnDataSource(data=dict(x=x, y=y, names=[graph.vertices[vertex_id].value for vertex_id in graph.vertices]))
-labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center',
-text_baseline='middle', source=labelSource, render_mode='canvas')
-
-plot.add_layout(labels)
-
-output_file('graph.html')
-show(plot)
+        output_file('graph.html')
+        show(plot)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -12,7 +12,29 @@ from graph import Graph
 
 
 def main(num_vertices=8, num_edges=8):
-    graph = Graph()
+    graph = Graph()  
+    graph.add_vertex(0)
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_vertex(8)
+    graph.add_vertex(9)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 5)
+    graph.add_edge(2, 4)
+    graph.add_edge(4, 9)
+    graph.add_edge(3, 7)
+    graph.add_edge(3, 6)
+
+    bokeh_graph = BokehGraph(graph)
+    bokeh_graph.draw()
+
     
     
 
