f5353c8e90e34352e51f0d3155125492d899571d
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-12 15:04:22 -0700

    graph displayed

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -36,10 +36,10 @@ class BokehGraph:
         graph_renderer = GraphRenderer()
 
         graph_renderer.node_renderer.data_source.add(list(self.graph.vertices.keys()), 'index')
-        # graph_renderer.node_renderer.data_source.add(self._get_random_colors(), 'color')        
+        graph_renderer.node_renderer.data_source.add(self._get_random_colors(), 'color')        
         graph_renderer.node_renderer.glyph = Circle(size=circle_size, fill_color='color')
-        # graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
-        # self.randomize()
+        graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
+        self.randomize()
 
         for i in range(len(graph_renderer.edge_renderer.data_source.data["start"])):
             self.plot.add_layout(
@@ -63,7 +63,7 @@ class BokehGraph:
 
         graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.pos)
         self.plot.renderers.append(graph_renderer)
-        # self._setup_labels()
+        self._setup_labels()
     
     def _get_random_colors(self, num_colors=None):
         colors = []
@@ -85,6 +85,25 @@ class BokehGraph:
                 checked.add(vertex)
 
         return dict(start=start_indices, end=end_indices)
+
+    def _setup_labels(self):
+        label_data = {'x': [], 'y': [], 'names': []}
+        for vertex, position in self.pos.items():
+            label_data['x'].append(position[0])
+            label_data['y'].append(position[1])
+            label_data['names'].append(str(vertex))
+        label_source = ColumnDataSource(label_data)
+        labels = LabelSet(x='x', y='y', text='names', level='glyph',
+                          text_align='center', text_baseline='middle',
+                          source=label_source, render_mode='canvas')
+        self.plot.add_layout(labels)
+
+    def randomize(self):
+        """Randomize vertex positions."""
+        for vertex in self.graph.vertices:
+            # TODO make bounds and random draws less hacky
+            self.pos[vertex] = (0.56 + random() * (self.width - 1.39),
+                                0.56 + random() * (self.height - 1.39))
 
 graph = Graph()  
 graph.add_vertex('0')
