56b3274a8e933cc66f2f892a650068f1643c8a27
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-09-12 16:02:21 -0400

    MVP done

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -8,11 +8,12 @@ from graph import Graph
 from draw import BokehGraph
 from random import randint
 
-def main(vertices=8, edges=8): 
+
+def main(vertices=8, edges=8):
     graph = Graph()
     vertices = vertices
     edges = edges
-    
+
     for n in range(0, vertices):
         graph.add_vertex(n)
 
@@ -20,15 +21,17 @@ def main(vertices=8, edges=8):
     while len(edgesOut) < edges:
         starting = randint(0, vertices - 1)
         ending = randint(0, vertices - 1)
-        if starting != ending and (starting, ending) not in edgesOut and (ending, starting) not in edgesOut:
-            edgesOut.append((starting,ending))
+        if starting != ending and (starting, ending) \
+                not in edgesOut and (ending, starting) not in edgesOut:
+            edgesOut.append((starting, ending))
     print("Edges Out:", edgesOut)
     for edge in edgesOut:
-        graph.add_edge(edge[0], edge[1])             
-    
+        graph.add_edge(edge[0], edge[1])
+
     b_graph = BokehGraph(graph)
-    
+
     b_graph.show()
+
 
 if __name__ == '__main__':
     vertices, edges = int(sys.argv[1]), int(sys.argv[2])
