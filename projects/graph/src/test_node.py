2e7d371c5e04e9de04ce2014fe9d07520ce16ca1
Simon <Simon_Freelance@hotmail.com>
2018-09-12 15:50:23 -0400

    Created tearDown method to keep tests from affecting each other

--- projects/graph/src/test_node.py
+++ projects/graph/src/test_node.py
@@ -6,15 +6,17 @@ class NodeTests(unittest.TestCase):
     def setUp(self):
         self.node = Node(5,4,.2)  # Instantiate your node
 
+    def tearDown(self):
+        Node.id = 0
+
     def test_instantiation(self):
         self.assertEqual(self.node.x, 5)
         self.assertEqual(self.node.y, 4)
         self.assertEqual(self.node.radius, .2)
         self.assertEqual(self.node.id, 0)
         self.node2 = Node(5,4,.2)  # Instantiate your node
-        self.assertEqual(self.node.id, 0)
         self.assertEqual(self.node2.id, 1)
-
+    
     def test_color_methods(self):
         self.assertEqual(self.node.color, 'white')
         self.node.assign_random_color()
