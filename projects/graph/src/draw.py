37e6762538619404f78d676f7d0a350927f8f3a8
snowcoding <vimalshah77@gmail.com>
2018-09-11 17:39:24 -0700

    Random Graph

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -5,43 +5,64 @@ import math
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet, Oval, 
-                        ColumnDataSource)
-from bokeh.palettes import Spectral8
+                        ColumnDataSource, MultiLine)
+from bokeh.models.graphs import NodesAndLinkedEdges, EdgesAndLinkedNodes
+from bokeh.palettes import Spectral8, Spectral4, inferno
 
-from graph import Graph
+# from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self, graph):
         self.graph = graph
 
-    def plot(self):
+    def render(self):
         N = len(self.graph.vertices)
 
         # Get ref to all node/vertex indexes from the keys of vertices
         node_indices = list(self.graph.vertices.keys())
         
-        # Get ref to all lists of each connected node
-        connected_nodes_lists = [list(v) for (k,v) in self.graph.vertices.items()]
-
         # Create a plot / figure
-        plot = figure(title='Graph Layout Demonstration', x_range=(-1,5), y_range=(-1,5),
+        plot = figure(title='Graph Layout Demonstration', 
+                    x_range=(-1,math.sqrt(N)+1), 
+                    y_range=(-1,math.sqrt(N)+1),
                     tools='', toolbar_location=None)
 
         # Add Data (Node/Edge) Sources
         graph = GraphRenderer()
         graph.node_renderer.data_source.add(node_indices, 'index')
-        graph.node_renderer.data_source.add(Spectral8, 'color')
-        graph.node_renderer.glyph = Circle(radius=0.2, fill_color='color')
+        graph.node_renderer.data_source.add(inferno(N), 'color')
+        graph.node_renderer.glyph = Circle(radius=.3, fill_color='color')
+
+        # Create the edge renderer data
+        start_indices = []
+        end_indices = []
+
+        for vertex in self.graph.vertices:
+            for edge_end in self.graph.vertices[vertex]:
+                start_indices.append(vertex)
+                end_indices.append(edge_end)
+        
+        print(start_indices)
+        print(end_indices)
+
         graph.edge_renderer.data_source.data = dict(
-            start=node_indices,
-            end=connected_nodes_lists)
+            start=start_indices,
+            end=end_indices)
+
+        # Sibhat's method, all edges don't get drawn?
+        # Get ref to all lists of each connected node
+        # connected_nodes_lists = [list(v) for (k,v) in self.graph.vertices.items()]
+        
+        # graph.edge_renderer.data_source.data = dict(
+        #     start=node_indices,
+        #     end=connected_nodes_lists)
 
         #Layout code
         # Grid or Square Layout
         grid = [int(v) for v in self.graph.vertices]
-        x = [(i // 3) for i in grid]
-        y = [(i % 3) for i in grid]
+        x = [(i // math.ceil(math.sqrt(N))) for i in grid]
+        y = [(i % math.ceil(math.sqrt(N))) for i in grid]
         graph_layout = dict(zip(node_indices, zip(x, y)))
         graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
@@ -63,14 +84,14 @@ class BokehGraph:
         show(plot)
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(f"Graph vertices: {graph.vertices}")
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# print(f"Graph vertices: {graph.vertices}")
 
-bg = BokehGraph(graph)
-bg.plot()
+# bg = BokehGraph(graph)
+# bg.render()
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,12 +5,37 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
+import random
 
+def main(v,e):
 
-def main():
-    pass  # TODO
+    graph = Graph()  # Instantiate your graph
+    for v in range(0,int(v)):
+        graph.add_vertex(str(v))
+    print(graph.vertices)
+    for e in range(1,int(e)):
+        verts = list(range(v+1))
+        random.shuffle(verts)
+        print(f"Shuffled verts: {verts}")
+        # rand_par = shuffled_verts[0]
+        # rand_chi = shuffled_verts[1]
 
+        # print(f"Rand Parent: {rand_par}")
+        # print(f"Rand Child: {rand_chi}")
+        graph.add_edge(str(verts[0]),str(verts[1]))
+    print(graph.vertices)
+    bg = BokehGraph(graph)
+    bg.render()
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+    #Parse the argv
+    if len(argv) == 1:
+        raise ValueError("Please enter the number of vertices and edges")
+    elif len(argv) == 2:
+        raise ValueError("Please enter the number of edges as the second argument")
+    elif len(argv) > 3:
+        raise ValueError("Too many arguments, expecting only two, vertices and edges")
+    else:
+        main(argv[1],argv[2])
