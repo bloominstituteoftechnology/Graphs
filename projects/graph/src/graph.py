1b5b1022b13bfb5b541e07cfebe9926a95a2ff52
raymondgrc <raymond_grc@yahoo.com>
2018-09-12 14:41:10 -0500

    Revise graph, to b non compat with Bokeh

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,36 +3,30 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
-# class Graph:
-#     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-#     def __init__(self):
-#         self.vertices = {}
-#     def add_vertex(self, vertix):
-#         self.vertices[str(vertix)] = set()
-#     def add_edge(self, start, end):
-#         self.vertices[start].add(end)
-# graph = Graph()  # Instantiate your graph
-# graph.add_vertex('0')
-# graph.add_vertex('1')
-# graph.add_vertex('2')
-# graph.add_vertex('3')
-# graph.add_edge('0', '1')
-# graph.add_edge('0', '3')
-# print(graph.vertices)
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = Vertex(vertex_id)
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)
-            self.vertices[v2].edges.add(v1)
+    def add_vertex(self, vertix):
+        self.vertices[str(vertix)] = set()
+    def add_edge(self, start, end):
+        start = str(start)
+        end = str(end)
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertex does not exist!')
+        else:
+            self.vertices[start].add(end)
+            self.vertices[end].add(start)
+    def add_directed_edge(self, start, end):
+        if start in self.vertices and end in self.vertices:
+            self.vertices[start].edges.add(end)
         else:
             raise IndexError("That vertex does not exist!")
-    def add_directed_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)
-        else:
-            raise IndexError("That vertex does not exist!")
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
