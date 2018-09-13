98de882aedb649c14309bcc7359b710cd543ceae
cmok4290 <cmok4290@gmail.com>
2018-09-13 16:36:55 -0400

    added vertex class, fixed bfs and dfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -32,6 +32,12 @@ class Stack:
     def size(self):
         return len(self.stack)
 
+class Vertex:
+    def __init__(self, v_id, value=None):
+        self.id = v_id
+        self.edges = set()
+        self.value = v_id if value is None else value
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -39,53 +45,81 @@ class Graph:
     
     def add_vertex(self, vertex):
         if vertex not in self.vertices:
-            self.vertices[vertex] = set()
+            self.vertices[vertex] = Vertex(vertex) 
         else:
             raise ValueError(f'Vertex {vertex} already exists')
     
     def add_edge(self, vertex_1, vertex_2):
-        if vertex_2 in self.vertices:
-            if vertex_1 not in self.vertices[vertex_2]:
-                self.vertices[vertex_1].add(vertex_2)
-                self.vertices[vertex_2].add(vertex_1)
+        if vertex_1 in self.vertices:
+            if vertex_2 in self.vertices:
+                self.vertices[vertex_1].edges.add(vertex_2)
+                self.vertices[vertex_2].edges.add(vertex_1)
             else:
                 raise ValueError(f'Edge {vertex_1} already exists with Vertex {vertex_2}')
         else:
             raise ValueError(f'Vertex {vertex_2} does not exist')
 
     def bfs(self, start, target=None):
-        visited, queue = set(), [str(start)]
+        # init queue
+        q = Queue()
 
-        while queue:
-            vertex = queue.pop(0)
+        # add start value
+        q.enqueue(start)
 
-            if vertex == target:
-                break
+        # init visited
+        visited = []
 
-            print(f'visited: {vertex}')
+        # loop over queue size
+        while q.size() > 0:
+
+            # declare current value
+            vertex = q.dequeue()
+
+            # check visited elements
             if vertex not in visited:
-                visited.add(vertex)
-                queue.extend(self.vertices[vertex] - visited)
-                # visited.update(self.vertices[vertex])
-        
-            return visited
-        
+
+                # print value
+                print(self.vertices[vertex].value)
+
+                # add to visited
+                visited.append(vertex)
+
+                # loop for children nodes
+                for next_vertex in self.vertices[vertex].edges:
+
+                    # add next value to queue
+                    q.enqueue(next_vertex)
+
     def dfs(self, start, target=None):
-        visited, stack = set(), [str(start)]
+        # init stack
+        s = Stack()
 
-        while stack:
-            vertex = stack.pop()
+        # add start value
+        s.push(start)
 
-            if vertex == target:
-                break
+        # init visited
+        visited = []
 
-            print(f'visited: {vertex}')
+        # loop over stack size
+        while s.size() > 0:
+
+            # declare current value
+            vertex = s.pop()
+
+            # check visited elements
             if vertex not in visited:
-                visited.add(vertex)
-                stack.extend(self.vertices[vertex] - visited)
-                # visited.update(self.vertices[vertex])
 
-            return visited
+                # print value
+                print(self.vertices[vertex].value)
+
+                # add to visited
+                visited.append(vertex)
+
+                # loop for children nodes
+                for next_vertex in self.vertices[vertex].edges:
+             
+                    # add next value to stack
+                    s.push(next_vertex)
 
     def search(self, method, start, target=None):
         if method == 'dfs':
@@ -102,7 +136,7 @@ demo_g.add_edge('0', '1')
 demo_g.add_edge('0', '3')
 demo_g.add_edge('1', '2')
 # for vertex in demo_g.vertices:
-#     print(f'{vertex}: {demo_g.vertices[vertex]}')
+#     print(f'{vertex}: {demo_g.vertices[vertex].edges}')
 
 # print(demo_g.bfs('0'))
 # print(demo_g.bfs('1'))
@@ -113,5 +147,5 @@ demo_g.add_edge('1', '2')
 # print(demo_g.dfs('2'))
 # print(demo_g.dfs('3'))
 
-print(demo_g.search(method=None, start='0', target='3'))
-print(demo_g.search(method='dfs', start='0', target='3'))
+# print(demo_g.search(method=None, start='0', target='3'))
+# print(demo_g.search(method='dfs', start='0', target='3'))
