4291d93c256e3ec9d287c2284731a5d4a39df884
Simon <Simon_Freelance@hotmail.com>
2018-09-13 10:03:20 -0400

    Adjusted Node class with the value attribute

--- projects/graph/src/node.py
+++ projects/graph/src/node.py
@@ -2,12 +2,13 @@ import random
 import math
 class Node:
     id = 0
-    def __init__(self, x, y, radius = .2):
+    def __init__(self, x, y, radius = .2, value = None):
         self.x = x
         self.y = y
         self.radius = radius
         self.id = Node.id
         self.color = 'white'
+        self.value = value
         Node.id += 1
     
     def assign_random_color(self):
@@ -15,7 +16,8 @@ class Node:
         self.color = '#%02X%02X%02X' % (r(),r(),r())
 
     @classmethod
-    def create_with_random_props(cls, graph_width, graph_height, radius = .2):
+    def create_with_random_props(cls, graph_width, graph_height, radius = .2, value = None):
+    
         random_x = random.random() * graph_width
         random_y = random.random() * graph_height
         if random_x + radius > graph_width:
@@ -24,4 +26,4 @@ class Node:
             random_y -= radius
         
         
-        return cls(random_x, random_y, radius)
+        return cls(random_x, random_y, radius, value)
