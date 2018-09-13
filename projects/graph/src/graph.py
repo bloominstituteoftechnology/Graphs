a4de958145cf01bdb96fcfc5e2f8ce79092ad5c2
Flintbean <ajmerando1@yahoo.com>
2018-09-13 13:50:02 -0700

    code tweaking. Had to reformat into nodes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,68 +1,65 @@
-import random
+
+class Vertex:
+    """Represent a vertex with a label and possible connected component."""
+    # pylint: disable=too-few-public-methods
+    # Using class so it's hashable, even though it doesn't have public methods
+    def __init__(self, label, component=-1):
+        self.label = str(label)
+        self.component = component
+
+    def __repr__(self):
+        return 'Vertex: ' + self.label
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-
     def __init__(self):
         self.vertices = {}
+        self.components = 0
 
-    def add_vertex(self, node):
-        self.vertices[node] = set()
-        return self.vertices
+    def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
 
-    def add_edge(self, from_node, to_node):
-        self.vertices[from_node].add(to_node)
-        self.vertices[to_node].add(from_node)
-        return
+    def add_edge(self, start, end, bidirectional=True):
+        """Add a edge (default bidirectional) between two vertices."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
 
-    def add_directed_edge(self, ver1, ver2):
-        if ver1 in self.vertices:
-            self.vertices[ver1].add(ver2)
-        else:
-            raise IndexError(
-                'That vertex value is not available. please add it first')
+    def search(self, start, target=None, method='dfs'):
+        """Search the graph using BFS or DFS."""
+        quack = [start]  # Queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set([start])
 
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
+                break
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
+            visited.update(self.vertices[current])
 
-    # def bfs(adjList, node_id):
-    #     frontier = []
-    #     frontier.append(node_id)
-    #     visited = []
-    #     while len(frontier > 0):
-    #         n = frontier.pop(0)
-    #         if n not in visited:
-    #             print(n)
-    #             visited.append(n)
-    #             for next_node in adjList[n]:
-    #                 frontier.append(next_node)
+        return visited
 
+    def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
 
-    def get_connected_components(self):
-        searched = []
-        for index in self.vertices:
-            if index not in searched:
-                searched.append(self.dfs(index))
-        return searched
-
-    def dfs(self):
-        start = self.vertices[:0]
-
-        random_color = '#' + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]) 
-        
-        stack = []
-        found = []
-        stack.append(start)
-        found.append(start)
-        
-        while (len(stack) > 0):
-            v = stack.pop()
-            if v not in found:
-                found.append(v)
-            for edge in v:
-                if edge not in found:
-                    stack.append(v)
-        return found
-
-    def __str__(self):
-        return f"{self.vertices}"
-
-
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
