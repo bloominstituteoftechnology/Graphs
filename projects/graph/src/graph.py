613dc46e08d809835711886bc3c5a38989ae3660
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-13 16:14:50 -0700

    fixes search

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -80,14 +80,14 @@ class Graph:
         return None
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-# print(graph.vertices)
-print(graph.Search('0', '0', 'bfs'))
-print(graph.Search('0', '3', 'dfs'))
-print(graph.Shortest('0', '1'))
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# # print(graph.vertices)
+# print(graph.Search('0', '0', 'bfs'))
+# print(graph.Search('0', '3', 'dfs'))
+# print(graph.Shortest('0', '1'))
