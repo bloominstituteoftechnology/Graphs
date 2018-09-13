322e9b0eb10a438b53be52308cd72e1fc0f86101
cmok4290 <cmok4290@gmail.com>
2018-09-13 15:27:59 -0400

    added queue/stack class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,6 +1,37 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        return None
+
+    def size(self):
+        return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        return None
+
+    def size(self):
+        return len(self.stack)
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
