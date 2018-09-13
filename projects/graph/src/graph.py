ab1098cde1e37f54befcd1f8a8d8b7e2815797cc
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-13 15:45:25 -0700

    bfs/dfs in one

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -25,28 +25,30 @@ class Graph:
             self.vertices[vertex1].add(vertex2)
             self.vertices[vertex2].add(vertex1)
 
-    def BFS(self, start, target=None):
-        """Search the graph using BFS"""
+    def Search(self, start, target=None, type="bfs"):
+        """Search the graph using BFS/DFS based on type"""
         queue = [start]
+        # remove first if a bfs (queue) and last if dfs (stack)
+        remove_index = 0 if type == "bfs" else -1
         # if we make visited a set, can do set operations with the vertices
         visited = set([start])
 
         while queue:
-            current = queue.pop(0)
+            current = queue.pop(remove_index)
             if current == target:
-                break
+                return True
             visited.add(current)
             # Add to visit vertices to queue
             queue.extend(self.vertices[current] - visited)
             visited.update(self.vertices[current])
 
-        return visited
+        return False
 
 
 class Vertex:
     """Represent a vertex with an id, value, color, edges"""
 
-    def __init__(self, vertex_id, value, color="white"):
+    def __init__(self, vertex_id, value, color="red"):
         self.vertex_id = str(vertex_id)
         self.value = value
         self.color = color
@@ -64,4 +66,5 @@ graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 # print(graph.vertices)
-print(graph.BFS('0'))
+print(graph.Search('0', '3', 'bfs'))
+print(graph.Search('0', '3', 'dfs'))
