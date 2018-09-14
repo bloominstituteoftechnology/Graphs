7d1f20ca5c53f0222268b7c89e1bfcfd1f1701b3
Alec <afroenygam@gmail.com>
2018-09-13 23:31:44 -0400

    Depth-First and Bredth-First traversals and searches updated

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -21,6 +21,32 @@ class Node:
 #     def __repr__(self):
 #         return 'Vertex: ' + self.label
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 class Vertex:
     def __init__(self, vertex_id, x=None, y=None, value=None, color=None):
         self.id = int(vertex_id)
@@ -83,16 +109,114 @@ class Graph:
 
         return visited
 
+    def dft(self, start_vert, visited=[]):
+        visited.append(start_vert)
+        print(self.vertices[start_vert].id, ': ', self.vertices[start_vert].value)
+
+        for child_vert in self.vertices[start_vert].edges:
+            if child_vert not in visited:
+                self.dft(child_vert, visited)
+
+    def dft_stack(self, start_vert): #for some reason it travers from right to left, weird
+        #create an empty Stack
+        stack = Stack()
+        # Put starting vert in the stack
+        stack.push(start_vert)
+        #Declare visited list
+        visited = []
+        #While stack is not empty...
+        while stack.size() > 0:
+            # remove the top item from the stack
+            v = stack.pop()
+            # if popped value hasn't been visited...
+            if v not in visited:
+                #... print it
+                print("{}: {}".format(self.vertices[v].id, self.vertices[v].value))
+                #Add visited vertex to list
+                visited.append(v)
+                #put the children into the stack
+                for next_vert in self.vertices[v].edges:
+                    stack.push(next_vert)
+
+
+    def bft(self, starting_vertex_id):
+        q = Queue()
+        q.enqueue(starting_vertex_id)
+        visited = []
+
+        while q.size() > 0:
+            v = q.dequeue()
+            # if v not visited...
+            if v not in visited:
+                print(self.vertices[v].id, ": ", self.vertices[v].value)
+                visited.append(v) #mark vertex as visited
+                for next_vert in self.vertices[v].edges:
+                    q.enqueue(next_vert)
+
+    def dfs(self, starting_vertex_id, target_value, visited=[]):
+        pass
+        visited.append(starting_vertex_id)
+        if starting_vertex_id == target_value:
+            return True
+
+        for child_vert in self.vertices[starting_vertex_id].edges:
+            if child_vert not in visited:
+                if self.dfs(child_vert, target_value, visited):
+                    return True
+
+        return False
+
+    def bfs(self, starting_vertex_id, target_value):
+        q = Queue()
+        q.enqueue(starting_vertex_id)
+        visited = []
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                if self.vertices[v].value == target_value:
+                    return True
+                visited.append(v)
+                for next_vert in self.vertices[v].edges:
+                    q.enqueue(next_vert)
+        return False
+
+
 graph = Graph()
 print(graph.vertices)
+graph.add_vertex(0, value="Shuri")
 graph.add_vertex(1, value="T'Challa!")
-graph.add_vertex(2, value="Captain America")
-graph.add_edge(1, 2)
-#graph.add_vertex('3')
-#graph.add_edge('1', '3')
-#graph.add_edge('2','3', False)
-graph.add_vertex(3)
+graph.add_vertex(2, value="Cap")
+graph.add_vertex(3, value="Spidey")
+graph.add_vertex(4, value="Tony")
+graph.add_vertex(5, value="Thanos")
+graph.add_vertex(6, value="The Maw")
+graph.add_vertex(7, value="Hulk")
+graph.add_vertex(8, value="Thor")
+graph.add_vertex(9, value="Erik")
+graph.add_vertex(10, value="Nat")
+graph.add_vertex(11, value="Vision")
+graph.add_vertex(12, value="Wanda")
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 3)
+graph.add_edge(2, 6)
+graph.add_edge(3, 7)
+graph.add_edge(1, 4)
+graph.add_edge(1, 5)
+graph.add_edge(4, 8)
+graph.add_edge(4, 9)
+
 for vertex, vertex_info in graph.vertices.items():
-    print(vertex, "space", vertex_info.value, "space", vertex_info.edges)
+    print(vertex, ": ", vertex_info.value, ", connected to vertices ", vertex_info.edges)
 
 #print(graph.search('1', method='bfs'))
+print('DFT: ')
+graph.dft(graph.vertices[0].id)
+print('\n DFT using Stack method')
+graph.dft_stack(graph.vertices[0].id)
+print('\nBFT:')
+graph.bft(graph.vertices[0].id)
+print('\nDFS:')
+print(graph.dfs(graph.vertices[0].id, 9))
+print('\nBFS:')
+print(graph.bfs(graph.vertices[0].id, "Thanoos"))
