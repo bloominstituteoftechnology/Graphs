722ecb27829da21225c4f64fd6be8a63eed21144
chainchompa <chainchompa@gmail.com>
2018-09-17 17:19:31 -0400

    added add_vertex and add_edge to Graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,19 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex in self.vertices:
+            raise Exception('Error: That vertex already exists')
+        else:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, start, end):
+        if start not in self.vertices:
+            raise Exception('Error: Start vertex not found')
+        if end not in self.vertices:
+            raise Exception('Error: End vertex not found')
+        else:
+            self.vertices[start].add(end)
+            self.vertices[end].add(start)
