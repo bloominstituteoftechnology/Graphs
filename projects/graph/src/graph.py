71a09379da820bbd9e804c29e154f0411c0bf732
Ari Mercado <ari@Apples-Mac-3.local>
2018-09-17 16:08:01 -0700

    added 2 methods to graph but they're incomplete

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,43 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+class Node:
+    def __init__(self):
+        self.neighbors = []
+    def addNeighbor(self, neightbor_node):
+        self.neighbors.append(neighbor_node)
+    def getNeighbors(self):
+        return self.neighbors
+    def isNeighbor(self, node):
+        return node in self.neighbors
+
+    def add_vertex(self):
+    
+    def add_edge(self):
+        
+
+edge_list = [
+    (1, 2),
+    (1, 4),
+    (1, 7),
+    (2, 3),
+    (2, 5),
+    (3, 6),
+    (4, 7),
+    (5, 6),
+    (6, 7)
+]
+
+adjacency_list = {
+    1: [2, 4, 7],
+    2: [1, 3, 5],
+    3: [2, 6],
+    4: [1, 7],
+    5: [2, 6],
+    6: [3, 5, 7],
+    7: [ 1, 4, 6]
+}
+
+# addNeighbor()
+
