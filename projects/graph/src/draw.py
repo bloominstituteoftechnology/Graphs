ec836bdc6aa1d4b79c88bb019003ca5acb1832ca
Randy Calderon <randycweb@gmail.com>
2018-09-18 14:47:01 -0700

    Setup graph class and added test code for draw

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,7 +1,8 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-
+import math
+from graph import Graph
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
@@ -10,5 +11,23 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
+    def __init__(self, graph):
+        self.graph = graph
         pass  # TODO
+
+N = 8
+node_indices = list(range(N))
+
+plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+              tools="", toolbar_location=None)
+
+graph = GraphRenderer()
+
+graph.node_renderer.glyph = Circle(height=0.1, width=0.2, fill_color="fill_color")
+graph.node_renderer.data_source.data = dict(
+    index=node_indices,
+    fill_color=Spectral8)
+
+graph.edge_renderer.data_source.data = dict(
+    start=[0]*N,
+    end=node_indices)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,21 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
+        self.vertices = {}
         pass  # TODO
+    
+    def add_vertex(self,value):
+        self.vertices[value] = set()
+
+    def add_edge(self, value, valuetwo):
+        self.vertices[value].add(valuetwo)
+        self.vertices[valuetwo].add(value)
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
