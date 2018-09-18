73a24285c3b9d807d76af1a81393a13a7ffceb75
chainchompa <chainchompa@gmail.com>
2018-09-18 16:54:13 -0400

    added depth first traversal to graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,6 +8,9 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
+    def __str__(self):
+        return f"{self.vertices}"
+
     def add_vertex(self, vertex):
         if vertex in self.vertices:
             raise Exception('Error: That vertex already exists')
@@ -23,5 +26,26 @@ class Graph:
             self.vertices[start].add(end)
             self.vertices[end].add(start)
 
-    def __str__(self):
-        return f"{self.vertices}"
+    def add_directed_edge(self, start, end):
+        if start not in self.vertices:
+            raise Exception('Error: Start vertex not found')
+        if end not in self.vertices:
+            raise Exception('Error: End vertex not found')
+        else:
+            self.vertices[start].add(end)
+
+def dft(adjList, node_id):
+    print(node_id)
+    for child_node in adjList[node_id]:
+        dft(adjList, child_node)
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_directed_edge('0', '1')
+graph.add_directed_edge('0', '2')
+graph.add_directed_edge('2', '3')
+
+dft(graph.vertices, '0')
