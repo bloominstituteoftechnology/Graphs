bc7757e8d6ed42cff966e25f043d26a665481d36
chainchompa <chainchompa@gmail.com>
2018-09-18 13:40:54 -0400

    used bokeh to draw graph in graph.html

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -22,3 +22,13 @@ class Graph:
         else:
             self.vertices[start].add(end)
             self.vertices[end].add(start)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
