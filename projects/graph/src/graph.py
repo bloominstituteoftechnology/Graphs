ecb9511fdc3cd35c442ae26ded09a5f610f1f34b
Adrian Adames <adrianadames@gmail.com>
2018-09-18 16:51:42 -0400

    Implemented lecture demo code to draw a plot using the bokeh library. Will continue to experiment with the different plot tools and settings.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,16 +9,16 @@ class Graph:
     def __init__(self):
         self.vertices = {}
     def add_vertex(self,vertex_id):
-        self.vertices(vertex_id) = set()
+        self.vertices[vertex_id] = set()
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices(v1).add(v2)
-            self.vertices(v2).add(v1)
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
         else:
             raise IndexError("That vertex does not exist!")
     def directed_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices(v1).add(v2)
+            self.vertices[v1].add(v2)
         else:
             raise IndexError("That vertex does not exist!")
 
@@ -48,14 +48,14 @@ class Graph:
 # indices of for the start and end of the edges.
 
 
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
+# graph = Graph()
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# print(graph.vertices)
 
 
 
