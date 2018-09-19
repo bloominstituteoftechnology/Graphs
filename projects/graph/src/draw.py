d72382a861d51f20dcdae6fcddff38c0ee7703f6
Ari Mercado <ari@Apples-Mac-3.local>
2018-09-19 15:35:47 -0700

    added properties to vertex class

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -9,110 +9,87 @@ from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnDataSource, Label, LabelSet
 from bokeh.palettes import Spectral8
 from graph import Graph
+from graph import dft
+from graph import dfs
+from graph import bft
+
 
 
 
 graph = Graph()  # Instantiate your graph
-# graph.add_vertex('0')
-# graph.add_vertex('1')
-# graph.add_vertex('2')
-# graph.add_vertex('3')
-# graph.add_vertex('4')
-# graph.add_vertex('5')
-# graph.add_vertex('6')
-# graph.add_vertex('7')
-# graph.add_vertex('8')
-# graph.add_vertex('9')
-# graph.add_vertex('10')
-# graph.add_vertex('11')
-# graph.add_vertex('12')
-# graph.add_vertex('13')
-# graph.add_vertex('14')
-# graph.add_vertex('15')
-# graph.add_vertex('16')
-# graph.add_vertex('17')
-# graph.add_vertex('18')
-# graph.add_vertex('19')
-# graph.add_vertex('20')
-# graph.add_vertex('21')
-# graph.add_vertex('22')
-# graph.add_vertex('23')
-# graph.add_vertex('24')
-# graph.add_vertex('25')
-# graph.add_vertex('26')
-# graph.add_vertex('27')
-# graph.add_vertex('28')
-# graph.add_vertex('29')
-# graph.add_vertex('30')
-# graph.add_vertex('31')
-# graph.add_vertex('32')
-# graph.add_vertex('33')
-# graph.add_vertex('34')
-# graph.add_vertex('35')
-# graph.add_vertex('36')
-# graph.add_vertex('37')
-# graph.add_vertex('37')
-# graph.add_vertex('38')
-# graph.add_vertex('39')
-# graph.add_vertex('40')
-# graph.add_vertex('41')
 
-for i in range(0, 300):
+#Vertices
+for i in range(0, 10):
     graph.add_vertex(str(i))
 
+#Edges
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('1', '2')
+graph.add_edge('2', '5')
+graph.add_edge('2', '4')
+graph.add_edge('4', '9')
+graph.add_edge('3', '7')
+graph.add_edge('3', '6')
+
+# graph.add_edge('0', '8')
+# graph.add_edge('8', '128')
+# graph.add_edge('128', '120')
+# graph.add_edge('120', '0')
+
+# graph.add_edge('8', '100')
+# graph.add_edge('0', '96')
+# graph.add_edge('120', '156')
+# graph.add_edge('128', '160')
 
 
-# Edges
-graph.add_edge('0', '8')
-graph.add_edge('8', '128')
-graph.add_edge('128', '120')
-graph.add_edge('120', '0')
+# graph.add_edge('96', '100')
+# graph.add_edge('100', '160')
+# graph.add_edge('160', '156')
+# graph.add_edge('156', '96')
 
-graph.add_edge('8', '100')
-graph.add_edge('0', '96')
-graph.add_edge('120', '156')
-graph.add_edge('128', '160')
+# graph.add_edge('100', '146')
+# graph.add_edge('96', '144')
+# graph.add_edge('156', '174')
+# graph.add_edge('160', '176')
+
+# graph.add_edge('144', '146')
+# graph.add_edge('146', '176')
+# graph.add_edge('176', '174')
+# graph.add_edge('174', '144')
+
+# dft(graph.vertices, '8', [])
+# dfs(graph.vertices, '8', [], '128')
+# graph.bft(graph.vertices, '0')
 
 
-graph.add_edge('96', '100')
-graph.add_edge('100', '160')
-graph.add_edge('160', '156')
-graph.add_edge('156', '96')
 
-graph.add_edge('100', '146')
-graph.add_edge('96', '144')
-graph.add_edge('156', '174')
-graph.add_edge('160', '176')
 
-graph.add_edge('144', '146')
-graph.add_edge('146', '176')
-graph.add_edge('176', '174')
-graph.add_edge('174', '144')
+#print(graph.vertices)
 
-print(graph.vertices)
 
 N = len(graph.vertices)
 node_indices = list(graph.vertices)
 
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1000), y_range=(-1.1,1000),
+plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,10.1), y_range=(-1.1,10.1),
               tools='', toolbar_location=None)
 
 graph_renderer = GraphRenderer()
 
 graph_renderer.node_renderer.data_source.add(node_indices, 'index')
-graph_renderer.node_renderer.data_source.add(['orange'], 'color')
+graph_renderer.node_renderer.data_source.add([graph.vertices[vertex_id].color for vertex_id in graph.vertices], 'color')
 graph_renderer.node_renderer.glyph = Circle(radius=0.4, fill_color='color')
 
 start_indices = []
 end_indices = []
 
-for vertex in graph.vertices:
-    for edge_end in graph.vertices[vertex]:
-        start_indices.append(vertex)
+for vertex_id in graph.vertices:
+    for edge_end in graph.vertices[vertex_id].edges:
+        start_indices.append(vertex_id)
         end_indices.append(edge_end)
-print(start_indices)
-print(end_indices)
+# print(start_indices)
+# print(end_indices)
 
 
 graph_renderer.edge_renderer.data_source.data = dict(
@@ -120,18 +97,19 @@ graph_renderer.edge_renderer.data_source.data = dict(
     end=end_indices)
 
 ### start of layout code
-grid = [int(v) for v in graph.vertices]
-x = [50 * (i // 15) for i in grid]
-y = [50 * (i % 15) for i in grid]
-# x = [i for i in grid]
-# y = [i ** 2 for i in grid]
+x = [graph.vertices[vertex_id].x for vertex_id in graph.vertices]
+y = [graph.vertices[vertex_id].y for vertex_id in graph.vertices]
+
+# x = [50 * (i // 15) for i in grid]
+# y = [50 * (i % 15) for i in grid]
+
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph_renderer)
 
-labelSource = ColumnDataSource(data=dict(x=x, y=y, names=grid))
+labelSource = ColumnDataSource(data=dict(x=x, y=y, names=[graph.vertices[vertex_id].value for vertex_id in graph.vertices]))
 labels = LabelSet(x='x', y='y', text='names', level='glyph',
 text_align='center', text_baseline='middle', source=labelSource, render_mode='canvas')
 
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,34 +7,40 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+    
+    def vertices(self):
+        return self.vertices
+
     def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = set()
+        self.vertices[vertex_id] = Vertex(vertex_id)
+
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-            self.vertices[v1].add(v1)
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v1].edges.add(v1)
         else:
             raise IndexError("THat vertex does not exist!")
+
     def add_directed_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
+            self.vertices[v1].edges.add(v2)
         else:
             raise IndexError("THat vertex does not exist!")
 
-# graph = Graph()  # Instantiate your graph
-# graph.add_vertex('0')
-# graph.add_vertex('1')
-# graph.add_vertex('2')
-# graph.add_vertex('3')
-# graph.add_edge('0', '1')
-# graph.add_edge('0', '3')
-# print(graph.vertices)
-
 class Vertex:
-    def __init__(self, id, color="white"):
-        self.id
-        self.color
-        self.edges = []
+    def __init__(self, vertex_id, x=None, y=None, value=None, color="white"):
+        self.id = int(vertex_id)
+        self.x = x
+        self.y = y
+        self.value = value
+        self.color = color
+        self.edges = set()
+        if self.x is None:
+            self.x = self.id
+        if self.y is None:
+            self.y = self.id
+        if self.value is None:
+            self.value = self.id
 
 class Node:
     def __init__(self):
@@ -75,17 +81,39 @@ adjacency_list = {
     7: [ 1, 4, 6]
 }
 
+# def dft(adjList, node_id, visited):
+#     print(node_id)
+#     visited.append(node_id)
+#     for child_node in adjList[node_id]:
+#         if child_node not in visited:
+#             dft(adjList, child_node, visited)
 
-# addNeighbor(): O(1)
-# getNeighbors(): O(n)
-# isNeighbor(): O(1)
+def dft(self, adjList, node_id, visited):
+    print(node_id)
+    visited.append(node_id)
+    for child_node in adjList[node_id]:
+        if child_node not in visited:
+            self.dft(adjList, child_node, visited)
 
-# graph = Graph()  # Instantiate your graph
-# graph.add_vertex('0')
-# graph.add_vertex('1')
-# graph.add_vertex('2')
-# graph.add_vertex('3')
-# graph.add_edge('0', '1')
-# graph.add_edge('0', '3')
-# print(graph.vertices)
+def dfs(self, adjList, node_id, visited, search_node):
+    if node_id == search_node: 
+        return True
+    visited.append(node_id)
+    for child_node in adjList[node_id]:
+        if child_node not in visited:
+            self.dfs(adjList, child_node, visited, search_node)
+
+def bft(self, adjList, node_id):
+    frontier = []
+    frontier.append(node_id)
+    visited = []
+    while len(frontier) > 0:
+        n = frontier.pop(0)
+        if n not in visited:           
+            print(n)
+            visited.append(n)
+            for next_node in adjList[n]:
+                frontier.append(next_node)
+
+
 
