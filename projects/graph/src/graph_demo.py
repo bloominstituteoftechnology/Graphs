51fbac0e2925efaa9cd4000d8bc7ab956293d7c2
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-19 11:52:59 -0700

    added BokehGraph class

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,11 +5,33 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
-
+from draw2 import BokehGraph
+from graph2 import Graph
 
 def main():
-    pass  # TODO
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex(0)
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_vertex(8)
+    graph.add_vertex(9)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 3)
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 5)
+    graph.add_edge(2, 4)
+    graph.add_edge(4, 9)
+    graph.add_edge(3, 7)
+    graph.add_edge(3, 6)
 
+    bokeh_graph = BokehGraph(graph)
+
+    bokeh_graph.draw()
 
 if __name__ == '__main__':
     # TODO - parse argv
