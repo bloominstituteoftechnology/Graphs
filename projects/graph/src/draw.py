761829a90f848ab12e33f051e1baf0992dc40455
Randy Calderon <randycweb@gmail.com>
2018-09-19 11:49:16 -0700

    Draw setup

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -2,32 +2,59 @@
 General drawing methods for graphs using Bokeh.
 """
 import math
-from graph import Graph
+
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle
+from graph import Graph
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_edge('4', '1')
+graph.add_edge('2', '3')
+
+print(graph.vertices)
 
 
-class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-    def __init__(self, graph):
-        self.graph = graph
-        pass  # TODO
+N = len(graph.vertices)
+node_indices = list(graph.vertices)
 
-N = 8
-node_indices = list(range(N))
-
-plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,10.1), y_range=(-1.1,10.1),
               tools="", toolbar_location=None)
 
-graph = GraphRenderer()
+graphs = GraphRenderer()
 
-graph.node_renderer.glyph = Circle(height=0.1, width=0.2, fill_color="fill_color")
-graph.node_renderer.data_source.data = dict(
-    index=node_indices,
-    fill_color=Spectral8)
+graphs.node_renderer.glyph = Circle(radius=0.5, fill_color='color')
+graphs.node_renderer.data_source.add(node_indices, 'index')
+graphs.node_renderer.data_source.add(['red'] * N, 'color')
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+start_indices = []
+end_indices = []
+
+for vertex in graph.vertices:
+    for edge_end in graph.vertices[vertex]:
+        start_indices.append(vertex)
+        end_indices.append(edge_end)
+
+graphs.edge_renderer.data_source.data = dict(
+    start = start_indices,
+    end = end_indices
+)
+
+circ = [int(v) for v in graph.vertices]
+x = [2 * (i // 3) for i in circ]
+y = [2 * (i % 3) for i in circ]
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+graph_renderer.layout_provider = StaticLayoutProvider(graph_layout = graph_layout)
+
+plot.renderers.append(graph_renderer)
+
+output_file('graph.html')
+show(plot)
+
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -15,12 +15,3 @@ class Graph:
     def add_edge(self, value, valuetwo):
         self.vertices[value].add(valuetwo)
         self.vertices[valuetwo].add(value)
-
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
