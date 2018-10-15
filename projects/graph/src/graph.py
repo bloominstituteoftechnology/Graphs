9e4d4a7c8dc9859c5415cb79855c6290c9aaedb3
Erik Kimsey <erik@erikkimsey.com>
2018-10-15 18:29:17 -0400

    Created Graph class and have add_vertex function working

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,28 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, a):
+        self.vertices[a] = set()
+        pass
+
+    def add_edge(self, a, b):
+        if(a not in self.vertices):
+            self.add_vertex((a))
+            self.vertices[a] = set(b)
+        elif(a in self.vertices):
+            self.vertices[a]
+        pass
+
+    def get_vertices(self):
+        print(self.vertices)
+        return self.vertices
+
+
+
+g = Graph()
+g.add_vertex("0")
+g.add_vertex("1")
+g.add_edge()
+g.get_vertices()
