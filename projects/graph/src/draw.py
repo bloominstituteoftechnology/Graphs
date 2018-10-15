145f12b9db966e301266897cba91372ba87fbb12
Charles Cortinas <crcortinas@gmail.com>
2018-10-15 17:53:59 -0500

    Graph
    created graph implementation

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -6,9 +6,10 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
+from graph import Graph
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  # TODO
+        self.graph = Graph()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,15 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.graph = dict()
+
+    def add_vertex(self, vert):
+        if vert not in self.graph.keys():
+            self.graph[vert] = set()
+
+    def add_edge(self, start, end):
+        if start in self.graph.keys():
+            self.graph[start].add(end)
+
+    def get_graph(self):
+        return self.graph
