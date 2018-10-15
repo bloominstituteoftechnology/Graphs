db76929aec52e99271fe0821ee2e177a98bc9eb5
LizBaker <lizbaker.pdx@gmail.com>
2018-10-15 14:45:09 -0700

    Changed graph to use numbers for better bokeh experience

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,18 +7,13 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {
-            "A": {"B", "C"},
-            "B": {"A"},
-            "C": {"B"}
+            0: {1, 2},
+            1: {1},
+            2: {0}
         }
     def add_vertex(self, name):
         self.vertices[name] = set()
 
     def add_edge(self, vertex1, vertex2):
         self.vertices[vertex1].add(vertex2)
-        
-lilgraph = Graph()
-lilgraph.add_vertex("D")
-lilgraph.add_edge("D", "C")
-lilgraph.add_edge("D", "B")
-print(lilgraph.vertices)
+
