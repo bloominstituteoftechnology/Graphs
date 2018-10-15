9c0aa5e41c34a1dd8e6320b80508abc126880897
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-15 16:54:06 -0500

    got dependencies installed properly i hope

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -8,7 +8,11 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
 
+
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
+
     def __init__(self):
         pass  # TODO
+
+
