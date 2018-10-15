b56d1d1a3120e22bfde9f14eac403c20f5972d2c
Carey <careycbaldwin@gmail.com>
2018-10-15 16:10:40 -0400

    worked on vertex and graph classes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,26 +1,68 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-class Node:
-    def __init__(self):
+# http://interactivepython.org/courselib/static/pythonds/Graphs/Implementation.html
+
+# the vertex class will create a single object with a key and an attached array of connections
+    # vertex = {'A': ['B', 'C']}
+
+
+# the Graph Class should create an object with vertices, each of which has a corresponding collection of neighbors
+    # graph = {'A': ['B', 'C'],
+    #          'B': ['C', 'D'],
+    #          'C': ['D'],
+    #          'D': ['C'],
+    #          'E': ['F'],
+    #          'F': ['C']}
+
+
+class Vertex:
+    def __init__(self, key):
         # storage space for neighboring nodes
+        self.id = key
         self.neighbors = []
+    def _repr__(self):
+        return 'Vertex: ' + self.key
         # method to add an adjoining node
-    def addNeighbor(self, neighbor_node):
+    def addNeighbor(self, neighbor):
         self.neighbors.append(neighbor_node)
         # method to return a list of adjoining nodes
     def getNeighbors(self):
         return self.getNeighbors
         # method to check if an inputted node is connected to the node
-    def isNeighbor(self,node):
+    def isNeighbor(self, node):
         return node in self.neighbors
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+        self.vertCount = 0
 
+    def add_vertex(self)
+        self.vertCount =+ 1
+        newVertex = Vertex(key)
+        self.vertCount[key] = newVertex
+        return newVertex
+    
+    def get_vertex(self,n):
+        if n in self.vertices:
+            return self.vertices[n]
+        else:
+            return None
+    
+    def __contains__(self,n):
+        return n in self.vertices
 
+    def add_edge(self, f, t)
+        if f not in self.vertices:
+            nv = self.add_vertex(f)
+        if t not in self.vertices:
+            nv = self.add_vertex(t)
+        self.vertices[f].add_neighbor(t)
+
+    def vertices(self):
+        return self.vertices.keys()
 
 
 # Tests for Graph Class
