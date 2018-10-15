b787d852dcda53cf78c36a4d3ac9d8ebd5e6a808
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-10-15 16:02:39 -0700

    edge cases

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -10,8 +10,34 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self, vertex):
-        self.vertices[vertex] = set()
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
 
     def add_edge(self, vertex, edge):
-        self.vertices[vertex].add(edge)
-        self.vertices[edge].add(vertex)
+        if vertex not in self.vertices or edge not in self.vertices:
+            print("not found")
+        else:
+            self.vertices[vertex].add(edge)
+            self.vertices[edge].add(vertex)
+
+
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
+
+
+class Edge:
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
