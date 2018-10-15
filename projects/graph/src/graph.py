7301f6a4e871c0ef54821322581526ec7a9d187b
MichaelBoegner <cmikeb@gmail.com>
2018-10-15 17:05:22 -0400

    Part 1 Fixed and Completed with Stretch.
    Originally built with arrays for values in graph but then realized needed dict.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -10,11 +10,34 @@ class Graph:
         
     def add_vertex(self, vertex):
         if vertex not in self.graph: 
-            self.graph[vertex] = []
+            self.graph[vertex] = {}
     
     def add_edge(self, vertex, edge):
-        if 
+        if edge in self.graph:
+            if self.graph[vertex] == {}:
+                self.graph[vertex] = {edge}
 
+            temp = []
 
-graph = Graph()
+            for k in self.graph[vertex]:
+                temp.append(k)
+
+            temp.append(edge)
+
+            self.graph[vertex].update(temp)
+        else:
+            print(f"Cannot add edge number {edge}. Corresponding vertex doesn't exst.")
+
+        
+
+graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('0', '5')
+
+
+print(graph.graph)
