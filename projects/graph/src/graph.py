8812ded60aba5fb02b2be3ebf2c058bcf9bd509f
Carey <careycbaldwin@gmail.com>
2018-10-15 15:03:35 -0400

    created node class in graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,7 +1,19 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+class Node:
+    def __init__(self):
+        # storage space for neighboring nodes
+        self.neighbors = []
+        # method to add an adjoining node
+    def addNeighbor(self, neighbor_node):
+        self.neighbors.append(neighbor_node)
+        # method to return a list of adjoining nodes
+    def getNeighbors(self):
+        return self.getNeighbors
+        # method to check if an inputted node is connected to the node
+    def isNeighbor(self,node):
+        return node in self.neighbors
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
