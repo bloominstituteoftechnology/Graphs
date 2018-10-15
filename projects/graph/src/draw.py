15010e5e8f6b092669102b7ad4270dd1e9912609
Erica Thompson <ericathompson.nc@gmail.com>
2018-10-15 16:16:06 -0700

    Erica Thompson:WIP

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,6 +7,16 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+graph = {
+    '0': {'1', '3'},
+    '1': {'0'},
+    '2': set(),
+    '3': {'0'}
+}
+
+p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+p.line(x, y, legend="Temp.", line_width=2)
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
