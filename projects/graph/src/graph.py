e4b609ed576578dae90751bd60b882924bf3f4a0
tulf <timzulf@gmail.com>
2018-10-15 18:44:51 -0400

    Tim Zulf WIP
    installed bokeh
    - [x] displays graph
    inital graph.py
    - [x] seems to print something

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,19 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+    def add_vertex(self, value):
+        self.vertices[value] = set()
+    def add_edge(self, v1, v2):
+        self.vertices[v1].add(v2)
+        self.vertices[v1].add(v2)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
