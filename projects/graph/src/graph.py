05529f30205c105e4531480fd4274802ec16a242
Kairi <kairijb@gmail.com>
2018-10-15 15:50:24 -0500

    can add edges

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,7 +12,11 @@ class Graph:
     def add_vertex(self,vertex):
         self.vertices.update({str(vertex): set()})
         print(self.vertices)
+    def add_edge(self, vertex, nodes):
+        self.vertices.update({str(vertex): {str(node) for node in nodes} })
+        print(self.vertices)
 # print(Graph({'0': {'1', '3'},'1': {'0'},'2': set(),'3': {'0'}}).vertices)
 graph = Graph({'0': {'1', '3'},'1': {'0'},'2': set(),'3': {'0'}})
 graph.add_vertex(8)
+graph.add_edge(3, [1,2,3])
 
