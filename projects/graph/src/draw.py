3d0283f1c9426d8dbd578086680d75b967be5c65
Erik Kimsey <erik@erikkimsey.com>
2018-10-15 16:16:31 -0400

    Finally setup. Draw.py is running a Boken example

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -4,11 +4,25 @@ General drawing methods for graphs using Bokeh.
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource)
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  # TODO
+        pass
+
+    def tellme(self):
+        print("sometihng")
+        output_file("lines.html")
+        x = [1, 2, 3, 4, 5]
+        y = [6, 7, 2, 4, 5]
+        p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+        p.line(x, y, legend="Temp.", line_width=2)
+        show(p)
+
+
+bok = BokehGraph()
+bok.tellme()
+
+
