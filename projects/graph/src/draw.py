d71aa016021b84ce9fb1aed989cbe15a90f84a81
Fred <sohnf90@yahoo.com>
2018-10-15 15:30:28 -0700

    Graph Done

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,6 +1,7 @@
 """
 General drawing methods for graphs using Bokeh.
 """
+from graph import Graph
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
@@ -10,5 +11,8 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO 
+    def __init__(self, Graph):
+        self.Graph = Graph
+
+    def show(self):
+        
