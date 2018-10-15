3cd12e7c90cfa8b28a2be0bea43278bd913db7ea
Fred <sohnf90@yahoo.com>
2018-10-15 09:27:22 -0700

    Pull Request

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -11,4 +11,4 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  # TODO
+        pass  # TODO 
