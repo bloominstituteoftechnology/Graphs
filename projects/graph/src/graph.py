6a14f314570dc4b00d9f7e0d0554b86e7c5686c7
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-15 11:36:33 -0700

    implement graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,27 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, value):
+        if value in self.vertices:
+            raise Exception('That vertex already exists.')
+        else:
+            self.vertices[value] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 not in self.vertices or v2 not in self.vertices:
+            raise Exception('Invalid vertex: one or more of the vertices provided is invalid.')
+        else:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
