3b46db5cdd73dae0fcf5cf67b52fcdaf3dd71b1b
LaikaFusion <newrad4@gmail.com>
2018-10-15 18:48:36 -0400

    working on parsing args

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -2,7 +2,6 @@
 from graph import Graph
 from draw import BokehGraph
 from random import randint
-
 """
 Demonstration of Graph and BokehGraph functionality.
 """
@@ -10,22 +9,36 @@ Demonstration of Graph and BokehGraph functionality.
 from sys import argv
 
 
-def main(numVertices=None):
+def main(numVertices=None,numEdges=None):
     if numVertices is None:
         numVertices = randint(2,12)
+
+
     verticesList = list(range(0,numVertices))
+
+    if numEdges is None:
+        numEdges = 0
+        for num in verticesList:
+            numEdges += randint(0,num)
+            numEdges -= randint(0,int(num*.5))
+
 
     graphConstruct = Graph()
 
     for i in verticesList:
         graphConstruct.add_vertex(i)
-    for i in verticesList:
-        for chance in verticesList[i+1:]:
-            if randint(1,4) ==1:
-                graphConstruct.add_edge(i,chance)
+        
+    while(numEdges >= 0):
+        ranNum1 = randint(0,numVertices-1)
+        ranNum2 = randint(0,numVertices-1)
+        if ranNum2 not in graphConstruct.vertices[ranNum1]:
+            graphConstruct.add_edge(ranNum1,ranNum2)
+            numEdges -=1
+
     graphDisplay = BokehGraph(graphConstruct.vertices)
     graphDisplay.show()
 
 if __name__ == '__main__':
     # TODO - parse argv
+    print(argv[1])
     main()
