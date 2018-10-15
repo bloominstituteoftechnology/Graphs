e8da659af09040742c18cd2a30e353e9cbacb1fa
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-10-15 15:49:11 -0400

    First pass Graph and Vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,36 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+from random import random
+
+
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+        self.edges = set()
+        self.x = random() * 5
+        self.y = random() * 5
+
+    def __str__(self):
+        return f'Vertex {self.label}'
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """
+    Represent a graph as a dictionary of vertices mapping labels to edges.
+    """
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = Vertex(vertex)
+        else:
+            raise Exception('That vertex already exists')
+
+    def add_edge(self, v1, v2):
+        if v1 not in self.vertices or v2 not in self.vertices:
+            raise Exception('One or both vertices are not in graph.')
+        self.vertices[v1].edges.add(v2)
+        self.vertices[v2].edges.add(v1)
