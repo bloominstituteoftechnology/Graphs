1fb693fefafde3ae05f29dd03ffa847ba08aecb8
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-15 17:38:58 -0400

    making good progress

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,44 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+class AdjNode:
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        self.vertices = dict()
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.graph = [None] * self.vertices
+
+    def add_edge(self, src, dest):
+        node = AdjNode(dest)
+        node.next = self.graph[src]
+        self.graph[src] = node
+
+        node = AdjNode(src)
+        node.next = self.graph[dest]
+        self.graph[dest] = node
+
+    def print_graph(self):
+        for i in range(self.vertices):
+            print("Adjacency list of vertex {}\n head".format(i), end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+
+if __name__ == "__main__":
+    V = 5
+    graph = Graph(V)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 4)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)
+
+    graph.print_graph()
