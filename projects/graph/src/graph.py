faab723cdd4fdf9efac2d512adec90f28979f583
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-10-15 16:08:14 -0500

    Working on MVP
    Nothing is working as of right now, I set everything up how I had thought, but currently gettign an error about how my graph.add_edge('0','1') is

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,22 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.verticies = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.verticies:
+            self.verticies[vertex] = []
+
+    def add_edge(self, vertex, edge):
+        if vertex not in self.verticies or edge not in self.verticies:
+            print("not found")
+        else:
+            self.verticies[vertex].add(edge)
+            self.verticies[edge].add(vertex)
+
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_edge('0', '1')
+print(graph.verticies)
