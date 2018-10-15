4056c3f527bfe03ff0dfb9c0486937aef64c1ba7
MichaelBoegner <cmikeb@gmail.com>
2018-10-15 16:01:03 -0400

    Able to add Vertex. Adding Edges

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,15 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.graph = {}
+        
+    def add_vertex(self, vertex):
+        if vertex not in self.graph: 
+            self.graph[vertex] = []
+    
+    def add_edge(self, vertex, edge):
+        if 
+
+
+graph = Graph()
+graph.add_vertex('0')
