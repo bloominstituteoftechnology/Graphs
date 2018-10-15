8c1b436736895669edaf9b420f8a16ffb6e79a88
your <jurgenkela@gmail.com>
2018-10-15 18:45:32 -0400

    Part 2 completed

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,14 +1,47 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-
+import random
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+from bokeh.palettes import Spectral8
+from graph import Graph
+
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
+
     def __init__(self):
-        pass  # TODO
+        self.graph = graph
+
+    def show(self):
+        node_indices = list(self.graph.vertices.keys())
+        edges = [list(v) for v in self.graph.vertices.values()]
+
+        plot = figure(title='Graph Layout Demonstration', x_range=(-1, 5), y_range=(-1, 5),
+                      tools='', toolbar_location=None)
+        plot.axis.visible = False
+        plot.grid.visible = False
+
+        graph = GraphRenderer()
+
+        graph.node_renderer.data_source.add(node_indices, 'index')
+        graph.node_renderer.data_source.add(Spectral8, 'color')
+        graph.node_renderer.glyph = Circle(size=20, fill_color='color')
+
+        graph.edge_renderer.data_source.data = dict(
+            start=node_indices,
+            end=edges)
+
+        position = {}
+        for vertex in self.graph.vertices:
+            position[vertex] = (random.random() * 4, random.random() * 4)
+
+        graph.layout_provider = StaticLayoutProvider(graph_layout=position)
+        plot.renderers.append(graph)
+
+        output_file('graph.html')
+        show(plot)
