25a520bc95fc5db65c80cebf3de053c34f73b80e
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-10-15 14:01:27 -0700

    First PR
    Started implementing adjacency matrix

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,25 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+class Vertex:
+    def __init__(self, n):
+        self.name = n
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+    vertices = {}
+    edges = []
+    edge_indices = {}
+
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+            self.vertices[vertex.name] = vertex
+            for row in self.edges:
+                row.append(0)
+            self.edges.append([0] * (len(self.edges) + 1))
+            self.edge_indices[vertex.name] = len(self.edge_indices)
+            return True
+        else:
+            return False
+            
+
