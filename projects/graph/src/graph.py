5303e3f4f5f22164a0ae8f7debc779e84c0fd917
codejoncode <jonathanjholloway@gmail.com>
2018-10-15 14:47:55 -0500

    Getting the project started created a method for adding a vertex  also started the Graph class off with a self.graph_dict and default value of false for direction.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,11 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, directed = False):
+        self.graph_dict = {}
+        self.directed = directed
+    #ability to add to the dict needed 
+    def add_vertex(self, vertex):
+        #self.graph_dict[] = vertex
+        pass
+    
