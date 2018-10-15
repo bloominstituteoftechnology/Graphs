bea84ac0bdc51a6e04cec861025c85f4736dbd0c
codejoncode <jonathanjholloway@gmail.com>
2018-10-15 16:38:28 -0500

    updated the graph.py to include Edge classes and Vertex classes

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -6,9 +6,20 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
+from graph import Graph
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.Graph = graph #should be a object. instance of Graph
+        
+
+    
+test = Graph()
+test.add_vertex("0")
+test.add_vertex("1")
+test.add_vertex("2")
+test.add_edge("0", "2")
+test.add_edge("1", "2")
+test.add_edge("0", "2")
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,17 +3,42 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Vertex:
+    def __init__(self,value):
+        self.value = value 
+        self.edges = set()
+
+    def add_edge(self, vertex):
+        self.edges.add(vertex)
+    
+    def get_edges(self):
+        return self.edges.keys()
+    
+class Edge:
+    def __init__(self, destination, weight = 0):
+        self.destination = destination
+        self.weight = weight 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self, directed = False):
         self.graph_dict = {}
         self.directed = directed
     #ability to add to the dict needed 
-    def add_vertex(self, vertex):
-        self.graph_dict[vertex] = set()
+    def add_vertex(self, value):
+        new_vertex = Vertex(value)
+        self.graph_dict[new_vertex] = set()
         
     def add_edge(self, from_vertex, to_vertex):
-        self.graph_dict[from_vertex] = to_vertex
-        self.graph_dict[to_vertex] = from_vertex
+        self.graph_dict[from_vertex].add(to_vertex)
+        self.graph_dict[to_vertex].add(from_vertex)
+        #not sure on this at the moment seems like 
+        # the same thing twice though the instructions say 
+        # to build a dictionary best practice is to have Edge
+        # and Vertex classes 
+        from_vertex.add_edge(to_vertex)
+        to_vertex.add_edge(from_vertex)
+
+
 
     
