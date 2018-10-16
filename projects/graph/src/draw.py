3cf565f1f0a66eba6d1835c0be119d964de9f393
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-16 16:33:23 -0700

    using random function to generate x and y coordinates

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -9,7 +9,6 @@ from bokeh.models.graphs import NodesAndLinkedEdges
 from graph import Graph
 import math
 
-
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self, graph):
@@ -20,34 +19,48 @@ class BokehGraph:
         N=len(nodes)
 
         #Create plot
-        plot = figure(title = 'Graph', x_range = (-1.5, 1.5) , y_range = (-1.5, 1.5), tools = "", toolbar_location = None)
+        plot = figure(title = 'Graph', x_range = (-7, 7) , y_range = (-7, 7), tools = "", toolbar_location = None)
 
         g = GraphRenderer()
         
-        g.node_renderer.glyph = Circle(radius = 0.1, fill_color = "fill_color")  
+        g.node_renderer.glyph = Circle(radius = 0.5, fill_color = "fill_color")  
         g.node_renderer.data_source.data = dict(
             index = nodes,
             fill_color = ['red']*N
             )
 
-        start_edge = []
+        '''start_edge = []
         end_edge = []
         for keys, values in self.graph.vertices.items():
             start_edge += [keys]*len(values)
-            end_edge += values
+            end_edge += values'''
+
+        edge_start = []
+        edge_end = []
+        
+        for vertex_id in nodes:
+            for value in graph.vertices[vertex_id].edges:
+                edge_start.append(vertex_id)
+                edge_end.append(value)    
 
         g.edge_renderer.data_source.data = dict(
-            start=start_edge,
-            end=end_edge)
+            start=edge_start,
+            end=edge_end)
 
         ##start of layout code
-        circ = [i*2*math.pi/len(nodes) for i in range(len(nodes))]
-        x = [math.cos(i) for i in circ]
-        y = [math.sin(i) for i in circ]
+        #circ = [i*2*math.pi/len(nodes) for i in range(len(nodes))]
+        #x = [math.cos(i) for i in circ]
+        #y = [math.sin(i) for i in circ]
+        
+        x = []
+        y = []
 
+        for vertex_id in nodes:
+            vertex = graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
         
         graph_layout = dict(zip(nodes, zip(x, y)))
-        #print(f'{graph_layout}')
         g.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
         
         #add labels to the graph
@@ -78,8 +91,17 @@ graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-#graph.add_edge('0','4')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_undirected_edge('0', '1')
+graph.add_undirected_edge('0', '3')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('1', '4')
+graph.add_directed_edge('4', '5')
+graph.add_directed_edge('4', '6')
+graph.breadth_first_search('0')
+#graph.add_directed_edge('7','4')
+#graph.add_directed_edge('7','4')
 g1 = BokehGraph(graph)
 g1.show()
