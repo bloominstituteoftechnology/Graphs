e011e683f75f81448838f9f39591210e30d75082
Erica Thompson <ericathompson.nc@gmail.com>
2018-10-15 22:11:18 -0700

    Day 1

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,24 +1,84 @@
-"""
-General drawing methods for graphs using Bokeh.
-"""
+# """
+# General drawing methods for graphs using Bokeh.
+# """
+
+# from bokeh.io import show, output_file
+# from bokeh.plotting import figure
+# from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+#                           ColumnDataSource)
+
+# graph = {
+#     '0': {'1', '3'},
+#     '1': {'0'},
+#     '2': set(),
+#     '3': {'0'}
+# }
+
+# p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# p.line(x, y, legend="Temp.", line_width=2)
+
+# class BokehGraph:
+#     """Class that takes a graph and exposes drawing methods."""
+#     def __init__(self):
+#         pass  # TODO
+
+import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+from graph import Graph
 
-graph = {
-    '0': {'1', '3'},
-    '1': {'0'},
-    '2': set(),
-    '3': {'0'}
-}
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
 
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+N = len(graph.vertices)
+node_indices = list(graph.vertices)
 
-p.line(x, y, legend="Temp.", line_width=2)
+plot = figure(
+    title="Graph Layout Demonstration",
+    x_range=(-1.1, 10.1),
+    y_range=(-1.1, 10.1),
+    tools="",
+    toolbar_location=None,
+)
 
-class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+graph_renderer = GraphRenderer()
+
+graph_renderer.node_renderer.data_source.add(node_indices, "index")
+graph_renderer.node_renderer.data_source.add(Spectral8, "color")
+graph_renderer.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color="color")
+
+start_indices = []
+end_indices = []
+
+for vertex in graph.vertices:
+    for edge_end in graph.vertices[vertex]:
+        start_indices.append(vertex)
+        end_indices.append(edge_end)
+
+graph_renderer.edge_renderer.data_source.data = dict(
+    start=start_indices, 
+    end=end_indices)
+
+### start of layout code
+circ = [int(v) for v in graph.vertices]
+print("circ ", circ)
+x = [2 * (i // 3) for i in circ]
+y = [2 * (i % 3) for i in circ]
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot.renderers.append(graph_renderer)
+
+output_file("graph.html")
+show(plot)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,18 +1,99 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-import BokehGraph
+
+# x = [1, 2, 3, 4, 5]
+# y = [6, 7, 2, 4, 5]
+# # output to static HTML file
+# output_file("graph.html")
+
+# # create a new plot with a title and axis labels
+# p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# # add a line renderer with legend and line thickness
+# p.line(x, y, legend="Temp.", line_width=2)
+
+# # show the results
+# show(p)
+
+
+# output_file("graph.html")
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        graph = Graph()  # Instantiate your graph
-        graph.add_vertex('0')
-        graph.add_vertex('1')
-        graph.add_vertex('2')
-        graph.add_vertex('3')
-        graph.add_edge('0', '1')
-        graph.add_edge('0', '3')
-        print(graph.vertices)
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist!")
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+
+# class MatrixGraph:
+#     """ Adjacency Matrix Represeation of a graph"""
+#     def __init__(self, num_vertices):
+#         """edges"""
+#         self.matrix  = [[0] * num_vertices] * num_vertices
+#         self.vertices = [Vertex(str(i)) for i in range(num_vertices)]
+        
+#     def add_edge(self, start_index, end_index, bidirectional=True):
+#         """ Add an edge from start vertex to end vertex. """
+#         self.matrix[start_index][end_index] = 1
+#         """ to be bidirectional """
+#         if bidirectional:
+#             self.matrix[end_index][end_index] = 1
+        
+# class Vertex:
+#     def __init__(self, label):
+#         self.label = label
+#         self.edges = set()
+
+# class ListGraph:
+#     def __init__(self):
+#         self.vertices = set()        
+        
+        
+        
+        
+        
+        
+        
+        # graph = {
+#     '0': {'1', '3'},
+#     '1': {'0'},
+#     '2': set(),
+#     '3': {'0'}
+# }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 
