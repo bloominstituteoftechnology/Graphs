2955aa09eae607afd74a1a19f8621c5a7b7dd438
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-16 08:11:01 -0700

    graphs.py is working. Thought I installed bokeh correctly...but no. Will try again this morning.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,28 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+    
+    def add_edge(self, vertex, dest):
+        self.vertices[vertex].add(dest)
+        self.vertices[dest].add(vertex)
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+    
+    
+
+
+
+
+
+
