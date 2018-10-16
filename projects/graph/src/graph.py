dd31fb50bf4ff58f2c804f0f78a988428a2c2389
sliw <slioweng@gmail.com>
2018-10-16 18:09:51 +0800

    done graph's initiate randomly by inputting number of vertices and edges

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,6 +2,8 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+from random import randint
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self, vertices_list={}):
@@ -23,11 +25,21 @@ class Graph:
     
         self.vertices_list[from_vertex].add(to_vertex)
 
+    def randomise_graph(self, num_vertices, num_edges):
+        for vertex in range(0, num_vertices):
+            self.add_vertex(str(vertex))
+        
+        for edge in range(0, num_edges):
+            self.add_edge(str(randint(0, num_vertices - 1)), str(randint(0, num_vertices - 1)))
+
+        self.show_graph()
+
 graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '2')
-graph.add_edge('2', '3')
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '2')
+# graph.add_edge('2', '3')
+graph.randomise_graph(3, 4)
