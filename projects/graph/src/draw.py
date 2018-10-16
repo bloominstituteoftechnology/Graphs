6524fd1bfc150dead849e5bbbbaef86801dc93e1
Arthur Bates <aabatesj@gmail.com>
2018-10-16 17:39:28 -0400

    Arthur Bates
    latest. not much...yet

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,8 +7,20 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  # TODO
+        # output to static HTML file
+        output_file("graph1.html")
+
+# create a new plot with a title and axis labels
+        p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# add a line renderer with legend and line thickness
+        p.line(x, y, legend="Temp.", line_width=2)
+
+# show the results
+        show(p)
