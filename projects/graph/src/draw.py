c5a76d8aa3dc577a826e0c9cb387716b9e012073
Josue Peralta <jperal77@gmail.com>
2018-10-16 17:59:08 -0500

    Completed Part 2 and 3

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,14 +1,76 @@
-"""
-General drawing methods for graphs using Bokeh.
-"""
+import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, Circle
+from bokeh.palettes import Spectral8
+from graph import Graph
+
+
 
 
 class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+
+        N = len( graph.vertices )
+        node_indices = list(graph.vertices.keys())
+
+        plot = figure(title="Graph Layout Demonstration", x_range=(-7,7), y_range=(-7,7),
+                      tools="", toolbar_location=None)
+
+        graph_renderer = GraphRenderer()
+
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        # node_colors = ['red'] * N
+        # graph.node_renderer.data_source.add(node_colors, 'color')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color="red")
+
+        edge_start = []
+        edge_end = []
+
+        # O(E), where E is the total number of edges
+        for vertex_id in node_indices:
+            for v in graph.vertices[vertex_id].edges:
+                edge_start.append(vertex_id)
+                edge_end.append(v)
+
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=edge_start,
+            end=edge_end)
+
+        ### start of layout code
+        # circ = [i*2*math.pi/8 for i in node_indices]
+        # x = [math.cos(i) for i in circ]
+        # y = [math.sin(i) for i in circ]
+        x = []
+        y = []
+        for vertex_id in node_indices:
+            vertex = graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
+
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        plot.renderers.append(graph_renderer)
+
+        output_file('graph.html')
+        show(plot)
+
+
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+
+
+bg = BokehGraph(graph)
+bg.draw()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,22 +1,82 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+import random
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
+        """
+        Create an empty graph
+        """
         self.vertices = {}
+    def add_vertex(self, vertex_id):
+        """
+        Add an vertex to the graph
+        """
+        self.vertices[vertex_id] = Vertex(vertex_id)
+    def add_edge(self, v1, v2):
+        """
+        Add an undirected edge to the graph
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError("That vertex does not exist!")
+    def add_directed_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph
+        """
+        if v1 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+    def dft(self, starting_node, visited=None):
+        # Mark the node as visited
+        if visited is None:
+            visited = []
+        visited.append(starting_node)
+        # For each child, if that child hasn't been visited, call dft() on that node
+        # for child in children:
+        #    if child not in visited:
+                  # dft(child, visted)
+    def bft(self, starting_node):
+        # create an empty queue
+        q = Queue()
+        # Put starting vert in the queue
+        q.enqueue(starting_node)
+        visited = []
+            
+            # Remove the first node from the queue...
+            # If it has not been visited yet,...
+            # Mark it as visited....
+            # Then put all it's children in the back of the queue
 
-    def add_vertex(self, label):
-            self.vertices[label] = set()
-
-    def add_edge(self, vertex, edge):
-            self.vertices[vertex].add(edge)
-            self.vertices[edge].add(vertex)
 
 
-graph = Graph()
+
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None):
+        """
+        Create an empty vertex
+        """
+        self.id = vertex_id
+        self.edges = set()
+        if x is None:
+           self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+        if y is None:
+           self.y = random.random() * 10 - 5
+        else:
+           self.y = y
+    def __repr__(self):
+        return f"{self.edges}"
+
+
+graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
