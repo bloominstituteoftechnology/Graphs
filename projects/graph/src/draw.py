c3a5266f99b6d90caaa3e2662f0effd19cac413c
sliw <slioweng@gmail.com>
2018-10-16 17:55:10 +0800

    adjusted p2 for p3

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -21,28 +21,47 @@ class BokehGraph:
     """
     def show(self):
         vertices_list = self.graph.vertices_list
-        plot = figure(title = "Boken Graph", x_range = (-2, 2), y_range= (-2, 2),
-                    tools = "", toolbar_location = None)
-        x = [0, 1, 0, 1]
-        y = [0, 0, 1, 1]
-        # initiate vertice in square shape
-        plot.circle(x, y, size = 15, color = "grey")
-        # initiate edges
-        for vertex in vertices_list:
-            # if set is not empty
-            if len(vertices_list[vertex]) != 0:
-                to_vertices = list(vertices_list[vertex])
-                for to_vertex in to_vertices:
-                    # draw edge 
-                    plot.line([x[int(vertex)], x[int(to_vertex)]],
-                              [y[int(vertex)], y[int(to_vertex)]],
-                              line_width = 3,
-                              color = "grey")
+        N = len(vertices_list)
+        node_indices = list(range(N))
 
-        output_file("graph.html")
+        plot = figure (title = 'Graph showing vertices and edges', 
+                       x_range = (-2, 2), 
+                       y_range = (-2, 2),
+                       tools = '',
+                       toolbar_location = None)
+
+        graph = GraphRenderer()
+
+        graph.node_renderer.data_source.add(node_indices, 'index')
+        graph.node_renderer.glyph = Circle(size = 8, fill_color = "grey")
+
+        # initiate edges
+        from_edges = []
+        to_edges = []
+        
+        for vertex in vertices_list:
+            # set contains element
+            if len(vertices_list[vertex]) != 0:
+                to_vertices = vertices_list[vertex]
+                for to_vertex in to_vertices:
+                    from_edges.append(vertex)
+                    to_edges.append(to_vertex)
+
+        graph.edge_renderer.data_source.data = dict(start = from_edges, end = to_edges)
+
+        ### start of layout code
+        circ = [ i*2*math.pi/8 for i in node_indices ]
+        x = [ math.cos(i) for i in circ ]
+        y = [ math.sin(i) for i in circ ]
+
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph.layout_provider = StaticLayoutProvider(graph_layout = graph_layout)
+
+        plot.renderers.append(graph)
+
+        output_file('graph.html')
         show(plot)
 
 graph = Graph()
-# print(graph.show_graph())
 graph = BokehGraph(graph)
 graph.show()
