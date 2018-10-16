a393009c4f13caf9950c77d39521177e31bee29b
chainchompa <chainchompa@gmail.com>
2018-10-16 16:43:00 -0400

    graph now gets random colors

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,32 +1,55 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+import random
 
-
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None, value=None, color=None):
+        self.id = int(vertex_id)
+        self.x = x
+        self.y = y
+        self.value = value
+        self.color = color
+        self.edges = set()
+        if self.x is None:
+            self.x = 2 * (self.id // 3) + self.id / 10 * (self.id % 3)
+        if self.y is None:
+            self.y = 2 * (self.id // 3) + self.id / 10 * (self.id % 3)
+        if self.value is None:
+            self.value = self.id
+        if self.color is None:
+            hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+            colorString = "#"
+            for i in range(0, 3):
+                colorString += hexValues[random.randint(0,len(hexValues) - 1)]
+            self.color = colorString
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    def add_vertex(self, vertex):
-        if vertex in self.vertices:
+
+    def __str__(self):
+        return f"{self.vertices}"
+
+    def add_vertex(self, vertex_id):
+        if vertex_id in self.vertices:
             raise Exception('Error: That vertex already exists')
         else:
-            self.vertices[vertex] = set()
+            self.vertices[vertex_id] = Vertex(vertex_id)
+
     def add_edge(self, start, end):
         if start not in self.vertices:
             raise Exception('Error: Start vertex not found')
         if end not in self.vertices:
             raise Exception('Error: End vertex not found')
         else:
-            self.vertices[start].add(end)
-            self.vertices[end].add(start)
+            self.vertices[start].edges.add(end)
+            self.vertices[end].edges.add(start)
 
-
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
+    def add_directed_edge(self, start, end):
+        if start not in self.vertices:
+            raise Exception('Error: Start vertex not found')
+        if end not in self.vertices:
+            raise Exception('Error: End vertex not found')
+        else:
+            self.vertices[start].edges.add(end)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -3,14 +3,65 @@
 """
 Demonstration of Graph and BokehGraph functionality.
 """
+import math
 
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
+
+def getGraph(**kwargs):
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex(0)
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_edge(0, 1)
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)
+    graph.add_edge(4, 5)
+    graph.add_edge(5, 6)
+
+    bokeh_graph = BokehGraph(graph)
+    bokeh_graph.draw()
 
 
-def main():
-    pass  # TODO
+def getRandomGraph(numVerts, numEdges):
+    graph = Graph()
+
+    for vert_id in range(0, numVerts):
+        graph.add_vertex(vert_id)
+
+    allEdges = []
+    for i in range(0, numVerts):
+        for j in range(0, numVerts):
+            if i < j:
+                allEdges.append( (i, j) )
+    randomEdges = allEdges[:numEdges]
+    for edge in randomEdges:
+        graph.add_edge(edge[0], edge[1])
+
+    return graph
 
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+    style = 'default'
+    num_verts = 5
+    num_edges = 5
+
+    for arg in argv[1:]:
+        arg_split = arg.split("=")
+        if len(arg_split) == 2:
+            if arg_split[0] == "style":
+                style = arg_split[1].lower()
+            elif arg_split[0] == "verts":
+                num_verts = int(arg_split[1])
+            elif arg_split[0] == "edges":
+                num_edges = int(arg_split[1])
+    print(style)
+
+    getGraph(style=style, num_verts=num_verts, num_edges=num_edges)
+    getRandomGraph(5, 2)
