1e86737c8436c09e4888934a34472a1606da4927
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-16 15:16:41 -0500

    changes to graph py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,35 +1,65 @@
+import random
+
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-class Edge:     
-    def __init__(self, destination):
-        self.destination = destination
-
-    def __repr__(self):
-        return self.destination
-
 
 
 class Vertex:
-    def __init__(self, label):
-        self.label = label
+    def __init__(self, vertex_id, x=None, y=None):
+        """
+        Create an empty vertex
+        """
+        self.id = vertex_id
         self.edges = set()
+
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+
+    def __repr__(self):
+        return f"{self.edges}"
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
+        """
+        Create an empty graph
+        """
         self.vertices = {}
 
-    def add_vertex(self, label):
-        if not label in self.vertices:
-            new_vertex = Vertex(label)
-            self.vertices[label] = new_vertex.edges
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertext to the graph
+        """
+        self.vertices[vertex_id] = Vertex(vertex_id)
 
-    def add_edge(self, label, destination):
-        # if label in self.vertices and des
-        new_edge = Edge(destination)
-        self.vertices[label].add(new_edge)
-        
+    def add_edge(self, v1, v2):
+        """
+        Add an undirected edge to the graph
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError("That vertext does not exist!")
+
+    def add_directed_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph
+        """
+        if v1 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertext does not exist!")
 
 
 
