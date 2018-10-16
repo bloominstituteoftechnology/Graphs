7e0f4083b303766c9aa0557f125062ecd4f60f6c
codejoncode <jonathanjholloway@gmail.com>
2018-10-16 18:25:51 -0500

    I have the lines showing up on the graph now

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -43,7 +43,7 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    #ability to add to the dict needed 
+    
     def add_vertex(self, value):
         new_vertex = Vertex(value)
         self.vertices[value] = new_vertex
@@ -51,35 +51,16 @@ class Graph:
         
         
     def add_edge_one_way(self, from_vertex, to_vertex):
-        # new_edge = Edge(to_vertex)
-        # from_vertex.edges.add(new_edge)#using sets must use add() method 
-        # 
-        
-        
-        #self.graph_dict[from_vertex] = from_vertex.edges
-
-        
-        #add to the edges then we swap the edges that is there
-        #out with self.edges though adding might be quicker.
-        #not sure copy o(N)  but add is O(1)
         if from_vertex in self.vertices and to_vertex in self.vertices:
-            self.vertices[from_vertex].edges.add(to_vertex)
+            self.vertices[from_vertex].add_edge(to_vertex)
         else:
             raise IndexError("That vertex does not exist!")
 
-        #went with this out of being unsure of line 35
-        #I know adding to the set is O(1) for sure though. 
-        # A seperate edge case is needed for if to_vertex
-        #links from_vertex   this method only handles 
-        #one direction
     def add_edge_two_way(self, vertex1, vertex2):
         """
         This is a bidirectional method to the edges.
         """
-        # new_edge = Edge(vertex1)
-        # new_edge2 = Edge(vertex2)
-        # vertex1.edges.add(vertex2)
-        # vertex2.edges.add(vertex1)
+       
         self.add_edge_one_way(vertex1, vertex2)
         self.add_edge_one_way(vertex2, vertex1)
 
@@ -90,7 +71,8 @@ class BokehGraph:
         self.color = color
 
     def show(self):
-        N = len(self.graph.vertices.keys()) #length of vertices
+        N = len(self.graph.vertices) #length of vertices
+        print(self.graph.vertices)
         vertex_indices = list(self.graph.vertices.keys())
 
         plot = figure(title="Random Generated Graph", x_range=(-7,7), y_range=(-7,7),
@@ -99,18 +81,20 @@ class BokehGraph:
         graph_renderer = GraphRenderer()
 
         graph_renderer.node_renderer.data_source.add(vertex_indices, 'index')
-        # node_colors = ["blue"] * N
-        # graph.node_renderer.data_source.add(node_colors, 'color')
+        
 
         graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color= self.color)
 
         graph_renderer.edge_renderer.data_source.data = dict(
-            start=[0]*N,
+            start=[vertex_indices[0]] * N,
+            end= vertex_indices
+        )
+        d = dict(
+            start=[vertex_indices[0]] * N,
             end=vertex_indices
         )
-        # circ =[i*2*math.pi/len(self.graph.vertices.keys()) for i in vertex_indices]
-        # x = [math.cos(i) for i in circ]
-        # y = [math.sin(i) for i in circ]
+        print(d)
+        
         x = []
         y = []
 
@@ -123,18 +107,9 @@ class BokehGraph:
         graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
         plot.renderers.append(graph_renderer)
-        output_file('graph.html')
+        output_file('random.html')
         show(plot)
 
-# test = Graph()
-# vertex_one = test.add_vertex("0")
-# vertex_two = test.add_vertex("1")
-# vertex_three = test.add_vertex("2")
-# test.add_edge_two_way(vertex_one, vertex_two)
-# test.add_edge_two_way(vertex_two, vertex_three)
-# test_Bokeh = BokehGraph(test)
-
-# test_Bokeh.show()
 
 def main():
     colors = ["blue", "red", "orange", "yellow"]
@@ -149,7 +124,7 @@ def main():
     vertices_count = 0
     edges_count = 0
 
-    while vertices_count < num_vertices and edges_count < num_edges:
+    while vertices_count <= num_vertices and edges_count <= num_edges:
         new_vertex = potential_labels[vertices_count]
         graph.add_vertex(new_vertex)
         if vertices_count > 0:
