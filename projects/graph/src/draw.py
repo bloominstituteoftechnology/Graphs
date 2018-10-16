5caf8c474fed99575a34f3dcd6db355ee229d1db
Kyle Meltzer <jimmy13@Kyles-MacBook-Pro.local>
2018-10-16 16:19:42 -0500

    completerecursive dfs

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -4,10 +4,10 @@ General drawing methods for graphs using Bokeh.
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, Oval, LabelSet,
                           ColumnDataSource)
-
-
+from bokeh.palettes import Spectral8
+from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
@@ -16,3 +16,16 @@ class BokehGraph:
         pass  # TODO
 
 
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '2')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+print(graph.dfs('0'))
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -61,13 +61,19 @@ class Graph:
         else:
             raise IndexError("That vertext does not exist!")
 
+    def dfs(self, starting_vertex, visited = None):
+        """
+        Mark vertex as visited
+        """
+        if visited is None:
+            visited = []
+        visited.append(starting_vertex)
+        """
+        For each child, if that child hasn't been visited, call dft() on that vertex
+        """
+        for child in self.vertices[starting_vertex].edges:
+            if child not in visited:
+                self.dfs(child, visited)
+        return visited
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
