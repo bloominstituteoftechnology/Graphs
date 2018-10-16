64628fc1ea1841e11fde6e2791a5d83ba39c098d
sca4cs <shannon.atkinson.ca@gmail.com>
2018-10-16 15:43:15 -0400

    updated graph class, added vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,28 +1,42 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+import random
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, label):
-        if not label in self.vertices:
-            self.vertices[label] = set()
+    def add_vertex(self, vertex_id):
+        if not vertex_id in self.vertices:
+            self.vertices[vertex_id] = Vertex(vertex_id)
 
-    def add_edge(self, vertex, destination):
-        if vertex in self.vertices and destination in self.vertices:
-            self.vertices[vertex].add(destination)
-            self.vertices[destination].add(vertex)
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
 
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '4')
-print(graph.vertices)
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None):
+        self.id = vertex_id
+        self.edges = set()
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+            
+    def __repr__(self):
+        return f"{self.edges}, x = {round(self.x, 2)}, y = {round(self.y, 2)}"
