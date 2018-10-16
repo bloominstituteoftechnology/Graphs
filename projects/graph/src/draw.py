886f937f3c40d23c92075c47f08f86e9d4bdbee1
farste <sfargali@gmail.com>
2018-10-16 17:24:09 -0400

    Stephen Fargali Refactoring graph to work with draw

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -2,39 +2,80 @@
 General drawing methods for graphs using Bokeh.
 """
 
+
+import math
+
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, Circle
+from bokeh.palettes import Spectral8
+from graph import Graph
+
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        self.vertices = {}
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
 
-    def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = Vertex(vertex_id)
+        N = len( graph.vertices )
+        node_indices = list(graph.vertices.keys())
 
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)
-            self.vertices[v2].edges.add(v1)
-        else:
-            raise IndexError("That vertex does not exist!")
+        plot = figure(title="Graph Layout Demonstration", x_range=(-7,7), y_range=(-7,7),
+                      tools="", toolbar_location=None)
 
-class Vertex:
-    def __init__(self, vertex_id):
-        self.id = vertex_id
-        self.edges = set()
-    def __repr__(self):
-        return f"{self.edges}"
+        graph_renderer = GraphRenderer()
 
-graph = BokehGraph()  # Instantiate your graph
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        # node_colors = ['red'] * N
+        # graph.node_renderer.data_source.add(node_colors, 'color')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color="red")
+
+        edge_start = []
+        edge_end = []
+
+        # O(E), where E is the total number of edges
+        for vertex_id in node_indices:
+            for v in graph.vertices[vertex_id].edges:
+                edge_start.append(vertex_id)
+                edge_end.append(v)
+
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=edge_start,
+            end=edge_end)
+
+        ### start of layout code
+        # circ = [i*2*math.pi/8 for i in node_indices]
+        # x = [math.cos(i) for i in circ]
+        # y = [math.sin(i) for i in circ]
+        x = []
+        y = []
+        for vertex_id in node_indices:
+            vertex = graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
+
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        plot.renderers.append(graph_renderer)
+
+        output_file('graph.html')
+        show(plot)
+
+
+
+
+graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
-print(graph.vertices)
+
+
+bg = BokehGraph(graph)
+bg.draw()
