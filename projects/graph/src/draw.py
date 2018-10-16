f441d01a240020048a88685b84c669ad139437ac
keegn <keegan.t.burkett@gmail.com>
2018-10-16 17:50:18 -0500

    Working on BokehGraph

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -6,9 +6,16 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
-
+from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        """
+        Store number of vertices in graph in variable V
+        """
+        V = len(graph.vertices)
+        
