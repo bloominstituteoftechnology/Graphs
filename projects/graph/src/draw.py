525e1dbb4e73d9ff16f1f7f474466e385bd61917
Kairi <kairijb@gmail.com>
2018-10-16 17:47:43 -0500

    started on today's assignment and added a vertex class

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,14 +1,59 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
+import math
+from graph import Graph
+from bokeh.io import show, output_file, show
+from bokeh.plotting import figure, output_file, show
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
+from bokeh.palettes import Spectral8
 
-
-class BokehGraph:
+class BokehGraph(Graph):
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        N = 8
+        node_indices = list(range(N))
+
+        plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+                    tools="", toolbar_location=None)
+
+        graph = GraphRenderer()
+
+        graph.node_renderer.data_source.add(node_indices, 'index')
+        graph.node_renderer.data_source.add(Spectral8, 'color')
+        graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color="color")
+
+        graph.edge_renderer.data_source.data = dict(
+            start=[0]*N,
+            end=node_indices)
+
+        ### start of layout code
+        circ = [i*2*math.pi/8 for i in node_indices]
+        x = [math.cos(i) for i in circ]
+        y = [math.sin(i) for i in circ]
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        ### Draw quadratic bezier paths
+        def bezier(start, end, control, steps):
+            return [(1-s)**2*start + 2*(1-s)*s*control + s**2*end for s in steps]
+
+        xs, ys = [], []
+        sx, sy = graph_layout[0]
+        steps = [i/100. for i in range(100)]
+        for node_index in node_indices:
+            ex, ey = graph_layout[node_index]
+            xs.append(bezier(sx, ex, 0, steps))
+            ys.append(bezier(sy, ey, 0, steps))
+        graph.edge_renderer.data_source.data['xs'] = xs
+        graph.edge_renderer.data_source.data['ys'] = ys
+
+        plot.renderers.append(graph)
+
+        output_file("graph.html")
+        show(plot)
+
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,16 +7,32 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self, vertices):
         self.vertices = vertices
-    def print_vertices(self):
-        print(self.vertices)
+        # self.vertices = {}
+    # def add_vertex(self,vertex_id):
+    #     self.vertices[vertex_id] = Vertex(vertex_id)
     def add_vertex(self,vertex):
         self.vertices.update({str(vertex): set()})
-        print(self.vertices)
-    def add_edge(self, vertex, nodes):
-        self.vertices.update({str(vertex): {str(node) for node in nodes} })
-        print(self.vertices)
-# print(Graph({'0': {'1', '3'},'1': {'0'},'2': set(),'3': {'0'}}).vertices)
+        print(f"Second line -> {self.vertices}")
+    # def add_edge(self, v1, v2):
+    #     if v1 in self.vertices and v2 in self.vertices:
+    #         self.vertices.edges.add(v2)
+    #         self.vertices.edges.add(v1)
+    #     else:
+    #         raise (IndexError("That vertex doesn't exist"))
+    def add_edge(self, vertex, edge):
+        self.vertices.update({str(vertex): {str(node) for node in edge} })
+        print(f"Third line -> {self.vertices}")
+
+# class Vertex:
+#     def __init__(self, vertex_id):
+#         self.id = vertex_id
+#         self.edges = set()
+#     def __repr__(self):
+#         return f"{self.edges}"
+
 graph = Graph({'0': {'1', '3'},'1': {'0'},'2': set(),'3': {'0'}})
-graph.add_vertex(8)
-graph.add_edge(3, [1,2,3])
+
+print(f"First line -> {graph.vertices}") #First line
+graph.add_vertex(8) #Second Line
+graph.add_edge(2, [1,2,3]) #Third line
 
