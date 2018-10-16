9321875c4b860a1d0a01c02a17aa1f327a1a1678
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-16 15:55:57 -0400

    completed inital bokeh implementation

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,12 +5,28 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
+
+def getGraph():
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_vertex('4')
+    graph.add_vertex('5')
+    graph.add_vertex('6')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '6')
+    graph.add_edge('2', '3')
+    graph.add_edge('3', '6')
+    graph.add_edge('4', '5')
+    graph.add_edge('5', '6')
 
 
-def main():
-    pass  # TODO
 
+    bokeh_graph = BokehGraph(graph)
+    bokeh_graph.draw()
 
-if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+getGraph()
