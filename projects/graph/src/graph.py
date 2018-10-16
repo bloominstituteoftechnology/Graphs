df28e2689920b5550b3ee5468aadcbdd20b73be8
sliw <slioweng@gmail.com>
2018-10-16 18:31:19 +0800

    improve randomise_graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -22,15 +22,30 @@ class Graph:
         if to_vertex not in self.vertices_list:
             print(f"⚠️ Vertice {to_vertex} is not in the graph")
             return False
+        # if edge already in set
+        if to_vertex in self.vertices_list[from_vertex]:
+            return False
     
         self.vertices_list[from_vertex].add(to_vertex)
+        return True
 
     def randomise_graph(self, num_vertices, num_edges):
+        # v * v edges to fill all vertices 
+        # including vertex pointing itself
+        if num_edges > num_vertices * num_vertices:
+            print(f"⚠️ Number of edges exceeds maximum")
+
         for vertex in range(0, num_vertices):
             self.add_vertex(str(vertex))
-        
-        for edge in range(0, num_edges):
-            self.add_edge(str(randint(0, num_vertices - 1)), str(randint(0, num_vertices - 1)))
+
+        edge = 0
+
+        # while loop to create new edge
+        while edge < num_edges:
+            from_vertex = str(randint(0, num_vertices - 1))
+            to_vertex = str(randint(0, num_vertices - 1))
+            if self.add_edge(from_vertex, to_vertex):
+                edge += 1 
 
         self.show_graph()
 
@@ -42,4 +57,4 @@ graph = Graph()  # Instantiate your graph
 # graph.add_edge('0', '1')
 # graph.add_edge('0', '2')
 # graph.add_edge('2', '3')
-graph.randomise_graph(3, 4)
+graph.randomise_graph(3, 5)
