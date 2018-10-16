428eb9bca1e050d754b75330ea6df422f9eb5220
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-16 15:34:14 -0500

    refactored Graph class and added a Vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,17 +1,40 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+import random
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    def add_vertex(self, vertex):
-        self.vertices[vertex] = set()
+    def add_vertex(self, vert_id):
+        self.vertices[vert_id] = Vertex(vert_id)
     def add_edge(self, vert1, vert2):
-        self.vertices[vert1].add(vert2)
-        self.vertices[vert2].add(vert1)
+        if vert1 in self.vertices and vert2 in self.vertices:
+            self.vertices[vert1].edges.add(vert2)
+            self.vertices[vert2].edges.add(vert1)
+        else:
+            raise IndexError('Vertex does not exist')
+    def add_directed_edge(self, vert1, vert2):
+        if vert1 in self.vertices:
+            self.vertices[vert1].edges.add(vert2)
+        else:
+            raise IndexError('Vertex does not exist')
+
+class Vertex:
+    def __init__(self, vert_id, x=None, y=None):
+        self.id = vert_id
+        self.edges = set()
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+    def __repr__(self):
+        return f"{self.edges}"
 
 
 graph = Graph()  # Instantiate your graph
