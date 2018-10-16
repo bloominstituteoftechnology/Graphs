ccc6a940600c20e41eeac4824989692568758c37
jaymzsocool <james.mcraejr@gmail.com>
2018-10-16 18:35:50 -0400

    bokeh working, random graph according to lecture

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,50 +1,64 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-
+import math
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet, Oval,
                           ColumnDataSource)
-
+from bokeh.palettes import Spectral8
+from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        N = len(graph.vertices)
+        node_indices = list(graph.vertices.keys())
 
-import math
+        plot = figure(title='Graph Layout Demonstration', x_range=(0,10), y_range=(0,10),
+                    tools='', toolbar_location=None)
 
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
-from bokeh.palettes import Spectral8
+        graph_renderer = GraphRenderer()
 
-N = 8
-node_indices = list(range(N))
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.data_source.add(Spectral8, 'color')
+        graph_renderer.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
-              tools='', toolbar_location=None)
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=[0]*N,
+            end=node_indices)
 
-graph = GraphRenderer()
+        ### start of layout code
+        # circ = [i*2*math.pi/8 for i in node_indices]
+        # x = [math.cos(i) for i in circ]
+        # y = [math.sin(i) for i in circ]
+        x = []
+        y = []
+        for vertex_id in node_indices:
+            vertex = graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
 
-graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+        plot.renderers.append(graph_renderer)
 
-### start of layout code
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+        output_file('graph.html')
+        show(plot)
 
-graph_layout = dict(zip(node_indices, zip(x, y)))
-graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
 
-plot.renderers.append(graph)
+bg = BokehGraph(graph)
+bg.draw()
 
-output_file('graph.html')
-show(plot)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,24 +1,58 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+import random
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
+        """
+        Create an empty graph
+        """
         self.vertices = {}
     def add_vertex(self, vertex_id):
+        """
+        Add an vertex to the graph
+        """
         self.vertices[vertex_id] = Vertex(vertex_id)
-    def add_edge(self,v1, v2):
+    def add_edge(self, v1, v2):
+        """
+        Add an undirected edge to the graph
+        """
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)  
-            self.vertices[v2].edges.add(v1) 
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
         else:
-            raise IndexError("That Vertex does not exist")
+            raise IndexError("That vertex does not exist!")
+    def add_directed_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph
+        """
+        if v1 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
 class Vertex:
-    def __init__(self, vertex_id):
+    def __init__(self, vertex_id, x = None, y = None):
         self.id = vertex_id
         self.edges = set()
+        if x is None:
+            self.x = random.random() * 10
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10
+        else:
+            self.y = y
     def __repr__(self):
         return f"{self.edges}git"
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
