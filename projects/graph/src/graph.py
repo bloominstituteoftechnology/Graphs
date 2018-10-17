040103a608958b2711ff00ab5386e763558bb835
William <kwontaewoongah@gmail.com>
2018-10-16 22:37:18 -0700

    refactored graph.py to follow lecture code

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,14 +7,19 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices={}
-    def add_vertex(self,vertex):
-        self.vertices[vertex]=set()
-    def add_edge(self,vertex,edge):
-        try:
-            if self.vertices[edge] is not None:
-                self.vertices[vertex].add(edge)
-                self.vertices[edge].add(vertex)
-        except:
-            print('No such vertex exists.')
+    def add_vertex(self,vertex_id):
+        self.vertices[vertex_id]=Vertex(vertex_id)
+    def add_edge(self,v1,v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError('That vertex does not exist.')
         
 
+class Vertex:
+    def __init__(self,vertex_id):
+        self.id=vertex_id
+        self.edges=set()
+    def __repr__(self):
+        return f'{self.edges}'
