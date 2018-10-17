9d3778465f9528f2ee1009b295b8602a76e5bc27
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-17 14:10:19 -0700

    Vertex class in graph.py, draw.py rendering graph with correct number/type of vertexes and edges

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -3,6 +3,7 @@ General drawing methods for graphs using Bokeh.
 """
 import math
 
+from graph import Graph
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
@@ -15,34 +16,67 @@ from bokeh.palettes import Spectral8
 #
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-       N = 8
-node_indices = list(range(N))
+    def __init__(self, graph):
+        self.graph = graph
+        self.start_verts = []
+        self.end_verts = []
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
-              tools='', toolbar_location=None)
+    def generate_edges(self):
+        """
+        {
+            '0', {'1', '3'},
+            '1', {'0'}
+        }
+        """
+        for key in self.graph.vertices:
+            for edge in self.graph.vertices[key].edges:
+                self.start_verts.append(key)
+                self.end_verts.append(edge)
 
-graph = GraphRenderer()
+        print('self.start_verts:', self.start_verts)
+        print('self.end_verts:', self.end_verts)
 
-graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+    def draw(self):
+        # N = 8
+        self.generate_edges()
+        node_indices = list(self.graph.vertices.keys())
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+        plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+                    tools='', toolbar_location=None)
 
-### start of layout code
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+        graph = GraphRenderer()
 
-graph_layout = dict(zip(node_indices, zip(x, y)))
-graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+        graph.node_renderer.data_source.add(node_indices, 'index')
+        # graph.node_renderer.data_source.add(Spectral8, 'color')
+        graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='pink')
 
-plot.renderers.append(graph)
+        # start = [0, 0, 1, 3]
+        # end = [1, 3, 0, 0]
+        graph.edge_renderer.data_source.data = dict(
+            start=self.start_verts,
+            end=self.end_verts)
 
-output_file('graph.html')
-show(plot) 
+        ### start of layout code
+        circ = [int(i)*2*math.pi/8 for i in node_indices]
+        x = [math.cos(i) for i in circ]
+        y = [math.sin(i) for i in circ]
 
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+        plot.renderers.append(graph)
+
+        output_file('graph.html')
+        show(plot) 
+
+graph = Graph()
+# TODO: Add vertices and edges here
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+bokeh = BokehGraph(graph)
+bokeh.draw()
 
