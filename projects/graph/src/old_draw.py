65fe91af5e147ae72f76eab966e04ac419eafb07
bri79319 <bri79319@gmail.com>
2018-10-17 17:29:48 -0400

    looking at brady's demo code and tweeking my code

--- projects/graph/src/old_draw.py
+++ projects/graph/src/old_draw.py
@@ -12,8 +12,7 @@ from bokeh.models import (
     LabelSet,
     ColumnDataSource,
 )
-
-# from bokeh.palettes import Spectral4
+from bokeh.palettes import Spectral4
 from graph import Graph
 
 
@@ -22,29 +21,12 @@ class BokehGraph:
 
     def __init__(self, graph):
         self.graph = graph
-        self.keys = []
-        self.values = []
-        """
-        {
-            '0': {'1', '3'},
-            '1': {'0'},
-            '2': set(),
-            '3': {'0'}
-        }
-        """
-        for key in self.graph.vertices:
-            for edge in self.graph.vertices[key]:
-                self.keys.append(key)
-                self.values.append(edge)
-
-        print("keys:", self.keys)
-        print("values:", self.values)
 
     def draw(self):
-        # N = 4
+        N = 4
 
-        node_indices = list(self.graph.vertices.keys())
-        print("Node indicies", node_indices)
+        node_indices = list(range(N))
+        # print("N ", node_indices)
 
         plot = figure(
             title="Graph Layout Demonstration",
@@ -59,19 +41,17 @@ class BokehGraph:
 
         g = graph.node_renderer.data_source.add(node_indices, "index")
         # print("NODE_INDICES: ", g)
-        # c = graph.node_renderer.data_source.add(Spectral4, "color")
+        c = graph.node_renderer.data_source.add(Spectral4, "color")
         # print("COLORS: ", c)
-        graph.node_renderer.glyph = Oval(height=0.1, width=0.1, fill_color="pink")
+        graph.node_renderer.glyph = Oval(height=0.1, width=0.1, fill_color="color")
 
         # start is a list that has N elements. Each element is '0'.
-        # start=[0, 0, 1, 3] end=[1, 3, 0, 0]
-        d = graph.edge_renderer.data_source.data = dict(
-            start=self.keys, end=self.values
-        )
+        # node_indicies is
+        d = graph.edge_renderer.data_source.data = dict(start=[0] * N, end=node_indices)
         # print("DICT: ", d)
 
         ### start of layout code
-        circ = [int(i) * 2 * math.pi / 4 for i in node_indices]
+        circ = [i * 2 * math.pi / 4 for i in node_indices]
         x = [math.cos(i) for i in circ]
         y = [math.sin(i) for i in circ]
         # print("CIRC, X, Y", circ, x, y)
