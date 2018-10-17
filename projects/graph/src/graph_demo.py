3161c1bd6fd9e7f4cc1c3b1066d5aa84d2738006
Erik Kimsey <erik@erikkimsey.com>
2018-10-17 17:24:43 -0400

    Completed minimum Part 3: completed Network_Graph_Builder class

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -6,9 +6,10 @@ Demonstration of Graph and BokehGraph functionality.
 from graph import Graph
 from draw import BokehGraph
 from sys import argv
+import random
 
 
-####################################
+#############################################
 # TODO::
 # Build Demo classm
 # ^^ takes:
@@ -33,22 +34,49 @@ from sys import argv
 #############################################
 
 class Network_Graph_Builder:
-    def __init__(self, num_vertices, num_edges):
-        self.num_vertices = num_vertices
-        if(num_edges > num_vertices):
+    def __init__(self, num_v, num_e):
+        self.num_v = num_v
+        if(num_e > num_v):
             print("Cannot have more edges than vertices")
         else:
-            self.num_edges = num_edges
+            self.num_e = num_e
+        self.grf = Graph()
+        self.verts = self.grf.get_vertices()
+        self.bok = BokehGraph()
+
+    def create_vertices(self):
+        for v in range(len(self.num_v)):
+            self.grf.add_vertex(v)
+
+    def create_edges(self):
+        for ez in range(self.num_e):
+            self.grf.add_edge(self.rndm_int(self.num_v),self.rndm_int(self.num_v))
+
+    def get_rndm_vertex(self):
+        rndm_int =  random.randint(self.num_v)
+        verts = self.grh.get_vertices()
+        return verts[rndm_int]
+
+    def rndm_int(self, qty):
+        return random.randint(qty)
+
+    def init_graph(self):
+        self.create_vertices()
+        self.create_edges()
 
 
 
 
 
-def main():
+# def main():
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     # TODO - parse argv
+#     main()
 
 
-
-
-if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+ntx = Network_Graph_Builder(5,5)
+ntx.init_graph()
