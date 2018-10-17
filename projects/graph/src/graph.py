f75dfaf6b0758ad43b983e7f17d5348e5510daaa
rverdi642 <rverdi642@gmail.com>
2018-10-16 19:48:45 -0500

    Richard Verdier - Part 2 Render complete

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,14 +1,18 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+import random
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self,label):
+    def __init__(self):
+     #   self.id
         self.vertices = {}
-        self.label = label
+        vertices = self.vertices
+        # self.label = label
         self.edges = ()
+    # self.vertices[key] = Vertex[key]
+    #  key is vertex_id
 
     def add_vertex(self,key):
         vertex = Vertex(key)
@@ -17,25 +21,55 @@ class Graph:
     def get_vertex(self,key):
         return self.vertices[key]
 
-    def add_edge(self, srcKey, destKey, weight):
-        self.vertices[srcKey].addNextEdge(self.vertices[destKey],weight)
-
-    def Edge_doesNot_exist(self, srcKey, destKey, weight):
-        return self.vertices[srcKey].whoDoes_itPointTo(self.vertices[destKey], weight)
-
-    def __iter__(self):
-        return iter(self.vertices.values())
+    def add_edge(self, srcKey, destKey):
+        if srcKey in self.vertices and destKey in self.vertices:
+            self.vertices[srcKey].edges.add(destKey)
+            self.vertices[destKey].edges.add(srcKey)
+        else:
+            raise IndexError("That vertex does not exist!")
+    # def add_edge(self, srcKey, destKey):
+    #     self.vertices[srcKey].addNextEdge(self.vertices[destKey])
+    #
+    # def Edge_doesNot_exist(self, srcKey, destKey):
+    #     return self.vertices[srcKey].whoDoes_itPointTo(self.vertices[destKey])
+    #
+    # def __iter__(self):
+    #     return iter(self.vertices.values())
 
 class Vertex:
-    def __init__(self, key):
+    def __init__(self, key, x=None, y=None):
         self.key = key
+        self.edges = set()
         self.points_to = {}
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+    # def addNextedge(self, dest):
+    #     self.points_to = dest
+    #
+    # def getNextEdge(self):
+    #     return self.points_to.keys
+    #
+    # def whoDoes_itPointTo(self,dest):
+    #     return dest in self.points_to
 
-    def addNextedge(self, dest, weight):
-        self.points_to[dest] = weight
+    def __repr__(self):
+        return f"{self.edges}"
 
-    def getNextEdge(self):
-        return self.points_to.keys
-
-    def whoDoes_itPointTo(self,dest):
-        return dest in self.points_to
+graph = Graph() # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+print(graph.vertices)
