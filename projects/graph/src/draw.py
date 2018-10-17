4f1d059479b3bb597a878a710f12f2b2bc2828a3
jaymzsocool <james.mcraejr@gmail.com>
2018-10-17 18:27:10 -0400

    following lecture example

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -58,15 +58,15 @@ class BokehGraph:
         output_file('graph.html')
         show(plot)
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# print(graph.vertices)
 
-bg = BokehGraph(graph)
-bg.draw()
+# bg = BokehGraph(graph)
+# bg.draw()
 
