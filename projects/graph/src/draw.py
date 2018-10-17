d42531cba10a6e10b208ee8609e813ca1fcd73f7
William <kwontaewoongah@gmail.com>
2018-10-17 13:26:09 -0700

    implemented graph_demo.py similar to lecture video sans createdefaultgraph

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -44,14 +44,3 @@ class BokehGraph:
 
         output_file('graph.html')
         show(plot)
-
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-
-bg=BokehGraph(graph)
-bg.draw()
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,12 +5,39 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from graph import Graph
+from draw import BokehGraph
+import random
 
+def create_random_graph(num_nodes,num_edges):
+    graph=Graph()
+    possible_edges=[]
+    for i in range(num_nodes):
+        for j in range(i+1,num_nodes):
+            possible_edges.append((i,j))
+    random.shuffle(possible_edges)
+    edges=possible_edges[:num_edges]
 
-def main():
-    pass  # TODO
+    for i in range(num_nodes):
+        graph.add_vertex(i)
+    for edge in edges:
+        graph.add_edge(edge[0],edge[1])
+    bg=BokehGraph(graph)
+    bg.draw()
 
+def main(num_nodes,num_edges):
+    create_random_graph(num_nodes,num_edges)
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main()
+    num_nodes=5
+    num_edges=5
+    for arg in argv[1:]:
+        arg=arg.split('=')
+        if len(arg)==2:
+            if arg[0]=='nodes':
+                num_nodes=int(arg[1])
+            elif arg[0]=='edges':
+                num_edges=int(arg[1])
+            else:
+                print('Command invalid.')
+    main(num_nodes,num_edges)
