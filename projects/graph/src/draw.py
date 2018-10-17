79b48e55ea50168f39b29f13a8fe916e5895cd63
Erik Kimsey <erik@erikkimsey.com>
2018-10-17 10:27:18 -0400

    Created graph by instantiated Graph and BokehGraph classes, applying own data.

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -4,7 +4,7 @@ General drawing methods for graphs using Bokeh.
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource, Diamond)
+from bokeh.models import (GraphRenderer, Diamond, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource, Diamond)
 from graph import Graph
 from bokeh.models.glyphs import Oval
 import random
@@ -29,33 +29,36 @@ class BokehGraph:
 
     def create_node_locations(self):
         for i in range(len(self.graph_data)):
-            x_val = random.randint(0,len(self.graph_data))
-            y_val = random.randint(0, len(self.graph_data))
+            x_val = random.randint(-len(self.graph_data), len(self.graph_data))
+            y_val = random.randint(-len(self.graph_data), len(self.graph_data))
             self.node_x.append(x_val)
             self.node_y.append(y_val)
-        return
+            self.node_y.append(y_val)
+
 
     def draw(self):
         self.create_edge_lists()
         self.create_node_locations()
-        # create plot = figure
-        v_len = len(self.graph_data)
-        plot = figure(title="Lil Sumpin", x_range=(-v_len-3, v_len+3))
 
-        # create GraphRenderer
+        v_len = len(self.graph_data)
+        plot = figure(title="Lil Sumpin", x_range=(-v_len-3, v_len+3), y_range=(-v_len-3, v_len+3), tools='', toolbar_location=None)
+
         graphR = GraphRenderer()
 
-        # apply render nodes to graph
-        graphR.node_renderer.data_source.add(self.node_x, 'index')
-        graphR.node_renderer.data_source.add('black', 'color')
-        graphR.node_renderer.glyph = Oval(height=0.5, width=0.4, fill_color='color')
 
-        # apply render edges to graph
+        graphR.node_renderer.data_source.add(self.edge_x, 'index')
+
+        graphR.node_renderer.glyph = Circle(radius=0.5, fill_color="red")
+
+        print(f"edge_x:::::::::{ self.edge_x }")
+        print(f"edge_y:::::::::{ self.edge_y }")
+
         graphR.edge_renderer.data_source.data = dict(
             start=self.edge_x,
             end=self.edge_y)
 
         graph_layout = dict(zip(self.edge_x, zip(self.node_x, self.node_y)))
+        print(f"graph layout:::::::::{graph_layout}")
 
         graphR.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
@@ -65,8 +68,8 @@ class BokehGraph:
         show(plot)
 
 
-
-g = Graph()  # Instantiate your graph
+#
+g = Graph()
 g.add_vertex('0')
 g.add_vertex('1')
 g.add_vertex('2')
@@ -81,7 +84,6 @@ g.add_edge('5', '2')
 g.add_edge('6', '1')
 g.add_edge('4', '6')
 g.add_edge('1', '6')
-print(g.vertices)
 
 bok = BokehGraph(g)
 bok.draw()
