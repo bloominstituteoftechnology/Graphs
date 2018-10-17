649d6008438f652c4e722f98d2fd5775cd714029
William <kwontaewoongah@gmail.com>
2018-10-17 01:23:55 -0700

    added code to draw with bokeh

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,11 +1,9 @@
 """
 General drawing methods for graphs using Bokeh.
 """
-import math
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, LabelSet,ColumnDataSource,Oval
-from bokeh.palettes import Spectral8
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle)
 from graph import Graph
 
 
@@ -15,26 +13,32 @@ class BokehGraph:
         self.graph=graph
     def draw(self):
         graph=self.graph
-        N = 8
-        node_indices = list(range(N))
+        N = len(graph.vertices)
+        vertex_indices = list(graph.vertices.keys())
 
-        plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),tools='', toolbar_location=None)
-        graph = GraphRenderer()
-        graph.node_renderer.data_source.add(node_indices, 'index')
-        graph.node_renderer.data_source.add(Spectral8, 'color')
-        graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
-        graph.edge_renderer.data_source.data = dict(
+        plot = figure(title='Graph Layout Demonstration', x_range=(-7,7), y_range=(-7,7),tools='', toolbar_location=None)
+        graph_renderer = GraphRenderer()
+        graph_renderer.node_renderer.data_source.add(vertex_indices, 'index')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color='red')
+        graph_renderer.edge_renderer.data_source.data = dict(
             start=[0]*N,
-            end=node_indices)
+            end=vertex_indices)
+        d=dict(
+            start=[0]*N,
+            end=vertex_indices
+        )
+        print(d)
+        x=[]
+        y=[]
+        for vertex_id in vertex_indices:
+            vertex=graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
 
-        circ = [i*2*math.pi/8 for i in node_indices]
-        x = [math.cos(i) for i in circ]
-        y = [math.sin(i) for i in circ]
+        graph_layout = dict(zip(vertex_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-        graph_layout = dict(zip(node_indices, zip(x, y)))
-        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
-
-        plot.renderers.append(graph)
+        plot.renderers.append(graph_renderer)
 
         output_file('graph.html')
         show(plot)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,7 +2,7 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-
+import random
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -18,8 +18,16 @@ class Graph:
         
 
 class Vertex:
-    def __init__(self,vertex_id):
+    def __init__(self,vertex_id,x=None,y=None):
         self.id=vertex_id
         self.edges=set()
+        if x is None:
+            self.x=random.random()*10-5
+        else:
+            self.x=x
+        if y is None:
+            self.y=random.random()*10-5
+        else:
+            self.y=y
     def __repr__(self):
         return f'{self.edges}'
