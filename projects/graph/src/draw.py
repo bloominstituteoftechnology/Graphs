80acbe81ba508cf8451692e5135254b138e68f1f
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-10-17 07:56:45 -0700

    Some of part two for testing, working through code to meet requirements.

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,16 +1,48 @@
 """
 General drawing methods for graphs using Bokeh.
 """
+import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+#from bokeh.io import show, output_file
+#from bokeh.plotting import figure
+#from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+#                          ColumnDataSource)
 
-
+#
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self):
-        pass  
+       N = 8
+node_indices = list(range(N))
+
+plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+              tools='', toolbar_location=None)
+
+graph = GraphRenderer()
+
+graph.node_renderer.data_source.add(node_indices, 'index')
+graph.node_renderer.data_source.add(Spectral8, 'color')
+graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+
+graph.edge_renderer.data_source.data = dict(
+    start=[0]*N,
+    end=node_indices)
+
+### start of layout code
+circ = [i*2*math.pi/8 for i in node_indices]
+x = [math.cos(i) for i in circ]
+y = [math.sin(i) for i in circ]
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot.renderers.append(graph)
+
+output_file('graph.html')
+show(plot) 
 
 
