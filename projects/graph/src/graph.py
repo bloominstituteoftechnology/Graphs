7fb4ff2deae62745f8dd3cee953d8a737a120561
your <jurgenkela@gmail.com>
2018-10-17 19:15:25 -0400

    Part 4 completed

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -25,16 +25,41 @@ class Graph:
                 self.vertices[v2].edges.add(self.vertices[v1])
 
     def dfs(self, start):
-        rand_color = '#' +
-        .join([random.choice('0123456789ABCDEF') for _ in range(6)])
+        rand_color = '#' + \
+            ''.join([random.choice('0123456789ABCDEF') for _ in range(6)])
         start_color = rand_color
         stack = [start]
+        visited = [start]
         while stack:
             curr = stack.pop()
             if curr not in visited:
                 visited.append(curr)
             for next_node in curr.edges:
-                if next_node
+                if next_node not in visited:
+                    next_node.color = curr.color
+                    stack.append(next_node)
+        return visited
+
+    def bfs(self, start):
+        queue = deque([start])
+        res = []
+        visited = [False] * len(self.vertices)
+        visited[start] = True
+        while queue:
+            curr = queue.popleft()
+            res.append(curr)
+            for next_node in self.vertices[curr].edges:
+                if visited[next_node] if False:
+                    queue.append(next_node)
+                    visited[next_node] = True
+        return res
+
+    def connected(self):
+        visited = []
+        for _, vertex in self.vertices.items():
+            if vertex not in visited:
+                visited.extend(self.dfs(vertex))
+        return visited
 
 
 class Vertex:
