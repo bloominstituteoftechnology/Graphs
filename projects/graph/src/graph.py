51d57110a5b9acd60d48e851feddbfce114daea1
mkerbleski <mkerbles@gmail.com>
2018-10-17 16:03:26 -0600

    implement depth first search

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,6 +4,33 @@ Simple graph implementation compatible with BokehGraph class.
 import random
 import math
 
+
+class Queue: 
+    def __init__(self):
+        self.queue = []
+    def enqueue(self,value):
+        self.queue.append(value)
+    def dequeue(self):
+        if(self.size())>0:
+            return self.queue.pop()
+        else: 
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if(self.size()) > 0:
+            return self.stack.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -15,25 +42,59 @@ class Graph:
     def add_edge(self, v1, v2):
         self.vertices[v1].edges.add(v2)
         self.vertices[v2].edges.add(v1)
-    def dfs(self, value, visited=None):
+    def dfs_R(self, value, visited=None):
         if visited is None: 
             visited = []
+        print(visited)
         visited.append(value)
         for vert in self.vertices:
             print(vert)
             if value == vert:
+                print('true')
                 return True 
             else:
                 print(self.vertices[vert].edges, '26')
-                verts = self.vertices[vert]
-                print(verts, '28')
-                for edge in verts.edges:
-                    self.dfs(edge, visited)
+                for edge in self.vertices[vert].edges:
+                    print(edge)
+                    return self.dfs(edge, visited)
+                # verts = self.vertices[vert].edges
+                # print(verts, '28')
         return False 
     def bfs(self, value): 
         # do BfS 
         searched = []
         queue = [self.vertices[0]]
+        return False
+    def dfs(self, value):
+        index = 0
+        s = Stack()
+        print(self.vertices[index].edges)
+        k = list(self.vertices.keys())
+        print(k,'k')
+        s.push(k[0])
+        print(s.stack,'s1')
+        visited = []
+        print(visited, 'visited')
+        while  s.size() > 0:
+            print(index, 'index', 'iteration')
+            print(s.stack,'s2')
+            node = s.pop()
+            print(node,'node')
+            if node == value:
+                print('True')
+                return True
+            else: 
+                visited.append(node)
+                print(visited, 'visited')
+                # print(self.vertices[index].edges,index,'edges')
+                for each in self.vertices[index].edges:
+                    if each not in visited:
+                        print(each)
+                        s.push(each)
+                    else:
+                        print('nope')
+                index = index +1
+        print('False')
         return False
 
 class Vertex: 
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -16,34 +16,35 @@ from sys import argv
 
 def main():
 
-    # graph = Graph()
-    # graph.add_vertex(0)
-    # graph.add_vertex(1)
-    # graph.add_vertex(2)
-    # graph.add_vertex(3)
-    # print(graph.vertices)
-    # graph.add_edge(0, 1)
-    # graph.add_edge(0, 3)
-    # print(graph.vertices)
-    # BokehGraph(graph).show()
+    graph = Graph()
+    graph.add_vertex(0)
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    print(graph.vertices)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 3)
+    print(graph.vertices)
+    BokehGraph(graph).show()
 
     #random
     rando_graph = Graph()
     verts = math.floor(random.random() * 10) + 2
-    print(verts, 'verts')
+    # print(verts, 'verts')
     for v1 in range(0, verts):
         rando_graph.add_vertex(v1)
         # print(v2s, 'v2s')
     keys = list(rando_graph.vertices.keys())
     for v in keys:
         rando_edges = math.floor(random.randrange(0, len(keys)))
-        print(rando_edges, "rando_edges")
+        # print(rando_edges, "rando_edges")
         for v2 in range(0, rando_edges):
             if v2 != v: #v1 if want to loop on itself
                 rando_graph.add_edge(v, v2)  
 
     BokehGraph(rando_graph).show()
-    rando_graph.dfs(2)
+    rando_graph.dfs(9)
+    # graph.dfs(9)
     
 
 if __name__ == '__main__':
