60520bef51d46fb0f93e8875cab74f5b1b02ac23
cubsfan46350 <cubsfan.46350@msn.com>
2018-10-17 12:45:43 -0500

    added code for graph but skipping implementing it because computer will not cooperate

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,8 +7,25 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        
+
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+
+bg = BokehGraph(graph)
+bg.draw()
