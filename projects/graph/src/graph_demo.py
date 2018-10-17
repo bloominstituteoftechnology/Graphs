47eaab5086d5e37637b9f0dba83f615aeb7ef8ed
skennedy93 <sean47900@gmail.com>
2018-10-17 16:44:11 -0500

    testing graphdemo.py and getting errors,looking into it

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -21,6 +21,43 @@ def createDefaultGraph():
     bg = BokehGraph(graph)
     bg.draw()
 
+def createRandomGraph(numNodes, numEdges):
+    graph = Graph()
+    
+    all_edges = []
+
+    for i in range(numNodes):
+        for j in range(i + 1, numNodes):
+            all_edges.append((i, j))
+    
+    random.shuffle(all_edges)
+
+    edges = all_edges[:numEdges]
+
+    if numEdges > len(all_edges):
+        print("Too many edges")
+
+    for edge in edges:
+        print(edge)
+
+    for i in range(numNodes):
+        graph.add_vertex(i)
+    
+    for edge in edges:
+        graph.add_edge(edge[0], edge[1])
+
+    print(len(edges))
+    bg = BokehGraph(graph)
+    bg.draw()
+
+def main(style, numNodes, numEdges):
+    if style == 'default':
+        createDefaultGraph()
+    elif style == 'random':
+        createRandomGraph(numNodes, numEdges)
+    else:
+        createDefaultGraph()
+
 def main():
     if style == 'default':
         createDefaultGraph()
