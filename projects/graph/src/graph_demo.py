8135f9de44c634b1afabd0cff74341824df4be0a
Carey <careycbaldwin@gmail.com>
2018-10-17 15:00:56 -0400

    test graph class created in graph_demo, starting to work on random graph

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -17,6 +17,28 @@ x = [2,4,6,8,10]
 random.shuffle(x)
 print(x)
 
+# make a graph with vertices and edges for demonstration purposes
+def createTestGraph():
+    graph = Graph() # instantiate the graph using the Graph class
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_vertex('4')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    graph.add_edge('1', '2')
+    graph.add_edge('1', '4')
+
+    # bkg = BokehGraph(graph)
+    # bkg.display()
+
+# create a random graph
+
+def createRandomGraph(vertices_count, edges_count):
+    
+
+
 def main():
     pass  # TODO
 
