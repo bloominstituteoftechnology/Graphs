7f0c18f78ef6d0a279a77c6f4b5ba82584b2a1d6
keegn <keegan.t.burkett@gmail.com>
2018-10-17 17:04:32 -0500

    Main function in demo graph

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -25,11 +25,45 @@ def defaultGraph():
     bg = BokehGraph(graph)
     bg.draw()
 
+
+# Random graph that takes number of nodes and number of edges as args
+def randomGraph(nNodes, nEdges)
+    graph = Graph()
+    all_the_edges = []
+
+    for i in range(nNodes):
+        for k in range(i + 1, nNodes):
+            all_the_edges.append((i , k))
+
+    # random.shuffle(all_the_edges)
+    # edges = all_the_edges[:nEdges]
+
+    edges = all_the_edges[:nEdges]
+
+    #print the edges in all edges list
+    for edge in edges:
+        print(edge)
+
+    for i in range(nNodes):
+        graph.add_vertex(i)
+
+   for edge in edges:
+       print(edge)
+       graph.add_edge(edge[0], edge[1])
+
+    bg = BokehGraph(graph)
+    bg.draw()
+
+def main(style, nNodes, nEdges):
+    # if our style is default call defaultGraph 
+    if style = "default":
+        defaultGraph()
+    #if style if random call randomGraph and pass in nNodes and nEdges args
+    elif style == "random"
+        randomGraph(nNodes, nEdges)
+    else:
+        defaultGraph()
     
-
-
-def main():
-    pass  # TODO
 
 
 if __name__ == '__main__':
