84912b93e8062967656e9d84c88691b9ca062ed3
jordan-massingill <jordan_massingill@hotmail.com>
2018-10-17 17:57:42 -0500

    Completed step 3 and most or all of step 4, which was bsf and dsf

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,6 +3,25 @@ Simple graph implementation compatible with BokehGraph class.
 """
 import random
 
+class Queue:
+  def __init__(self):
+    self.size = 0
+    self.storage = []
+
+  def enqueue(self, item):
+    self.size = self.size + 1
+    self.storage.append(item)
+
+  def dequeue(self):
+    if self.size > 0:
+        self.size = self.size - 1
+        return self.storage.pop(0)
+    else:
+        return None
+
+  def len(self):
+    return self.size
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -15,6 +34,34 @@ class Graph:
             self.vertices[v2].edges.add(v1)
         else:
             raise IndexError("One or both of these vertices does not exist!")
+    def bft(self, starting_v):
+        q = Queue()
+        visited = []
+        q.enqueue(self.vertices[starting_v])
+        while len(q.storage) != 0:
+            edges = q[0].edges
+            for edge in edges:
+                q.enqueue(self.vertices[edge])
+            if q[0].id not in visited:
+                visited.append(q.dequeue())
+            else:
+                q.dequeue()
+        visited = [i.id for i in visited]
+        return visited
+
+    def dft(self, starting_v):
+        stack = []
+        visited = []
+        stack.append(self.vertices[starting_v])
+        while len(stack) != 0:
+            edges = stack[0].edges
+            vstd_node = stack.pop()
+            if q[0] not in visited:
+                visited.append(vstd_node)
+            for edge in edges:
+                stack.append(self.vertices[edge])
+            
+
 
 
 class Vertex:
