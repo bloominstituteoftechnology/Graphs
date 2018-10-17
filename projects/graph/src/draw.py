1c22f98343de07c93ea84305a26c81dcf62f4ca8
Fred <sohnf90@yahoo.com>
2018-10-17 14:33:38 -0700

    Part 2 Done?

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -16,44 +16,37 @@ class BokehGraph:
         self.graph = graph
     def draw(self):
         graph = self.graph
-        N = 8
+        N = len(graph.vertices)
         node_indices = list(range(N))
 
-        plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+        plot = figure(title="Graph Layout Demonstration", x_range=(-4, 4), y_range=(-4, 4),
                     tools="", toolbar_location=None)
 
-        graph = GraphRenderer()
+        graph_renderer = GraphRenderer()
 
-        graph.node_renderer.data_source.add(node_indices, 'index')
-        # graph_render.node_renderer.data_source.add(Spectral4, 'color')
-        graph.node_renderer.glyph = Circle(radius=0.25, fill_color='green')
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.25, fill_color='green')
 
-        graph.edge_renderer.data_source.data = dict(
+        graph_renderer.edge_renderer.data_source.data = dict(
             start=[0]*N,
             end=node_indices)
 
+        d = dict(
+            start=[0]*N,
+            end=node_indices)
+        print('d is: ', d)
+
         ### start of layout code
-        circ = [i*2*math.pi/8 for i in node_indices]
-        x = [math.cos(i) for i in circ]
-        y = [math.sin(i) for i in circ]
+        circ = [i for i in node_indices]
+        x = [i for i in circ]
+        y = [i for i in circ]
+        
+        
         graph_layout = dict(zip(node_indices, zip(x, y)))
-        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
-        ### Draw quadratic bezier paths
-        def bezier(start, end, control, steps):
-            return [(1-s)**2*start + 2*(1-s)*s*control + s**2*end for s in steps]
 
-        xs, ys = [], []
-        sx, sy = graph_layout[0]
-        steps = [i/100. for i in range(100)]
-        for node_index in node_indices:
-            ex, ey = graph_layout[node_index]
-            xs.append(bezier(sx, ex, 0, steps))
-            ys.append(bezier(sy, ey, 0, steps))
-        graph.edge_renderer.data_source.data['xs'] = xs
-        graph.edge_renderer.data_source.data['ys'] = ys
-
-        plot.renderers.append(graph)
+        plot.renderers.append(graph_renderer)
 
         output_file("graph.html")
         show(plot)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -14,3 +14,121 @@ def main():
 if __name__ == '__main__':
     # TODO - parse argv
     main()
+
+"""
+Simple graph implementation compatible with BokehGraph class.
+"""
+import random
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)
+
+    def add_edge(self, vertex1, vertex2):
+        '''
+        Add an undirected edge to the graph
+        '''
+        if vertex1 in self.vertices and vertex2 in self.vertices:
+            self.vertices[vertex1].edges.add(vertex2)
+            self.vertices[vertex2].edges.add(vertex1)
+        else:
+            raise IndexError("That vertex does not exist.")
+
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None):
+        self.id = vertex_id
+        self.edges = set()
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else: 
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+    def __repr__(self):
+        return f"{self.edges}"
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+"""
+General drawing methods for graphs using Bokeh.
+"""
+from graph import Graph
+import math
+
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+                          ColumnDataSource)
+
+
+class BokehGraph:
+    """Class that takes a graph and exposes drawing methods."""
+    def __init__(self, graph):
+        self.graph = graph
+    def draw(self):
+        graph = self.graph
+        N = len(graph.vertices)
+        node_indices = list(graph.vertices.keys())
+
+        plot = figure(title="Graph Layout Demonstration", x_range=(-7, 7), y_range=(-7, 7),
+                    tools="", toolbar_location=None)
+
+        graph_renderer = GraphRenderer()
+
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.glyph = Circle(radius=0.25, fill_color='green')
+
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start=[0]*N,
+            end=node_indices)
+
+        d = dict(
+            start=[0]*N,
+            end=node_indices)
+        print('d is: ', d)
+
+        ### start of layout code
+        # circ = [i*2*math.pi/8 for i in node_indices]
+        # x = [math.cos(i) for i in circ]
+        # y = [math.sin(i) for i in circ]
+        x = []
+        y = []
+        for vertex_id in node_indices:
+            vertex = graph.vertices[vertex_id]
+            x.append(vertex.x)
+            y.append(vertex.y)
+        
+        graph_layout = dict(zip(node_indices, zip(x, y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+
+        plot.renderers.append(graph_renderer)
+
+        output_file("graph.html")
+        show(plot)
+                
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
+
+bg = BokehGraph(graph)
+bg.draw()
