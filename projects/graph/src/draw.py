b3b2c4055e817073b88ba3c3c53f45081a36431d
Arthur Bates <aabatesj@gmail.com>
2018-10-17 17:41:26 -0400

    Graphs day 3
    mostly catch up from the beginning of the week. still reading documentation and commenting as I go

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -2,25 +2,148 @@
 General drawing methods for graphs using Bokeh.
 """
 
+from random import choice, random
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import (
+    GraphRenderer,
+    StaticLayoutProvider,
+    Circle,
+    LabelSet,
+    ColumnDataSource,
+    VeeHead,
+    Arrow,
+)
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        # output to static HTML file
-        output_file("graph1.html")
+    # initial Bokeh graph parameters
+    def __init__(
+        self,
+        graph,
+        title="Graph",
+        width=10,
+        height=10,
+        show_axis=False,
+        show_grid=False,
+        circle_size=35,
+    ):
+    # check if graph has vertices
+        if not graph.vertices:
+            raise Exception("Graph should contain vertices!")
+        self.graph = graph
 
-# create a new plot with a title and axis labels
-        p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+        # Setup plot
+        self.width = width
+        self.height = height
+        self.pos = {}  # dict to map vertices by x, y positions
+        # new plot with title and ranges
+        """The bokeh.plotting interface provides a Figure class to help with assembling all the necessary objects.
+            the figure() function creates Figure objects."""
+        self.plot = figure(
+            title=title,
+            # Ranges describe the data-space bounds of a plot.
+            x_range=(0, width),
+            y_range=(0, height))
+        self.plot.axis.visible = show_axis
+        self.plot.grid.visible = show_grid
+        self._setup_graph_renderer(circle_size)
 
-# add a line renderer with legend and line thickness
-        p.line(x, y, legend="Temp.", line_width=2)
+    def _setup_graph_renderer(self, circle_size):
+        graph_renderer = GraphRenderer()
 
-# show the results
-        show(p)
+        graph_renderer.node_renderer.data_source.add(
+            list(self.graph.vertices.keys()), "index"
+        )
+        graph_renderer.node_renderer.data_source.add(self._get_random_colors(), "color")
+        graph_renderer.node_renderer.glyph = Circle(
+            size=circle_size, fill_color="color"
+        )
+        graph_renderer.edge_renderer.data_source.data = self._get_edge_indexes()
+        self.randomize()
+        for i in range(len(graph_renderer.edge_renderer.data_source.data["start"])):
+            self.plot.add_layout(
+                Arrow(
+                    end=VeeHead(size=20, fill_color="black"),
+                    x_start=self.pos[
+                        graph_renderer.edge_renderer.data_source.data["start"][i]
+                    ][0],
+                    y_start=self.pos[
+                        graph_renderer.edge_renderer.data_source.data["start"][i]
+                    ][1],
+                    x_end=self.pos[
+                        graph_renderer.edge_renderer.data_source.data["end"][i]
+                    ][0],
+                    y_end=self.pos[
+                        graph_renderer.edge_renderer.data_source.data["end"][i]
+                    ][1],
+                )
+            )
+
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=self.pos)
+        self.plot.renderers.append(graph_renderer)
+        self._get_labels()
+
+    def _get_random_colors(self):
+        colors = []
+        for _ in range(len(self.graph.vertices)):
+            color = "#" + "".join([choice("0123456789ABCDEF") for j in range(6)])
+            colors.append(color)
+        return colors
+
+    def _get_edge_indexes(self):
+        start_indices = []
+        end_indices = []
+        checked = set()
+
+        for vertex, edges in self.graph.vertices.items():
+            if vertex not in checked:
+                for destination in edges:
+                    start_indices.append(vertex)
+                    end_indices.append(destination)
+                checked.add(vertex)
+
+        return dict(start=start_indices, end=end_indices)
+
+    # Ask Bokeh to show() or save() the results.
+    # These functions save the plot to an HTML file and optionally display it in a browser.
+
+    def show(self, output_path="./graph.html"):
+        output_file(output_path)
+        show(self.plot)
+
+    def randomize(self):
+        """Randomize vertex positions."""
+        for vertex in self.graph.vertices:
+            # TODO make bounds and random draws less hacky
+            self.pos[vertex] = (
+                1 + random() * (self.width - 2),
+                1 + random() * (self.height - 2),
+            )
+            # random stuff for making vertex
+            # make if statement to take into account positions already in self.pos
+
+    def _get_labels(self):
+        label_data = {"x": [], "y": [], "name": []}
+        for vertex, edges in self.pos.items():
+            label_data["x"].append(edges[0])
+            label_data["y"].append(edges[1])
+            label_data["name"].append(vertex)
+        print("label", label_data)
+        label_source = ColumnDataSource(label_data)
+        print("label source:", label_source)
+
+        labels = LabelSet(
+            x="x",
+            y="y",
+            text="name",
+            text_color="white",
+            level="glyph",
+            text_align="center",
+            text_baseline="middle",
+            text_line_height=1,
+            source=label_source,
+            render_mode="canvas",
+        )
+        self.plot.add_layout(labels)
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,32 +2,47 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+from draw import BokehGraph
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        self.vertices = set()
+        self.vertices = {}
 
-    def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = vertex(vertex_id)
-        
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)
-            self.vertices[v2].edges.add(v1)
-        else:
-            raise IndexError("That vertex does not exist")
+    def add_edge(self, start, end, bidirectional=True):
+        """Add an edge from start to end."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception("Error - vertices not in graph!")
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
 
-class Vertex:
-    def __init__(self, vertex_id):
-        self.id = vertex_id
-        self.edges = set()
+    def add_vertex(self, vertex, edges=()):
+        if not set(edges).issubset(self.vertices):
+            raise Exception("Error: Cannot have edge to nonexistent vertices")
+        if vertex in self.vertices:
+            raise Exception("Error: Vertex already exists.")
+        self.vertices[vertex] = set(edges)
 
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
 
-print(graph.vertices)
+def main():
+    graph = Graph()
+    graph.add_vertex("0")
+    graph.add_vertex("1")
+    graph.add_vertex("2")
+    graph.add_vertex("3")
+    graph.add_vertex("4")
+    graph.add_vertex("5")
+    graph.add_vertex("6")
+    graph.add_vertex("7")
+    graph.add_edge("0", "1")
+    graph.add_edge("0", "3")
+    graph.add_edge("7", "5", False)
+
+    b = BokehGraph(graph)
+    b.show()
+
+
+if __name__ == "__main__":
+    main()
