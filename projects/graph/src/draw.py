f99fb8f1eb0302a1cc2a4f823e147a6f787b542f
bri79319 <bri79319@gmail.com>
2018-10-18 15:36:48 -0400

    trying to test out code

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -85,13 +85,13 @@ class BokehGraph:
         show(plot)
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex("0")
-graph.add_vertex("1")
-graph.add_vertex("2")
-graph.add_vertex("3")
-graph.add_edge("0", "1")
-graph.add_edge("0", "3")
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex("0")
+# graph.add_vertex("1")
+# graph.add_vertex("2")
+# graph.add_vertex("3")
+# graph.add_edge("0", "1")
+# graph.add_edge("0", "3")
 
-bg = BokehGraph(graph)  # this should only create the object instance
-bg.draw()  # should actually generate the graph
+# bg = BokehGraph(graph)  # this should only create the object instance
+# bg.draw()  # should actually generate the graph
