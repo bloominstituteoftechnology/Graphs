b70f5ab72efb17cb4ba8e7bb0b906f076594d5a8
Arthur Bates <aabatesj@gmail.com>
2018-10-18 18:46:50 -0400

    Post Lecture dft/bft
    Part 4 catch up with help from Brady

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -17,6 +17,65 @@ class Graph:
         self.vertices[start].add(end)
         if bidirectional:
             self.vertices[end].add(start)
+    """
+    DFS(graph):
+    for v of graph.verts:
+        v.color = white
+        v.parent = null
+
+    for v of graph.verts:
+        if v.color == white:
+            DFS_visit(v)
+    """
+
+    def dft(self, starting_node, visited=None):
+        """
+        Depth first traversal using recursion
+        """
+        # Mark the node as visited
+        if visited is None:
+            # quese of visited nodes
+            visited = []
+        visited.append(starting_node)
+        # For each child, if that child hasn't been visited, call dft() on that node
+        for node in self.vertices[starting_node].edges:
+            if node not in visited:
+                self.dft(node, visited)
+        return visited   
+
+    """
+    BFS(graph, startVert):
+  for v of graph.vertexes:
+    v.color = white
+
+  startVert.color = gray
+  queue.enqueue(startVert)
+
+  while !queue.isEmpty():
+    u = queue[0]  // Peek at head of queue, but do not dequeue!
+
+    for v of u.neighbors:
+      if v.color == white:
+        v.color = gray
+        queue.enqueue(v)
+
+    """
+
+    def bft(self, starting_node):
+        visited = []
+        # create an empty queue
+        q = Queue()
+        # Put starting vert in the queue
+        q.enqueue(starting_node)
+        while q.size() > 0:  # whlie queue is not empty...
+            dequeued = q.dequeue() # Dequeue the first element
+            visited.append(dequeued)  # Mark it as visited
+            print(dequeued)
+            for edge in self.vertices[dequeued].edges:  #For each child
+                if edge not in visited:  # If it hasn't been visited
+                    q.enqueue(edge)  # Add it to the back of the queue
+        return visited
+
 
     def add_vertex(self, vertex, edges=()):
         if not set(edges).issubset(self.vertices):
