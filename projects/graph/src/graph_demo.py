92bee34f0b0f4dc45e9435d48be3aa6b196f7b6e
adamcglee <adamcglee@gmail.com>
2018-10-18 14:20:29 -0700

    updated code based on lecture from Brady today to get functionality working, not sure what it is doing though

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -1,58 +1,99 @@
+#!/usr/bin/python
+
 """
-General drawing methods for graphs using Bokeh.
+Demonstration of Graph and BokehGraph functionality.
 """
-import math
-import random
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, Circle
-from bokeh.palettes import Spectral8
+
+from sys import argv
 from graph import Graph
-class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-    def __init__(self, graph):
-        self.graph = graph
-    def draw(self):
-        graph = self.graph
-        N = len(graph.vertices)
-        node_indices = list(graph.vertices.keys())
-        plot = figure(title='graph_demo', x_range = (-1, 10), y_range = (-1, 10), tools="", toolbar_location=None)
-        graph_renderer = GraphRenderer()
-        graph_renderer.node_renderer.data_source.add(node_indices,'index')
-        graph_renderer.node_renderer.glyph = Circle(radius=0.25, fill_color='green')
-        edge_start = []
-        edge_end = []
-        for vertex_id in node_indices:
-            for v in graph.vertices[vertex_id].edges:
-                edge_start.append(vertex_id)
-                edge_end.append(v)
-        
-        graph_renderer.edge_renderer.data_source.data = dict(
-            start=edge_start,
-            end=edge_end)
-        axis = {}
-        for vertex in self.graph.vertices:
-           axis[vertex] = (random.random() * 10, random.random() * 10)
-        # x = []
-        # y = []
-        # for vertex_id in node_indices:
-        #     vertex = graph.vertices[vertex_id]
-        #     x.append(int(vertex.id))
-        #     y.append(vertex)
-         # graph_layout = dict(zip(node_indices, zip(x, y)))
-        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout=axis)
-        plot.renderers.append(graph_renderer)
-        output_file('graph.html')
-        show(plot)
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_vertex('4')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-bg = BokehGraph(graph)
-bg.draw()
+from draw import BokehGraph
+import random
+
+
+def createDefaultGraph():
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex(5)
+    graph.add_vertex(2)
+    graph.add_vertex(6)
+    graph.add_vertex(1)
+    graph.add_vertex(4)
+    graph.add_vertex(7)
+    graph.add_vertex(3)
+    graph.add_edge(5, 2)
+    graph.add_edge(5, 6)
+    graph.add_edge(2, 1)
+    graph.add_edge(2, 4)
+    graph.add_edge(4, 3)
+    graph.add_edge(6, 7)
+
+    print(graph.dfs_path(5, 6))
+
+    bg = BokehGraph(graph)
+    bg.draw()
+
+
+# O(n^2)
+def createRandomGraph(numNodes, numEdges):
+    graph = Graph()  # Instantiate your graph
+
+    all_edges = []
+
+    # O(n^2)
+    for i in range(numNodes):
+        for j in range(i + 1, numNodes):
+            all_edges.append( (i,  j) )
+
+    # O(n)
+    random.shuffle(all_edges)
+    edges = all_edges[:numEdges]
+
+    # O(1)
+    if numEdges > len(all_edges):
+        print("Warning: Too many edges")
+
+    # O(n)
+    for edge in edges:
+        print(edge)
+
+    # O(n)
+    for i in range(numNodes):
+        graph.add_vertex(i)
+
+    # O(n)
+    for edge in edges:
+        print(edge)
+        graph.add_edge(edge[0], edge[1])
+
+    print(len(edges))
+
+    bg = BokehGraph(graph)
+    bg.draw()
+
+
+def main(style, numNodes,  numEdges):
+    if style  == "default":
+        createDefaultGraph()
+    elif style  == "random":
+        createRandomGraph(numNodes, numEdges)
+    else:
+        createDefaultGraph()
+
+
+if __name__ == '__main__':
+    style = "random"
+    numNodes = 10
+    numEdges = 20
+
+    for arg in argv[1:]:
+        arg_split = arg.split("=")
+        if len(arg_split) == 2:
+            if arg_split[0] == "style":
+                style = arg_split[1].lower()
+            elif arg_split[0] == "nodes":
+                numNodes = int(arg_split[1])
+            elif arg_split[0] == "edges":
+                numEdges = int(arg_split[1])
+            else:
+                print("I don't understand that command.\n")
+
+    main(style, numNodes, numEdges)
