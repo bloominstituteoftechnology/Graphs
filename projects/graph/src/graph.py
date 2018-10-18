3c12750b726ff2abe5482163eea30f804f057d72
francistse23 <wtse.ucsd@gmail.com>
2018-10-18 11:23:19 -0700

    refactored dft, dfs, bft, bfs codes. also refactored most of the code in graph_demo.py based on lecture's code.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -18,7 +18,6 @@ class Queue:
         return len(self.queue)
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = dict()
 
@@ -32,49 +31,70 @@ class Graph:
         else:
             raise IndexError("That vertex does not exist")
 
-    # def dfs(self, starting_node, visited=None):
-    #     if visited is None:
-    #         visited = [False] * len(self.vertices)
-    #     stack = [starting_node]
-    #     results = []
-    #     visited[starting_node] = True
-    #     while stack:
-    #         current_node = stack.pop()
-    #         results.append(current_node)
-    #         for next_node in self.vertices[current_node]:
-    #             if visited[next_node] == False:
-    #                 stack.append(next_node)
-    #                 visited[next_node] = True
-    #     return results
+    def dft(self, current, visited=None):
+        if visited is None:
+            visited = []
+        stack = [current]
+        while stack:
+            current_node = stack.pop()
+            visited.append(current_node)
+            for next_node in self.vertices[current_node].edges:
+                if next_node not in visited:
+                    stack.append(next_node)
+        return visited
 
-    #recursion
+    # recursion
+    # def dft(self, current, visited=None):
+    #     if visited is None:
+    #         visited = []
+    #     visited.append(current)
+    #     for vertex in self.vertices[current].edges:
+    #         if vertex not in visited:
+    #             self.dft(vertex, visited)
+    #     return visited
+
     def dfs(self, current, target, visited=None):
         if visited is None:
             visited = []
         visited.append(current)
         if current == target:
-            return True
-        for vertex in self.vertices[current]:
+            return visited
+        for vertex in self.vertices[current].edges:
             if vertex not in visited:
-                if self.dfs(vertex, target):
-                    return True
-        return False
+                if self.dfs(vertex, target, visited):
+                    return visited
+        return visited
 
-    def bfs(self, current, target, visited=None):
+    def bft(self, current, visited=None):
         q = Queue()
         if visited is None:
-            visited = [False] * len(self.vertices)
-        q.enqueue(current)
-        visited[current] = True
+            visited = []
+        q.enqueue(self.vertices[current])
         while q.size() > 0:
-            if q.dequeue() == target:
-                return True
-            for i in self.vertices:
-                if visited[i] == False:
-                    q.enqueue(i)
-                    visited[i] == True
-        return False
+            node = q.dequeue()
+            if node not in visited:
+                visited.append(node)
+                edges = node.edges
+                for edge in edges:
+                    q.enqueue(self.vertices[edge])
+        return visited
         
+    # def bfs(self, current, target, visited=None):
+    #     q = Queue()
+    #     if visited is None:
+    #         visited = []
+    #     q.enqueue(current)
+    #     if current == target:
+    #         return True
+    #     visited[current] = True
+    #     while q.size() > 0:
+    #         node = q.dequeue()
+    #         if node not in visited:
+    #             visited.append(node)
+    #             edges = node.edges
+    #             for edge in edges:
+    #                 q.enqueue(self.vertices[edge])
+    #     return visited
 
     # def add_directed_edge(self, vertex1, vertex2):
     #     if vertex1 in self.vertices:
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -10,22 +10,107 @@ import math
 from graph import Graph
 from draw import BokehGraph
 
-def main(v, e):
-    graph = Graph()
-    for vertex in range(v):
-        graph.add_vertex(vertex)
-    edges = []
-    for i in range(v):
-        for j in range(v):
-            if i < j:
-                edges.append((i,j))
-    random.shuffle(edges)
-    randomEdges = edges[:e]
-    for edge in randomEdges:
-        graph.add_edge(edge[0], edge[1])
+def createDefaultGraph():
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex(5)
+    graph.add_vertex(2)
+    graph.add_vertex(6)
+    graph.add_vertex(1)
+    graph.add_vertex(4)
+    graph.add_vertex(7)
+    graph.add_vertex(3)
+    graph.add_edge(5, 2)
+    graph.add_edge(5, 6)
+    graph.add_edge(2, 1)
+    # graph.add_edge(2, 4)
+    # graph.add_edge(4, 3)
+    # graph.add_edge(6, 7)
     bg = BokehGraph(graph)
     bg.show()
+    print(graph.dfs(5, 1))
+
+def createRandomGraph(numNodes, numEdges):
+    graph = Graph()  # Instantiate your graph
+
+    all_edges = []
+
+    # O(n^2)
+    for i in range(numNodes):
+        for j in range(i + 1, numNodes):
+            all_edges.append( (i,  j) )
+
+    # O(n)
+    random.shuffle(all_edges)
+    edges = all_edges[:numEdges]
+
+    # O(1)
+    if numEdges > len(all_edges):
+        print("Warning: Too many edges")
+
+    # O(n)
+    for edge in edges:
+        print(edge)
+
+    # O(n)
+    for i in range(numNodes):
+        graph.add_vertex(i)
+
+    # O(n)
+    for edge in edges:
+        print(edge)
+        graph.add_edge(edge[0], edge[1])
+
+    print(len(edges))
+
+    bg = BokehGraph(graph)
+    bg.draw()
+
+# def main(v, e):
+#     graph = Graph()
+#     for vertex in range(v):
+#         graph.add_vertex(vertex)
+#     edges = []
+#     for i in range(v):
+#         for j in range(v):
+#             if i < j:
+#                 edges.append((i,j))
+#     random.shuffle(edges)
+#     randomEdges = edges[:e]
+#     for edge in randomEdges:
+#         graph.add_edge(edge[0], edge[1])
+#     # graph.dfs(0, 3)
+#     bg = BokehGraph(graph)
+#     # bg.show()
+
+
+# if __name__ == '__main__':
+#     # TODO - parse argv
+#     main(6,3)
+
+def main(style, numNodes,  numEdges):
+    if style  == "default":
+        createDefaultGraph()
+    elif style  == "random":
+        createRandomGraph(numNodes, numEdges)
+    else:
+        createDefaultGraph()
+
 
 if __name__ == '__main__':
-    # TODO - parse argv
-    main(10,15)
+    style = "default"
+    numNodes = 5
+    numEdges = 5
+
+    for arg in argv[1:]:
+        arg_split = arg.split("=")
+        if len(arg_split) == 2:
+            if arg_split[0] == "style":
+                style = arg_split[1].lower()
+            elif arg_split[0] == "nodes":
+                numNodes = int(arg_split[1])
+            elif arg_split[0] == "edges":
+                numEdges = int(arg_split[1])
+            else:
+                print("I don't understand that command.\n")
+
+    main(style, numNodes, numEdges)
