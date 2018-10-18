f8fdb8f5513abd2c7b72f8f8d07b3a5f06d2c42d
codejoncode <jonathanjholloway@gmail.com>
2018-10-18 16:15:16 -0500

    current rating 8.48/10 many changes made adding methods where there were too few  changing one letter variables into correct naming convention and adding doc strings.

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -7,12 +7,24 @@ from bokeh.io import show, output_file
 from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
-from bokeh.palettes import Spectral8
-from graph import Graph
+
+
+def random_color():
+    """
+    This is a random_color generator that will return a #color.
+    """
+    valid_chars = '0123456789ABCDEF'
+    new_color = '#'
+    while len(new_color) <= 6:
+        new_color += valid_chars[random.randint(0, 15)]
+    return new_color
 
 
 class Vertex:
+    """Vertex class featuring add edge get edge methods"""
+
     def __init__(self, value, x=None, y=None):
+        """Setting up the Vertex class required value optional x y """
         self.value = value
         self.edges = set()
 
@@ -26,27 +38,40 @@ class Vertex:
             self.y = y
 
     def add_edge(self, vertex):
+        """ will add an edge to the set of the vertex"""
         self.edges.add(vertex)
 
     def get_edges(self):
-        return self.edges.keys()
+        """ will return the edges for the vertex"""
+        return self.edges
 
     def __repr__(self):
+        """ use the print method and this method will return the edges as a string"""
         return f"{self.edges}"
 
 
 class Edge:
+    """ this is the Edge class that shows connections between Vertex's"""
+
     def __init__(self, label, destination, weight=0, color=None):
+        """Initailizing the Edge class label destination required weight and color are defaults"""
         self.label = label
         self.destination = destination  # edges
         self.weight = weight
         self.color = color
+
+    def __repr__(self):
+        """this will produce a string of the edge"""
+        return f"Label: {self.label} Destinations: {self.destination} \
+         Weight: {self.weight} Color: {self.color}"
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
+        """ initializing the graph class no parmaters needed
+        vertices start off as a empty dictionary"""
         self.vertices = {}
 
     def add_vertex(self, value):
@@ -63,8 +88,6 @@ class Graph:
         else:
             raise IndexError("That vertex does not exist!")
 
-        new_edge = Edge(from_vertex, self.vertices[from_vertex].edges)
-
     def add_edge_two_way(self, vertex1, vertex2):
         """
         This is a bidirectional method to the edges.
@@ -79,19 +102,19 @@ class Graph:
         adj = self.vertices
         level = {start: 0}
         parent = {start: None}
-        i = 1
+        index = 1
         frontier = [start]
         while frontier:
-            next = []
-            for u in frontier:
-                for v in adj[u].edges:
-                    if v not in level:
-                        level[v] = i
-                        parent[v] = u
-                        next.append(v)
+            nexts = []
+            for item in frontier:
+                for vertex in adj[item].edges:
+                    if vertex not in level:
+                        level[vertex] = index
+                        parent[vertex] = item
+                        nexts.append(vertex)
 
-            frontier = next
-            i += 1
+            frontier = nexts
+            index += 1
         print("bfs levels")
         print(level)
     #
@@ -103,33 +126,28 @@ class Graph:
 
         def visited(adj, start):
             """ this is a function used inside of depth_first that is recursive"""
-            for v in adj[start].edges:
-                if v not in parent:
-                    parent[v] = start
-                    visited(adj, v)
+            for vertex in adj[start].edges:
+                if vertex not in parent:
+                    parent[vertex] = start
+                    visited(adj, vertex)
         # end of for loop
         visited(adj, start)
-        print("\nParents dfs")
+        print("Parents dfs")
         print(parent)
-    def random_color(self):
-        """
-        This is a random_color generator that will return a #color. 
-        """
-        valid_chars = '0123456789ABCDEF'
-        new_color = '#'
-        while len(new_color) <= 6:
-            new_color += valid_chars[random.randint(0,15)]
-        return new_color
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
 
     def __init__(self, graph):
+        """ initialze the bokeh graph only requirement is a graph make use of the graph class"""
         self.graph = graph  # should be a object. instance of Graph
 
-    def show(self):
+    def __repr__(self):
+        """Will show what the graph looks like return self.graph"""
+        return f"{self.graph}"
 
+    def show(self):
         """ this method makes use of the bokeh package to produce a graph in html """
         vertex_indices = list(self.graph.vertices.keys())
         print(vertex_indices[0], "starting point for traversal")
@@ -145,19 +163,18 @@ class BokehGraph:
 
         x_coordinates = []
         y_coordinates = []
-        valid_chars = '0123456789ABCDEF'
 
-        disconnected_color = self.graph.random_color()
-        connected_color = self.graph.random_color()
+        disconnected_color = random_color()
+        connected_color = random_color()
 
         colors_layout = []
 
         edge_start = []
         edge_end = []
         for vertex_id in vertex_indices:
-            for v in self.graph.vertices[vertex_id].edges:
+            for vertices_edges in self.graph.vertices[vertex_id].edges:
                 edge_start.append(vertex_id)
-                edge_end.append(v)
+                edge_end.append(vertices_edges)
 
         for vertex_id in vertex_indices:
             vertex = self.graph.vertices[vertex_id]
@@ -168,7 +185,7 @@ class BokehGraph:
             else:
                 colors_layout.append(disconnected_color)
 
-        # graph_renderer.node_renderer.glyph = Circle(radius=0.5, fill_color= self.color)
+
         graph_renderer.node_renderer.data_source.add(colors_layout, 'color')
         graph_renderer.node_renderer.glyph = Circle(
             radius=0.5, fill_color='color')
@@ -178,16 +195,18 @@ class BokehGraph:
             end=edge_end
         )
 
-        graph_layout = dict(zip(vertex_indices, zip(x_coordinates, y_coordinates)))
+        graph_layout = dict(
+            zip(vertex_indices, zip(x_coordinates, y_coordinates)))
         graph_renderer.layout_provider = StaticLayoutProvider(
             graph_layout=graph_layout)
 
         plot.renderers.append(graph_renderer)
 
-        labelSource = ColumnDataSource(data=dict(x=x_coordinates, y=y_coordinates, names=[
+        label_source = ColumnDataSource(data=dict(x=x_coordinates, y=y_coordinates, names=[
             self.graph.vertices[vertex_id].value for vertex_id in self.graph.vertices]))
-        labels = LabelSet(x='x', y='y', text='names', level='glyph', text_align='center',
-                          text_baseline='middle', source=labelSource, render_mode='canvas', text_color='white')
+        labels = LabelSet(x='x', y='y', text='names', level='glyph', \
+        text_align='center', text_baseline='middle', source=label_source, \
+        render_mode='canvas', text_color='white')
 
         plot.add_layout(labels)
 
@@ -196,6 +215,7 @@ class BokehGraph:
 
 
 def main():
+    """This function will create parmaters neccessary for a random graph"""
     potential_labels = ["A", "B", "C", "D", "E",
                         "F", "G", "1", "2", "3", "4", "5", "6", "7"]
     # to make it random we shuffle the labels.
@@ -207,18 +227,13 @@ def main():
     vertices_count = 0
     edges_count = 0
     vertices_created = []  # keep track of each creation.
-    edges_created = []  # keep track avoid duplicates.
     edge_track = {}
     while vertices_count <= num_vertices:
-        try:
-            new_vertex = potential_labels[vertices_count]
-        except:
-            print(vertices_count)
-            print(len(potential_labels))
+        new_vertex = potential_labels[vertices_count]
+
         graph.add_vertex(new_vertex)
         vertices_created.append(new_vertex)
         vertices_count += 1
-
     # shuffle the vertices_created so that the edges are randomly created
     random.shuffle(vertices_created)
     vertices_index_track = 0
@@ -243,11 +258,9 @@ def main():
         edges_count += 1
         vertices_index_track += 1
 
-    
     bokeh_graph = BokehGraph(graph)
     bokeh_graph.show()
 
 
 if __name__ == '__main__':
-    # I chose not to go with arguments since I am making everything i can think of completely random.
     main()
