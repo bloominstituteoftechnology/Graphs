bf505dab3f7b44dbfa82a8de4dbaeaa5b35576e7
Sukhada Gholba <sukhadagholba@gmail.com>
2018-10-18 14:17:48 -0700

    added a add _color method to Vertex class to add a different color than default when notes are connected

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -13,62 +13,73 @@ class Graph:
     def add_vertex(self, vertex_id):
         if vertex_id in self.vertices:
             raise ValueError(f'Duplicate vertex {value} found')
-        self.vertices[vertex_id] = set()
+        self.vertices[vertex_id] = Vertex(vertex_id)        
+
 
     def add_undirected_edge(self, start_edge, end_edge):
         if start_edge not in self.vertices:
             raise ValueError(f'Provided vertex {start_edge} does not exist')
         if end_edge not in self.vertices:
               raise ValueError(f'Provided vertex {end_edge} does not exist')
-        self.vertices[start_edge].add(end_edge)
-        self.vertices[end_edge].add(start_edge)
+        self.vertices[start_edge].edges.add(end_edge)
+        self.vertices[start_edge].add_color()       ##adding color to connected nodes
+        self.vertices[end_edge].edges.add(start_edge)
+        self.vertices[end_edge].add_color()         #adding color to connected nodes
+        
 
     def add_directed_edge(self, start_edge, end_edge):
         if start_edge not in self.vertices:
             ValueError(f'Vertex {start_edge} does not exist!')
         if end_edge not in self.vertices:
             ValueError(f'Vertex {end_edge} does not exist!')
-        self.vertices[start_edge].add(end_edge)    
+        self.vertices[start_edge].edges.add(end_edge)
+        self.vertices[start_edge].add_color()           #adding color to connected nodes
+        self.vertices[end_edge].add_color()
+
 
     def breadth_first_search(self, start_node):
-        queue = []
-        queue.append(start_node)
-        visited = []
-        while len(queue) > 0:
-            current = queue.pop(0)
-            visited.append(current)
-            for edge in self.vertices[current]: 
-                if edge not in visited and edge not in queue:
-                    queue.append(edge)
-        print(f'breadth first seacrh {visited}')
+         q = []
+         q.append(start_node)
+         visited = []
+         while len(q) > 0:
+             current = q.pop(0)
+             visited.append(current)
+             for edge in self.vertices[current].edges:       
+                 if edge not in visited and edge not in q:
+                     q.append(edge)
+         print(f'breadth first seacrh {visited}')
+
 
     def depth_first_search(self, start_node, visited=[]):
         visited=visited
-        
-        if self.vertices[start_node]!=None:
+         
+        if self.vertices[start_node]!=None:               
             if start_node not in visited:
-               visited.append(start_node)
-               for edge in self.vertices[start_node]:
-                   self.depth_first_search(edge, visited) #using recurssion           
-        print(f'depth first search{visited}')        
+                visited.append(start_node)
+                for edge in self.vertices[start_node].edges:        
+                    self.depth_first_search(edge, visited) #using recurssion   
+        
 
-                
+        print(f'depth first search{visited}')                
         
 
             
 
-
 class Vertex:
-    def __init__ (self, vertex_id, x=None, y=None, value=None, color="white"):
+    def __init__ (self, vertex_id, x=None, y=None, value=None):
         self.id = int(vertex_id)
         self.x = x
         self.y = y
+        self.color='Blue'   #default color 
         self.edges = set()
         if self.x is None:
             self.x = random.random() * 10 - 5
         if self.y is None:
             self.y = random.random() * 10 - 5
-
+        
+        
+    def add_color(self):  #  method to add a different color when the nodes get connected
+        self.color='Red'
 
 
 '''graph = Graph()  # Instantiate your graph
