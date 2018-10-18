6c1990530f3339de7744de0994ec8238c05906d5
Noah-Amar <ne.amar@outlook.com>
2018-10-18 10:23:01 -0500

    graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,11 +8,11 @@ class Graph:
     def __init__(self):
         self.vertices ={}
     def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = Vertex(vertex_id)
+        self.vertices[vertex_id] = set()
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add(v2)
-            self.vertices[v2].edges.add(v1)
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
         else:
             raise IndexError("That vertex does not exist!")
     def add_directed_edge(self, v1, v2):
