cad26070468e3c7b93db2134b9be30f9d55834a9
Adrian Adames <adrianadames@gmail.com>
2018-11-18 02:32:07 -0500

    Watched CS11 graphs lectures 3 and 4 and copied the code for graph data structure, drawing the graph via bokeh, bft, dft via recursion, dft via stack, bfs, dfs, bfs_path, and dfs_path.

--- projects/graph/src/draw1.py
+++ projects/graph/src/draw1.py
@@ -10,17 +10,19 @@
 """
 General drawing methods for graphs using Bokeh.
 """
+import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
-from graph1 import Graph
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, LabelSet, ColumnDataSource
 from bokeh.palettes import Spectral8
+from graph1 import Graph
+
 
 ############## Attempt 2 (code heavily based on BokehGraph implementation in graphs day) ########################
 # NOTE: I didn't finish this first attempt at the BokehGraph class. In interest of time
-# I decided to pause and implement the solution code from graph lecture day 2/3 (?).
+# I decided to pause and implement the solution code from graph lecture day 3/4.
+
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
@@ -28,20 +30,71 @@ class BokehGraph:
         self.graph = graph
     
     def drawGraph(self):
-        pass
+        graph = self.graph
+        N = len(graph.vertices)
+        node_indices = list(graph.vertices)
+
+        plot = figure(title= "Graph Layout Demonstration", x_range= (-1.1,10.1), y_range=(-1.1,10.1), tools = '', toolbar_location = None )
+
+        graph_renderer = GraphRenderer()
+
+        graph_renderer.node_renderer.data_source.add(node_indices, 'index')
+        graph_renderer.node_renderer.data_source.add([graph.vertices[vertex_id].color for vertex_id in graph.vertices], 'color') 
+        graph_renderer.node_renderer.glyph = Circle(radius = 0.5, fill_color = 'color')
+
+        start_indices = []
+        end_indices = []
+
+        for vertex_id in graph.vertices: #here we're looking at the keys of our vertex dictionary
+            for edge_end in graph.vertices[vertex_id].edges:
+                start_indices.append(vertex_id)
+                end_indices.append(edge_end)
+
+        graph_renderer.edge_renderer.data_source.data = dict(
+            start = start_indices,
+            end = end_indices
+        )
+
+        ### start of layout code
+        x = [graph.vertices[vertex_id].x for vertex_id in graph.vertices]
+        y = [graph.vertices[vertex_id].y for vertex_id in graph.vertices]
 
 
-#test
-graph1 = Graph()  # Instantiate your graph
-graph1.add_vertex(0)
-graph1.add_vertex(1)
-graph1.add_vertex(2)
-graph1.add_vertex(3)
-graph1.add_edge(0, 1)
-graph1.add_edge(0, 3)
+        graph_layout = dict(zip(node_indices, zip(x,y)))
+        graph_renderer.layout_provider = StaticLayoutProvider(graph_layout = graph_layout)
 
-bokehGraph1 = BokehGraph(graph1)
-bokehGraph1.drawGraph()
+        plot.renderers.append(graph_renderer)
+
+        labelSource = ColumnDataSource(data = dict(x=x, y=y, names = [graph.vertices[vertex_id].value for vertex_id in graph.vertices]))
+        labels = LabelSet(x= 'x', y = 'y', text = 'names', level = 'glyph', 
+            text_align= 'center', text_baseline = 'middle', source = labelSource, render_mode = 'canvas' )
+
+
+        plot.add_layout(labels)
+
+        output_file('graph1.html')
+        show(plot)
+
+# class BokehGraph:
+#     """Class that takes a graph and exposes drawing methods."""
+#     def __init__(self, graph):
+#         self.graph = graph
+    
+#     def drawGraph(self):
+#         pass
+
+
+# #test
+# graph1 = Graph()  # Instantiate your graph
+# graph1.add_vertex(0)
+# graph1.add_vertex(1)
+# graph1.add_vertex(2)
+# graph1.add_vertex(3)
+# graph1.add_edge(0, 1)
+# graph1.add_edge(0, 3)
+
+# bokehGraph1 = BokehGraph(graph1)
+# bokehGraph1.drawGraph()
 
 
 
--- projects/graph/src/graph1.py
+++ projects/graph/src/graph1.py
@@ -1,66 +1,298 @@
+##### Attempt 3 #######
+
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+import random
 
-class Vertex:
-    def __init__(self, value = None, x = None, y = None):
-        self.value = value
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self,value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self,value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+class Vertex: 
+    def __init__(self,vertex_id, x = None, y = None, value= None, color = None): #you use vertex_id to uniquely identify the vertex. In value, you can have a number, a name, a location, etc 
+        self.id = int(vertex_id)
         self.x = x
         self.y = y
-        self.adjVertices = set()
-
+        self.value = value
+        self.color = color
+        self.edges = set()
         if self.x is None:
-            self.x = self.value
+            self.x = 2 * (self.id // 3) + self.id / 10 * (self.id % 3)
         if self.y is None:
-            self.y = self.value
+            self.y = 2 * (self.id % 3) + self.id /10 * (self.id // 3)
+        if self.value is None:
+            self.value = self.id
+        if self.color is None:
+            #what is the run time complexity of making the color string below? O(1)
+            hexValues = ['0', '1', '2','3', '4', '5','6', '7', '8','9', 'A', 'B','C', 'D','E', 'F']
+            colorString = "#"
+            for i in range(0, 3):
+                colorString += hexValues[random.randint(0,len(hexValues)-1)]
+            self.color = colorString
+                
 
-    def getAdjVertices(self):
-        adjVerticesValues = set()
-        for i in self.adjVertices:
-            adjVerticesValues.add(i.value)
-        return adjVerticesValues
 
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self, vertices = dict()):
-        self.vertices = vertices
 
-    def add_vertex(self, vertexValue):
-        vertex = Vertex(vertexValue)
-        self.vertices[vertex] = vertex
-    
-    def add_edge(self, originVertexValue, destinationVertexValue):
-        inVertices1 = False
-        inVertices2 = False
+class Graph: 
+    """Represent a graph as a dictionary of vertices mapping lables to edges."""
+    def __init__(self):
+        self.vertices = {}  #dictionary 
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id) #we're indexing the vertex in the dictionary by id, and we're setting the value to a vertex object. 
 
-        for vertex in self.vertices:
-            if originVertexValue == vertex.value:
-                inVertices1 = True
-                originVertex = vertex
-            if destinationVertexValue == vertex.value:
-                inVertices2 = True
-                destinationVertex = vertex
-        
-        if inVertices1 == True and inVertices2 == True:
-            originVertex.adjVertices.add(destinationVertex)
-            destinationVertex.adjVertices.add(originVertex)
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
         else:
             raise IndexError("That vertex does not exist!")
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+    
 
 
-# ### Test ###
-# # To test, instantiate empty graph and run the following:
-# graph = Graph()  # Instantiate your graph
-# graph.add_vertex(0)
-# graph.add_vertex(1)
-# graph.add_vertex(2)
-# graph.add_vertex(3)
-# graph.add_edge(0, 1)
-# graph.add_edge(0, 3)
-# # print(graph.vertices)
+    #Recursion method
+    def dft(self, start_vert, visited = []):
+        # Visited checks if we've visited the node before
+        visited.append(start_vert)
+        # Touch visited node (you can change color here)
+        print(self.vertices[start_vert].value)
+        #call DFS on each child (that hasn't been visited)
+        for child_vert in self.vertices[start_vert].edges:
+            #check if child has been visited
+            if child_vert not in visited:
+                #If not, call dfs
+                self.dft(child_vert)
+    
+    # stack method
+    def dft_stack(self, starting_vertex_id):
+        # Create empty queue
+        stack = Stack()
+        # Put starting vert in the queue
+        stack.push(starting_vertex_id)
+        # Declare visited list
+        visited = []
+        # While the queue is not empty..
+        while stack.size() > 0:
+            while stack.size()>0:
+                # remove the first item from the queue...
+                v = stack.pop()
+                # ... then  if it has not been visited
+                if v not in visited:
+                    # ... print its value...
+                    print(self.vertices[v].value)
+                    visited.append(v) # ... mark as visited
+                    # ... then put its children into the queue
+                    for next_vert in self.vertices[v].edges:
+                        stack.push(next_vert)
+    
+    def bft(self, starting_vertex_id):
+        # create empty queue
+        q = Queue()
+        # put starting vert in the queue
+        q.enqueue(starting_vertex_id)
+        # declare visited list
+        visited = []
+        # While the queue is not empty...
+        while q.size() > 0:
+            # ... remove the first item from the queue...
+            v = q.dequeue()
+            # ... then if it has not been visited...
+            if v not in visited:
+                # ... print its value...
+                print(self.vertices[v].value)
+                visited.append(v) # ...mark as visited...
+                # ... then put its children in the queue
+                for next_vert in self.vertices[v].edges:
+                    q.enqueue(next_vert)
 
-# for i in graph.vertices:
-#     print("Value: ", i.value, " ; AdjVertices: ", i.getAdjVertices())
+    def dfs(self,start_vert, target_value, visited=[]):
+        visited.append(start_vert)
+        # print(self.vertices[start_vert].value)
+        if self.vertices[start_vert].value == target_value:
+            return True
+        for child_vert in self.vertices[start_vert].edges:
+            if child_vert not in visited:
+                if self.dfs(child_vert, target_value, visited):
+                    return True
+        return False
+    
+    def bfs(self, starting_vertex_id, target_value):
+        q = Queue()
+        q.enqueue(starting_vertex_id)
+        visited = []
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                print(self.vertices[v].value)
+                if self.vertices[v].value == target_value:
+                    return True
+                visited.append(v) # ...mark as visited...
+                for next_vert in self.vertices[v].edges:
+                    q.enqueue(next_vert)
+        return False
+
+    
+    def dfs_path(self,start_vert, target_value, visited=[], path=[]):
+        visited.append(start_vert)
+        path=path+[start_vert]
+        if self.vertices[start_vert].value == target_value:
+            return path
+        for child_vert in self.vertices[start_vert].edges:
+            print(path)
+            if child_vert not in visited:
+                new_path = self.dfs_path(child_vert, target_value, visited, path)
+                if new_path:
+                    return new_path
+        return None
+    
+    def bfs_path(self, starting_vertex_id, target_value):
+        q = Queue()
+        q.enqueue([starting_vertex_id])
+        visited = []
+        while q.size() > 0:
+            print(q.queue) #prints every path checked before shortest path is found
+            path = q.dequeue()
+            v = path[-1]
+            if v not in visited:
+                if self.vertices[v].value == target_value:
+                    return path
+                visited.append(v) # ...mark as visited...
+                for next_vert in self.vertices[v].edges:
+                    # q.enqueue(next_vert)
+                    new_path = list(path)
+                    new_path.append(next_vert)
+                    q.enqueue(new_path)
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### Attempt two in graph1.py --> Wasn't able to get this to work with bokeh. Going to just copy code while following 
+#### along in lecture
+# """
+# Simple graph implementation compatible with BokehGraph class.
+# """
+
+# class Vertex:
+#     def __init__(self, value = None, x = None, y = None):
+#         self.value = value
+#         self.x = x
+#         self.y = y
+#         self.adjVertices = set()
+
+#         if self.x is None:
+#             self.x = self.value
+#         if self.y is None:
+#             self.y = self.value
+
+#     def getAdjVertices(self):
+#         adjVerticesValues = set()
+#         for i in self.adjVertices:
+#             adjVerticesValues.add(i.value)
+#         return adjVerticesValues
+
+# class Graph:
+#     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+#     def __init__(self, vertices = dict()):
+#         self.vertices = vertices
+
+#     def add_vertex(self, vertexValue):
+#         vertex = Vertex(vertexValue)
+#         self.vertices[vertex] = vertex
+    
+#     def add_edge(self, originVertexValue, destinationVertexValue):
+#         inVertices1 = False
+#         inVertices2 = False
+
+#         for vertex in self.vertices:
+#             if originVertexValue == vertex.value:
+#                 inVertices1 = True
+#                 originVertex = vertex
+#             if destinationVertexValue == vertex.value:
+#                 inVertices2 = True
+#                 destinationVertex = vertex
+        
+#         if inVertices1 == True and inVertices2 == True:
+#             originVertex.adjVertices.add(destinationVertex)
+#             destinationVertex.adjVertices.add(originVertex)
+#         else:
+#             raise IndexError("That vertex does not exist!")
+
+
+# # ### Test ###
+# # # To test, instantiate empty graph and run the following:
+# # graph = Graph()  # Instantiate your graph
+# # graph.add_vertex(0)
+# # graph.add_vertex(1)
+# # graph.add_vertex(2)
+# # graph.add_vertex(3)
+# # graph.add_edge(0, 1)
+# # graph.add_edge(0, 3)
+# # # print(graph.vertices)
+
+# # for i in graph.vertices:
+# #     print("Value: ", i.value, " ; AdjVertices: ", i.getAdjVertices())
 
 
 
