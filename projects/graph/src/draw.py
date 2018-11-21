5fe0ffe3571f310f77ce293dd469207e02f4f1e3
marshall <marshall@marshalls-MacBook-Air.local>
2018-11-21 00:01:37 -0800

    added bokeh to make a binary tree graph from the graph object with its vertices and edges

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -1,14 +1,80 @@
-"""
-General drawing methods for graphs using Bokeh.
-"""
+from graph import binary_tree
 
-from bokeh.io import show, output_file
-from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnDataSource, Range1d, LabelSet, Label
+from bokeh.palettes import Set3
 
 
 class BokehGraph:
-    """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+	"""Class that takes a graph and exposes drawing methods."""
+	def __init__(self, graph):
+		self.graph = graph
+	def __str__(self):
+		return f'{self.graph}'
+
+	def draw_graph(self):
+
+		# I first get the keys and values of my vertices as two separate arrays
+
+		keys = [i for i in self.graph.vertices.keys()]
+		values = [i for i in self.graph.vertices.values()]
+
+		#I start up two emtpy arrays that will store the starting and ending points
+		#that I will use in the graph
+
+		start = []
+		end = []
+
+		#I then loop through the array of values, each being its own python set
+
+		for i, j in enumerate(values):
+
+			#if I come across a set bigger than 0 it has an edge 
+			#thus connecting to something
+
+			if len(j) > 0:
+				for q in range (0, len(j)):
+
+					#I add the current index for spot in the set
+					start.append(i)
+
+					#I also add the current set value
+					end += list(j)[q]
+
+		graph = GraphRenderer()
+
+		#I use my ordered values to form the connected lines in my graphs
+		graph.edge_renderer.data_source.data = dict(
+			start=start,
+			end=end,
+		)
+
+		plot = figure(title='Binary Tree', x_range=(-200,200), y_range=(0,55), tools='', toolbar_location=None)
+
+		graph.node_renderer.data_source.add(keys, 'index')
+		graph.node_renderer.data_source.add(Set3[7], 'color')
+		graph.node_renderer.glyph = Circle(radius=15, fill_color='color')
+
+		y = [50, 40, 40, 30, 30, 30, 30]
+		x = [0, -100, 100, -150, -50, 50, 150]
+
+		source = ColumnDataSource(data=dict(
+			y = y,
+			x = x,
+			names = keys
+		))
+		labels = LabelSet(x='x', y='y', text='names', level='glyph', x_offset=-5, y_offset=-10, source=source, render_mode='canvas')
+
+
+		graph_layout = dict(zip(keys, zip(x, y)))
+		graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+		plot.renderers.append(graph)
+		plot.add_layout(labels)
+
+		output_file('graph.html')
+		show(plot)
+
+drawn_tree = BokehGraph(binary_tree)
+
+drawn_tree.draw_graph()
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,8 +7,15 @@ class Graph:
 	def __init__(self):
 		self.vertices = {}
 
+	#makes takes in the location paremeter and uses that to make the key
+	#value on the self.vertices object, then it makes its value an empty set
+	
 	def add_vertex(self, location):
 		self.vertices[location] = set()
+
+	#checks if they key is in the object
+	#if it is it will add the value destination to its set list
+	#sets are good because they will ever add the same item twice
 
 	def add_edge(self, location, destination):
 		if location in self.vertices:
@@ -20,18 +27,21 @@ class Graph:
 		return f'graph, vertices: {self.vertices}'
 
 #to fit example from readme
-graph = Graph()
+binary_tree = Graph()
 
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
+binary_tree.add_vertex('0')
+binary_tree.add_vertex('1')
+binary_tree.add_vertex('2')
+binary_tree.add_vertex('3')
+binary_tree.add_vertex('4')
+binary_tree.add_vertex('5')
+binary_tree.add_vertex('6')
 
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('1', '0')
-graph.add_edge('3', '0')
+binary_tree.add_edge('0', '1')
+binary_tree.add_edge('0', '2')
 
-graph.add_edge('4', '0')
+binary_tree.add_edge('1', '3')
+binary_tree.add_edge('1', '4')
 
-print(graph)
+binary_tree.add_edge('2', '5')
+binary_tree.add_edge('2', '6')
