a1e2bc2de6474c25885bacb5f476119c63419692
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-26 14:01:52 -0800

    Added working bfs method to graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -21,12 +21,29 @@ class Graph:
             if (not vert2 in self.vertices[vert1]) and (not vert1 in self.vertices[vert2]):
                 self.vertices[vert1].add(vert2)
                 self.vertices[vert2].add(vert1)
+            # if the edge had already been created, print error message.
             else:
                 print('This edge had already been created.  Please try again.')
-                
+        # if one of vert1 and vert2 is an invalid vertex, print error message
         else:
             print('Please provide a valid set of vertices in the graph.')
 
+    def bfs(self, starting_vert, target_vert):
+        visited = []
+        queue = [starting_vert]
+        while len(queue) > 0:
+            current_vert = queue.pop(0)
+            if current_vert == target_vert:
+                return True
+            else:
+                if not current_vert in visited:
+                    visited.append(current_vert)
+                    for vert in self.vertices[current_vert]:
+                        queue.append(vert)
+
+        return False
+    
+    
 # test
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
@@ -36,3 +53,5 @@ graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 print(graph.vertices)
+
+print(graph.bfs('3', '1'))
