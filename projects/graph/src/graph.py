9a4f3941b882cd085d648eed447722738b2814d6
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-26 14:01:21 -0800

    Added self.vertices = {} to Graph Class to store vertices in dictiuonary in graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,8 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        # store vertices in dictionary
+        self.vertices = {}
+
+graph = Graph()  # Instantiate your graph
+print(graph.vertices)
