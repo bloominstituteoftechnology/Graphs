455ed3cc080aa204549601383b255aa5c5249223
thomaskisner <tjkisner@gmail.com>
2018-11-26 17:57:20 -0500

    finished objective 1: graph, vertex, edge classes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,35 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = dict()
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices.keys():
+            self.vertices[vertex]= {0}
+
+
+    def add_edge(self, edge, vertex):
+        if vertex in self.vertices.keys():
+            if self.vertices[vertex] == {0}:
+                self.vertices[vertex] = {edge}
+                print(self.vertices[vertex])
+            else:
+                self.vertices[vertex].update(edge)
+                 
+        else:
+            raise ValueError
+    
+    
+
+
+graph = Graph()  # Instantiate your graph
+print(graph)
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('2', '1')
+graph.add_edge('1', '2')
+graph.add_edge('3', '2')
+graph.add_edge('4', '4')
+print(graph.vertices)
