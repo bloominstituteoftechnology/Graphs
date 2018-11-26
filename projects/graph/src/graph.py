87e6e84dde2b89d60fa7709968fa965959568d19
Nicocchi <nicocchilover@gmail.com>
2018-11-26 16:00:14 -0500

    bfs complete

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,6 +2,7 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+from queue import *
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -19,6 +20,26 @@ class Graph:
         val2.add(value1)
         self.verticies[value2] = val2
 
+    def bfs(self, start):
+        # Keep track of all visited nodes
+        explored = []
+        # Keep track of nodes to be checked
+        queue = [start]
+
+        # Keep looping until there are nodes still to be checked
+        while queue:
+            # Pop shallowest node (first node) from queue
+            node = queue.pop(0)
+            if node not in explored:
+                # Add node to list of checked nodes
+                explored.append(node)
+                neighbours = self.verticies[node]
+
+                # Add neighbors of node to queue
+                for neighbour in neighbours:
+                    queue.append(neighbour)
+        return explored
+
 
 graph = Graph()
 graph.add_vertex('0')
@@ -28,3 +49,5 @@ graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 print(graph.verticies)
+
+print(graph.bfs('1'))
