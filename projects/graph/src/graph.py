8856dee32eac3af01a32da6045a48361889adaf5
Nicocchi <nicocchilover@gmail.com>
2018-11-26 15:40:50 -0500

    graph class add_edge and add_vertext complete

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,25 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.verticies = {}
+
+    def add_vertex(self, value):
+        self.verticies[value] = set()
+
+    def add_edge(self, value1, value2):
+        val = self.verticies[value1]
+        val.add(value2)
+        self.verticies[value1] = val
+        val2 = self.verticies[value2]
+        val2.add(value1)
+        self.verticies[value2] = val2
+
+
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.verticies)
