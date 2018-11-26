e76582ea2fe4dd01eec2cff5b32d74cf5f65f764
Timothy Hoang <timh1203@gmail.com>
2018-11-26 14:11:11 -0600

    Initial commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,6 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.graph = {}
