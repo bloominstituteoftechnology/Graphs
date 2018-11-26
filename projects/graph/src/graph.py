a7293740d365983841d31b1f31e243e3ecb93748
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-26 14:03:40 -0800

    Added add_vertex method to add a vertex .

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,5 +9,13 @@ class Graph:
         # store vertices in dictionary
         self.vertices = {}
 
+    # add a vertex which is an empty set
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
 graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
 print(graph.vertices)
