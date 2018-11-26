2b40c50a37c482008b6b88f2c7486bb082a663ee
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-26 14:59:57 -0800

    implements a Vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,11 +9,15 @@ class Graph:
         vertices = []
 
     def add_vertex(self, vertex):
-        vertex_plus_edge = {vertex, set()}
-        self.vertices.append(vertex_plus_edge)
+        self.vertices.append(Vertex(vertex))
 
     def add_edge(self, vertex_one, vertex_two):
         first_index = vertices.index(vertex_one)
         vertices[first_index] = {vertex_one, vertex_two}
         second_index = vertices.index(vertex_two)
         vertices[second_index] = {vertex_two, vertex_one}
+
+class Vertex:
+    def __init__(self, vertex, edge=set()):
+        self.vertex = vertex
+        self.edge = edge
