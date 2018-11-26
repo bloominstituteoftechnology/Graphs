8e7fdd83f4873091da945ad086ef1140004434b9
Timothy Hoang <timh1203@gmail.com>
2018-11-26 14:24:59 -0600

    Add add_vertex method

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,4 +7,21 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
     def __init__(self):
-        self.graph = {}
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+            return True
+        else:
+            return False
+
+
+graph = Graph()
+graph.add_vertex("0")
+graph.add_vertex("1")
+graph.add_vertex("2")
+graph.add_vertex("3")
+# graph.add_edge("0", "1")
+# graph.add_edge("0", "3")
+print(graph.vertices)
