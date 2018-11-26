19bd7eb96f85856c827e9c51779ab22b99941455
mparedes003 <michelle.g.paredes@gmail.com>
2018-11-26 14:04:45 -0800

    Added a bidirectional edge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -13,9 +13,17 @@ class Graph:
     def add_vertex(self, vertex):
         self.vertices[vertex] = set()
 
+    # add a bidirectional edge 
+    def add_edge(self, from_vertex, to_vertex):
+        if from_vertex in self.vertices and to_vertex in self.vertices:
+            self.vertices[from_vertex].add(to_vertex)
+            self.vertices[to_vertex].add(from_vertex)
+
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
 print(graph.vertices)
