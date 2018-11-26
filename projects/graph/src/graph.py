2d58d6e3b2d4741b0c7be5fa1a99721642b22174
Luisan <luisangelsc@gmail.com>
2018-11-26 13:12:20 -0600

    instantiated Graph class + reject unfound vertex in add_edge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,28 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+        self.vertices = { }
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, vertex_a, vertex_b ):
+        if vertex_a in self.vertices.keys() and vertex_b in self.vertices.keys():
+            self.vertices[vertex_a].add(vertex_b)
+            self.vertices[vertex_b].add(vertex_a)
+        else:
+            print ("Can't find all vertices")
+
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('4', '3')
+
+print(graph.vertices)
+
