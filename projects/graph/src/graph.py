f393114228546c2e326be012f576bd596feede4b
Anthony Greb <agreb255@gmail.com>
2018-11-26 18:54:15 -0500

    Graph class done

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,47 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, label):
+        if label in self.vertices:
+            print('Already accounted for')
+        else:
+            self.vertices[label] = set()
+
+    def add_edge(self, label, destination):
+        if label in self.vertices:
+            if destination in self.vertices:
+                self.vertices[label].add(destination)
+            else:
+                print('No destination')
+        else:
+            print('No vertex')
+
+
+class Vertex:
+    def __init__(self, vertex_id, x=None, y=None):
+        self.id = vertex_id
+        self.edges = set()
+        if x is None:
+            self.x = random.random() * 10 - 5
+        else:
+            self.x = x
+        if y is None:
+            self.y = random.random() * 10 - 5
+        else:
+            self.y = y
+
+    def __repr__(self):
+        return f"{self.edges}"
+
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    print(graph.vertices)
