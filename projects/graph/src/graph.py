61049ae8ba02a688b91bace0e1bd3fae6bda605f
Joseph Chretien-Golden <joechre@gmail.com>
2018-11-26 16:12:02 -0600

    GRAPH as dictionary

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,12 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.verticies = {}
+    def add_vertex(self, vertex_id):
+        self.verticies[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.verticies and v2 in self.verticies:
+            self.verticies[v1].add(v2)
+            self.verticies[v2].add(v1)
+        else:
+            raise IndexError("No vertex!")
