8b8e4e43873b10aa7c2db2cd3f7ec57e982d45f2
lyang9 <lyang9@buffalo.edu>
2018-11-26 18:21:17 -0500

    Modified class Graph and completed class Vertex

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,12 +8,28 @@ class Graph:
     def __init__(self):
         self.vertices = {}
     
-    def add_vertex(self,vertex_id):
-        self.vertices[vertex_id] = Vertex(vertex_id)
+    def add_vertex(self, value):
+        self.vertices[value] = Vertex(value)
 
-    def add_edges(self, v1, v2):
+    def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].edges.add(v2)
             self.vertices[v2].edges.add(v1)
         else:
             raise IndexError('Vertex not found')
+
+
+class Vertex:
+    def __init__(self, value):
+        self.node = value
+        self.edges = set()
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
