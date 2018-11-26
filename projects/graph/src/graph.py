98328c8f86f043e50118c79373aefbf2c4b30938
Kevin Nguyen <kevinn858@gmail.com>
2018-11-26 13:17:21 -0800

    finished graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -8,3 +8,26 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
+    def add_vertex(self, label):
+        if label in self.vertices:
+            raise "Vertex already exists.", label
+        else:
+            self.vertices[label] = set()
+    
+    def add_edge(self, label, destination):
+        if label in self.vertices:
+            if destination in self.vertices:
+                self.vertices[label].add(destination)
+            else:
+                raise 'Destination does not exist.', destination
+        else:
+            raise 'Vertex does not exist.', label
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
