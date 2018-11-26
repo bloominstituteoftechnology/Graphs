2ca8c3bed1c5891028881af02726d75e36c1a832
IlanL75 <anatoly.leytman@gmail.com>
2018-11-26 15:56:22 -0800

    Adding graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,18 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+       self.vertices = {}
+       
+    def add_vertex(self, key):
+        self.vertices[key] = []
+        return self.vertices[key]
+
+    def add_edge(self, key, t):
+        if key not in self.vertices or t not in self.vertices:
+            print('Vertex could not be faund!')
+        else:   
+            self.vertices[key].append(t)
+            return self.vertices[key]
