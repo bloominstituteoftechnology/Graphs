7cd69789cc20fa735a44e4822f5ccbf743476c30
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-26 14:01:28 -0800

    added vertices to the graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,8 +2,19 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+mygraph = {
+    '0': {'1', '3'},
+    '1': {'0'},
+    '2': set(),
+    '3': {'0'}
+}
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.value = value
+    def add_edge(self, target):
+        myg = nx.Graph(mygraph)
+    def add_vertex(self, target):
+        copy = self.copy()
+        copy.update(target)
