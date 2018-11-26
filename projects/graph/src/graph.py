761dc1b3262b76c1af1b187bec8da9dc1cdd3b8e
decagondev <decagondevelopment@gmail.com>
2018-11-26 21:17:17 +0000

    (MVP) : DAY 1 : Added __str__ method to the Vector class and run some usage tests

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -15,7 +15,8 @@ class Vertex:
         self.pos = Vec2(0, 0) if pos is None else pos
         self.colour = "white" if colour is None else colour
         self.data = F"v{self.id}" if data is None else data
-
+    def __str__(self):
+        return F"Vertex( id: {self.id}, x: {self.pos.x}, y: {self.pos.y}, data: {self.data})"
 
 
 
@@ -24,3 +25,9 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+# some basic tests for the vertex class
+
+v0 = Vertex('0', Vec2(3, 4))
+
+print(v0)
