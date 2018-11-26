02cf88c85c92a119557db8b547e15d656a611bcc
decagondev <decagondevelopment@gmail.com>
2018-11-26 20:53:08 +0000

    (MVP) : DAY 1 : Started implementing a basic Vec2 class for position of Vertex, and the beginnings of the Vertex class (takes in id, pos, colour, data as arguments for the constructor / init)

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,6 +2,23 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+# super really basic vec2 class
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# trying to implement the basics of a vertex class
+class Vertex:
+    def __init__(self, id, pos, colour = None, data = None):
+        self.id = int(id)
+        self.pos = Vec2(0, 0) if pos is None else pos
+        self.colour = "white" if colour is None else colour
+        self.data = F"v{self.id}" if data is None else data
+
+
+
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
