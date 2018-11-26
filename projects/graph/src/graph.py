6f0a7a8fd911257e6a8a3975ea936c31c025da03
Timothy Hoang <timh1203@gmail.com>
2018-11-26 17:47:23 -0600

    Add bft

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -36,13 +36,45 @@ class Graph:
             elif v2 not in self.vertices:
                 raise IndexError(f"Vertex {v2} is nonexistent!")
 
+    def bft(self, target):
+        q = []
+        visited = []
+        q.append(int(list(self.vertices.keys())[0]))
+
+        # Could run an initial check of existence off the bat? Does not follow BFT Flow.
+        # if str(target) not in self.vertices:
+        #     raise IndexError(f"Vertex {target} is nonexistent!")
+
+        while len(q) > 0:
+            discovered = q.pop(0)
+            if discovered not in visited:
+                visited.append(discovered)
+                if discovered == target:
+                    break
+                for c in self.vertices[str(discovered)]:
+                    q.append(int(c))
+
+        if target in visited:
+            print(f"vertices: {self.vertices}")
+            print(f"visited: {visited}")
+            return target
+        else:
+            print(f"vertices: {self.vertices}")
+            print(f"visited: {visited}")
+            return False
+
 
 graph = Graph()
 graph.add_vertex("0")
 graph.add_vertex("1")
 graph.add_vertex("2")
 graph.add_vertex("3")
+graph.add_vertex("4")
 graph.add_edge("0", "1")
-graph.add_edge("0", "3")
-graph.add_edge("0", "4")
-print(graph.vertices)
+graph.add_edge("1", "2")
+graph.add_edge("2", "3")
+graph.add_edge("2", "4")
+# graph.add_edge("5", "2")  # checks arg1, non-existent vertex 5 raises IndexError
+# graph.add_edge("2", "5")  # checks arg2, non-existent vertex 5 raises IndexError
+print(graph.bft(4))  # returns 4
+print(graph.bft(5))  # non-existent vertex 5, returns False
