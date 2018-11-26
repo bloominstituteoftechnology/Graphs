da8179fe0f7b001d06832c0e7afc81bdd48b920d
jlaluces123 <jlalucescareer123@gmail.com>
2018-11-26 14:42:17 -0800

    Graph.py--Day 1: add_vertex() done and add_edge() done, Also added a check for non-existant vertices

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -10,13 +10,24 @@ class Graph:
         there should be a field vertices that contains a dictionary
         mapping vertex labels to edges.
         '''
-        self.verticies = {}
+        self.vertices = {}
 
     def add_vertex(self, value): # --> Takes in a value
-        self.verticies[value] = set() # --> Creates a set with given value
+        self.vertices[value] = set() # --> Creates a set with given value
 
-    def add_edge(self, vertex_one, vertex_two):
+    def add_edge(self, vertex_one, vertex_two): # --> vertex_one and vertex_two 
         # --> Need to check first if the vertex exists
-        # --> To do this, we can use set-methods: get() to check if it is self.verticies
-        if self.vertices.get(vertex_one) not None and self.verticies.get(vertex_two) not None:
+        # --> To do this, we can use set-methods: get() to check if it is self.vertices
+        if self.vertices.get(vertex_one) is not None and self.vertices.get(vertex_two) is not None:
             # --> Connect edges!
+            self.vertices[vertex_one].add(vertex_two)
+            self.vertices[vertex_two].add(vertex_one)
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
