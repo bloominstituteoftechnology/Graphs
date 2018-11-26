d87ed0d56dc42da636de2311cda47156274db189
jlaluces123 <jlalucescareer123@gmail.com>
2018-11-26 14:06:19 -0800

    Graph.py--Day 1: add_vertex() done and add_edge() almost done

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,17 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        '''
+        there should be a field vertices that contains a dictionary
+        mapping vertex labels to edges.
+        '''
+        self.verticies = {}
+
+    def add_vertex(self, value): # --> Takes in a value
+        self.verticies[value] = set() # --> Creates a set with given value
+
+    def add_edge(self, vertex_one, vertex_two):
+        # --> Need to check first if the vertex exists
+        # --> To do this, we can use set-methods: get() to check if it is self.verticies
+        if self.vertices.get(vertex_one) not None and self.verticies.get(vertex_two) not None:
+            # --> Connect edges!
