97ac839c4f540bb9588d7491c01eff8e012ae021
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-26 14:39:37 -0800

    beginnings of working graph object

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,14 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        vertices = []
+
+    def add_vertex(self, vertex):
+        vertex_plus_edge = {vertex, set()}
+        self.vertices.append(vertex_plus_edge)
+
+    def add_edge(self, vertex_one, vertex_two):
+        first_index = vertices.index(vertex_one)
+        vertices[first_index] = {vertex_one, vertex_two}
+        second_index = vertices.index(vertex_two)
+        vertices[second_index] = {vertex_two, vertex_one}
