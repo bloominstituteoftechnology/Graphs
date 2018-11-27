f22269cf597614e28a8d507c68e149f3068a690f
Michael Trevino <preince560m@gmail.com>
2018-11-27 17:48:21 -0600

    working

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,6 +7,15 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# print(graph.vertices)
+
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -19,6 +19,19 @@ Simple graph implementation compatible with BokehGraph class.
 #     def __repr__(self):
 #         return 'Vertex: ' + self.label
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
