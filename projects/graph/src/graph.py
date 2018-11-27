6c502f4866c27344bde08799366416dffca73172
Nicocchi <nicocchilover@gmail.com>
2018-11-27 15:54:39 -0500

    refactored to Brady's structure and implemented dfs,bfs, and recursive dfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,87 +2,270 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-from queue import *
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if (self.size()) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
+class Vertex:
+    def __init__(self, vertex_id):
+        self.id = vertex_id
+        self.edges = set()
+
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.verticies = {}
+        self.vertices = {}
 
-    def add_vertex(self, value):
-        self.verticies[value] = set()
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)
 
-    def add_edge(self, value1, value2):
-        # Check's if the node exists, if not, return
-        if value2 not in self.verticies:
-            return
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
 
-        val = self.verticies[value1]
-        val.add(value2)
-        self.verticies[value1] = val
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
 
-        val2 = self.verticies[value2]
-        val2.add(value1)
-        self.verticies[value2] = val2
+    def bft(self, start):
+        # Create empty Queue
+        q = Queue()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the queue
+        q.enqueue(start)
+        # While the Queue is not empty...
+        while q.size() > 0:
+            # Remove the first node from the Queue
+            node = q.dequeue()
+            # If it hasn't been visited
+            if node not in visited:
+                # Mark it as visited
+                print(node)
+                visited.add(node)
+                # then put all it's children in the queue
+                for child in self.vertices[node].edges:
+                    q.enqueue(child)
 
-    def dfs(self, start):
-        # Keep track of all the visited nodes and setup a stack
-        visited, stack = set(), [start]
+    def dft(self, start):
+        # Create empty Stack
+        s = Stack()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the stack
+        s.push(start)
+        # While the Stack is not empty...
+        while s.size() > 0:
+            # Remove the first node from the Queue
+            node = s.pop()
+            # If it hasn't been visited
+            if node not in visited:
+                # Mark it as visited
+                print(node)
+                visited.add(node)
+                # then put all it's children in the stack
+                for child in self.vertices[node].edges:
+                    s.push(child)
 
-        # Keep looping until there are nodes still to be checked
-        while stack:
-            vertex = stack.pop()
-            # Add the node if it is not in visited
-            if vertex not in visited:
-                visited.add(vertex)
-                stack.extend(self.verticies[vertex] - visited)
-        return visited
-
-    # Recursive solution
-    def dfs_recursive(self, start, visited=None):
-        # Set visited as a new set if not already
+    # I could probably simplify this more
+    def dft_r(self, node, visited=None, s=None):
         if visited is None:
             visited = set()
+        if s is None:
+            s = Stack()
+        s.push(node)
 
-        # Add the node
-        visited.add(start)
-
-        # Repeat until no more nodes left
-        for next in self.verticies[start] - visited:
-            self.dfs_recursive(next, visited)
+        node = s.pop()
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            for child in self.vertices[node].edges:
+                s.push(child)
+                self.dft_r(child, visited, s)
         return visited
 
-    def bfs(self, start):
-        # Keep track of all visited nodes
-        explored = []
-        # Keep track of nodes to be checked
-        queue = [start]
+    def bfs(self, start, destination):
+        # Create empty Queue
+        q = Queue()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the queue
+        q.enqueue(start)
+        path = []
 
-        # Keep looping until there are nodes still to be checked
-        while queue:
-            # Pop shallowest node (first node) from queue
-            node = queue.pop(0)
-            if node not in explored:
-                # Add node to list of checked nodes
-                explored.append(node)
-                neighbours = self.verticies[node]
+        if start == destination:
+            return "Start = Destination!"
+        # While the Queue is not empty...
+        while q.size() > 0:
+            # Remove the first node from the Queue
+            node = q.dequeue()
+            path.append(node)
+            # If it hasn't been visited
+            if node not in visited:
+                # Mark it as visited
 
-                # Add neighbors of node to queue
-                for neighbour in neighbours:
-                    queue.append(neighbour)
-        return explored
+                visited.add(node)
+                # then put all it's children in the queue
+                for child in self.vertices[node].edges:
+                    new_path = list(path)
+                    new_path.append(child)
+                    q.enqueue(child)
+                    if child == destination:
+                        return new_path
+        return False
+
+    def dfs(self, start, destination):
+        # Create empty Stack
+        s = Stack()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the stack
+        s.push(start)
+        path = []
+        # While the Stack is not empty...
+        while s.size() > 0:
+            # Remove the first node from the Queue
+            node = s.pop()
+            path.append(node)
+            # If it hasn't been visited
+            if node not in visited:
+                # Mark it as visited
+                visited.add(node)
+                # then put all it's children in the stack
+                for child in self.vertices[node].edges:
+                    new_path = list(path)
+                    new_path.append(child)
+                    s.push(child)
+                    if child == destination:
+                        return new_path
 
 
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '5')
-print(graph.verticies)
+"""
+First pass representation of Graph - without Vertex class
+"""
 
-print(graph.bfs('1'))
-print(graph.dfs('1'))
-print(graph.dfs_recursive('1'))
+
+# class Graph:
+#     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+#     def __init__(self):
+#         self.verticies = {}
+#
+#     def add_vertex(self, value):
+#         self.verticies[value] = set()
+#
+#     def add_edge(self, value1, value2):
+#         # Check's if the node exists, if not, return
+#         if value2 not in self.verticies:
+#             return
+#
+#         val = self.verticies[value1]
+#         val.add(value2)
+#         self.verticies[value1] = val
+#
+#         val2 = self.verticies[value2]
+#         val2.add(value1)
+#         self.verticies[value2] = val2
+#
+#     def dfs(self, start):
+#         # Keep track of all the visited nodes and setup a stack
+#         visited, stack = set(), [start]
+#
+#         # Keep looping until there are nodes still to be checked
+#         while stack:
+#             vertex = stack.pop()
+#             # Add the node if it is not in visited
+#             if vertex not in visited:
+#                 visited.add(vertex)
+#                 stack.extend(self.verticies[vertex] - visited)
+#         return visited
+#
+#     # Recursive solution
+#     def dfs_recursive(self, start, visited=None):
+#         # Set visited as a new set if not already
+#         if visited is None:
+#             visited = set()
+#
+#         # Add the node
+#         visited.add(start)
+#
+#         # Repeat until no more nodes left
+#         for next in self.verticies[start] - visited:
+#             self.dfs_recursive(next, visited)
+#         return visited
+#
+#     def bfs(self, start, goal):
+#         # Keep track of all visited nodes
+#         explored = []
+#         # Keep track of nodes to be checked
+#         queue = [[start]]
+#
+#         # Keep looping until there are nodes still to be checked
+#         while queue:
+#             # Pop shallowest node (first node) from queue
+#             path = queue.pop(0)
+#             node = path[-1]
+#             if node not in explored:
+#                 # Add node to list of checked nodes
+#                 neighbours = self.verticies[node]
+#
+#                 # Add neighbors of node to queue
+#                 for neighbour in neighbours:
+#                     new_path = list(path)
+#                     new_path.append(neighbour)
+#                     queue.append(new_path)
+#                     if neighbour == goal:
+#                         return new_path
+#
+#                 explored.append(node)
+#         return explored
+
+
+# graph = Graph()
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# graph.add_edge('0', '5')
+# print(graph.verticies)
+#
+# print(graph.bfs('1'))
+# print(graph.dfs('1'))
+# print(graph.dfs_recursive('1'))
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,10 +5,39 @@ Demonstration of Graph and BokehGraph functionality.
 """
 
 from sys import argv
+from graph import Graph
 
 
 def main():
-    pass  # TODO
+    graph = Graph()
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_vertex(8)
+    graph.add_directed_edge(1, 2)
+    graph.add_directed_edge(2, 3)
+    graph.add_directed_edge(2, 4)
+    graph.add_directed_edge(3, 5)
+    graph.add_directed_edge(4, 6)
+    graph.add_directed_edge(4, 7)
+    graph.add_directed_edge(5, 3)
+    graph.add_directed_edge(6, 3)
+    graph.add_directed_edge(7, 6)
+    graph.add_directed_edge(7, 1)
+    print("BFT")
+    graph.bft(1)
+    print("DFT")
+    graph.dft(1)
+    print("Recursive")
+    graph.dft_r(1)
+    print("BFS")
+    print(graph.bfs(2, 5))
+    print("DFS")
+    print(graph.dfs(2, 5))
 
 
 if __name__ == '__main__':
