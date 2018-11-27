cb3d629344708257919b8b1a7a2349390014d7b5
Anthony Greb <agreb255@gmail.com>
2018-11-27 17:32:24 -0500

    added bfs and dfs traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,49 +3,119 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+class Queue:
+    def __init__(self):
+        self.queue = []
 
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if (self.size()) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
+class Graph:
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, label):
-        if label in self.vertices:
-            print('Already accounted for')
-        else:
-            self.vertices[label] = set()
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)
 
-    def add_edge(self, label, destination):
-        if label in self.vertices:
-            if destination in self.vertices:
-                self.vertices[label].add(destination)
-            else:
-                print('No destination')
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
         else:
-            print('No vertex')
+            raise IndexError("Vertex does not exist")
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("Vertex does not exist")
+
+    def bft(self, starting_node):
+        # Create an empty Queue
+        q = Queue()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the queue
+        q.enqueue(starting_node)
+        # While the Queue is not empty
+        while q.size() > 0:
+            # Remove the first node from the queue
+            node = q.dequeue()
+            # if it has been visited
+            if node not in visited:
+                # Mark it as visited
+                visited.add(node)
+                # Then put its children in the Queue
+                for child in self.vertices[node].edges:
+                    q.enqueue(child)
+
+    def dft(self, starting_node):
+        # Create an empty Stack
+        s = Stack()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the Stack
+        s.push(starting_node)
+        # While the Stack is not empty
+        while s.size() > 0:
+            # Remove the first node from the Stack
+            node = s.pop()
+            # if it has been visited
+            if node not in visited:
+                # Mark it as visited
+                visited.add(node)
+                # Then put its children in the Stack
+                for child in self.vertices[node].edges:
+                    s.push(child)
 
 
 class Vertex:
-    def __init__(self, vertex_id, x=None, y=None):
+    def __init__(self, vertex_id):
         self.id = vertex_id
         self.edges = set()
-        if x is None:
-            self.x = random.random() * 10 - 5
-        else:
-            self.x = x
-        if y is None:
-            self.y = random.random() * 10 - 5
-        else:
-            self.y = y
 
-    def __repr__(self):
-        return f"{self.edges}"
 
-    graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
-    print(graph.vertices)
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('3', '4')
+graph.add_edge('1', '2')
+graph.add_edge('3', '6')
+graph.add_edge('6', '7')
+graph.add_edge('4', '5')
+print(graph.vertices)
