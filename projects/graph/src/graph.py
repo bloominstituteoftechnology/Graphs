7ba281164e021e34050f9842718513139bcd7283
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-11-27 17:58:45 -0500

    BFS/DFS Used queues to add BFS to graph class Used stacks to add DFS to graph class Created a Queue and Stack class prior to adding BFS/DFS to Graph Class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,7 +2,33 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Queue:
+	def __init__(self):
+		self.size = 0
+		self.storage = []
+ 	def enqueue(self, item):
+		self.storage.append(item)
+		self.size += 1
+ 	def dequeue(self):
+		if self.size:
+			self.size -= 1
+		return self.storage.pop(0)
+ 	def len(self):
+		return self.size
 
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+        
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -14,12 +40,45 @@ class Graph:
             self.vertices[v1].edges.add(v2)
             self.vertices[v2].edges.add(v1)
         else:
-            raise IndexError('Unable to locate vertex')
-        
+            raise IndexError('That vertex does not exist.')
+    
+    def bfs(self, start_vertex):
+        queue = Queue()
+        visited = []
+        queue.enqueue(start_vertex) 
+        while queue.len() > 0:
+            vertex = queue.dequeue()
+            if vertex not in visited:            
+                visited.append(vertex)
+                for child in self.vertices.get(vertex):
+                    queue.enqueue(child)
+        return visited
+    
+    def dfs(self, start_vertex):
+        stack = []
+        visited = []
+        stack.append(start_vertex)
+        while len(stack) > 0:    
+            vertex = stack.pop()  
+            if vertex not in visited:       
+                visited.append(vertex)   
+                for child in self.vertices.get(vertex):
+                    stack.append(child)
+        return visited
 
 class Vertex:
-    def __init__(self,vertex_id):
-        self.id=vertex_id
-        self.edges=set()
-    def __repr__(self):
-        return f'{self.edges}'
+    def __init__(self,value):
+        self.value = value 
+        self.edges = set()
+
+
+  
+  
+graph = Graph()  
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
