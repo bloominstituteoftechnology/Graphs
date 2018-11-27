8cf899c6889c2140d7f44e0d5b8c6d6ac9427015
Brandon White <bwhite06@stac.edu>
2018-11-27 18:37:44 -0500

    working on graph problem

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,38 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices={}
+    def add_vertex(self,vertex):
+        self.vertices[vertex_id] = Vertex(vertex_id)
+    def add_edge(self,v1,v2):
+        if v1 in self.vertices and v2 in self.vertices
