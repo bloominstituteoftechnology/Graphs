04b511f39376493fed132f16e4632c0446eae42d
duckets615 <33557104+duckets615@users.noreply.github.com>
2018-11-27 11:02:11 -0500

    bfsearch

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -24,3 +24,25 @@ graph.vertex('3')
 graph.edges('0', '1')
 graph.edges('0', '3')
 print(graph.vertices)
+
+def bfsearch(self, root):
+    if self.vertices == None:
+        return
+     if root not in self.vertices:
+        raise IndexError('No vertex with that value in the graph.')
+     # initialize our visited list
+    visited = []
+    # initialize a queue
+    storage = queue.Queue()
+    # put root value in the queue
+    storage.put(self.vertices[root])
+     while not storage.empty():
+        # start at the root node
+        current = storage.get()
+         if current not in visited:
+            visited.append(current)
+         for edge in current.edges:
+            if self.vertices[edge] not in visited:
+                storage.put(self.vertices[edge])
+     print(f'visited: {visited}')
+    return visited
