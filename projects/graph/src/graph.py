c9ef15fe7db2115045bf1b4ad567d0f8fa3d8b4a
Michael Trevino <preince560m@gmail.com>
2018-11-27 10:18:39 -0600

    test from friend

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,31 +7,34 @@ Simple graph implementation compatible with BokehGraph class.
 #     '2': set(),
 #     '3': {'0'}
 # }
-class Vertex:
-    def __init__(self, label, component=-1):
-        self.label = str(label)
-        self.component = component
-        # self.edge = set()
+# class Vertex:
+#     def __init__(self, label, component=-1):
+#         self.label = str(label)
+#         self.component = component
+#         # self.edge = set()
     
-    # def __str__(self):
-    #     print(self.label)
+#     # def __str__(self):
+#     #     print(self.label)
 
-    def __repr__(self):
-        return 'Vertex: ' + self.label
+#     def __repr__(self):
+#         return 'Vertex: ' + self.label
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-        self.components = 0
+        # self.components = 0
 
-    def add_vertex(self, Vertex):
-        self.vertices(Vertex) = set{}
+    def add_vertex(self, key):
+        self.vertices[key] = []
+        return self.vertices[key]
 
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].edges.add[v2]
-            self.vertices[v2].edges.add[v1]
+    def add_edge(self, key, t):
+        if key not in self.vertices or t not in self.vertices:
+            print('vertex not found')
+        else:
+            self.vertices[key].append(t)
+            return self.vertices[key]
 
     # def add_vertex(self, vertex, edges=()):
     #     if vertex in self.vertices:
@@ -46,3 +49,12 @@ class Graph:
     #     self.vertices[start].add(end)
     #     if bidirectional:
     #         self.vertices[end].add(start)
+
+# graph = Graph()  # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '3')
+# print(graph.vertices)
