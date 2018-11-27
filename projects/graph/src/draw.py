f32a1befa9f25702bd696dd541fe5d77275079de
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-27 13:53:48 -0800

    Fixed dft and bft methods

--- projects/graph/src/draw.py
+++ projects/graph/src/draw.py
@@ -7,8 +7,12 @@ from bokeh.plotting import figure
 from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
                           ColumnDataSource)
 
+from graph import Graph
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, graph_obj):
+        self.vertices = graph_obj.vertices
+    
+    def show(self):
+        
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,7 +1,9 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
-
+class Vertex:
+    def __init__(self, vertex_id):
+        self.id = vertex_id
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -30,48 +32,74 @@ class Graph:
         else:
             print('Please provide a valid set of vertices in the graph.')
             return
-
-    def bfs(self, starting_vert, target_vert):
-        visited = []
-        queue = [starting_vert]
-        while len(queue) > 0:
-            current_vert = queue.pop(0)
-            # print(current_vert)
-            if current_vert == target_vert:
-                visited.append(current_vert)
-                return visited
-            else:
-                if not current_vert in visited:
-                    visited.append(current_vert)
-                    for vert in self.vertices[current_vert]:
-                        queue.append(vert)
-        return 'No path found that linked vertex ' + starting_vert + ' to vertex ' + target_vert
     
-    def dfs(self, starting_vert, target_vert):
-        visited = []
-        stack = [starting_vert]
-        while len(stack) > 0:
-            current_vert = stack.pop(len(stack)-1)
-            # print(current_vert)
-            if current_vert == target_vert:
-                visited.append(current_vert)
-                return visited
+    def add_directed_edge(self, vert1, vert2):
+        if vert1 in self.vertices and vert2 in self.vertices:
+            if vert2 not in self.vertices[vert1]:
+                self.vertices[vert1].add(vert2)
             else:
-                if not current_vert in visited:
-                    visited.append(current_vert)
-                    for vert in self.vertices[current_vert]:
-                        stack.append(vert)
-        return 'No path found that linked ' + starting_vert + ' to ' + target_vert
+                print('This edge had already been created in the graph.')
+                return
+        # if one of vert1 and vert2 is an invalid vertex, print error message
+        else:
+            print('Please provide a valid set of vertices in the graph.')
+            return
+    # Breadth first traversal
+    def bft(self, starting_vert):
+        # create storage for vertices visited
+        visited = []
+        # create queue to maintain BFT order of vertices visited
+        queue = [starting_vert]
+        # While we have not finished checking all edges, as depicted by the number of elements left in the queue:
+        while len(queue) > 0:
+            # dequeue and check if element has been visited; if it hadn't, add it to the visited list, follow by adding its children vertices to the queue
+            current_vert = queue.pop(0)
+            if not current_vert in visited:
+                visited.append(current_vert)
+                for vert in self.vertices[current_vert]:
+                    queue.append(vert)
+        return visited
+    
+    # Depth first traversal
+    def dft(self, starting_vert):
+        # create storage for vertices visited
+        visited = []
+        # create stack to maintain BFT order of vertices visited
+        stack = [starting_vert]
+        # While we have not finished checking all edges, as depicted by the number of elements left in the queue:
+        while len(stack) > 0:
+             # dequeue and check if element has been visited; if it hadn't, add it to the visited list, follow by adding its children vertices to the stack
+            current_vert = stack.pop(-1)
+            if current_vert not in visited:
+                visited.append(current_vert)
+                for vert in self.vertices[current_vert]:
+                    stack.append(vert)
+        return visited
+
+    # Depth first traversal - recursive
+    def dft_recursive(self, starting_vert):
+        visited = []
+        
+
 # test
 graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '4')
-graph.add_edge('3','0')
-print(graph.vertices)
-print(graph.bfs('3', '1'))
-print(graph.dfs('0', '3'))
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+graph.add_vertex('7')
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('2', '3')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('4', '6')
+graph.add_directed_edge('4', '7')
+graph.add_directed_edge('6', '3')
+graph.add_directed_edge('7', '6')
+graph.add_directed_edge('7', '1')
+graph.add_edge('3', '5')
+
+# print(graph.vertices)
+# print(graph.bft('1'))
+print(graph.dft('1'))
