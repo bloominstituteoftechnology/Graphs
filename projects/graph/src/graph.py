04608f2c06ebf1ab0c793684ffee89a674a26b09
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-26 16:07:48 -0800

    Graph class defined and about to test it

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,10 +2,24 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
+class Vertex:
+    """Vertices have a label and a set of edges."""
+    # pylint: disable=too-few-public-methods
+    def __init__(self, label):
+        self.label = label  # each vertex has a label
+        self.edges = set()  # each vertex has a set of multiple edges
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         # Start here:
-        self.vertices = []
-        self.
+        self.vertices = {}
+    
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+    
