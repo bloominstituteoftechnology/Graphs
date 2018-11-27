c23f182a44b686a43d2a762f466f8b41faba1127
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-27 15:49:57 -0800

    added BFS

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,28 +1,125 @@
-"""
-Simple graph implementation compatible with BokehGraph class.
-"""
-
-mygraph = {
-    '0': {'1', '3'},
-    '1': {'0'},
-    '2': set(),
-    '3': {'0'}
-}
-
-class Graph(object):
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self, graph_dict=None):
-        if graph_dict == None:
-            graph_dict = {}
-        self.__graph_dict = graph_dict
-    def add_edge(self, target):
-        # myg = nx.Graph(mygraph)
-        edge = set(edge)
-        (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
         else:
-            self.__graph_dict[vertex1] = [vertex2]
-    def add_vertex(self, target):
-        if target not in self.__graph_dict:
-            self.__graph_dict[target] = []
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+            self.vertices[v2].edges.add(v1)
+        else:
+            raise IndexError("That vertex doesn't exist")
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError("That vertex doesn't exist")
+    def bft(self, starting_node):
+        # Create an empty Queue
+        q = Queue()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the queue
+        q.enqueue(starting_node)
+        # While the Queue is not empty...
+        while q.size() > 0:
+            # remove the first node from the Queue
+            node = q.dequeue()
+            # If it hasnt been visited
+            if node not in visited:
+                # Mark it as visited
+                print(node)
+                visited.add(node)
+                # then put all its children in the queue
+                for child in self.vertices[node].edges:
+                    q.enqueue(child)
+
+    def dft(self, starting_node):
+        # Create an empty Stack
+        s = Stack()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the stack
+        s.push(starting_node)
+        # While the Stack is not empty
+        while s.size() > 0:
+            # remove the first node from the Stack
+            node = s.pop()
+            # If it hasnt been visited
+            if node not in visited:
+                # Mark it as visited
+                print(node)
+                visited.add(node)
+                # then put all its children in the stack
+                for child in self.vertices[node].edges:
+                    s.push(child)
+
+    def dft_r(self, starting_node):
+        # Mark starting_node as visited
+        visited = set()
+        visited.push(starting_node)
+        # Then call dft_r on each child
+        
+
+
+    visited = [1, 2, 3, 4]
+    queue = [[1, 2, 3, 5], [1, 2, 4, 6], [1, 2, 4, 7]]
+    path = []
+
+    def bfs(self, starting_node, destination_node):
+        # Create an empty Queue
+        q = Queue()
+        # Create an empty visited list
+        visited = set()
+        # Add the start node to the queue
+        q.enqueue(starting_node)
+        # While the Queue is not empty...
+        while q.size() > 0:
+            # remove the first node from the Queue
+            node = q.dequeue()
+            print(node, end = ' ')
+            # If it hasnt been visited
+            if node not in visited:
+                # Mark it as visited
+                if destination_node == node:
+                    return True
+                visited.add(node)
+                # then put all its children in the queue
+                for child in self.vertices[node].edges:
+                    q.enqueue(child)
+                    visited[child] = True
+        return False
+
+class Vertex:
+    def __init__(self, vertex_id):
+        self.id = vertex_id
+        self.edges = set()
+
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -3,13 +3,22 @@
 """
 Demonstration of Graph and BokehGraph functionality.
 """
-
+from graph import Graph
 from sys import argv
 
-
 def main():
-    pass  # TODO
-
+    graph = Graph()
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_directed_edge(1, 2)
+    graph.add_directed_edge(2, 3)
+    graph.add_directed_edge(2, 4)
+    
 
 if __name__ == '__main__':
     # TODO - parse argv
