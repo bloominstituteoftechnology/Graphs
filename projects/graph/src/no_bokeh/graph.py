78feeeaf4ae81210fa907f308dbf0743cc765f1c
marshall <marshall@marshalls-MacBook-Air.local>
2018-11-27 14:36:57 -0800

    removed extra code

--- projects/graph/src/no_bokeh/graph.py
+++ projects/graph/src/no_bokeh/graph.py
@@ -5,20 +5,6 @@ class Vertex:
 	def __str__(self):
 		return f'edges: {self.edges}'
 
-
-class Stack:
-  def __init__(self):
-      self.stack = []
-  def push(self, value):
-      self.stack.append(value)
-  def pop(self):
-      if self.size() > 0:
-          return self.stack.pop()
-      else:
-          return None
-  def size(self):
-      return len(self.stack)
-
 class Graph:
 	def __init__(self):
 		self.vertices = {}
