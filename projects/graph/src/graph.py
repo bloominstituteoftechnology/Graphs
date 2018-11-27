cb3a2680f7532140456f708c7d2f47f3f4168b22
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-11-26 16:01:44 -0800

    add edges

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,12 +9,20 @@ mygraph = {
     '3': {'0'}
 }
 
-class Graph:
+class Graph(object):
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        self.value = value
+    def __init__(self, graph_dict=None):
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
     def add_edge(self, target):
-        myg = nx.Graph(mygraph)
+        # myg = nx.Graph(mygraph)
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
     def add_vertex(self, target):
-        copy = self.copy()
-        copy.update(target)
+        if target not in self.__graph_dict:
+            self.__graph_dict[target] = []
