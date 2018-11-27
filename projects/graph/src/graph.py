3bcd12be59bf55df9e173d4c4c81660ecc3def3a
lyang9 <lyang9@buffalo.edu>
2018-11-27 17:31:58 -0500

    Modified class Graph and Vertex. Created add_directed_edge method. Added comments

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,30 +6,28 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {}                                  # initialize an empty dictionary of verticies
     
-    def add_vertex(self, value):
-        self.vertices[value] = Vertex(value)
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = Vertex(vertex_id)        # key to be vertex_id and value to be Vertex 
 
-    def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
+    def add_edge(self, v1, v2):                             # undirected graph
+        if v1 in self.vertices and v2 in self.vertices:     # if v1 and v2 both exist in the vertex list
             self.vertices[v1].edges.add(v2)
             self.vertices[v2].edges.add(v1)
         else:
             raise IndexError('Vertex not found')
 
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].edges.add(v2)
+        else:
+            raise IndexError('Vertex not found')
+
 
 class Vertex:
-    def __init__(self, value):
-        self.node = value
+    def __init__(self, vertex_id):
+        self.id = vertex_id
         self.edges = set()
 
 
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-print(graph.vertices)
