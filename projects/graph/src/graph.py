e71d7473004b2703fa87282a42fac54733a81db9
Andrew Liao <andrewoliao@gmail.com>
2018-11-26 21:23:49 -0800

    Implement Breadth First Search to return path

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 """
 Simple graph implementation compatible with BokehGraph class.
 """
@@ -23,15 +25,71 @@ class Graph:
     	self.vertices[vertex1].add(vertex2)
     	self.vertices[vertex2].add(vertex1)
     	
+    def search(self,starting_vertex, vertex):
+    	if starting_vertex not in self.vertices:
+    		raise Exception(f'Starting vertex {starting_vertex} does not exist')
+    	if vertex not in self.vertices:
+    		raise Exception(f'Vertex {vertex} does not exist')
     	
+    	queue = deque()
+    	queue.append(starting_vertex)
+
+    	#keeps track if vertex is visited and its previous node
+    	tally = {starting_vertex:[False,None]}
+
+
+    	while queue:
+    		#dequeue vertex
+    		current_vertex = queue.popleft()
+    		#stops while loop if vertex has been found
+    		if current_vertex == vertex:
+    			print(True)
+    			break
+    		#gets neighbors of vertex and adds it to queue
+    		neighbors = self.vertices[current_vertex]
+
+    		queue.extend(neighbors)
+    		#update tally to include neighbors keeping track of prev node
+    		for neighbor in neighbors:
+    			if neighbor not in tally:
+    				tally[neighbor] = [False, current_vertex]
+    		#mark vertex as visited
+    		tally[current_vertex][0] = True
+
+    	reverse_path = [vertex]
+    	previous = tally[vertex][1]
+    	while previous:
+    		reverse_path.append(previous)
+    		previous = tally[previous][1]
+    	reverse_path.reverse()
+    	return reverse_path
+
+
+
+
+
+
+
+
+
+
 
 
 
 graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
+graph.add_vertex('A')
+graph.add_vertex('B')
+graph.add_vertex('C')
+graph.add_vertex('D')
+graph.add_vertex('E')
+graph.add_vertex('F')
+graph.add_edge('A', 'B')
+graph.add_edge('A', 'F')
+graph.add_edge('B', 'D')
+graph.add_edge('B', 'E')
+graph.add_edge('B', 'F')
+graph.add_edge('C', 'E')
+
 print(graph.vertices)
+print(graph.search('F', 'C'))
+
