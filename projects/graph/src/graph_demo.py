a372cb1a610743ba2e9439d0d112e1303c88ad67
jaymzsocool <james.mcraejr@gmail.com>
2018-11-27 17:47:16 -0500

    DFS completed, still working on BFS

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,11 +5,30 @@ Demonstration of Graph functionality.
 """
 
 from sys import argv
+from graph import Graph
 
 
 def main():
-    pass  # TODO
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_directed_edge(1,2)
+    graph.add_directed_edge(2,3)
+    graph.add_directed_edge(2,4)
+    graph.add_directed_edge(3,5)
+    graph.add_directed_edge(4,6)
+    graph.add_directed_edge(4,7)
+    graph.add_directed_edge(5,3)
+    graph.add_directed_edge(6,3)
+    graph.add_directed_edge(7,6)
+    graph.add_directed_edge(7,1) 
 
+    graph.dfs(1,6)
 
 if __name__ == '__main__':
     # TODO - parse argv
