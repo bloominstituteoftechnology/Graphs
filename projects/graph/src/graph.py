82f48095e0fe4da96623dc07eb625a9d3cb20700
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-11-26 16:47:10 -0800

    Successfully created instance of Graph class with separate Vertex class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,13 +1,17 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+print('\nNEW FILE RUN: graph.py')
 
 class Vertex:
     """Vertices have a label and a set of edges."""
     # pylint: disable=too-few-public-methods
     def __init__(self, label):
-        self.label = label  # each vertex has a label
-        self.edges = set()  # each vertex has a set of multiple edges
+        self.label = int(label)  # each vertex has a label
+        self.edges = set()       # each vertex has a set of multiple edges
+    
+    def __repr__(self):
+        return f'{self.label}'
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -23,3 +27,16 @@ class Graph:
             self.vertices[v1].edges.add(v2)
             self.vertices[v2].edges.add(v1)
     
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+graph.add_edge('1', '2')
+graph.add_edge('0', '4')
+print(len(graph.vertices))
+print(graph.vertices)
