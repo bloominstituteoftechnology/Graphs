5dbad50d24a6a11426927a00ae022d74560eb752
MichelleOk <umbralcompendium@gmail.com>
2018-11-27 17:02:09 -0700

    lecture notes complete, dft recursionattempt

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -3,14 +3,37 @@
 """
 Demonstration of Graph and BokehGraph functionality.
 """
-
+from graph import Graph
 from sys import argv
 
 
 def main():
-    pass  # TODO
+   graph = Graph()
+   graph.add_vertex(1)
+   graph.add_vertex(2)
+   graph.add_vertex(3)
+   graph.add_vertex(4)
+   graph.add_vertex(5)
+   graph.add_vertex(6)
+   graph.add_vertex(7)
+   graph.add_directed_edge(1, 2)
+   graph.add_directed_edge(2, 3)
+   graph.add_directed_edge(2, 4)
+   graph.add_directed_edge(3, 5)
+   graph.add_directed_edge(4, 6)
+   graph.add_directed_edge(4, 7)
+   graph.add_directed_edge(5, 3)
+   graph.add_directed_edge(6, 3)
+   graph.add_directed_edge(7, 6)
+   graph.add_directed_edge(7, 1)
+
+   graph.bft(1)
+
 
 
 if __name__ == '__main__':
     # TODO - parse argv
     main()
+
+#Traversal, will look at each node a Search will stop when the serached item returns True
+#You traverse when doing a search, Search stops when item is found
