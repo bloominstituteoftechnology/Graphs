a29efa8a5af945445d55856bcbde848f31f323b8
decagondev <decagondevelopment@gmail.com>
2018-11-28 00:34:00 +0000

    (MVP DAY 3) and some stretch : added a draw_block function to help the map drawing for my grid maze

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -212,7 +212,6 @@ class Graph:
                     return path
                 visited.append(vert)
                 for next_vert in self.vertices[vert].edges:
-                    # q.enqueue(next_vert) is no more
                     new_path = list(path)
                     new_path.append(next_vert)
                     queue.enqueue(new_path)
@@ -224,6 +223,24 @@ class Graph:
 ## helper function for the grid maze (calculation of width bias to make square)
 def width_bias(id, width):
     return (id % width, id // width)
+
+
+def draw_block(graph, id, style, width):
+    block = "."
+    if 'number' in style and id in style['number']: block = "%d" % style['number'][id]
+    if 'point_to' in style and style['point_to'].get(id, None) is not None:
+        (x1, y1) = id
+        (x2, y2) = style['point_to'][id]
+        if x2 == x1 + 1: block = ">"
+        if x2 == x1 - 1: block = "<"
+        if y2 == y1 + 1: block = "v"
+        if y2 == y1 - 1: block = "^"
+    if 'start' in style and id == style['start']: block = "A"
+    if 'goal' in style and id == style['goal']: block = "Z"
+    if 'path' in style and id in style['path']: block = "@"
+    if id in graph.walls: block = "#" * width
+    return block
+    
 # data
 WALLS = [width_bias(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
 
