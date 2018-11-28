31d4aacd56923f8d44d54e171b24700a1be81d4a
Carlos Vargas <cornielleandres@gmail.com>
2018-11-28 15:57:43 -0500

    Comment out graph tests.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -131,23 +131,23 @@ class Graph:
         return None
 
 
-# Testing implementation
-graph = Graph() # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_vertex('4')
-graph.add_vertex('5')
-graph.add_edge('0', '1')
-graph.add_edge('0', '2')
-graph.add_edge('0', '3')
-graph.add_edge('2', '3')
-graph.add_edge('3', '4')
-graph.add_edge('4', '5')
-print('Graph:', graph.vertices)
-print('BFT:', graph.bft('0'))
-print('DFT:', graph.dft('0'))
-print('DFT_R:', graph.dft_r('0'))
-print('BFS:', graph.bfs('0', '5'))
-print('DFS:', graph.dfs('0', '5'))
+# # Testing implementation
+# graph = Graph() # Instantiate your graph
+# graph.add_vertex('0')
+# graph.add_vertex('1')
+# graph.add_vertex('2')
+# graph.add_vertex('3')
+# graph.add_vertex('4')
+# graph.add_vertex('5')
+# graph.add_edge('0', '1')
+# graph.add_edge('0', '2')
+# graph.add_edge('0', '3')
+# graph.add_edge('2', '3')
+# graph.add_edge('3', '4')
+# graph.add_edge('4', '5')
+# print('Graph:', graph.vertices)
+# print('BFT:', graph.bft('0'))
+# print('DFT:', graph.dft('0'))
+# print('DFT_R:', graph.dft_r('0'))
+# print('BFS:', graph.bfs('0', '5'))
+# print('DFS:', graph.dfs('0', '5'))
