43c26a3b63bc95fa1febec2f6f16e47d8697268b
lyang9 <lyang9@buffalo.edu>
2018-11-28 03:10:15 -0500

    Created class Queue

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,6 +3,23 @@ Simple graph implementation compatible with BokehGraph class.
 """
 
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+    
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
