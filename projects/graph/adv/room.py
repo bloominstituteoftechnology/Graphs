2761a4fc9276475723b25bcbd24017e8f4237fb8
Tsai Huang <tsaihuangsd@gmail.com>
2018-11-28 15:11:10 -0800

    Current Progress
    Updated files to include instructor codes, plus modification to include room coordinates, random selection of available rooms and their available directions, and coordinate assignment of new rooms after room connection

--- projects/graph/adv/room.py
+++ projects/graph/adv/room.py
@@ -1,7 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
-    def __init__(self, name, description):
+   def __init__(self, name, description, id=None, coordinates=None):
         self.name = name
         self.description = description
         self.n_to = None
@@ -9,48 +9,66 @@ class Room:
         self.e_to = None
         self.w_to = None
         self.items = []
-    def __str__(self):
-        return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.getItemsString()}\n"
-    def printRoomDescription(self, player):
-        print(str(self))
-    def getItemsString(self):
-        if len(self.items) > 0:
-            return f"The room contains: {', '.join([item.name for item in self.items])}"
-        else:
-            return "The room is empty"
-    def connectRooms(self, direction, connectingRoom):
-        if direction == "n":
-            self.n_to = connectingRoom
-            connectingRoom.s_to = self
-        elif direction == "s":
-            self.s_to = connectingRoom
-            connectingRoom.n_to = self
-        elif direction == "e":
-            self.e_to = connectingRoom
-            connectingRoom.w_to = self
-        elif direction == "w":
-            self.w_to = connectingRoom
-            connectingRoom.e_to = self
-        else:
-            print("INVALID ROOM CONNECTION")
-            return None
-    def getRoomInDirection(self, direction):
-        if direction == "n":
-            return self.n_to
-        elif direction == "s":
-            return self.s_to
-        elif direction == "e":
-            return self.e_to
-        elif direction == "w":
-            return self.w_to
-        else:
-            return None
-    def addItem(self, item):
-        self.items.append(item)
-    def removeItem(self, item):
-        self.items.remove(item)
-    def findItemByName(self, name):
-        for item in self.items:
-            if item.name.lower() == name.lower():
-                return item
-        return None
+        # if id == None:
+        #     id = -1
+        self.id = id
+        if coordinates == None:
+            coordinates = (0, 0)
+        self.coordinates = coordinates
+
+   def __str__(self):
+       return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.getItemsString()}\n{self.getExits()}\n"
+   def printRoomDescription(self, player):
+       print(str(self))
+   def getExits(self):
+       exits = []
+       if self.n_to is not None:
+           exits.append("n")
+       if self.s_to is not None:
+           exits.append("s")
+       if self.w_to is not None:
+           exits.append("w")
+       if self.e_to is not None:
+           exits.append("e")
+       return f"Exits: [{', '.join(exits)}]"
+   def getItemsString(self):
+       if len(self.items) > 0:
+           return f"The room contains: {', '.join([item.name for item in self.items])}"
+       else:
+           return "The room is empty"
+   def connectRooms(self, direction, connectingRoom):
+       if direction == "n":
+           self.n_to = connectingRoom
+           connectingRoom.s_to = self
+       elif direction == "s":
+           self.s_to = connectingRoom
+           connectingRoom.n_to = self
+       elif direction == "e":
+           self.e_to = connectingRoom
+           connectingRoom.w_to = self
+       elif direction == "w":
+           self.w_to = connectingRoom
+           connectingRoom.e_to = self
+       else:
+           print("INVALID ROOM CONNECTION")
+           return None
+   def getRoomInDirection(self, direction):
+       if direction == "n":
+           return self.n_to
+       elif direction == "s":
+           return self.s_to
+       elif direction == "e":
+           return self.e_to
+       elif direction == "w":
+           return self.w_to
+       else:
+           return None
+   def addItem(self, item):
+       self.items.append(item)
+   def removeItem(self, item):
+       self.items.remove(item)
+   def findItemByName(self, name):
+       for item in self.items:
+           if item.name.lower() == name.lower():
+               return item
+       return None
--- projects/graph/adv/world.py
+++ projects/graph/adv/world.py
@@ -1,10 +1,36 @@
 from room import Room
-
+import random
 
 class World:
     def __init__(self):
         self.startingRoom = None
         self.rooms = {}
+        self.avail_rm_dir = []
+
+    def getRandomRoom(self):
+        if len(self.avail_rm_dir) > 0:
+            random_rooms = list(self.rooms)
+            random.shuffle(random_rooms)
+            return random_rooms[0]
+        else: 
+            return None
+
+    def getRandomDirection(self):
+        random_room = self.getRandomRoom()
+        available_dir = []
+        if room.n_to is None:
+            available_dir.append("n")
+        if room.s_to is None:
+            available_dir.append("s")
+        if room.w_to is None:
+            available_dir.append("w")
+        if room.e_to is None:
+            available_dir.append("e")
+        random.shuffle(available_dir)
+        if len(available_dir) > 0:
+            return (random_room, available_dir[0])
+        else:
+            return None
 
     def generateDefaultRooms(self):
         self.rooms = {
@@ -31,6 +57,17 @@ class World:
         self.rooms['narrow'].connectRooms("n", self.rooms['treasure'])
         self.startingRoom = self.rooms['outside']
 
+    def _updateCoordinates(self, coords, dir):
+        new_coords = list(coords)
+        if dir == "n":
+            new_coords[1] += 1
+        if dir == "s":
+            new_coords[1] -= 1
+        if dir == "e":
+            new_coords[0] += 1
+        if dir == "w":
+            new_coords[0] -= 1
+
     ####
     # MODIFY THIS CODE
     ####
@@ -44,19 +81,27 @@ class World:
         # Create n rooms
         for i in range(0, numRooms):
             # Create n rooms.
-            self.rooms[i] = Room(f"Room {i}", "You are standing in an empty room.")
-
-        # Hard-code a single room connection.
-        # You should replace this with procedural connection code.
-        if numRooms > 1:
-            self.rooms[0].connectRooms("n", self.rooms[1])
+            new_room = Room(f"Room {i}", "You are standing in an empty room.", i)
+            
+            self.rooms[i] = new_room
+            # if first room, add new room's available directions to avail_rm_dirs
+            if i = 0:
+                self.avail_rm_dir.append(new_room, "n")
+                self.avail_rm_dir.append(new_room, "s")
+                self.avail_rm_dir.append(new_room, "e")
+                self.avail_rm_dir.append(new_room, "w")
+            # If it's not the first room....
+            if i > 0:
+                # ...connect to the previous room in a random direction
+                random_rm_tuple = self.getRandomDirection()
+                if random_rm_tuple is not None:
+                    room_id = random_rm_tuple[0].id
+                    room_dir = random_rm_tuple[1]
+                    self.rooms[room_id].connectRooms(room_dir, new_room)
+                    if room_dir = 
+                    new_room.coordinates = ()
 
         # Set the starting room to the first room. Change this if you want a new starting room.
         self.startingRoom = self.rooms[0]
 
-        return self.rooms
-
-
-
-
-
+        return self.rooms   
