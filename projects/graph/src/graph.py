4b0291c09640009b3bb1c0a636f085373bb45eb4
Lucas Beemer <lucasbeemer@gmail.com>
2018-11-27 16:32:16 -0800

    minor changes and testing

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -86,7 +86,7 @@ class Graph:
         s = Stack()
         visited = set()
         # Add the start node to the stack
-        s.push(starting_node)
+        s.append(starting_node)
         # While the Stack is not empty...
         while s.size() > 0:
             # Remove the first node from the Stack
@@ -98,24 +98,34 @@ class Graph:
                 visited.add(node)
                 # Then put all its children in the queue
                 for child in self.vertices[node].edges:
-                    s.push(child)
+                    s.append(child)
 
-    def dftr(self, target, visited=[]):
-        while target not in visited:
-            visited.append(target)
-            for c in self.vertices[str(target)]:
-                self.dftr(int(c), visited)
+    def dftr(self, starting_node, visited=[]):
+        while starting_node not in visited:
+            visited.append(starting_node)
+            for child in self.vertices[str(starting_node)]:
+                self.dftr(int(child), visited)
         return f"dftr: {visited}"
 
 
 
 
-graph = Graph()
-graph.add_vertex("0")
-graph.add_vertex("1")
-graph.add_vertex("2")
-graph.add_vertex("3")
-graph.add_edge("0", "1")
-graph.add_edge("0", "3")
-graph.add_edge("0", "4")
+graph1 = Graph()
+graph1.add_vertex("0")
+graph1.add_vertex("1")
+graph1.add_vertex("2")
+graph1.add_vertex("3")
+graph1.add_edge("0", "1")
+graph1.add_edge("0", "3")
+graph1.add_edge("0", "4")
 print(graph.vertices)
+
+graph2 = Graph()
+graph2.add_vertex("1")
+graph2.add_vertex("2")
+graph2.add_vertex("3")
+graph2.add_vertex("4")
+graph2.add_vertex("5")
+graph2.add_vertex("6")
+graph2.add_vertex("7")
+print(graph2.dftr(7))
