c4c66b827a5e30a9b04b8ed1704970a0eecabe4a
Joseph Chretien-Golden <joechre@gmail.com>
2018-11-28 17:15:36 -0600

    ROOMS -add overlook and norrow rooms with descrip

--- projects/graph/adv/adv.py
+++ projects/graph/adv/adv.py
@@ -19,6 +19,17 @@ class World:
                 """Dim light filters in from the south. Dusty
         passages run north and east.""",
             ),
+            "overlook": Room(
+                    "Grand Overlook",
+                    """A steep cliff appears before you, falling
+            into the darkness. Ahead to the north, a light flickers in
+            the distance, but there is no way across the chasm.""",
+                ),
+                "narrow": Room(
+                    "Narrow Passage",
+                    """The narrow passage bends here from west
+            to north. The smell of gold permeates the air.""",
+                ),
 # Uncomment this to generate the default rooms from Week 1
 # world.generateDefaultRooms()
 
