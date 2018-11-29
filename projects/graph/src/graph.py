8609217d0293cd667257379fdd29f42354206416
lyang9 <lyang9@buffalo.edu>
2018-11-29 18:03:16 -0500

    Completed class Stack

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,35 +6,42 @@ Simple graph implementation compatible with BokehGraph class.
 class Queue:
     def __init__(self):
         self.queue = []
-    
     def enqueue(self, value):
         self.queue.append(value)
-    
     def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
-
     def size(self):
         return len(self.queue)
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}                                  # initialize an empty dictionary of verticies
-    
     def add_vertex(self, vertex_id):
         self.vertices[vertex_id] = Vertex(vertex_id)        # key to be vertex_id and value to be Vertex 
-
     def add_edge(self, v1, v2):                             # undirected graph
         if v1 in self.vertices and v2 in self.vertices:     # if v1 and v2 both exist in the vertex list
             self.vertices[v1].edges.add(v2)
             self.vertices[v2].edges.add(v1)
         else:
             raise IndexError('Vertex not found')
-
     def add_directed_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].edges.add(v2)
@@ -52,9 +59,9 @@ class Graph:
         while q.size() > 0:
             # remove the first node from the Queue
             node = q.dequeue()
-            # if it has not been visited
+            # check if node is visited
             if node not in visited:
-                # mark it as visited
+                # if not, mark node as visited
                 print(node)
                 visited.add(node)
                 # print(self.vertices[node].edges)
@@ -63,6 +70,8 @@ class Graph:
                     # print(f'add child: {child}')
                     # if child not in visited:
                         q.enqueue(child)
+
+
 
 
 class Vertex:
