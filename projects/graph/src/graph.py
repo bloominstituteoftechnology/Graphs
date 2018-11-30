d47d6c62966b97848370a1bcbc3e9568fadc3a8c
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-11-29 16:42:26 -0800

    implements working graph and vertices classes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,18 +6,30 @@ Simple graph implementation compatible with BokehGraph class.
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        vertices = []
+        self.vertices = {}
 
     def add_vertex(self, vertex):
-        self.vertices.append(Vertex(vertex))
+        self.vertices[vertex] = Vertex(vertex)
 
     def add_edge(self, vertex_one, vertex_two):
-        first_index = vertices.index(vertex_one)
-        vertices[first_index] = {vertex_one, vertex_two}
-        second_index = vertices.index(vertex_two)
-        vertices[second_index] = {vertex_two, vertex_one}
+        # first_index = vertices.index(vertex_one)
+        self.vertices[vertex_one].edge.add(vertex_two)
+        # second_index = vertices.index(vertex_two)
+        self.vertices[vertex_two].edge.add(vertex_one)
 
 class Vertex:
-    def __init__(self, vertex, edge=set()):
+    def __init__(self, vertex):
         self.vertex = vertex
-        self.edge = edge
+        self.edge = set()
+
+    def __repr__(self):
+        return f"{self.vertex} : {self.edge} \n"
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
