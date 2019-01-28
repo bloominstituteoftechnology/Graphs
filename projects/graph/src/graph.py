a9831e71af0b34c256f24c634534d73d42e4b803
Jeanette <jeanfern555@gmail.com>
2019-01-28 17:01:47 -0600

    Completed part 2 - implemented breadth first traversal, bft().

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,6 +1,15 @@
 """
 Simple graph implementation
 """
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+    
+    def dequeue(self):
+        return self.queue.pop(0)
 
 
 class Graph:
@@ -10,29 +19,40 @@ class Graph:
 
     # v is the vertex
     def add_vertex(self, v):
-        self.vertices[v] = set()
+        if v not in self.vertices.keys():
+            self.vertices[v] = set()
+        else:
+            print(f'{v} vertex already exists, vertex was not added.')
     
     # v1 is vertex1, v2 is vertex2
     def add_edge(self, v1, v2):
         if (v1 and v2) in self.vertices:
             self.vertices[v1].add(v2)
             self.vertices[v2].add(v1)
-        if not v1 in self.vertices:
-            print(f'No {v1} vertex, edge was not added.')
-        if not v2 in self.vertices:
-            print(f'No {v2} vertex, edge was not added.')
+        elif not v1 in self.vertices:
+            print(f'There is no {v1} vertex, edge was not added.')
+        else:
+            print(f'There is no {v2} vertex, edge was not added.')
             
-
-    # def bfs(self, starting_node):
-    #     # create a queue
-    #     q = Queue()
-    #     visited = set()
-    #     # enquue the starting node
-    #     q.Enqueue(starting_node)
-    #     # while the queue is not empty
-    #         # dequeue a node from the queue
-    #         # mark it as visited
-    #         # enqueue all of its children that have not been visited
+    # Breadth-First Traversal
+    def bft(self, starting_node):
+        # create a queue
+        q = Queue()
+        visited = set()
+        # enqueue the starting node
+        q.enqueue(starting_node)
+        # while the queue is not empty
+        while len(q.queue) > 0:
+            # dequeue a node from the queue
+            node = q.dequeue()
+            # mark it as visited
+            if node not in visited:
+                visited.add(node)
+                print('visited:', visited)
+            # enqueue all of its children that have not been visited
+            for child in self.vertices[node]:
+                if child not in visited:
+                    q.enqueue(child)
 
     # def dfs(self, starting_node):
     #     # create a stack
@@ -65,14 +85,4 @@ class Graph:
     #         # ---*---- if node == target_node: return true
     #         #enqueue all of it's children that have not been visited 
     #     #return false 
-
-graph = Graph()  # Instantiate your graph
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('0', '4')
-print(graph.vertices)
 
