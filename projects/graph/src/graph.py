e8d291f96f476d770ba2ef7b509e550a3b639e81
Max <maxkajiwara@gmail.com>
2019-01-28 15:48:42 -0800

    Initial commit

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,6 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
