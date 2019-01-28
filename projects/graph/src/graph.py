0a541f4bf4c2749f404374b557e46884837c0e0b
Jeanette <jeanfern555@gmail.com>
2019-01-28 14:42:34 -0600

    Initial Commit - Added instructor psuedo code from lecture.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,28 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+
+  # node and vertex are the same
+    def bfs(self, starting_node):
+        # create a queue
+        q = Queue()
+        visited = set()
+        # enquue the starting node
+        q.Enqueue(starting_node)
+        # while the queue is not empty
+            # dequeue a node from the queue
+            # mark it as visited
+            # enqueue all of its children that have not been visited
+
+      # node and vertex are the same
+    def dfs(self, starting_node):
+        # create a stack
+        q = Stack()
+        visited = set()
+        # push the starting node
+        q.Push(starting_node)
+        # while the stack is not empty
+            # pop a node from the stack
+            # mark it as visited
+            # push all of its children that have not been visited
+
