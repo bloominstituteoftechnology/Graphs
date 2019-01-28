bdf800fa1010ed881e71a3db6e5dfe15ae33a4ce
michaelagard <mdagard@gmail.com>
2019-01-28 16:05:49 -0500

    add vertices variable with add_vertex and add_edge methods

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,18 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, vertex1, vertex2):
+
+        if vertex1 in self.vertices and vertex2 in self.vertices:
+            self.vertices[vertex1].add(vertex2)
+            self.vertices[vertex2].add(vertex1)
+        else:
+            raise Exception("Edges to nonexistent vetices")
