f8bc0280c14d1b0424a09681d98faa0c42b0600b
jsmnglng <jsmn.glng@gmail.com>
2019-01-28 13:37:02 -0800

    Completed BFT

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,4 +1,5 @@
 # Simple graph implementation
+from queue import Queue
 
 
 class Graph:
@@ -16,4 +17,25 @@ class Graph:
             self.vertices[vertex].add(edge)
         else:
             print(f"No {vertex} vertex")
+
+    def bf_traversal(self, starting_vertex):
+        # Create a queue
+        queue = Queue()
+        # Enqueue starting vertex
+        queue.enqueue(starting_vertex)
+        visited = []
+
+        # while queue is not empty
+        while queue.len() > 0:
+            # Dequeue vertex from queue
+            current_node = queue.dequeue()
+            # print("current node", current_node)
+            if current_node not in visited:
+                # Mark vertex as visited
+                visited.append(current_node)
+                # Enqueue current_vertex's child vertices
+                for edge in self.vertices[current_node]:
+                    queue.enqueue(edge)
+
+        print(visited)
 
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -11,13 +11,18 @@ from graph import Graph
 
 def main():
     graph = Graph()  # Instantiate your graph
-    graph.add_vertex("0")
-    graph.add_vertex("1")
-    graph.add_vertex("2")
-    graph.add_vertex("3")
-    graph.add_edge("0", "1")
-    graph.add_edge("0", "3")
-    graph.add_edge("0", "4")
+    graph.add_vertex("A")
+    graph.add_vertex("B")
+    graph.add_vertex("C")
+    graph.add_vertex("D")
+    graph.add_vertex("E")
+    graph.add_edge("B", "A")
+    graph.add_edge("C", "B")
+    graph.add_edge("D", "B")
+    graph.add_edge("E", "C")
+    graph.add_edge("A", "D")
+    graph.add_edge("D", "E")
+    graph.bf_traversal("A")
     print(graph.vertices)
 
 
