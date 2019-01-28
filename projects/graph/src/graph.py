9b799402a4eaa0009237bcb635de1c6b948811bc
Simon Elhoej <simonelhoej@gmail.com>
2019-01-28 23:06:18 +0100

    added test

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,3 +12,11 @@ class Graph:
 
 
 
+graph = Graph()
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
