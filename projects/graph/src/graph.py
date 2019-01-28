fbef2156256cbb92d92aa6568b0b553362e7e0ca
Andrew <ghobrial.andrew@gmail.com>
2019-01-28 17:51:13 -0500

    bfs method initial layout

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -16,3 +16,22 @@ class Graph:
         self.vertices[vertex].add(value)
         # Add vertex as a value to the edge
         self.vertices[value].add(vertex)
+    
+    def bfs(self, starting_node):
+        # create a queue
+        q = Queue()
+        visited = set()
+        # Enqueue the starting node
+        q.Enqueue(starting_node)
+       # While the queue is not empty
+        while len(q.storage) > 0:
+            # Dequeue a node from the queue
+            node = q.dequeue()
+            # Mark it as visited
+            visited.append(node)
+            # Enqueue all of its children that have not been visted
+            for child in self.vertices[node]:
+                if child not in visited:
+                    q.enqueue(child)
+
+        return visited
