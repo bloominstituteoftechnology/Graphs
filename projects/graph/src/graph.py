eccb66962d36b6c3c4d20ae7f6f61dd142b53f2a
jsmnglng <jsmn.glng@gmail.com>
2019-01-28 12:26:52 -0800

    Implemented Graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,19 @@
-"""
-Simple graph implementation
-"""
+# Simple graph implementation
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    # Represent a graph as a dictionary of vertices mapping labels to edges.
     def __init__(self):
-        pass  # TODO
+        # TODO
+        # Create an empty graph
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, edge, vertex):
+        if vertex in self.vertices:
+            self.vertices[vertex].add(edge)
+        else:
+            print(f"No {vertex} vertex")
+
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -11,14 +11,16 @@ from graph import Graph
 
 def main():
     graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
-    graph.add_vertex('1')
-    graph.add_vertex('2')
-    graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
+    graph.add_vertex("0")
+    graph.add_vertex("1")
+    graph.add_vertex("2")
+    graph.add_vertex("3")
+    graph.add_edge("0", "1")
+    graph.add_edge("0", "3")
+    graph.add_edge("0", "4")
     print(graph.vertices)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # TODO - parse argv
     main()
