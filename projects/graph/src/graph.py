fd864cbb96b80183503c6457b1874ba557601805
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-28 16:54:09 -0500

    implemented graph

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,4 +12,16 @@ class Graph:
         if vertex:
             self.vertices[vertex] = set()
 
-    def add_edge(self, edge):
+    def add_edge(self, vertex, edge):
+        if vertex in self.vertices:
+            self.vertices[vertex].add(edge)
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
