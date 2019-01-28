86c118fd7c9fcbc2cf6e4196a2b41b688f04b712
Simon Elhoej <simonelhoej@gmail.com>
2019-01-28 23:04:31 +0100

    Add vertex and edge

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,14 @@
-"""
-Simple graph implementation
-"""
-
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, value):
+        self.vertices[value] = set()
+
+    def add_edge(self, vert, value):
+        self.vertices[vert].add(value)
+        self.vertices[value].add(vert)
+
+
+
