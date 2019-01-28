e414c34cb4bdbc5ecdbc31a045426bf391aa69e5
Andrew <ghobrial.andrew@gmail.com>
2019-01-28 17:58:31 -0500

    recursive dft method and stack class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -35,6 +35,20 @@ class Graph:
                     q.enqueue(child)
 
         return visited
+
+    def dft_r(self, starting_node, visited=None):
+        if visited is None:
+            visited = []
+        # If the node has not been visited
+        if starting_node not in visited:
+            visited.append(starting_node)
+            # Mark the node as visited
+            # Recursion through children
+            for child in self.vertices[starting_node]:
+                if child not in visited:
+                    self.dft_r(child, visited)
+        return visited
+
 class Queue:
     def __init__(self):
         self.storage = []
@@ -44,3 +58,14 @@ class Queue:
 
     def dequeue(self):
         return self.storage.pop(0)
+
+class Stack:
+    def __init__(self):
+        pass  # TODO
+        self.storage = []
+
+    def push(self, value):
+        self.storage.append(value)
+
+    def pop(self):
+        return self.storage.pop()
