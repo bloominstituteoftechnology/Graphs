93964212dc3a1dd9396fe20f256716127f81dbe1
Brady Fukumoto <bradyfukumoto@gmail.com>
2019-01-28 13:08:09 -0500

    Adding sample code to demo

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -6,10 +6,18 @@ Demonstration of Graph functionality.
 
 from sys import argv
 
+from graph import Graph
+
 
 def main():
-    pass  # TODO
-
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    print(graph.vertices)
 
 if __name__ == '__main__':
     # TODO - parse argv
