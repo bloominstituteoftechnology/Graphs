03a94bdd1643697e60862f6f753386c382f40776
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-28 17:53:52 -0500

    bft first pass

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -17,3 +17,14 @@ class Graph:
             self.vertices[parent].add(child)
         else:
             print("One or more vertices, not in graph")
+
+    def bft(self, vertex):
+        queue = []
+        visited = []
+        if vertex in self.vertices:
+            queue.append(vertex)
+            while queue:
+                for vertex in self.vertices[queue[0]]:
+                    queue.append(vertex)
+                visited.append(queue.pop(0))
+            return(visited)
