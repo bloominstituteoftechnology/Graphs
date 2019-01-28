0f378c2fa56616c762c7093251e98703048a471c
Brandon Strellis <bstrellis@gmail.com>
2019-01-28 18:11:48 -0500

    Breadth first traversal implemented in graph.py

--- projects/graph/backup/graph.py
+++ projects/graph/backup/graph.py
@@ -1,7 +1,7 @@
 """
-Simple graph implementation compatible with BokehGraph class.
+Simple graph implementation compatible with BokehGraph class. // No Bokeh in this???
 """
-
+from queue import Queue 
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -12,17 +12,31 @@ class Graph:
         self.vertices[new_vertex] = set()
 
     def add_edge(self, vertex_a, vertex_b):
-        # add b to a's vertices entry
         self.vertices[vertex_a].add(vertex_b)
-        # add a to b's vertices entry
         self.vertices[vertex_b].add(vertex_a)
+    
+    def bft(self, starting_vert):
+
+        queue = Queue()
+        queue.enqueue(starting_vert) # Why does this have to be a separate step from instantiation?
+        visited = set() 
+
+        while queue.len() > 0: 
+            cur_vert = queue.dequeue()
+            print(f'\ncur_vert: {cur_vert}')
+            visited.add(cur_vert)
+
+            for vert in self.vertices[cur_vert]:
+                if vert is not None:
+                    if not vert in visited:
+                        queue.enqueue(vert)
 
 
-graph = Graph()  # Instantiate your graph
+graph = Graph()
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
-print(graph.vertices)
+graph.bft('0')
