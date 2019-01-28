fbb4d3ad0306021c0199e8ff9b913ea5598ba3d4
alejandrok93 <alejandrok93@gmail.com>
2019-01-28 14:55:32 -0800

    Completed Breadth First Traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,16 +2,39 @@
 Simple graph implementation
 """
 
-
+from collections import deque
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
     
     def add_vertex(self, vertex):
-        self.vertices[vertex] = {}
+        self.vertices[vertex] = set()
     
     def add_edge(self, vertex, edge):
-            self.vertices[vertex] += edge
-        
+        if vertex in self.vertices:
+            self.vertices[vertex].add(edge)
 
+    def breadth_first_traversal(self, starting_node):
+        q = deque()
+        visited = []
+        q.append(starting_node)
+        while len(q) > 0:
+            node = q.popleft()
+            visited.append(node)
+            for child in node:
+                if child not in visited
+                    q.append(child)
+        return visited
+            
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+# print(graph.vertices)
+print(graph.breadth_first_traversal(graph.vertices['0']))
