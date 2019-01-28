ce2edae52e3edeaf1e5e093175c1c9e85dd439fc
Andrew <ghobrial.andrew@gmail.com>
2019-01-28 17:12:44 -0500

    initial graph.py class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,9 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {
+                            '0': {'1', '3'},
+                            '1': {'0'},
+                            '2': set(),
+                            '3': {'0'}
+                        }
