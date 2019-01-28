23e4c67945af077009586d5c03cd3569aff21236
Katie Cleary <katiecleary@airmaster.lan>
2019-01-28 15:34:50 -0500

    initial push and functions outlined

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -7,3 +7,17 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         pass  # TODO
+    #day 1
+    def bf_traversal():
+    
+    def df_traversal():
+    
+    def dft_stack():
+    
+    def dft_recursion():
+    
+    #day 2
+    def bfs_search():
+    
+    def dft_search():
+
