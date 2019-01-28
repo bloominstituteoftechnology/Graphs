1f0aac0265bdf1d4b9a92d66b5b747322333f226
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-28 18:27:43 -0500

    implemented breadth-first traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,3 +1,4 @@
+from queue import Queue
 """
 Simple graph implementation
 """
@@ -16,12 +17,23 @@ class Graph:
         if vertex in self.vertices:
             self.vertices[vertex].add(edge)
 
+    def bft(self, starting_node):
+        q = Queue()
+        visited = set()
+        visited.add(starting_node)
+        q.Enqueue(starting_node)
+        while q.len() > 0:
+            node = q.dequeue()
+            if node not in visited:
+                visited.append(node)
+                for i in self.vertices[node]:
+                    q.enqueue(i)
 
 graph = Graph()  # Instantiate your graph
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
-graph.add_edge('0', '1')
+graph.add_edge('0', '1') 
 graph.add_edge('0', '3')
 print(graph.vertices)
---
+++ projects/graph/src/queue.py
@@ -0,0 +1,79 @@
+########## Linked List #############
+class Node:
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    self.next_node = new_next
+
+
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.max = None
+
+  def add_to_tail(self, value):
+    node = Node(value)
+    if self.tail is not None:
+      self.tail.set_next(node)
+      self.tail = node
+      if self.max.value < node.value:
+        self.max = node
+    else:
+      self.head = node
+      self.tail = node
+      self.max = node
+
+  def remove_head(self):
+    if self.head is not None:
+      new_head = self.head
+      del(self.head)
+      self.head = new_head.next_node
+
+      if self.head is None:
+        self.tail = None
+      return new_head.value
+
+  def contains(self, value):
+    curr_node = self.head
+    while True:
+      if curr_node is None:
+        return False
+      elif curr_node.value == value:
+        return True
+      else:
+        curr_node = curr_node.next_node
+
+  def get_max(self):
+    if self.max is not None:
+      return self.max.value
+    else:
+      return None
+
+########## Queue #############
+class Queue:
+  def __init__(self):
+    self.size = 0
+    self.storage = LinkedList()
+
+  def enqueue(self, item):
+    self.storage.add_to_tail(item)
+
+  def dequeue(self):
+    return self.storage.remove_head()
+
+  def len(self):
+    self.size = 0
+    curr_node = self.storage.head
+    while curr_node is not None:
+      self.size += 1
+      curr_node = curr_node.next_node
+    return self.size
