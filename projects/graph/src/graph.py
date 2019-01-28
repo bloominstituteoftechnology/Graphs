1fd675f606a43794ce3f93b7a38f1988d9cb8c26
Eva <evalee904@gmail.com>
2019-01-28 15:53:48 -0600

    added queue and stack

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,38 @@
 """
 Simple graph implementation
 """
+class Queue:
+    def __init__(self):
+        self.queue = []
 
+    def size(self):
+        return len(self.queue)
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else: return None
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+    
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else: return None
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        pass
+
