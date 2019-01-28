185bc35344480d87f6b52f3b9e5805470ade6b39
Conner Alegre <cga0@pm.me>
2019-01-28 14:26:36 -0700

    Implement basic bidirectional Graph class Update graph_demo.py to use new Graph class Implement breadth-first-search

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,41 @@
+from collections import deque
 """
 Simple graph implementation
 """
 
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, id):
+        if id not in self.vertices:
+            self.vertices[id] = set()
+    
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("No vertex")
+
+    def bfs(self, starting_vertex):
+        visited = []
+
+        queue = deque()
+        queue.append(starting_vertex)
+        
+        while len(queue) > 0:
+            current = queue.popleft()
+            visited.append(current)
+
+            for item in self.vertices[current]:
+                if item not in visited:
+                    queue.append(item)
+                
+
+        # While Queue is not Empty
+            # Dequeue a node from the queue
+            # If that node has not been visited
+                # Mark it as visited
+                # Enqueue all of its children that have not been visited
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -5,11 +5,18 @@ Demonstration of Graph functionality.
 """
 
 from sys import argv
+from graph import Graph
 
 
 def main():
-    pass  # TODO
-
+    graph = Graph()  # Instantiate your graph
+    graph.add_vertex('0')
+    graph.add_vertex('1')
+    graph.add_vertex('2')
+    graph.add_vertex('3')
+    graph.add_edge('0', '1')
+    graph.add_edge('0', '3')
+    graph.bfs('0')
 
 if __name__ == '__main__':
     # TODO - parse argv
