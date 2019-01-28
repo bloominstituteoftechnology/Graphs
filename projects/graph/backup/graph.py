0162d689d60a93a436856ad45f99bf913e0bb009
Kevin <brackkevin@gmail.com>
2019-01-28 15:42:12 -0600

    Adds bft dft and dft_r method pseudocode

--- projects/graph/backup/graph.py
+++ projects/graph/backup/graph.py
@@ -17,6 +17,32 @@ class Graph:
         else:
             return 'exception occoured, one of the vertices is not in the Graph yet'
 
+    def bft(self, starting_node):
+        pass
+        # create a queue
+        # enqueue the starting node
+        # while the queue is not empty:
+            # Dequeue a node from the queue
+            # mark it as visited
+            # enqueue all of it's children that have not been visited
+
+    def dft(self, starting_node):
+        pass
+        # Create a stack
+        # push the starting node
+        # while the stack is not empty:
+            # pop a node from the stack
+            # mark it as visited
+            # push all of it's children that have not been visited
+
+    def dft_r(self, starting_node, visited = None):
+        if visited is None:
+            visited = set()
+        # if the node has not been visited:
+            # mark the node as visited
+            # call dft_r on all children
+                # dft_r(child_node, visited)
+
 # graph = Graph()
 # graph.add_vertex('0')
 # graph.add_vertex('1')
