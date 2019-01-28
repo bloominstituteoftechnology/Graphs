da8c32e10389651b5535af730c6061dae4e1e3af
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-28 16:16:47 -0500

    add vertex

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,9 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = dict()
+
+    def add_vertex(self, value):
+        self.vertices[value] = set()
