aec447093d1f6a467e236b739fdc1e7aeb568610
Chiboba <briangfelix@gmail.com>
2019-01-28 15:02:13 -0800

    BFT

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,6 +1,8 @@
 """
 Simple graph implementation
 """
+from queue import Queue
+
 
 
 class Graph:
@@ -16,6 +18,20 @@ class Graph:
         if vertex1 in self.vertices and vertex2 in self.vertices:
             self.vertices[vertex1].add(vertex2)
             self.vertices[vertex2].add(vertex1)
+    
+    def bft(self, start):
+        if start in self.vertices:
+            nextItems = Queue()
+            visited = []
+            nextItems.put(start)
+            while nextItems.empty() is not True:
+                first = nextItems.queue[0]
+                for num in self.vertices[first]:
+                    if num not in visited:
+                        nextItems.put(num)
+                nextItems.get()
+                visited.append(first)
+            print(visited)
 
 class Vertex:
     def __init__(self, label):
