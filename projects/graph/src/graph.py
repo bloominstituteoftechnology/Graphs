dd252cd16a2033fa487d4346886a828ce982f4d5
Max <maxkajiwara@gmail.com>
2019-01-29 14:19:34 -0800

    Complete breadth-first traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -19,3 +19,27 @@ class Graph:
             self.vertices[vertex2].add(vertex1)
         else:
             print('Graph does contain both of those vertices')
+
+    def bft(self, start_vertex):
+        if start_vertex not in self.vertices:
+            print('Graph does not contain that vertex')
+
+        else:
+            visited = {}
+            queue = [start_vertex]
+            i = 0
+
+            for vertex in self.vertices.keys():
+                visited[vertex] = 0
+
+            while i < len(queue):
+                vertex = queue[i]
+                visited[vertex] = 1
+
+                for neighbor in self.vertices[vertex]:
+                    if not visited[neighbor]:
+                        queue.append(neighbor)
+
+                i += 1
+
+            return queue
