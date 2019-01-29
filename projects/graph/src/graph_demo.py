4da7ab777a17b739fbd85854062052f384e1a4f5
Hunter315 <hunter.mckay.smith@gmail.com>
2019-01-29 09:43:35 -0700

    put test main in graph demo

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -8,15 +8,33 @@ from sys import argv
 from graph import Graph
 
 def main():
-    graph = Graph() #my graph instance
+
+    graph = Graph()  # my graph instance
     graph.add_vertex('0')
     graph.add_vertex('1')
     graph.add_vertex('2')
     graph.add_vertex('3')
+    graph.add_vertex('4')
+    graph.add_vertex('5')
+    graph.add_vertex('6')
+    graph.add_vertex('7')
+    graph.add_vertex('8')
+    graph.add_vertex('9')
     graph.add_edge('0', '1')
     graph.add_edge('0', '3')
-    graph.add_edge('4', '3')
+    graph.add_edge('1', '3')
+    graph.add_edge('9', '2')
+    graph.add_edge('2', '3')
+    graph.add_edge('4', '5')
+    graph.add_edge('3', '4')
+    graph.add_edge('5', '9')
+    graph.add_edge('6', '7')
+    graph.add_edge('7', '9')
+    graph.add_edge('2', '8')
+
     print(graph.vertices)
+
+    graph.bft('2')
 
 
 if __name__ == '__main__':
