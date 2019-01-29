311c406a8309f547c3fc66385d65e1176eca24d9
Andrew <ghobrial.andrew@gmail.com>
2019-01-29 18:07:27 -0500

    dfs complete but needs refining

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -78,6 +78,29 @@ class Graph:
                 if child not in visited:
                     self.dft_r(child, visited)
         return visited
+    
+    ## DFS below ##
+
+    # The function to do DFS traversal. It uses 
+    # recursive DFSUtil() 
+    def DFS(self,v): 
+
+        # A helper fxn used by dfs method 
+        def DFSUtil(self,v,visited): 
+            # Mark the current node as visited and print it 
+            visited[v]= True
+            print (v) 
+            # Recursively do this for all the vertices adjacent to this vertex 
+            for i in self.graph[v]: 
+                if visited[i] == False: 
+                    self.DFSUtil(i, visited)
+
+        # Mark all the vertices as not visited 
+        visited = [False]*(len(self.graph)) 
+  
+        # Call the recursive helper function to print 
+        # DFS traversal 
+        self.DFSUtil(v,visited) 
 
 class Queue:
     def __init__(self):
