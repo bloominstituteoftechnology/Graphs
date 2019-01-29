f7307ef8e525814dee71ba130d8898701404cded
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-29 14:29:08 -0500

    Refactored code from yesterday's MVP, added bfs_r method, and also completed bfs_path method.

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,6 +2,39 @@
 Simple graph implementation
 """
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def size(self):
+        return len(self.queue)
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else: 
+            return None
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+    
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else: 
+            return None
+
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -10,72 +43,96 @@ class Graph:
         self.vertices = {
         }
 
-    def add_vertex(self, string):
-        self.vertices[string] = set()
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
 
-    def add_edge(self, from_vertex, to_vertex):
-        self.vertices[to_vertex].add(from_vertex)
-        self.vertices[from_vertex].add(to_vertex)
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('That vertex does not exist.')
 
-    def bst_traversal(self, start_node):
-        nodes = []
-        visited = []
-        node = start_node
-        nodes.append(node)
-        while nodes:
-            node = nodes.pop(0)
-            if len(self.vertices[node]) is 0:
-                visited.append(node)
-                pass
-            else:
-                edge_nodes = list(self.vertices[node].copy())
-                visited.append(node)
-                for i in edge_nodes:
-                    if i in visited or i in nodes:
-                        pass
-                    else:
-                        nodes.append(i)
-                print(nodes)
+    def bft(self, start_node):
+        queue = Queue()
+        visited = set()
+        queue.enqueue(start_node)
+        while queue.size() > 0:
+            node = queue.dequeue()
+            if node not in visited:
+                print(node)
+                visited.add(node)
+            for next_node in self.vertices[node]:
+                queue.enqueue(next_node)
 
-        print(visited)
+    def dft(self, start_node):
+        stack = Stack()
+        visited = set()
+        stack.push(start_node)
+        while stack.size() > 0:
+            node = stack.pop()
+            if node not in visited:
+                print(node)
+                visited.add(node)
+            for next_node in self.vertices[node]:
+                stack.push(next_node)
 
-    def dft_traversal(self, start_node):
-        nodes = []
-        visited = []
-        node = start_node
-        nodes.append(node)
-        while nodes:
+    def dft_r(self, start_node, visited = None):
+        if visited == None:
+            visited = set()
+        visited.add(start_node)
+        print(start_node)
+        for child_node in self.vertices[start_node]:
+            if child_node not in visited:
+                self.dft_r(child_node, visited)
 
-            node = nodes.pop()
-            if len(self.vertices[node]) is 0:
-                visited.append(node)
+    def bfs(self, start_node, target_node):
+        queue = Queue()
+        visited = set()
+        queue.enqueue(start_node)
+        while queue.size() > 0:
+            node = queue.dequeue()
+            if node not in visited:
+                if node == target_node:
+                    return True
+                print(node)
+                visited.add(node)
+            for next_node in self.vertices[node]:
+                queue.enqueue(next_node)
+        return False
+    
+    # modify to return a path of search
 
-                pass
-            else:
-                edge_nodes = list(self.vertices[node].copy())
-                visited.append(node)
-                for i in edge_nodes:
-                    if i in visited or i in nodes:
-                        pass
-                    else:
-                        nodes.append(i)
+    def dfs(self, start_node, target_node):
+        stack = Stack()
+        visited = set()
+        stack.push(start_node)
+        while stack.size() > 0:
+            node = stack.pop()
+            if node not in visited:
+                if node == target_node:
+                    return True
+                print(node)
+                visited.add(node)
+            for next_node in self.vertices[node]:
+                stack.push(next_node)
+        return False
 
-                print (nodes)
-        print(visited)
-        pass
-
-
-graph = Graph()
-graph.add_vertex('0')
-graph.add_vertex('1')
-graph.add_vertex('2')
-graph.add_vertex('3')
-graph.add_vertex('4')
-graph.add_vertex('5')
-graph.add_edge('0', '1')
-graph.add_edge('0', '3')
-graph.add_edge('3', '5')
-graph.add_edge('1', '4')
-print(graph.vertices)
-graph.bst_traversal('0')
-graph.dft_traversal('0')
+    def bfs_path(self, start_node, target_node):
+        queue = Queue()
+        visited = set()
+        queue.enqueue([start_node])
+        while queue.size() > 0:
+            path = queue.dequeue()
+            node = path[-1]
+            if node not in visited:
+                visited.add(node)
+                if node == target_node:
+                    return path
+            for next_node in self.vertices[node]:
+                dupl_path = list(path)
+                dupl_path.append(next_node)
+                queue.enqueue(dupl_path)
+            
+        return None
+    
+    # modify ^ to return a path of search
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -11,13 +11,42 @@ from graph import Graph
 
 def main():
     graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
+    # graph.add_vertex('0')
+    # graph.add_vertex('1')
+    # graph.add_vertex('2')
+    # graph.add_vertex('3')
+    # graph.add_edge('0', '1')
+    # graph.add_edge('0', '3')
+    # print(graph.vertices)
     graph.add_vertex('1')
     graph.add_vertex('2')
     graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
-    print(graph.vertices)
+    graph.add_vertex('4')
+    graph.add_vertex('5')
+    graph.add_vertex('6')
+    graph.add_vertex('7')
+    graph.add_vertex('8')
+    graph.add_vertex('9')
+    graph.add_edge('1', '6')
+    graph.add_edge('1', '8')
+    graph.add_edge('1', '2')
+    graph.add_edge('6', '7')
+    graph.add_edge('8', '3')
+    graph.add_edge('2', '3')
+    graph.add_edge('7', '9')
+    graph.add_edge('7', '5')
+    graph.add_edge('9', '5')
+    graph.add_edge('7', '9')
+    graph.add_edge('3', '4')
+    graph.add_edge('4', '5')
+
+    # print(graph.vertices)
+
+    print(graph.bfs_path('1', '3')) # correct
+    print(graph.bfs_path('1', '7')) # correct
+    print(graph.bfs_path('8', '5')) # correct
+
+    
 
 if __name__ == '__main__':
     # TODO - parse argv
