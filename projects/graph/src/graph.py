da539a821fa53b689db6516d425764f38cbabfba
sean-one <basement365@gmail.com>
2019-01-29 12:59:10 -0800

    split vertex into its own class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,6 +1,12 @@
 """
 Simple graph implementation
 """
+class Vertex:
+    """Extracts the vertex from the graph, I think this way will scale better"""
+    def __init__(self, vertex_val):
+        self.vertex = vertex_val
+        self.edges = set()
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -9,17 +15,14 @@ class Graph:
         pass  # TODO
 
     def add_vertex(self, vertex):
-        new_vertex = { vertex : set() }
-        self.vertices.update(new_vertex)
+        new_vertex = Vertex(vertex)
+        self.vertices[new_vertex.vertex] = new_vertex.edges
 
     def add_edge(self, edge_one, edge_two):
         if edge_one in self.vertices.keys() and edge_two in self.vertices.keys():
             self.vertices[edge_one].add(edge_two)
             self.vertices[edge_two].add(edge_one)
         else:
-            raise KeyError
-        # for key in self.vertices.keys():
-        #     if key == edge_one and edge_two in self.vertices.keys():
-        #     elif key == edge_two and edge_one in self.vertices.keys():
+            raise IndexError("That vertex does not exist")
         pass
 
