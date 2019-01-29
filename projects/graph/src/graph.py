4490cf8a27cdd3be47b86857bf4d511031478e62
michaelagard <mdagard@gmail.com>
2019-01-29 11:29:33 -0500

    fixed bft

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -29,10 +29,17 @@ class Graph:
         visited = []
         # Enqueues the starting node
         queue.enqueue(start_node)
+        print(f'queue length: {queue.len()}')
         # Checks if the queue size is greater than 0
-        while len(queue.storage) > 0:
-            visited.append(queue.dequeue())
-            for c_node in self.vertices[queue.dequeue()]:
+        while queue.len() > 0:
+            node = str(queue.dequeue())
+            print(f'node: {node}')
+            visited.append(node)
+            print(f'self.vertices: {self.vertices}')
+            # print(f'self.vertices[node]: {self.vertices['0']}')
+            print(f'self.vertices[node]: {self.vertices[node]}')
+            for c_node in self.vertices[node]:
+                print(f'c_node: {c_node}')
                 if c_node not in visited:
                     queue.enqueue(c_node)
         return visited
