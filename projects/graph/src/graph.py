91bad03be38dbd49eb78ebccea4f06c1bbadcf2b
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-29 16:09:52 -0700

    Complete Breadth First Traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -9,7 +9,7 @@ class Graph:
         self.vertices = {}
     
     def add_vertex(self, value):
-        if not value in self.vertices:
+        if value not in self.vertices:
             self.vertices[value] = set()
     
     def add_edge(self, vertex, target):
@@ -21,15 +21,46 @@ class Graph:
         else:
             print('One of the provided vertices does not exist.')
 
+    def bft(self, vertex):
+        # Keep track of vertices to be visited
+        queue = [vertex]
+
+        # Keep track of vertices visited
+        visited = set(vertex)
+
+        while len(queue) > 0:
+        # Add all edges of given vertex to queue
+            for i in self.vertices[queue[0]]:
+                if i not in visited:
+                    visited.add(i)
+                    queue.append(i)
+
+        # Print and Remove current vertex
+            print(queue.pop(0))
+
+
 # Test
 graph = Graph()
 graph.add_vertex('0')
 graph.add_vertex('1')
 graph.add_vertex('2')
 graph.add_vertex('3')
+graph.add_vertex('5')
+graph.add_vertex('12')
+graph.add_vertex('99')
+graph.add_vertex('14')
 graph.add_edge('0', '1')
 graph.add_edge('0', '3')
 graph.add_edge('1', '0')
 graph.add_edge('3', '0')
+graph.add_edge('3', '12')
+graph.add_edge('12', '5')
+graph.add_edge('1', '5')
+graph.add_edge('5', '1')
+graph.add_edge('5', '2')
+graph.add_edge('12', '14')
+
 print(graph.vertices)
-graph.add_edge('0', '4')
+# graph.add_edge('0', '4')
+
+graph.bft('0')
