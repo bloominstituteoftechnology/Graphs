fae90ce9c1ade11a0b09e19f85157365532951b6
Eva <evalee904@gmail.com>
2019-01-28 23:11:03 -0600

    added tests

--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -19,6 +19,12 @@ def main():
     graph.add_edge('0', '3')
     print(graph.vertices)
 
+    print(graph.bft('0'))
+    print(graph.bft('1'))
+    print(graph.dft('0'))
+    print(graph.dft('3'))
+    print(graph.dft_r('0'))
+
 if __name__ == '__main__':
     # TODO - parse argv
     main()
