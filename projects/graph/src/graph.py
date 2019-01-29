351424816b84b4db623316a292113845a2b53602
Max <maxkajiwara@gmail.com>
2019-01-29 07:05:03 -0800

    Complete Part 1

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -4,7 +4,18 @@ Simple graph implementation
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    "Represent a graph as a dictionary of vertices mapping labels to edges."
 
     def __init__(self):
         self.vertices = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 and vertex2 in self.vertices:
+            self.vertices[vertex1].add(vertex2)
+            self.vertices[vertex2].add(vertex1)
+        else:
+            print('Graph does contain both of those vertices')
