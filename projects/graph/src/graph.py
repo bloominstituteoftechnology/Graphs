85e14aa92821c658c50194e408299a154f354d0a
michaelagard <mdagard@gmail.com>
2019-01-29 11:01:55 -0500

    add broken btf code

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -16,7 +16,6 @@ class Graph:
             self.vertices[vertex] = set()
 
     def add_edge(self, vertex1, vertex2):
-
         if vertex1 in self.vertices and vertex2 in self.vertices:
             self.vertices[vertex1].add(vertex2)
             self.vertices[vertex2].add(vertex1)
@@ -24,8 +23,16 @@ class Graph:
             raise Exception("Edges to nonexistent vetices")
 
     def bredth_first_traversal(self, start_node):
+        # Create a new queue using the imported queue file
         queue = Queue()
-        visited = set()
+        # Storage for the nodes that were visted
+        visited = []
+        # Enqueues the starting node
         queue.enqueue(start_node)
-        while queue:
-            visited.add(queue.dequeue())
+        # Checks if the queue size is greater than 0
+        while len(queue.storage) > 0:
+            visited.append(queue.dequeue())
+            for c_node in self.vertices[queue.dequeue()]:
+                if c_node not in visited:
+                    queue.enqueue(c_node)
+        return visited
