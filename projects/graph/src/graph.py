cae17ecd4c562f0cd22836129dc907a9d0792385
Scott Bren <smbren@ncsu.edu>
2019-01-28 19:33:23 -0500

    implemented graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,16 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertexList = {}
+
+    def vertices(self):
+        return self.vertexList
+
+    def add_vertex(self, vertex):
+        self.vertexList[vertex] = set()
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.vertexList:
+            self.vertexList[vertex1].add(vertex2)
+        else:
+            self.vertexList[vertex1] = [vertex2]
