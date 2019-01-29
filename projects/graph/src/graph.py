f7ffcd1d51aa739c4cdb9030b75d21fc7018ead2
Simon Elhoej <simonelhoej@gmail.com>
2019-01-29 21:56:30 +0100

    Added stack, queue, breadth first traversal and depth first traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,5 +1,40 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -7,9 +42,38 @@ class Graph:
         self.vertices[value] = set()
 
     def add_edge(self, vert, value):
-        self.vertices[vert].add(value)
-        self.vertices[value].add(vert)
+        if vert in self.vertices and value in self.vertices:
+            self.vertices[vert].add(value)
+            self.vertices[value].add(vert)
+        else:
+            IndexError()
 
+    def bft(self, start_node):
+        queue = Queue()
+        visited = set()
+        queue.enqueue(start_node)
+
+        while queue.size > 0:
+            node = queue.dequeue()
+            if node not in visited:
+                print("not visited:", node)
+                visited.add(node)
+                for next in self.vertices[node]:
+                    queue.enqueue(next)
+
+    def dft(self, start_node):
+        stack = Stack()
+        visited = set()
+        stack.push(start_node)
+        while stack.size() > 0:
+            node = stack.pop()
+            if node not in visited:
+                print("not visited:", node)
+                visited.add(node)
+                for next in self.vertices[node]:
+                    stack.push(next)
+
+    
 
 
 graph = Graph()
