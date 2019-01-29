f076bceaf6ada709f7b05d2223d11e990d31fd49
michaelagard <mdagard@gmail.com>
2019-01-29 15:56:20 -0500

    uploading what I have

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,6 +5,23 @@ Simple graph implementation
 """
 
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
@@ -20,7 +37,7 @@ class Graph:
             self.vertices[vertex1].add(vertex2)
             self.vertices[vertex2].add(vertex1)
         else:
-            raise Exception("Edges to nonexistent vetices")
+            raise Exception(f'Vertex {vertex1} does not exist.')
 
     def bredth_first_traversal(self, start_node):
         # Create a new queue using the imported queue file
@@ -29,17 +46,24 @@ class Graph:
         visited = []
         # Enqueues the starting node
         queue.enqueue(start_node)
-        print(f'queue length: {queue.len()}')
         # Checks if the queue size is greater than 0
         while queue.len() > 0:
             node = str(queue.dequeue())
-            print(f'node: {node}')
             visited.append(node)
-            print(f'self.vertices: {self.vertices}')
-            # print(f'self.vertices[node]: {self.vertices['0']}')
-            print(f'self.vertices[node]: {self.vertices[node]}')
             for c_node in self.vertices[node]:
-                print(f'c_node: {c_node}')
                 if c_node not in visited:
                     queue.enqueue(c_node)
         return visited
+
+    # def depth_first_traversal(self, start_node):
+    #     stack = []
+    #     visited = []
+    # def depth_first_traversal_recursive(self, start_node):
+    #     if visited is None:
+    #         visited = set()
+    #     visited.add(start_node)
+    #     print(Starting_node)
+    #     for child_node in self.vertices[start_node]
+    #     if child_node not in visited:
+    #             self.depth_first_traversal_recursive(child_node, visited)
+    #             depth_first_Traversal_recursive(child_node, visited)
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -9,14 +9,31 @@ from sys import argv
 
 def main():
     graph = Graph()  # Instantiate your graph
-    graph.add_vertex('0')
     graph.add_vertex('1')
     graph.add_vertex('2')
     graph.add_vertex('3')
-    graph.add_edge('0', '1')
-    graph.add_edge('0', '3')
+    graph.add_vertex('4')
+    graph.add_vertex('5')
+    graph.add_vertex('6')
+    graph.add_vertex('7')
+    graph.add_edge('1', '2')
+    graph.add_edge('2', '3')
+    graph.add_edge('2', '4')
+    graph.add_edge('3', '5')
+    graph.add_edge('4', '6')
+    graph.add_edge('4', '7')
+    graph.add_edge('5', '3')
+    graph.add_edge('6', '3')
+    graph.add_edge('7', '6')
+    graph.add_edge('7', '1')
+    # graph.add_vertex('0')
+    # graph.add_vertex('1')
+    # graph.add_vertex('2')
+    # graph.add_vertex('3')
+    # graph.add_edge('0', '1')
+    # graph.add_edge('0', '3')
     print(graph.vertices)
-    print(graph.bredth_first_traversal(0))
+    # print(graph.bredth_first_traversal(1))
 
 
 if __name__ == '__main__':
