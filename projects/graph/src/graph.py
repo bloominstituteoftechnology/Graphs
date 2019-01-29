4d66761b41229eb0596d3c29523da91928123097
Scott Bren <smbren@ncsu.edu>
2019-01-29 11:30:55 -0500

    fixed a bug with the graph code

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,16 +6,13 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertexList = {}
-
-    def vertices(self):
-        return self.vertexList
+        self.vertices = {}
 
     def add_vertex(self, vertex):
-        self.vertexList[vertex] = set()
+        self.vertices[vertex] = set()
 
     def add_edge(self, vertex1, vertex2):
-        if vertex1 in self.vertexList:
-            self.vertexList[vertex1].add(vertex2)
-        else:
-            self.vertexList[vertex1] = [vertex2]
+        if vertex1 and vertex2 in self.vertices:
+            self.vertices[vertex1].add(vertex2)
+            self.vertices[vertex2].add(vertex1)
+      
