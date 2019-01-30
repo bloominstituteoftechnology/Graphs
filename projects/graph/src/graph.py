58718fc1a158f53bd5fecb3fe88f43b64355f26d
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-29 17:03:05 -0700

    Finish Breadth First Search

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -12,17 +12,17 @@ class Graph:
         if value not in self.vertices:
             self.vertices[value] = set()
     
-    def add_edge(self, vertex, target):
-        # print(self.vertices)
-        # print(self.vertices[vertex])
-        # print(hasattr(self.vertices, vertex))
-        if vertex in self.vertices and target in self.vertices:
-            self.vertices[vertex].add(target)
+    def add_edge(self, origin, target):
+        if origin == target:
+            print('Origin must not match target.')
+        if origin in self.vertices and target in self.vertices:
+            self.vertices[origin].add(target)
         else:
             print('One of the provided vertices does not exist.')
 
     # Breadth First Traversal
     def bft(self, vertex):
+
         # Keep track of vertices to be visited
         queue = [vertex]
 
@@ -41,6 +41,7 @@ class Graph:
 
     # Depth First Traversal
     def dft(self, vertex):
+
         # Keep track of vertices to be visited
         stack = [vertex]
 
@@ -60,6 +61,7 @@ class Graph:
 
     # Recursive Depth First Traversal
     def dft_r(self, vertex, cache = set()):
+
         # Print the vertex
         print(vertex)
 
@@ -68,10 +70,48 @@ class Graph:
 
         # Check all edges of current vertex 
         for i in self.vertices[vertex]:
-            
+
             # with vertices not already checked
             if i not in cache:
                 self.dft_r(i, cache)
+
+    # Breadth First Search
+    def bfs(self, origin, target):
+
+        # Check if target matches the origin
+        if target == origin:
+            return 'Self matches origin.'
+        
+        # Keep track of vertices to be visited
+        queue = [origin]
+
+        # Keep track of the paths need to reach vertices
+        paths = {origin:[origin]}
+
+        while len(queue) > 0:
+            
+            # Add all edges of given vertex to queue
+            for i in self.vertices[queue[0]]:
+
+                # If vertex has not been visited, cache its path
+                if i not in paths:
+                    paths[i] = list(paths[queue[0]])
+                    paths[i].append(i)
+                    queue.append(i)
+
+                    # If vertex matches target, return target
+                    if i == target:
+                        return paths[i]
+
+            # Remove last item checked
+            queue.pop(0)
+        
+        # Return a message if target was not found
+        return 'A path to the given target vertex could not be found.'
+                
+
+
+    # Depth First Search
 
 
 # Test
@@ -95,11 +135,13 @@ graph.add_edge('5', '1')
 graph.add_edge('5', '2')
 graph.add_edge('12', '14')
 
-print(graph.vertices)
+# print(graph.vertices)
 # graph.add_edge('0', '4')
 
-graph.bft('0')
-print('')
-graph.dft('0')
-print('')
-graph.dft_r('0')
+# graph.bft('0')
+# print('')
+# graph.dft('0')
+# print('')
+# graph.dft_r('0')
+
+print(graph.bfs('3', '2'))
