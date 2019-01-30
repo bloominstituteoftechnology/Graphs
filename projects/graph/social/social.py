bff59ffeeafc1cac0812c1f0db1d3ab2b25e97aa
Hunter315 <hunter.mckay.smith@gmail.com>
2019-01-30 15:55:08 -0700

    social.py getAllSocialPaths and bfs implemented

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,9 +1,12 @@
 import random
 from itertools import combinations
+from collections import deque
+
 
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -51,17 +54,26 @@ class SocialGraph:
         for i in range(numUsers):
             self.addUser(f"User {i}")
 
-            #==========OR=========
-        
-       
+            # ==========OR=========
+        possible_friendships = []
+        # a range of all user ids from 1 to 10
+        for i in range(1, self.lastID + 1):
+            for j in range(1, self.lastID + 1):
+                if i < j and i != j:
+                    possible_friendships.append((i, j))
 
-        # Create friendships
-        possible_friendships = list(combinations(range(1, numUsers +1), 2))
+        # ----Create friendships----
+        # possible_friendships = list(combinations(range(1, numUsers +1), 2))
+        # ----shuffle all possible----
         random.shuffle(possible_friendships)
-        actual_friendships = possible_friendships[:10]
+        # ----slice off what i need----
+        sliced = round((numUsers * avgFriendships) / 2)
+        sliced_friendships = possible_friendships[:sliced]
+        # actual_friendships = possible_friendships[:10]
 
-        for friendship in actual_friendships:
+        for friendship in sliced_friendships:
             self.addFriendship(friendship[0], friendship[1])
+
     def getAllSocialPaths(self, userID):
         """
         Takes a user's userID as an argument
@@ -72,13 +84,41 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        if self.friendships[userID]:
+            for i in range(1, self.lastID + 1):
+                visited[i] = self.bfs(userID, i)
+
+            return visited
+        else:
+            return 'No friends'
+
+
+        # Breadth First Search
+
+    def bfs(self, starting_vert, target_vert):
+        queue = deque()
+        visited = set()
+        queue.append([starting_vert])
+        while queue:
+            # -> dequeue a list from queue
+            dequeued_list = queue.popleft()
+            path_end = dequeued_list[-1]
+            # -> mark it as visited
+            if path_end not in visited:
+                # check if target vert == last item in list
+                if path_end == target_vert:
+                    return dequeued_list
+                visited.add(path_end)
+                # -> enqueue all of it's children
+                for vert in self.friendships[path_end]:
+                    path_copy = list(dequeued_list)
+                    path_copy.append(vert)
+                    queue.append(path_copy)
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
     print(sg.friendships)
-    # connections = sg.getAllSocialPaths(1)
-    # print(connections)
+    connections = sg.getAllSocialPaths(1)
+    print(connections)
