4ba910603463332d543bc18937c1013fe81f5111
Ryan Walker <rytwalker@gmail.com>
2019-01-30 15:36:02 -0500

    populate graph function works

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,3 +1,4 @@
+import random
 
 
 class User:
@@ -17,7 +18,8 @@ class SocialGraph:
         """
         if userID == friendID:
             print("WARNING: You cannot be friends with yourself")
-        elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
+        elif (friendID in self.friendships[userID] or
+              userID in self.friendships[friendID]):
             print("WARNING: Friendship already exists")
         else:
             self.friendships[userID].add(friendID)
@@ -27,7 +29,8 @@ class SocialGraph:
         """
         Create a new user with a sequential integer ID
         """
-        self.lastID += 1  # automatically increment the ID to assign the new user
+        # automatically increment the ID to assign the new user
+        self.lastID += 1
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
 
@@ -39,7 +42,8 @@ class SocialGraph:
         Creates that number of users and a randomly distributed friendships
         between those users.
 
-        The number of users must be greater than the average number of friendships.
+        The number of users must be greater than the
+        average number of friendships.
         """
         # Reset graph
         self.lastID = 0
@@ -48,8 +52,24 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(numUsers):
+            self.addUser(f"User {i}")
+        # create list of every possible friendship
+        possible_friendships = []
+        # a range of all user ids from 1 to 10
+        for i in range(1, self.lastID + 1):
+            for j in range(1, self.lastID + 1):
+                if i < j and i != j:
+                    possible_friendships.append((i, j))
 
+        # shuffle all possible friendships
+        random.shuffle(possible_friendships)
+        # slice off
+        sliced = round((numUsers * avgFriendships) / 2)
+        sliced_friendships = possible_friendships[:sliced]
         # Create friendships
+        for friendship in sliced_friendships:
+            self.addFriendship(friendship[0], friendship[1])
 
     def getAllSocialPaths(self, userID):
         """
