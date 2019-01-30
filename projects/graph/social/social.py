25bcb523aab9cac0f01f3489f51b7cf6df7868e8
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-01-30 15:57:37 -0700

    starts social

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,4 +1,4 @@
-
+from itertools import combinations
 
 class User:
     def __init__(self, name):
@@ -11,9 +11,9 @@ class SocialGraph:
         self.friendships = {}
 
     def addFriendship(self, userID, friendID):
-        """
-        Creates a bi-directional friendship
-        """
+        
+        #Creates a bi-directional friendship
+        
         if userID == friendID:
             print("WARNING: You cannot be friends with yourself")
         elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
@@ -23,42 +23,41 @@ class SocialGraph:
             self.friendships[friendID].add(userID)
 
     def addUser(self, name):
-        """
-        Create a new user with a sequential integer ID
-        """
+        
+        #Create a new user with a sequential integer ID
+        
         self.lastID += 1  # automatically increment the ID to assign the new user
         self.users[self.lastID] = User(name)
         self.friendships[self.lastID] = set()
 
     def populateGraph(self, numUsers, avgFriendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
-
-        Creates that number of users and a randomly distributed friendships
-        between those users.
-
-        The number of users must be greater than the average number of friendships.
-        """
+        
+        # Takes a number of users and an average number of friendships
+        # as arguments
+        # Creates that number of users and a randomly distributed friendships
+        # between those users.
+        # The number of users must be greater than the average number of friendships.
+        
         # Reset graph
         self.lastID = 0
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        # Add numUsers
+        for i in range(numUsers):
+            self.addUser(f"User: {i}")
 
         # Create friendships
+        friendshipator = list(combinations(range(1, numUsers+1), 2))
 
     def getAllSocialPaths(self, userID):
-        """
-        Takes a user's userID as an argument
-
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
-
-        The key is the friend's ID and the value is the path.
-        """
+        
+        # Takes a user's userID as an argument
+        # Returns a dictionary containing every user in that user's
+        # extended network with the shortest friendship path between them.
+        # The key is the friend's ID and the value is the path.
+        
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
         return visited
