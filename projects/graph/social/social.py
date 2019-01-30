1e828ebcb0a1a07be5db5a38b5201aacb9694807
Kevin Sooter <kevin.m.sooter@gmail.com>
2019-01-30 16:31:02 -0600

    Finished MVP

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,4 +1,6 @@
-
+from itertools import combinations
+from random import shuffle
+from collections import deque
 
 class User:
     def __init__(self, name):
@@ -34,10 +36,8 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -47,21 +47,52 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(numUsers):
+            self.addUser(f"User {i}")
 
         # Create friendships
+        possible_friendships = list(combinations(range(1, numUsers+1), 2))
+        shuffle(possible_friendships)
+        num_friendships = avgFriendships * numUsers
+        friendship_list = possible_friendships[:num_friendships//2]
+        for friendship in friendship_list:
+            self.addFriendship(friendship[0], friendship[1])
+        
+        
 
-    def getAllSocialPaths(self, userID):
+    def getAllSocialPaths(self, userID, q=None, visited=None, path=None):
         """
         Takes a user's userID as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        if not visited:
+            q = deque()
+            path = [userID]
+            visited = {userID: path}
+        for related in self.friendships[userID]:
+            new_path = path[:]
+            new_path.append(related)
+            if related not in visited:
+                q.append((related, new_path))
+                visited[related] = new_path
+        if len(q) == 0:
+            return visited
+        if len(q) > 0:
+            user, path = q.popleft()
+            return self.getAllSocialPaths(user, q, visited, path)
+
+
+"""
+To create 100 users with an average of 10 friends each, how many times would you need to call addFriendship()? Why?
+    - 500 becuase friendships are bi-directional and 1000 friendships would average out to 10 per user.
+    
+If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network?
+    - About 100%
+    - Avg is 3 intermediate people
+    - They say there's about ~6 degrees of separation between any person on the planet and another
+"""
 
 
 if __name__ == '__main__':
