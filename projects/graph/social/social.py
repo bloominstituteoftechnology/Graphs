f30be7908a0dc73c3b155971b1e07f6293a24760
Linh Bouniol <paulalinh89@gmail.com>
2019-01-30 21:19:35 -0800

    Completed part 3 of social

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -46,19 +46,19 @@ class SocialGraph:
         self.friendships = {}
       
         # Add users
-        for i in range(numUsers):
-            self.addUser(f"User {i}")
+        for i in range(numUsers):    # O(n)
+            self.addUser(f"User {i}")    # O(1)
 
         # Create friendships
         # Combinations return back a slice of the list
         possible_friendships = list(combinations(range(1, len(self.users)+1), 2))
         random.shuffle(possible_friendships)
-        # Want the average number of friends to be 2
-        number_connections = numUsers * avgFriendships // 2
+        # Since addFriendsip creates two connections internally, we divide by 2 that way we create only half of the connections
+        number_connections = numUsers * avgFriendships // 2 
         actual_friendships = possible_friendships[:number_connections]
 
-        for friendship in actual_friendships:
-            sg.addFriendship(friendship[0], friendship[1])
+        for friendship in actual_friendships:    # O(n)
+            sg.addFriendship(friendship[0], friendship[1])    # O(1)
             
     def getAllSocialPaths(self, userID):
         """
@@ -94,14 +94,37 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print(sg.friendships)
+    sg.populateGraph(1000, 5)
+#    print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
-    print(connections)
+    print(len(connections))
+    
+    total_degree_of_separation = 0
+    total_number_of_samples = 0
+    
+    for friendID, path in connections.items():
+        # Add the path length of each node minus 1 for yourself
+        total_degree_of_separation += len(path) - 1
+        # Keep count of the number of times you add path lengths
+        total_number_of_samples += 1
+    
+    # Print the average
+    
+    print(f"Average degrees of separation: {total_degree_of_separation/total_number_of_samples}")
 
 
+"""
+To create 100 users with an average of 10 friends each, how many times would you need to call addFriendship()? Why?
+- 100 * 10 // 2 => 500, number of connections. This is used to get the actual_friendships, since we're looping thru it, its that number of calls
 
+If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network?
+- Running line 97 several times seems to be about 99%.
+- Using the algorithm between lines 102 - 109, it seems the average is about 4 - 5 degrees of separation. (Not including yourself)
 
+You might have found the results from question #2 above to be surprising. Would you expect results like this in real life? If not, what are some ways you could improve your friendship distribution model for more realistic results?
+- 
+
+"""
 
 #all_friendships = [(1,2), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5), (3,4), (3,5), (4,5)]
 
