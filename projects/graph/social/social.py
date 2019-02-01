c8fa9a02faaebd6e39afbfed9bcc71026f1fe268
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-31 17:03:27 -0700

    Finish MVP

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -1,4 +1,5 @@
-
+import random
+import itertools
 
 class User:
     def __init__(self, name):
@@ -46,9 +47,26 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        # Check input
+        if int(numUsers) <= int(avgFriendships):
+            print('WARNING: Number of users must be greater than average number of friendships.')
 
-        # Create friendships
+        # Add users
+        else:
+            for i in range(1, numUsers + 1):
+                self.addUser(i)
+
+            # for user in self.users:
+            #     for i in range(random.randint(1, avgFriendships - len(self.friendships[user])):
+            #         randomFriend = random.randint(1, numUsers)
+            #         if randomFriend < 
+            #         self.addFriendship(user, random.randint(1, numUsers))
+            possibleCombinations = list(itertools.combinations(list(self.users), 2))
+            random.shuffle(possibleCombinations)
+
+            for i in range(0, numUsers * avgFriendships):
+                self.addFriendship(possibleCombinations[i][0], possibleCombinations[i][1])
+
 
     def getAllSocialPaths(self, userID):
         """
@@ -59,14 +77,39 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+        
+        # Keep track of vertices to be visited
+        queue = [userID]
+
+        # Keep track of the paths need to reach vertices
+        paths = {userID:[userID]}
+
+        while len(queue) > 0:
+            
+            # Add all edges of given vertex to queue
+            for i in self.friendships[queue[0]]:
+
+                # If vertex has not been visited, cache its path
+                if i not in paths:
+                    paths[i] = list(paths[queue[0]])
+                    paths[i].append(i)
+                    queue.append(i)
+
+                    # If vertex matches target, return target
+                    # if i == target:
+                    #     return paths[i]
+
+            # Remove last item checked
+            queue.pop(0)
+        
+        # Return a message if target was not found
+        return paths
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
     print(sg.friendships)
+    print('')
     connections = sg.getAllSocialPaths(1)
     print(connections)
