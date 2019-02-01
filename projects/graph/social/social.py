40aae0522e5dcf62d61b1f621f9ddf151fe74f69
pedro <pedroluismontesinosrivera@gmail.com>
2019-01-31 19:00:00 -0500

    question 1

--- projects/graph/social/social.py
+++ projects/graph/social/social.py
@@ -100,3 +100,12 @@ if __name__ == '__main__':
     print(connections)
 
 
+"""
+    1. To create 100 users with an average of 10 friends each, how many times would you need to call addFriendship()? Why? 
+    
+        A: 500 because it's bidirectional
+
+
+
+    2. If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network? 
+"""
