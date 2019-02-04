b5ccabaa32e0c6513ebdab2f22aae65442f647ab
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-04 10:40:57 -0800

    initial commit plus created graph's init method and add_edge method

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,12 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+
+    def __init__(self, size):
+        self.size = size
+        self.vertices = [[] for i in range(size)]
+
+    def add_edge(self, i, j):
+        if not (i in self.vertices[j]):
+            self.vertices[j].append(i)
+            self.vertices[i].append(j)
