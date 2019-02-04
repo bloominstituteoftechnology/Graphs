250ad2b64cbcade819199a157fa7b6573582a073
Riley-Brown <riley@riley.gg>
2019-02-04 13:46:54 -0500

    Added simple graph implementation

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,13 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {
+             "A": {"B"},
+                          "B": {"C", "D"},
+                          "C": {"E"},
+                          "D": {"F", "G"},
+                          "E": {"C"},
+                          "F": {"C"},
+                          "G": {"A", "F"}
+                        }
+        }
