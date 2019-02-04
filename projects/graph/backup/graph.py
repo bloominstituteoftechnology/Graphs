29bf351d51ee4eaa0386ebc9823908427323d3ba
txiong000 <tkx422@gmail.com>
2019-02-04 17:06:15 -0600

    bfs complete

--- projects/graph/backup/graph.py
+++ projects/graph/backup/graph.py
@@ -2,7 +2,6 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 
-
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
@@ -15,5 +14,21 @@ class Graph:
     def add_edge(self, vert_1, vert_2):
         self.vertices[vert_1].add(vert_2)
         self.vertices[vert_2].add(vert_1)
-       
+    
+    def bfs(self, node):
+        visisted = set()
+        queue = []
+
+        queue.append(node)
+
+        while len(queue) > 0:
+            current_node = queue.pop()
+            visisted.add(current_node)
+
+            if self.vertices[current_node] is not set():
+                for item in self.vertices[current_node]:
+                    if item not in visisted:
+                        queue.append(item)
+
+        
         
