d643b9b3affa2f4d56e37d45b4d59abe7d103a14
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-04 16:25:49 -0500

    fixed add_vertex and add_edge to address a dict/str typing error, now printing results successfully

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -11,7 +11,19 @@ class Graph:
     # placeholders for add_vertex and add_edge methods
     # add_vertex needs only a vertex, while add_edge needs both a vertex and edge
     def add_vertex(self, vertex):
-        self.vertices[vertex] = {}
-        
+        self.vertices[vertex] = set()
+
     def add_edge(self, vertex, edge):
-        pass #TODO
+        if vertex in self.vertices:
+            self.vertices[vertex].add(edge)
+
+# testing
+
+graph = Graph() 
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
