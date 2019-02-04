ae545d91240bf2d1664b8c1f48b114f98f1648c5
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-04 15:35:59 -0800

    debuggin bfs

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,11 +6,30 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
-    def __init__(self, size):
-        self.size = size
-        self.vertices = [[] for i in range(size)]
+    def __init__(self, vertices=None):
+        # includes all vertices
+        self.vertices = {} if vertices is None else vertices
 
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    # adding/creating an edge to a graph between vertices i and j
     def add_edge(self, i, j):
+        # check if vertices are connected
         if not (i in self.vertices[j]):
-            self.vertices[j].append(i)
-            self.vertices[i].append(j)
+            # makes sure that i is included in the list of vertices that connect to j
+            self.vertices[j].add(i)
+            # viseversa makes sure that j is included in the list of vertices that connect to i
+            self.vertices[i].add(j)
+
+    def bfs(self, target):
+        queue = [target]
+        visited = set([target])
+        while queue:
+            current = queue.pop()
+            if current == target:
+                break
+            visited.add(current)
+            queue.extend(self.vertices[current] - visited)
+            visited.update(self.vertices[current])
+        return visited
--- projects/graph/src/graph_demo.py
+++ projects/graph/src/graph_demo.py
@@ -18,6 +18,8 @@ def main():
     graph.add_edge('0', '1')
     graph.add_edge('0', '3')
     print(graph.vertices)
+    print(graph.bfs('3'))
+
 
 if __name__ == '__main__':
     # TODO - parse argv
