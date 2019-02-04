7925cd398dc5687cafc015941a6c5a8d94a269c4
Riley-Brown <riley@riley.gg>
2019-02-04 17:24:37 -0500

    updated graph.py

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,18 +1,27 @@
 """
 Simple graph implementation
 """
-
+test_graph =  { 
+    "A": {"B"},
+    "B": {"C", "D"},
+    "C": {"E"},
+    "D": {"F", "G"},
+    "E": {"C"},
+    "F": {"C"},
+    "G": {"A", "F"}
+                }
+        
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        self.vertices = {
-             "A": {"B"},
-                          "B": {"C", "D"},
-                          "C": {"E"},
-                          "D": {"F", "G"},
-                          "E": {"C"},
-                          "F": {"C"},
-                          "G": {"A", "F"}
-                        }
-        }
+        self.vertices = {}
+        print(self.vertices)
+        
+    
+    def add_edge(self, from_vertex, to_vertex):
+        self.vertices[from_vertex].add(to_vertex)
+    
+    def add_vertex(self, value):
+        self.vertices[value] = set()
+
