eb6229bdf8dd6803f44294fcf7d2178d8cb54790
sumi419 <sasgar04@gmail.com>
2019-02-04 11:20:02 -0800

    Graph Class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,5 +6,18 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  
-        # TODO
+        self.vertices = {} #nodes
+        # self.edges = edges
+        # self.weight = weight
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = set()
+
+    def add_edge(self, vertex, edge):
+        if vertex in self.vertices:
+            self.vertices[vertex] = edge
+            # self.vertices.update({vertex: edge})
+
+
+
+
