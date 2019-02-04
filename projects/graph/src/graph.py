39ce1d4123367a4ef53f098a7a29e21d27d35fc7
AM_Myrick <amyrick.820@gmail.com>
2019-02-04 13:24:29 -0500

    completes part 1 with exception raises

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,5 +6,17 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
 
+    def add_vertex(self, vertex):
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set()
+    
+    def add_edge(self, v1, v2):
+        if v1 and v2 in self.vertices:
+            self.vertices[v1].update(v2)
+            self.vertices[v2].update(v1)
+        if v1 not in self.vertices:
+            raise Exception("v1 is not in self.vertices")
+        if v2 not in self.vertices:
+            raise Exception("v2 is not in self.vertices")
