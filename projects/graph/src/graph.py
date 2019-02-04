f2c5e664caddb7ca60c7e58fd9c3a4d417bd325d
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-04 13:38:21 -0500

    added initial definition to Graph class and created placeholders for add_vertex and add_edge methods

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,11 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        # add a dictionary of vertices
+        self.vertices = {}
+    # placeholders for add_vertex and add_edge methods
+    # add_vertex needs only a vertex, while add_edge needs both a vertex and edge
+    def add_vertex(self, vertex):
+        pass #TODO
+    def add_edge(self, vertex, edge):
+        pass #TODO
