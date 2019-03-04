b37864a1ca3f4e3ffd2ca094a3b60679c4fd2bd2
Carlos Lantigua <clantigua954@gmail.com>
2019-03-04 14:27:53 -0500

    adding initial stuff

--- projects/graph/backup/graph.py
+++ projects/graph/backup/graph.py
@@ -5,5 +5,19 @@ Simple graph implementation compatible with BokehGraph class.
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
+        self.vertices = {}
         pass  # TODO
+
+    def add_vertex(self, vert):
+        if self.vertices.vert:
+            return False
+        else:
+            return self.vertices.append(vert)
+
+    def add_edge(self, vert, edge):
+        if self.vertices.vert:
+            return self.vertices.vert.append(edge)
+        else:
+            return False
--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,7 +3,98 @@ Simple graph implementation
 """
 
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.queue))
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def bft(self, starting_vertex_id):
+        # Create an empty queue
+        q = Queue()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Queue
+        q.enqueue(starting_vertex_id)
+        # While the queue is not empty....
+        while q.size() > 0:
+            # Dequeue the first node from the queue
+            v = q.dequeue()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the queue
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        # Create an empty stack
+        s = Stack()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Stack
+        s.push(starting_vertex_id)
+        # While the stack is not empty....
+        while s.size() > 0:
+            # Pop the top node from the stack
+            v = s.pop()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the stack
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
