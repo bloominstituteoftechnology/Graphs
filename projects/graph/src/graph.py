47b3e0e397b3fb458cb45db10a14eba11def4a34
JacobABrennan <jacobabrennan@gmail.com>
2019-03-04 15:24:18 -0500

    Finish Part 1: Graph, Vertex Classes

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,9 +1,28 @@
-"""
-Simple graph implementation
-"""
+
+
+"""Simple graph implementation"""
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """
+    Represent a graph as a dictionary of vertices mapping labels to edges.
+    """
     def __init__(self):
-        pass  # TODO
+        self.vertices = dict()
+
+    def add_vertex(self, vertex_id):
+        """Creates a new vertex with no edges."""
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, source_id, end_id):
+        """Connects two vertices along an edge, bidirectionally."""
+        # Ensure endpoints are valid vertices on the graph
+        if not (source_id in self.vertices):
+            raise Exception('Invalid vertex id for edge source.')
+        if not (end_id in self.vertices):
+            raise Exception('Invalid vertex id for edge destination.')
+        # Connect vertices symmetrically
+        vertex_source = self.vertices[source_id]
+        vertex_source.add(end_id)
+        vertex_end = self.vertices[end_id]
+        vertex_end.add(source_id)
