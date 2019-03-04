0ec7144dc9cb31b70c8ecaf6bc0453fc7c57a3d1
kyranmccann <kyran.mccann@gmail.com>
2019-03-04 11:16:45 -0800

    code from class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,7 +3,90 @@ Simple graph implementation
 """
 
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.queue))
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("The vertex does not exist")
+
+    def bft(self, starting_vertex_id):
+        # create an empty queue
+        q = Queue()
+        # create an empty set of visited nodes
+        visited = set()
+        # put starting vertex in queue
+        q.enqueue(starting_vertex_id)
+        # while queue is not empty
+        while q.size() > 0:
+            # dequeue the first node from the queue
+            v = q.dequeue()
+            # if that node has not been visited
+            if v not in visited:
+                print(v)
+                # mark it as visited
+                visited.add(v)
+                # put all of its neighbors into the queue
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex_id)
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
