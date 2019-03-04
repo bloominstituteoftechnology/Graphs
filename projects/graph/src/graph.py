bf168224c7633207a9989bd34dde790bebc0d010
adamhinckley <adamhinckley@mac.com>
2019-03-04 14:47:22 -0700

    Depth-First Traversal using Recursion working (i think)

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -3,7 +3,138 @@ Simple graph implementation
 """
 
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return (len(self.queue))
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
+
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist")
+
+    def bft(self, starting_vertex_id):
+        # Create an empty queue
+        q = Queue()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Queue
+        q.enqueue(starting_vertex_id)
+        # While the queue is not empty....
+        while q.size() > 0:
+            # Dequeue the first node from the queue
+            v = q.dequeue()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the queue
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
+
+    def dft(self, starting_vertex_id):
+        # Create an empty stack
+        s = Stack()
+        # Create an empty set of visited vertices
+        visited = set()
+        # Put the starting vertex in our Stack
+        s.push(starting_vertex_id)
+        # While the stack is not empty....
+        while s.size() > 0:
+            # Pop the top node from the stack
+            v = s.pop()
+            # If that node has not been visted...
+            if v not in visited:
+                # Mark it as visited
+                print(v)
+                visited.add(v)
+                # Then, put all of it's children into the stack
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
+
+    # def dft_recursion(self, starting_vertex, path=[]):
+    #     path += [starting_vertex]
+
+    #     for neighbor in self.vertices[starting_vertex]:
+    #         if neighbor not in path:
+    #             path = self.dft_recursion(neighbor, path)
+
+    #     return path
+
+    def dft_recursion(self, starting_vert, path=[]):
+        path += [starting_vert]
+
+        for neighbor in self.vertices[starting_vert]:
+            if neighbor not in path:
+                path = self.dft_recursion(neighbor, path)
+
+        return path
+
+g = Graph()
+
+g.add_vertex('1')
+g.add_vertex('2')
+g.add_vertex('3')
+g.add_vertex('4')
+g.add_vertex('5')
+g.add_vertex('6')
+g.add_vertex('7')
+g.add_directed_edge('5', '3')
+g.add_directed_edge('6', '3')
+g.add_directed_edge('7', '1')
+g.add_directed_edge('4', '7')
+g.add_directed_edge('1', '2')
+g.add_directed_edge('7', '6')
+g.add_directed_edge('2', '4')
+g.add_directed_edge('3', '5')
+g.add_directed_edge('2', '3')
+g.add_directed_edge('4', '6')
+
+print(g.dft_recursion('2'))
