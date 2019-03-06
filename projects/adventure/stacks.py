b1b6ca38a5edebf084a3f02ad6850ac29ea6523d
Carlos Lantigua <clantigua954@gmail.com>
2019-03-06 17:38:41 -0500

    started stuff, didn't get much done

---
+++ projects/adventure/stacks.py
@@ -0,0 +1,15 @@
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return (len(self.stack))
