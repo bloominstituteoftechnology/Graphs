6a7ae4ece9772195519e98b95e6dcdb51937e768
derpwanda <indigoblow@gmail.com>
2019-03-12 16:40:08 -0500

    comments from laptop

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,7 +6,21 @@ Simple graph implementation
 # add vertex method
 # add edge method
 
-#
+# Brady's Queue class for use in his bft and dft
+# comment out later
+# class Queue:
+#     def __init__(self):
+#         self.queue = []
+#     def enqueue(self, value):
+#         self.queue.append(value)
+#     def dequeue(self):
+#         if self.size() > 0:
+#             return self.queue.pop(0)
+#         else:
+#             return None
+#     def size(self):
+#         return (len(self.queue))
+
 graph1 = {
     '0': {'1', '3'},
     #   ^ key : ^ value
@@ -48,11 +62,14 @@ class Graph:
 
     def __init__(self, graph):
         self.vertices = graph
+        # self.vertices = {}
 
     def add_vertex(self, key):
         self.vertices[key] = set()
 
     def add_edge(self, key, value):
+        # key is the vertex we connecting
+        # value is the vertex that we are connecting to
         if not self.vertices[key] and not self.vertices[value]:
             print("error: no vertex exists here")
         else:
@@ -61,13 +78,16 @@ class Graph:
 
     def add_directed_edge(self, key, value):
         if not self.vertices[key] and not self.vertices[value]:
-            print("error: no vertex exists here")
+            raise IndexError("That vertex does not exist")
         else:
             self.vertices[key].add(value)
+            # value is the vertex that we are connecting to
 
     def add_weighted_edge(self):
         pass    # TODO
 
+# You need to have a visited set to keep track of what you have already seen.
+# If you did not, in cyclic graphs, you'd just keep looping over and over
     def breadth_first_traversal(self, start_vertex):
         # create a queue for bfs
         queue = []
@@ -90,8 +110,20 @@ class Graph:
             print(visited)
             print(tracker)
 
-    def depth_first_traversal(self, start_vertex):
+        # put the starting vertex in queue
+        # while the queue is not empty:
+            # for each key in the graph
+            # dequeue the first node from the queue
+            # if that node has not been visited
+            # mark as visited (we add to set and print at end)
+            # then, put all of it's children into the queue
 
+
+    def depth_first_traversal(self, start_vertex):
+        # dft for graphs is very close to bft for graphs
+        # the difference is that we use stack in dft and queue in bft
+        # this also means we add/remove from the front/index[0] in bft
+        # but add/remove to the end in dft
         stack = []  # initialize a queue
         stack.append(start_vertex)
 
@@ -125,8 +157,6 @@ class Graph:
         #     print(tracker)
 
 
-
-
-g = Graph(graph2)
-# g.breadth_first_traversal("1")
-g.depth_first_traversal("1")
+g = Graph(graph1)
+g.breadth_first_traversal("0")
+#g.depth_first_traversal("2")
