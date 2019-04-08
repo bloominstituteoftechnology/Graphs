317ebcd2f907824dc0c8aeaed86fcf65a0e05a45
jamespagedev <jamespagedev@outlook.com>
2019-04-08 07:12:04 -0700

    graph/src/graph - added methods and stretch throw vertex error

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -5,5 +5,16 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        pass  # TODO
+    def __init__(self, vertices={}):
+        self.vertices = vertices
+
+    def add_vertex(self, verNum):
+        self.vertices[verNum] = set()
+
+    def add_directed_edge(self, verNum, edgeNum):
+        if verNum not in self.vertices: # Stretch, O(1)
+            raise ValueError("No %s vertex" % (verNum))
+        elif edgeNum not in self.vertices: # Stretch, O(1)
+            raise ValueError("No %s vertex" % (edgeNum))
+        self.vertices[verNum].add(edgeNum)
+
