6de0c4a30154bab2e4b334dc738e45eb4ee28c39
Nikita Thomas <nikcxz@gmail.com>
2019-04-08 17:21:26 -0400

    Graph class

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -6,4 +6,31 @@ Simple graph implementation
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
-        pass  # TODO
+        self.vertices = {}
+
+    def addVertex(self, num):
+        if num not in self.vertices:
+            self.vertices[num] = set()
+
+    def addSingleEdge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+
+
+    def addDoubleEdge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v2].add(v1)
+            self.vertices[v1].add(v2)
+
+graph = Graph()  # Instantiate your graph
+graph.addVertex('0')
+graph.addVertex('1')
+graph.addVertex('2')
+graph.addVertex('3')
+graph.addDoubleEdge('0', '1')
+graph.addDoubleEdge('0', '3')
+print(graph.vertices)
+
+
+
+
