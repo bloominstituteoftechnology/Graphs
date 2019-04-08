5688ac064187f723b6aa291e139f8054d26a4360
Nikita Thomas <nikcxz@gmail.com>
2019-04-08 18:04:29 -0400

    breadth first traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -2,17 +2,36 @@
 Simple graph implementation
 """
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.items.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.items)
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
-    def addVertex(self, num):
+    def add_vertex(self, num):
         if num not in self.vertices:
             self.vertices[num] = set()
 
-    def addSingleEdge(self, v1, v2):
+    def add_directed_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
 
@@ -22,15 +41,38 @@ class Graph:
             self.vertices[v2].add(v1)
             self.vertices[v1].add(v2)
 
-graph = Graph()  # Instantiate your graph
-graph.addVertex('0')
-graph.addVertex('1')
-graph.addVertex('2')
-graph.addVertex('3')
-graph.addDoubleEdge('0', '1')
-graph.addDoubleEdge('0', '3')
-print(graph.vertices)
+    def bft(self, node):
+        q = Queue()
+        visited = set()
+        q.enqueue(node)
+
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+
+                for nextVertice in self.vertices:
+                    q.enqueue(nextVertice)
+
+
+graph = Graph()
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_vertex('4')
+graph.add_vertex('5')
+graph.add_vertex('6')
+
+graph.add_directed_edge('1', '2')
+graph.add_directed_edge('1', '3')
+graph.add_directed_edge('2', '4')
+graph.add_directed_edge('3', '1')
+graph.add_directed_edge('3', '4')
+graph.add_directed_edge('3', '5')
+graph.add_directed_edge('5', '6')
 
 
 
+graph.bft('1')
 
