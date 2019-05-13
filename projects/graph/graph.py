a7d0713c915612b03d58d4e5201a4004225da657
Leianne Louis <leiannelouis@Leiannes-MacBook-Pro.local>
2019-05-13 16:02:03 -0400

    completed portion of graph.py

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -11,12 +11,15 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)    
+        else:
+            raise IndexError('That vertex does not exists!')      
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
