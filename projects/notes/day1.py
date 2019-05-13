073212ec35a9499b5313550f950bd3782d3e0ff4
Matt Basile <matthewbasile2@gmail.com>
2019-05-13 18:08:20 -0400

    All caught up

---
+++ projects/notes/day1.py
@@ -0,0 +1,7 @@
+#Graphs are a data structures used to represent relationships
+#Nodes in a graph are called vertexes, connections between verts are links
+#Network a good indicator for graphs
+
+
+#Adjacency Matrix vs Lists
+
