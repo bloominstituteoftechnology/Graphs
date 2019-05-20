494c1e266b154a0ed7f2a370f88834f992a52e83
Jacob Bryan <jtb36688@gmail.com>
2019-05-20 18:48:58 -0400

    Jbryan Monday Graphs

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -8,21 +8,14 @@ class Graph:
     def __init__(self):
         self.vertices = {}
     def add_vertex(self, vertex):
-        """
-        Add a vertex to the graph.
-        """
-        pass  # TODO
+        self.vertices(vertex_id) = set()
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices(v1).add(v2)
+        else:
+            raise IndexError("One or more vertices supplied do not exist")
     def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
