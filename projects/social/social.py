3f3c4c3c3809e8f16eb0c99711bde7eb83e6de3f
Sergey Osipyan <osipyansergey@gmail.com>
2019-05-21 17:10:37 -0700

    MVP

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,18 @@
 import random
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -34,29 +47,31 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
         self.lastID = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
-        for i in range(numUsers):
+        for i in range(0, numUsers):
             self.addUser(f"User {i + 1}")
-        # Create friendships
+        # Generate all possible friendship combinations
         possibleFriendships = []
+        # Avoid duplicates by ensuring the first number is always smaller than the second number
         for userID in self.users:
             for friendID in range(userID + 1, self.lastID + 1):
-                possibleFriendships.append((userID, friendID))
+                possibleFriendships.append((userID,friendID))
+        # Shuffle the possible frienships (use Fisher-Yates)
         random.shuffle(possibleFriendships)
-        # print(possibleFriendships)
-        for i in range(numUsers * avgFriendships // 2):
+        # Create friendships for the first X pairs of the list
+        # X = Friendships_needed
+        # Friendships_needed = numusers * (avgFriendships // 2)
+        # Need to divide by 2 since each addFriendship() creates 2 friendships
+        for i in range(numUsers * (avgFriendships // 2)):
             friendships = possibleFriendships[i]
             self.addFriendship(friendships[0], friendships[1])
 
@@ -71,6 +86,23 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        queue = Queue()
+        queue.enqueue([userID])
+
+        while queue.size() > 0:
+            path = queue.dequeue()
+            friendID = path[-1]
+
+            if friendID not in visited:
+                visited[friendID] = path
+
+                for next_friendID in self.friendships[friendID]:
+                    new_path = path + [next_friendID]
+                    queue.enqueue(new_path)
+        
+        return visited
+
+
         return visited
 
 
@@ -80,3 +112,4 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
+
