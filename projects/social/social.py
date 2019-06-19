eed0890a8d4ca4465e52b4dc85895bd0fe356553
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-19 17:03:22 -0600

    Add BFS skeleton to getAllSocialPaths method.

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,5 +1,33 @@
 import random
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -61,7 +89,6 @@ class SocialGraph:
         # ------------------
         # Create while loop that goes while count < numUsers * avgFriendships
         count = 0
-        print((numUsers * avgFriendships) // 2)
         while count < (numUsers * avgFriendships) // 2:
             # add a random friend to a random user, checking first to see if they are already friends or if the user is trying to befriend himself
             friend_key = random.randint(1, numUsers)
@@ -80,18 +107,46 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
         
-        
-        
-        
-        
-        return visited
+        # Store userID pointer in starting_vertex property for sake of using graph terminology
+        starting_vertex = userID
+        # Create an empty set to store visited nodes
+        visited = {}
+        # Create an empty connections dictionary
+        connections = {}
+        # Create an empty Queue and enqueue A PATH TO the starting vertex
+        queue = Queue()
+        queue.enqueue([starting_vertex])
+        # While the queue is not empty...
+        while queue.size() > 0:
+            # Dequeue the first PATH
+            path = queue.dequeue()
+            # GRAB THE VERTEX FROM THE END OF THE PATH
+            vertex = path[-1]
+            # If that vertex has not been visited...
+            if vertex not in visited:
+                # Mark it as visited
+                visited[userID] = path
+                
+                # IF VERTEX = TARGET, RETURN PATH
+                if vertex == destination_vertex:
+                    return path
+            
+                # Then add A PATH TO all of its neighbors to the back of the queue
+                for friend in self.vertices[vertex]:
+                    if friend not in visited:
+                        # Copy the path
+                        path_copy = path.copy()
+                        # Append neighbor to the back of the copy
+                        path_copy.append(neighbor)
+                        
+                        # Enqueue copy
+                        queue.enqueue(path_copy)
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 4)
-    print(sg.friendships)
+    #print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
