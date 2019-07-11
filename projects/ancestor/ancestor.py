0404b7ce6878595d3bf65578d645828bf10998c7
ItelSunday <iteldomingotech@gmail.com>
2019-07-11 00:06:07 -0700

    trying on ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,6 +1,24 @@
 class Stack():
     def __init__(self):
       self.stack = []
+      
+    def push(self, value):
+        self.stack.append(value)
+        
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        
+    def size(self):
+        return len(self.stack)
+    
+class Stack():
+    def __init__(self):
+      self.stack = []
+
+        
+        
+    
 
 
 def earliest_ancestor(ancestors, starting_node):
