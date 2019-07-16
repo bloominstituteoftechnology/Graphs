fc0fd83f865d4ed6ca9bd2fb61f608f6824edbb7
unknown <kyleteeter12@gmail.com>
2019-07-15 23:12:30 -0500

    Add vertices and direct edges

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,10 +1,20 @@
 
 def earliest_ancestor(ancestors, starting_node):
+    def__int__(self):
+        self.vertices = {}
+
+    #Add a vertex to the graph 
+    def add_vertices(self, vertex):
+        self.vertices[vertex] = set()
+    # Add  a directed edge to the
+    def add_edge(self, v1, v2):
+        if v1 not in self.vertices:
+            self.add_vertices(v1);
+        if v2 not in self.vertices:
+            self.add_vertices(v2);
+        self.vertices[v1].add(v2)
+    # Build graph
     
-    Add a vertex to the graph
-    Add a directed edge to the 
-    Build graph
-    
-    BFT
-    Create Count
-    
+    # BFT
+    # Create Count
+
