7a3b9468bd9fd1015b07fd0b7105cec6a498cc47
Eddy <edward.gonzalez9708@gmail.com>
2019-07-17 18:17:53 -0400

    Modified spacing of social.py and its read me file.

--- projects/social/README.md
+++ projects/social/README.md
@@ -57,4 +57,3 @@ Note that in this sample, Users 3, 4 and 9 are not in User 1's extended social n
 1. You might have found the results from question #2 above to be surprising. Would you expect results like this in real life? If not, what are some ways you could improve your friendship distribution model for more realistic results?
 
 2. If you followed the hints for part 1, your `populateGraph()` will run in O(n^2) time. Refactor your code to run in O(n) time. Are there any tradeoffs that come with this implementation?
-
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,72 +1,78 @@
-
-
 class User:
-    def __init__(self, name):
-        self.name = name
+  def __init__(self, name):
+    self.name = name
 
 class SocialGraph:
-    def __init__(self):
-        self.lastID = 0
-        self.users = {}
-        self.friendships = {}
+  def __init__(self):
+    self.lastID = 0
+    self.users = {}
+    self.friendships = {}
 
-    def addFriendship(self, userID, friendID):
-        """
-        Creates a bi-directional friendship
-        """
-        if userID == friendID:
-            print("WARNING: You cannot be friends with yourself")
-        elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
-            print("WARNING: Friendship already exists")
-        else:
-            self.friendships[userID].add(friendID)
-            self.friendships[friendID].add(userID)
+  def addFriendship(self, userID, friendID):
+    """
+    Creates a bi-directional friendship
+    """
+    
+    if userID == friendID:
+      print("WARNING: You cannot be friends with yourself")
+    elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
+      print("WARNING: Friendship already exists")
+    else:
+      self.friendships[userID].add(friendID)
+      self.friendships[friendID].add(userID)
 
-    def addUser(self, name):
-        """
-        Create a new user with a sequential integer ID
-        """
-        self.lastID += 1  # automatically increment the ID to assign the new user
-        self.users[self.lastID] = User(name)
-        self.friendships[self.lastID] = set()
+  def addUser(self, name):
+    """
+    Create a new user with a sequential integer ID
+    """
 
-    def populateGraph(self, numUsers, avgFriendships):
-        """
-        Takes a number of users and an average number of friendships
-        as arguments
+    # Automatically increment the ID to assign the new user
+    self.lastID += 1
 
-        Creates that number of users and a randomly distributed friendships
-        between those users.
+    self.users[self.lastID] = User(name)
+    self.friendships[self.lastID] = set()
 
-        The number of users must be greater than the average number of friendships.
-        """
-        # Reset graph
-        self.lastID = 0
-        self.users = {}
-        self.friendships = {}
-        # !!!! IMPLEMENT ME
+  def populateGraph(self, numUsers, avgFriendships):
+      """
+      Takes a number of users and an average number of friendships
+      as arguments
 
-        # Add users
+      Creates that number of users and a randomly distributed friendships
+      between those users.
 
-        # Create friendships
+      The number of users must be greater than the average number of friendships.
+      """
+      
+      # Reset graph
+      self.lastID = 0
+      self.users = {}
+      self.friendships = {}
+      
+      # !!!! IMPLEMENT ME
 
-    def getAllSocialPaths(self, userID):
-        """
-        Takes a user's userID as an argument
+      # Add users
 
-        Returns a dictionary containing every user in that user's
-        extended network with the shortest friendship path between them.
+      # Create friendships
 
-        The key is the friend's ID and the value is the path.
-        """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
-        return visited
+  def getAllSocialPaths(self, userID):
+    """
+    Takes a user's userID as an argument
 
+    Returns a dictionary containing every user in that user's
+    extended network with the shortest friendship path between them.
+
+    The key is the friend's ID and the value is the path.
+    """
+
+    # Note that this is a dictionary, not a set
+    visited = {}  
+    
+    # !!!! IMPLEMENT ME
+    return visited
 
 if __name__ == '__main__':
-    sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print(sg.friendships)
-    connections = sg.getAllSocialPaths(1)
-    print(connections)
+  sg = SocialGraph()
+  sg.populateGraph(10, 2)
+  print(sg.friendships)
+  connections = sg.getAllSocialPaths(1)
+  print(connections)
