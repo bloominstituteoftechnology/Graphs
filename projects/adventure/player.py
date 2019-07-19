10fdd8f73260f91b0b717017b22ce06e3c4f8d9e
Eddy <edward.gonzalez9708@gmail.com>
2019-07-18 21:18:54 -0400

    Modified spacing for adventure exercise files.

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -1,12 +1,15 @@
 class Player:
-    def __init__(self, name, startingRoom):
-        self.name = name
-        self.currentRoom = startingRoom
-    def travel(self, direction, showRooms = False):
-        nextRoom = self.currentRoom.getRoomInDirection(direction)
-        if nextRoom is not None:
-            self.currentRoom = nextRoom
-            if (showRooms):
-                nextRoom.printRoomDescription(self)
-        else:
-            print("You cannot move in that direction.")
+  def __init__(self, name, startingRoom):
+    self.name = name
+    self.currentRoom = startingRoom
+  
+  def travel(self, direction, showRooms = False):
+    nextRoom = self.currentRoom.getRoomInDirection(direction)
+    
+    if nextRoom is not None:
+      self.currentRoom = nextRoom
+      
+      if (showRooms):
+        nextRoom.printRoomDescription(self)
+    else:
+      print("You cannot move in that direction.")
