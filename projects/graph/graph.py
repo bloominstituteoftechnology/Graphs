876cba8a21578b62cba23b1bc3208ea13f044814
AmarachiOE <ao.ejiawoko@gmail.com>
2019-08-13 01:12:53 -0700

    bft() and dft()

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,10 +1,13 @@
 """
 Simple graph implementation
 """
+# using visited = set() and visited.add(vertex) adds to the set but sorts the values (doesn't maintain order)
 from util import Stack, Queue  # These may come in handy
+
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,8 +16,10 @@ class Graph:
         Add a vertex to the graph.
         """
         # TODO
-        self.vertices[vertex] = set()
-
+        if not vertex in self.vertices:
+            self.vertices[vertex] = set()
+        else:
+            print("Warning: Vertex already exists.")
 
     def add_edge(self, v1, v2):
         """
@@ -23,21 +28,75 @@ class Graph:
         # TODO
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
-            # self.vertices[v2].add(v1) # bi-directional, necessary for this project?
+            # self.vertices[v2].add(v1) #  for if bi-directional edge
+        else:
+            print("Warning: One or more of the vertices do not exist")
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # TODO
+        # Create empty queue, enqueue the starting vertex - FIFO
+        # Create a set to store the visited vertices
+        # While queue is not empty...
+            # Dequeue the first vertex
+            # If that vertex has not been visited...
+                # Mark visited
+                 # Add all of its neighbors (node's siblings/share same parent/those nodes on the same level) to the back of queue
+        q = Queue()
+        q.enqueue(starting_vertex)
+        visited = []
+        while q.size() > 0:
+            v = q.dequeue()
+            if v not in visited:
+                print("now visited: ", v)
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    q.enqueue(next_vert)
+
+        print("BFT: ", visited)
+        return visited
+
+        # while q.size() > 0:
+        #     v = q.queue[0]
+        #     for vertex in self.vertices[v]:
+        #         if vertex not in visited:
+        #             q.enqueue(vertex)
+        #             visited.add(vertex)
+        #     q.dequeue()
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # TODO
+        # Create an empty stack and push the starting vertex ID
+        # Create a Set to store visited vertices
+        # While the stack is not empty...
+            # Pop the first vertex
+            # If that vertex has not been visited...
+                # Mark it as visited...
+                # Then add all of its neighbors to the top/end of the stack
+
+        s = Stack()
+        s.push(starting_vertex)
+        visited = []
+
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print("now visited: ", v)
+                visited.append(v)
+
+                for next_vert in self.vertices[v]:
+                    s.push(next_vert)
+
+        print("DFT: ", visited)
+        return visited
 
     def dft_recursive(self, starting_vertex):
         """
@@ -62,9 +121,6 @@ class Graph:
         depth-first order.
         """
         pass  # TODO
-
-
-
 
 
 if __name__ == '__main__':
@@ -101,7 +157,7 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
-    # graph.dft(1)
+    graph.dft(1)
 
     '''
     Valid BFT paths:
@@ -118,7 +174,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    # graph.bft(1)
+    graph.bft(1)
 
     '''
     Valid DFT recursive paths:
