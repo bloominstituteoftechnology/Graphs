0599a7d22434a4311e06f6c0ec6d7028855b09c3
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-09-09 19:34:53 -0400

    graph.py complete

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,56 +3,84 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
-        """
-        Add a vertex to the graph.
-        """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            print('Invalid Vertex')
+
     def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        q = Queue()
+        v = set()
+        q.enqueue(starting_vertex)
+
+        while q.size() > 0:
+            x = q.dequeue()
+
+            if x not in v:
+                print("BFT", v)
+                v.add(x)
+                for n in self.vertices[x]:
+                    q.enqueue(n)
+
     def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
-    def dft_recursive(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        This should be done using recursion.
-        """
-        pass  # TODO
+        s = Stack()
+        v = set()
+        s.push(starting_vertex)
+
+        while s.size() > 0:
+            x = s.pop()
+            if x not in v:
+                print("DFT", x)
+                v.add(x)
+                for n in self.vertices[x]:
+                    s.push(n)
+
+    def dft_recursive(self, starting_vertex, v=None):
+        if v is None:
+            v = set()
+        print('DFT_R', starting_vertex)
+        v.add(starting_vertex)
+
+        for x in self.vertices[starting_vertex]:
+            if x not in v:
+                self.dft_recursive(x, v)
+
     def bfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing the shortest path from
-        starting_vertex to destination_vertex in
-        breath-first order.
-        """
-        pass  # TODO
+        q = Queue()
+        v = set()
+        q.enqueue([starting_vertex])
+        while q.size() > 0:
+            path = q.dequeue()
+            node = path[-1]
+            if node == destination_vertex:
+                return path
+            for x in self.vertices[node]:
+                new_path = path.copy()
+                new_path.append(x)
+                q.enqueue(new_path)
+
     def dfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-        """
-        pass  # TODO
+        s = Stack()
+        s.push((starting_vertex, [starting_vertex]))
 
-
-
+        while s.size() > 0:
+            v = s.pop()
+            for x in self.vertices[v[0]]:
+                if x == destination_vertex:
+                    return v[1] + [x]
+                else:
+                    s.push((x, v[1] + [x]))
 
 
 if __name__ == '__main__':
@@ -80,7 +108,7 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    print("verticles", graph.vertices)
 
     '''
     Valid DFT paths:
@@ -121,11 +149,11 @@ if __name__ == '__main__':
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    print(graph.bfs(1, 6))
+    print("BFS", graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    print(graph.dfs(1, 6))
+    print("DFS", graph.dfs(1, 6))
