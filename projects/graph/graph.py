3b00a31df51027af4d026d3877ef0659c17faf4f
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-09-30 12:45:58 -0400

    finished add_vertex and add_edge

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -7,16 +7,19 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1] = v2
+    #
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
