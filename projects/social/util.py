16a6614759adbe1553c8bf473aa0416ad95901c5
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-09 16:36:28 -0400

    made size a @property

--- projects/social/util.py
+++ projects/social/util.py
@@ -1,26 +1,35 @@
 class Queue():
     def __init__(self):
         self.queue = []
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
-        if self.size() > 0:
+        if self.size > 0:
             return self.queue.pop(0)
         else:
             return None
+
+    @property
     def size(self):
         return len(self.queue)
 
 class Stack():
+
     def __init__(self):
-        self.stack = []
+        self.stack = list()
+
     def push(self, value):
         self.stack.append(value)
+
     def pop(self):
-        if self.size() > 0:
+        if self.size > 0:
             return self.stack.pop()
         else:
             return None
+
+    @property
     def size(self):
         return len(self.stack)
 
