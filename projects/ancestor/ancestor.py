eb9c300c28bcb1fcf09c828d85c0db9791e808eb
PetejStone <petejstoney@gmail.com>
2019-11-05 15:12:14 -0800

    testing

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -20,19 +20,36 @@ def earliest_ancestor(ancestors, starting_node):
    
     visited = set()
     # While the queue is not empty...
-    while q.size() > 0:
-        # Dequeue the first vertex
-        v = q.dequeue()
-        # print(graph.vertices[1])
-        # If that vertex has not been visited...
-        if v not in visited:
-            # Mark it as visited...
-            visited.add(v)
-            # print(visited)
-            # print(graph.vertices[1])
-            for neighbor in graph.vertices[v]:
-                # print(neighbor)
-                q.enqueue(neighbor)
+    # print(graph.vertices)
+    # print(graph.vertices)
+    sample = graph.vertices[5]
+    # print(sample)
+    check_number = ''
+    for d in graph.vertices:
+        if graph.vertices[d] != set():
+            
+            print(f'{d}: {graph.vertices[d]}')   
+            check_number = graph.vertices[d].pop()
+            print(check_number)
+            if check_number == starting_node:
+                return d
+    return -1
+            
+
+        
+    # while q.size() > 0:
+    #     # Dequeue the first vertex
+    #     v = q.dequeue()
+    #     # print(graph.vertices[1])
+    #     # If that vertex has not been visited...
+    #     if v not in visited:
+    #         # Mark it as visited...
+    #         visited.add(v)
+    #         # print(v)
+    #         # print(graph.vertices[1])
+    #         for neighbor in graph.vertices[v]:
+    #             # print(neighbor)
+    #             q.enqueue(neighbor)
 
                
             # Then add all of its neighbors to the back of the queue
