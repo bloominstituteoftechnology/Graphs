15073b27cb1a6b7c13912f569b1c9e1d4d416f1a
PWalis <pwilky1016@gmail.com>
2019-11-05 14:44:52 -0700

    Completed MVP for today, earliest_ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,60 @@
+from graph import Graph
+from util import Queue
+
+
+def bfs(graph, starting_vertex):
+    """
+    Return a list containing the longest path 
+    from starting_vertex  
+    """
+    earliest_an = None
+    # Create an empty queue and enqueue A PATH TO the starting vertex ID
+    q = Queue()
+    initial_path = [starting_vertex]
+    q.enqueue(initial_path)
+    # Create a Set to store visited vertices
+    visited = set()
+    # While the queue is not empty...
+    while q.size() > 0:
+        # Dequeue the first PATH
+        path = q.dequeue()
+        # save the length of the path
+        path_length = len(path)
+        # Grab the last vertex from the PATH
+        last_vert = path[-1]
+        # If that vertex has not been visited...
+        if last_vert not in visited:
+            # Mark it as visited...
+            visited.add(last_vert)
+            # Then add A PATH TO its neighbors to the back of the 
+            for v in graph.vertices[last_vert]:
+                # COPY THE PATH
+                path_copy = path[:]
+                # APPEND THE NEIGHOR TO THE BACK
+                path_copy.append(v)
+                q.enqueue(path_copy)
+                # If the new path is longer than previous path 
+                # Save the longer path as earliest_ancestor
+                if len(path_copy) > path_length:
+                    earliest_an = path_copy
+    if earliest_an:
+        return earliest_an[-1]
+    return -1
+        
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+    for a in ancestors:
+        # Add all vertices to graph
+        graph.add_vertex(a[0])
+        graph.add_vertex(a[1])
+    for a in ancestors:
+        # Create child to parent relationship 
+        graph.add_edge(a[1], a[0])
+    return bfs(graph, starting_node)
+
+
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(test_ancestors, 1))
---
+++ projects/ancestor/graph.py
@@ -0,0 +1,23 @@
+"""
+Simple graph implementation
+"""
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+        
+    def add_vertex(self, vertex):
+        """
+        Add a vertex to the graph.
+        """
+        self.vertices[vertex] = set()
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError('Vertex does not exist')
