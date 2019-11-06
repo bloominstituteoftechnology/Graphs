567065ec4b34c2b54474fb054f2aea3839b62ef1
PetejStone <petejstoney@gmail.com>
2019-11-06 13:41:27 -0800

    building graph working

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,8 +1,14 @@
-
+import random
+import sys
+sys.path.append('../graph')
+from graph import Graph
+from util import Queue, Stack
 
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -47,8 +53,33 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        # call addUser() until our number of users is numUsers
+        for i in range(numUsers):
+            self.addUser(f"User {i+1}")
 
-        # Create friendships
+        # Create random friendships
+        # totalFriendships = avgFriendships * numUsers
+        # Generate a list of all possible friendships
+        possibleFriendships = []
+        # Avoid dups by ensuring the first ID is smaller than the second
+        for userID in self.users:
+            for friendID in range(userID + 1, self.lastID + 1):
+                possibleFriendships.append( (userID, friendID) )
+
+        # Shuffle the list
+        random.shuffle(possibleFriendships)
+        # print("random friendships:")
+        # print(possibleFriendships)
+
+        # Slice off totalFriendships from the front, create friendships
+        totalFriendships = avgFriendships * numUsers // 2
+        # print(f"Friendships to create: {totalFriendships}\n")
+        for i in range(totalFriendships):
+            friendship = possibleFriendships[i]
+            self.addFriendship( friendship[0], friendship[1] )
+
+
+
 
     def getAllSocialPaths(self, userID):
         """
@@ -59,6 +90,28 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        graph = Graph()
+        connections = []
+        
+        
+        for i in range(1, len(sg.friendships) + 1):
+            if sg.friendships[i] != set():
+                #popping off each item and getting ALL connections
+                while len(sg.friendships[i]) > 0:
+                    friend = sg.friendships[i].pop()
+                    connections.append((i, friend))
+                
+        print(connections)
+
+        #building graph
+        for d in range(1,11):
+            graph.add_vertex(d)
+
+        #making connections
+        for pair in connections:
+            graph.add_edge(pair[0],pair[1])
+
+        print(f'Graph Vertices {graph.vertices}')
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
         return visited
@@ -67,6 +120,9 @@ class SocialGraph:
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populateGraph(10, 2)
-    print(sg.friendships)
+    # print("USERS:")
+    # print(sg.users)
+    # print("FRIENDSHIPS:")
+    # print(sg.friendships)
     connections = sg.getAllSocialPaths(1)
     print(connections)
