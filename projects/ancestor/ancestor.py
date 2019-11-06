8fc3b0eee6daaa7a88bb55129f930d9eb17c602e
PetejStone <petejstoney@gmail.com>
2019-11-05 17:47:32 -0800

    added comments

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,5 @@
+
+# Importing graph
 import sys
 sys.path.append('../graph')
 from graph import Graph
@@ -5,94 +7,42 @@ from util import Queue, Stack
 
 
 def earliest_ancestor(ancestors, starting_node):
+    # Building graph from data coming in
     graph = Graph()
     for d in range(1,12):
         graph.add_vertex(d)
     for pair in ancestors:
         graph.add_edge(pair[0],pair[1])
    
-    # # Create an empty queue and enqueue the starting vertex ID
-    # print(starting_node)
-    
     q = Queue()
     q.enqueue(starting_node)
-    # Create a Set to store visited vertices
-   
-    visited = set()
-    # While the queue is not empty...
-    # print(graph.vertices)
-    # print(graph.vertices)
-    sample = graph.vertices[5]
-    # print(sample)
-    check_number = ''
-    # print(graph.vertices)
-    # removed = ''
-    # print(graph.vertices)
+
+    check_number = ''   #variable to check against
+
+ 
     print(f'STARTING NODE  {starting_node}')
-    check_array = []
-    # print(graph.vertices[2])
-    parents = []
-    children = []
-    for d in graph.vertices:
-        if graph.vertices[d] != set():
-            
-            # print(f'{d}: {graph.vertices[d]}')
-            
-            while len(graph.vertices[d]) > 0:
-                check_number = graph.vertices[d].pop()
-                # print(f'popped of {check_number}')
-                check_array.append((check_number, d))
-                children.append(check_number)
-                parents.append(d)
-            #    checked[check_number] = d
-            # print(check_array)
-            # print(parents)
-            # print(check_array)
+    check_array = [] #check array to check against
+    children = [] #children array to check against
+    for d in graph.vertices: #for every instance in the graph
+        if graph.vertices[d] != set():  # if that value != set()   aka if it has nodes pointing to it, if it has parents
+            while len(graph.vertices[d]) > 0: #while the length of each set is >0
+                check_number = graph.vertices[d].pop() #pop of each set item (each child it points to)
+  
+                check_array.append((check_number, d)) #add that number to the checkarray and append it's parent as the second argument
+                children.append(check_number) #just append that popped of item into the chldren array -- used to check if an item is NOT in it, ergo it is a top node
+         
+      
     print(check_array)
-    for x in check_array:
-        if x[0] == starting_node:
+    for x in check_array: # for every instance in check array
+        if x[0] == starting_node: #if the first item (the child) exists, then you will want to grab the parent and call recursion to climb up the tree
+
             print(f'true, it is here at {x[1]} and does equal {starting_node}')
-            if x[1] in children:
-                # new_pass = x[1]
+            if x[1] in children: #if the parent node is a child (not a top node)
                 print(f'passing in {x[1]}')
-                return earliest_ancestor(ancestors,x[1])
+                return earliest_ancestor(ancestors,x[1]) #call recursion on that node
             else:
-                return x[1]
-    return -1
+                return x[1] #if it is not a child, you have reached the top
+    return -1 #if none above, it does not exists
                 
                 
                     
-                    # print(x)
-            # if starting_node in check_array:
-            #     for x in check_array:
-                    # if x == starting_node:
-                    #     print(x)
-            #     print(d)
-            #     if check_number == starting_node:
-            #         break
-                
-            # # print(f'current is {starting_node}')
-            # if check_number == starting_node:
-            #     # print(f'passing in {d}')
-            #     earliest_ancestor(ancestors, d)
-            
-    # return -1
-            
-
-        
-    # while q.size() > 0:
-    #     # Dequeue the first vertex
-    #     v = q.dequeue()
-    #     # print(graph.vertices[1])
-    #     # If that vertex has not been visited...
-    #     if v not in visited:
-    #         # Mark it as visited...
-    #         visited.add(v)
-    #         # print(v)
-    #         # print(graph.vertices[1])
-    #         for neighbor in graph.vertices[v]:
-    #             # print(neighbor)
-    #             q.enqueue(neighbor)
-
-               
-            # Then add all of its neighbors to the back of the queue
