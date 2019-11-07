833ac9e7cb5e69a1cc9a75cd55203288387a447d
hectorsvill <hectorstevenvillasano@gmail.com>
2019-11-07 09:26:31 -0800

    social

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,9 +1,10 @@
-
+import random
 
 class User:
     def __init__(self, name):
         self.name = name
-
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -47,22 +48,34 @@ class SocialGraph:
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # add users
+        # Add users
         # call addUser() until our number of users is numUsers
         for i in range(numUsers):
-            self.addUser(f"user: {i}")
+            self.addUser(f"User {i+1}")
+
+        # Create random friendships
+        # totalFriendships = avgFriendships * numUsers
+        # Generate a list of all possible friendships
+        possibleFriendships = []
+        # Avoid dups by ensuring the first ID is smaller than the second
+        for userID in self.users:
+            for friendID in range(userID + 1, self.lastID + 1):
+                possibleFriendships.append( (userID, friendID) )
+
+        # Shuffle the list
+        random.shuffle(possibleFriendships)
+        print("random friendships:")
+        print(possibleFriendships)
+
+        # Slice off totalFriendships from the front, create friendships
+        totalFriendships = avgFriendships * numUsers // 2
+        print(f"Friendships to create: {totalFriendships}\n")
+        for i in range(totalFriendships):
+            friendship = possibleFriendships[i]
+            self.addFriendship( friendship[0], friendship[1] )
 
 
-        
 
-        # create random friendships
-
-        # avgFriendships = totalFriendships / nusUsers
-        # totalFriendships = avgFriendships * / numUsers
-
-        # Add users
-
-        # Create friendships
 
     def getAllSocialPaths(self, userID):
         """
@@ -80,10 +93,11 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populateGraph(10, 2)
-    print("USERS: ")
-    print("Friendships: ")
-    print("social: ")
+    sg.populateGraph(11, 3)
+    print("USERS:")
+    print(sg.users)
+    print("FRIENDSHIPS:")
     print(sg.friendships)
-    connections = sg.getAllSocialPaths(1)
-    print(connections)
+    # connections = sg.getAllSocialPaths(1)
+    # print(connections)
+
