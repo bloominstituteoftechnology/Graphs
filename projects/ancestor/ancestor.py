c57ca39157455f76858402cb700b1d63b29e4daf
Zolton <gmzesq@gmail.com>
2019-12-10 14:39:22 -0500

    Starting work on day 2 project

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,8 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # 1) Build ancestors data into a set or whatever its called
+    # 2) Traverse from starting node to find longest string/path
+    # 3) There is no path or destionation - just traverse the entire 
+    # thing and find the longest possible string
+
+    
