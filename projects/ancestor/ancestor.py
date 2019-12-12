3c6e376782a2a43b5c7830805ece85d0f0282aac
jtkernan7 <jtkernan7@gmail.com>
2019-12-12 16:30:47 -0500

    sprint startk

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,4 @@
-from util import Stack, Queue  # These may come in handy
+from collections import deque
 
 class Graph:
 
@@ -20,38 +20,44 @@ class Graph:
             self.vertices[v1].add(v2)
         else:
             raise IndexError("That vertex does not exist")
-
+ 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
         return self.vertices[vertex_id]
+        
 
 
 def earliest_ancestor(ancestors, starting_node):
 
-    # Build the graph
-    graph = Graph()
-    for pair in ancestors:
-        graph.add_vertex(pair[0])
-        graph.add_vertex(pair[1])
-        # Build edges in reverse
-        graph.add_edge(pair[1], pair[0])
+    # Build the fam tree graph
 
-    # Do a BFS (storing the path)
-    q = Queue()
-    q.enqueue([starting_node])
-    max_path_len = 1
-    earliest_ancestor = -1
-    while q.size() > 0:
-        path = q.dequeue()
-        v = path[-1]
-        # If the path is longer or equal and the value is smaller, or if the path is longer)
-        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
-            earliest_ancestor = v
-            max_path_len = len(path)
-        for neighbor in graph.vertices[v]:
-            path_copy = list(path)
-            path_copy.append(neighbor)
-            q.enqueue(path_copy)
-    return earliest_ancestor
+    fam = Graph()
+    for pair in ancestors:
+        fam.add_vertex(pair[0])
+        fam.add_vertex(pair[1])
+        fam.add_edge(pair[1], pair[0])
+ 
+
+
+    #initialize
+    ancestor = starting_node
+    generations = 0
+    stack = deque([(ancestor, generations)])
+    
+    while len(stack) > 0:
+        (child, childs_generation) = stack.pop()
+
+        if childs_generation > generations:
+            ancestor = child
+            generations = childs_generation
+
+        elif childs_generation == generations and child < ancestor:
+           ancestor = child
+
+        parents = fam.get_neighbors(child)
+        stack.extend([(parent, childs_generation + 1) for parent in parents])
+        
+
+    return -1 if generations == 0 else ancestor
