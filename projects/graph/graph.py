65b7c7604b001e9331e2147db65e44fc0c6431e2
tania <tanislavens@yahoo.com>
2020-01-21 11:53:55 -0500

    working on graph file

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,13 +13,18 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
+        If both exist, add a connection from v1 to v2.
+        If not, raise an error via Python exception.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
     def get_neighbors(self, vertex_id):
         """
