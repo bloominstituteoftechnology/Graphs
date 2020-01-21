e0f87ca53ff7c9bb7491b105ea305310a7bc6b2d
brellin <cekakid@gmail.com>
2020-01-21 10:54:32 -0600

    index error after commit to master by accident

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -23,6 +23,8 @@ class Graph:
         """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
+        else:
+            raise IndexError('Not Found')
 
     def get_neighbors(self, vertex_id):
         """
