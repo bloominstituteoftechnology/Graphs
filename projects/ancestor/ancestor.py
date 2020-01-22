2a6c856d68c9212a7a5e1a217fde702b25bc3dc9
tania <tanislavens@yahoo.com>
2020-01-22 13:02:29 -0500

    added DFS solution to ancestor file

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -33,3 +33,37 @@ def earliest_ancestor(ancestors, starting_node):
     return earliest_ancestor
 
 # print('earliest ancestor', earliest_ancestor(ancestors_data, 8))
+
+'''
+DFS Solution:
+def earliest_ancestor(ancestors, starting_node):
+    # Build graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # Build edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    
+    # Do a BFS (storing the path)
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+
+        # If the path is longer or equal and the value is smaller, or if the path is longer
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+            
+    return earliest_ancestor
+
+'''
