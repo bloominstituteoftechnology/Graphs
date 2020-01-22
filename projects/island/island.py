e451e5d6bb4db536f08417eec45067cd3994377c
tania <tanislavens@yahoo.com>
2020-01-22 13:22:07 -0500

    added island folder

---
+++ projects/island/island.py
@@ -0,0 +1,11 @@
+# Write a function that takes a 2D binary array and returns 
+# the number of 1 islands. An island consists of 1s that are 
+# connected to the north, south, east or west. For example:
+
+# islands = [[0, 1, 0, 1, 0],
+#            [1, 1, 0, 1, 1],
+#            [0, 0, 1, 0, 0],
+#            [1, 0, 1, 0, 0],
+#            [1, 1, 0, 0, 0]]
+
+# island_counter(islands) # returns 4
