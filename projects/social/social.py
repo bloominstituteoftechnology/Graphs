8163232a42e0bdbf2bb3281995fe42560700851d
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2020-01-23 19:33:37 -0500

    added avg path to social

--- projects/social/social.py
+++ projects/social/social.py
@@ -38,11 +38,14 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
             print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -66,22 +69,35 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # Add users
+        # # Add users
         for i in range(num_users):
             self.add_user(f'User {i + 1}')
-        # Create friendships
-        possible_friendships = []
-        for user_id in self.users:
-            for friend_id in range(user_id + 1, self.last_id + 1):
-                possible_friendships.append((user_id, friend_id))
+        # # Create friendships
+        # possible_friendships = []
+        # for user_id in self.users:
+        #     for friend_id in range(user_id + 1, self.last_id + 1):
+        #         possible_friendships.append((user_id, friend_id))
+        #
+        # random.shuffle(possible_friendships)
+        #
+        # print(possible_friendships)
+        #
+        # for i in range(num_users * avg_friendships // 2):
+        #     friendship = possible_friendships[i]
+        #     self.add_friendship(friendship[0], friendship[1])
+        target_friendships = (num_users * avg_friendships)
+        total_friendships = 0
+        collisions = 0
+        while total_friendships < target_friendships:
+            # Create a random friendship
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
 
-        random.shuffle(possible_friendships)
-
-        print(possible_friendships)
-
-        for i in range(num_users * avg_friendships // 2):
-            friendship = possible_friendships[i]
-            self.add_friendship(friendship[0], friendship[1])
+        print(f'COLLISIONS: {collisions}')
 
     def get_all_social_paths(self, user_id):
         """
@@ -103,14 +119,14 @@ class SocialGraph:
         while queue.size() > 0:
             # Dequeue the first path from the queue
             path = queue.dequeue()
-            vertex = path[-1]
+            curr_friend = path[-1]
             # Check if it's been visited
-            if vertex not in visited:
+            if curr_friend not in visited:
                 # if not, mark it as visited
-                visited[vertex] = path
+                visited[curr_friend] = path
                 # When an unvisited node is found, add the path the to visited dictionary
-            # Add a pth th each neighbor to the end of the queue
-                for friend_id in self.friendships[vertex]:
+            # Add a path th each neighbor to the end of the queue
+                for friend_id in self.friendships[curr_friend]:
                     path_copy = path.copy()
                     path_copy.append(friend_id)
                     queue.enqueue(path_copy)
@@ -120,7 +136,7 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(100, 5)
+    sg.populate_graph(500, 2)
     print('-----Users-----')
     print(sg.users)
     print('-----Friendships-----')
@@ -128,3 +144,10 @@ if __name__ == '__main__':
     print('----Social Paths------')
     connections = sg.get_all_social_paths(1)
     print(connections)
+
+    connection_lens = [len(v) for v in connections.values()]
+
+    print('Lengths of all Social Paths')
+    print(connection_lens)
+    print('Average Lengths of Social Paths')
+    print(sum(connection_lens) // len(connection_lens))
