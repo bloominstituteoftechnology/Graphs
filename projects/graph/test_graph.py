b78752b8e0ae3264a4b66ca46ac39ecbcc8cbf1f
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2020-02-17 16:21:00 -0500

    Added line breaks for readability

--- projects/graph/test_graph.py
+++ projects/graph/test_graph.py
@@ -6,6 +6,7 @@ from graph import Graph
 class Test(unittest.TestCase):
     def setUp(self):
         self.graph = Graph()
+
         self.graph.add_vertex(1)
         self.graph.add_vertex(2)
         self.graph.add_vertex(3)
@@ -13,6 +14,7 @@ class Test(unittest.TestCase):
         self.graph.add_vertex(5)
         self.graph.add_vertex(6)
         self.graph.add_vertex(7)
+        
         self.graph.add_edge(5, 3)
         self.graph.add_edge(6, 3)
         self.graph.add_edge(7, 1)
@@ -25,7 +27,15 @@ class Test(unittest.TestCase):
         self.graph.add_edge(4, 6)
 
     def test_vertices(self):
-        vertices = {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
+        vertices = {
+          1: {2},
+          2: {3, 4},
+          3: {5},
+          4: {6, 7}, 
+          5: {3},
+          6: {3},
+          7: {1, 6}
+        }
         self.assertDictEqual(self.graph.vertices, vertices)
 
     def test_bft(self):
