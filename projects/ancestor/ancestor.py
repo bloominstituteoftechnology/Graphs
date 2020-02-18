20954a2e047ca39ad642dc0ff4af755ee9e20e6b
Sean Doyle <sdoylelambda@gmail.com>
2020-02-18 17:27:34 -0500

    Add vertices to graphs, displays data sets.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
+# import graph.py --vertex and edge
+from util import Graph, Stack
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # instantiat graph
+    graphs = Graph()
+    print('graphs1::', graphs.vertices)
+    # loop thru to pop graph
+    for ancestor in ancestors:
+        print('ancestor:', ancestor)
+        # graphs.append(ancestor)
+        graphs.add_vertex(ancestor)
+    print('graphs2::', graphs)
+    # dfs
+    # stack
+
+    # add starting node to stack
+
+
+
+    # UPER
+    # FIND OLDEST ANCESTOR
+    # The data is formatted as a list of (parent, child) pairs
+
+    # follow the edges - to find - vertex
