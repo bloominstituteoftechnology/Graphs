12848081a275be75d93f874f395ad8411950862c
Michael Redig <mredig@gmail.com>
2020-02-18 14:38:26 -0600

    created ancestor graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,12 +4,17 @@ from util import Stack, Queue  # These may come in handy
 class GraphNode:
     def __init__(self, value):
         self.children = set()
-        self.parent = None
+        self.parents = set()
         self.value = value
 
     def addChild(self, node):
         self.children.add(node)
-        node.parent = self
+        node.parents.add(self)
+
+    def __repr__(self):
+        children = [x.value for x in self.children]
+        parents = [x.value for x in self.parents]
+        return f"Node: {self.value} Children: {children} parents: {parents}"
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -21,15 +26,37 @@ class Graph:
             print("That node already exists!")
 
     def addEdge(self, value1, value2):
-        if value1 in self.vertices and value2 in self.vertices:
-            node1 = self.vertices[value1]
-            node2 = self.vertices[value2]
-            node1.addChild(node2)
-        else:
-            raise IndexError("That vertex doesn't exist!")
+        if value1 not in self.vertices:
+            self.addNode(value1)
+        if value2 not in self.vertices:
+            self.addNode(value2)
+        node1 = self.vertices[value1]
+        node2 = self.vertices[value2]
+        node1.addChild(node2)
 
     def getNode(self, value):
         return self.vertices.get(value, None)
 
+    # def ancestorPaths(self, value):
+    #     node = self.getNode(value)
+    #     if node is None:
+    #         return None
+    #     s = Stack()
+    #     stack.push([value])
+
+    #     while s.size() > 0:
+    #         path = s.pop()
+
+
+    def __repr__(self):
+        for value in self.vertices:
+            print(self.vertices[value])
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = Graph()
+    for pair in ancestors:
+        parentValue = pair[0]
+        childValue = pair[1]
+        g.addEdge(parentValue, childValue)
+    print(g)
+
