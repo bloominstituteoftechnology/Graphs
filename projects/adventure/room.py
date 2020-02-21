a316cf149fa61a7e7f808b267ff4a8f432a79d3a
Jeff <theta_158@yahoo.com>
2020-02-21 08:40:08 -0500

    completed mvp

--- projects/adventure/room.py
+++ projects/adventure/room.py
@@ -1,5 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
 class Room:
     def __init__(self, name, description, id=0, x=None, y=None):
         self.id = id
@@ -11,10 +13,13 @@ class Room:
         self.w_to = None
         self.x = x
         self.y = y
+
     def __str__(self):
         return f"\n-------------------\n\n{self.name}\n\n   {self.description}\n\n{self.get_exits_string()}\n"
+
     def print_room_description(self, player):
         print(str(self))
+
     def get_exits(self):
         exits = []
         if self.n_to is not None:
@@ -26,8 +31,10 @@ class Room:
         if self.e_to is not None:
             exits.append("e")
         return exits
+
     def get_exits_string(self):
         return f"Exits: [{', '.join(self.get_exits())}]"
+
     def connect_rooms(self, direction, connecting_room):
         if direction == "n":
             self.n_to = connecting_room
@@ -44,6 +51,7 @@ class Room:
         else:
             print("INVALID ROOM CONNECTION")
             return None
+
     def get_room_in_direction(self, direction):
         if direction == "n":
             return self.n_to
@@ -55,5 +63,6 @@ class Room:
             return self.w_to
         else:
             return None
+
     def get_coords(self):
         return [self.x, self.y]
