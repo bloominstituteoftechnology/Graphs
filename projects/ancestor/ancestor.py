0a077a92c0e73dec89957dfb6ffda075eb5f6940
Gatrrr <davidgatrmacaulay@gmail.com>
2020-03-18 17:21:50 -0700

    day 2 mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,34 @@
+from util import Queue
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for pair in ancestors:
+        parent = pair[0]
+        child = pair[1]
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        graph.add_edge(child, parent)
+
+    queue = Queue()
+    queue.enqueue([starting_node])
+
+    longest_path = 1 
+    earliest_ancestor = -1
+
+    while queue.size() > 0:
+        path = queue.dequeue()
+        current_node = path[-1]
+
+        if len(path) > longest_path or (len(path) >= longest_path and current_node < earliest_ancestor):
+            # if (len(path) >= longest_path_length and current_node < earliest_ancestor) or len(path) > longest_path_length:
+            longest_path = len(path)
+            earliest_ancestor = current_node
+
+        children = graph.vertices[current_node]
+        for ancestor in children:
+            path_copy = list(path)
+            path_copy.append(ancestor)
+            queue.enqueue(path_copy)
+    return earliest_ancestor
---
+++ projects/ancestor/graph.py
@@ -0,0 +1,16 @@
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            print("Error: Vertex aint here homie.")
+
