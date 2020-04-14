05188b19bda50a99c648fc06f9d8d5073843e2a7
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-04-14 14:46:34 -0600

    Update ancestor.py
    get the imports for ancestors

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,8 @@
+from projects.graph.util import Stack, Queue  # These may come in handy
+from projects.ancestor.graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    parent = 0
+    child = 1
+    
+    graph_child_to_parent = Graph()
