c370d9a1d4a1fb0f3e41eaa639d2ac195393d5ee
Jonathan Traverso <jtraverso7@gmail.com>
2020-04-20 21:25:06 -0400

    Initial commit. add_vertex/add_edge methods implemented.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,22 +3,59 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+
+    # def retrieve_set(self, vert):
+    #     """
+    #     Expects a vertex as an arg and returns connected vertices
+    #     """
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # U =>
+        # a vertex is an entry in the dictionary self.vertices, where the value at this
+        # entry is a set of vertices that this specific vertex connects to
+        # P =>
+        # the vertex_id will be the key in the self.vertices dictionary
+        # the value the key points to will be an emoty set()
+        # E =>
+        self.vertices[vertex_id] = set()
+        # print(self.vertices[key], "empty set")
+
+        # R =>
+        # I can fetch the set in the self.vertices dict but it returns the actual function invocation..
+        # not sure if it will cause problems later or if it'll change format when we add an edge
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
+        # U =>
+        # an edge represents the connection of two vertices
+        # these edges will be unweighted (unnamed)
+        # adding an edge creates a v1 to v2 connection...
+        # to make it a bidirectional graph, invoke this function again but swap v2 and v1 to make the
+        # connection mutual
+        # P =>
+        # use v1 as the key
+        # use v2 as the value to be added to the set that v1 key points to
+        # use the set method .add() add value
+        # E =>
+        self.vertices[v1].add(v2)
+        # R =>
+        # I think maybe the add_edge method should check to see if the connecting vertex (the 2nd arg)
+        # is a vertex already and if not, create a new vertex in self.vertices and then continue
+        # with the rest of this add_edge() invocation
+        #
+
         pass  # TODO
 
     def get_neighbors(self, vertex_id):
@@ -75,6 +112,14 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
+
+# graph = Graph()
+# graph.add_vertex("2")
+# graph.add_edge("2", "3")
+# graph.add_edge("2", "50")
+
+# print(graph.vertices, "important check")
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
