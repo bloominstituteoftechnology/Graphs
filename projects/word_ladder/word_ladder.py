50ac9c379c14d466ce0004b1495abedfa027bf8c
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-04-23 21:49:57 -0400

    Lecture final code

--- projects/word_ladder/word_ladder.py
+++ projects/word_ladder/word_ladder.py
@@ -1,4 +1,4 @@
-from util import Stack, Queue  # These may come in handy
+# from util import Stack, Queue  # These may come in handy
 
 # Given two words (begin_word and end_word), and a dictionary's word list,
 # return the shortest transformation sequence from begin_word to end_word, such that:
@@ -19,50 +19,123 @@ from util import Stack, Queue  # These may come in handy
 # beginWord = "hungry"
 # endWord = "happy"
 # None
+# f = open('words.txt', 'r')
+# words = f.read().split("\n")
+# f.close()
+# print(words)
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+#             'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+
+# # create a set() of words, for ease of lookup
+# words_set = set()
+# for word in words:
+#     words_set.add(word)
+
+
+# def get_neighbors(vertex):
+#     neighbors = []
+#     # for each letter in word
+#     list_of_chars = list(word)
+#     for i in range(len(list_of_chars)):
+#         # change that letter with ever letter of alphabet
+#         for letter in alphabet:
+#             new_word = list(list_of_chars)
+#             new_word[i] = letter
+#             # check if new word exists in our word_set
+#             new_word_string = ''.join(new_word)
+#             if (new_word_string != word and new_word_string in words_set):
+#                 # if yes, add word as neighbor
+#                 neighbors.append(new_word_string)
+#     # return list of neighbors
+#     return neighbors
+
+
+# def find_path(begin_word, end_word):
+#     visited = set()
+#     words_to_visit = Queue()
+#     words_to_visit.enqueue([begin_word])
+#     # remove current vertex and path from q
+#     while words_to_visit.size() > 0:
+#         path = words_to_visit.dequeue()
+#         current_word = path[-1]
+#         # make sure we haven't visited this word
+#         if current_word not in visited:
+#             # add to visited
+#             visited.add(current_word)
+#             # check if this word is our target
+#             if current_word == end_word:
+#                 return path
+#             # otherwise, find all neighbors and create new paths
+#             for neighbor in get_neighbors(current_word):
+#                 path_copy = list(path)
+#                 path_copy.append(neighbor)
+#                 words_to_visit.enqueue(path_copy)
+
+
+# begin_word = 'hit'
+# end_word = 'cog'
+# print(find_path(begin_word, end_word))
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 f = open('words.txt', 'r')
 words = f.read().split("\n")
 f.close()
-print(words)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
 
-# create a set() of words, for ease of lookup
+# create a set of words, for easy lookup
 words_set = set()
 for word in words:
     words_set.add(word)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def get_neighbors(vertex):
+def get_neighbors(word):
     neighbors = []
     # for each letter in word
     list_of_chars = list(word)
     for i in range(len(list_of_chars)):
-        # change that letter with ever letter of alphabet
+        # change that letter with every letter of alphabet
         for letter in alphabet:
             new_word = list(list_of_chars)
             new_word[i] = letter
-            # check if new word exists in our word_set
-            new_word_string = ''.join(new_word)
+            # check if the new word exists in our word_set
+            new_word_string = "".join(new_word)
             if (new_word_string != word and new_word_string in words_set):
-                # if yes, add word as neighbor
+                # if yes, add the word as a neighbor
                 neighbors.append(new_word_string)
-    # return list of neighbors
+    # return a list of neighbors
     return neighbors
 
 
 def find_path(begin_word, end_word):
-    visited = set()
+    visted = set()
     words_to_visit = Queue()
     words_to_visit.enqueue([begin_word])
-    # remove current vertex and path from q
     while words_to_visit.size() > 0:
+        # remove the current vertex and path from queue
         path = words_to_visit.dequeue()
         current_word = path[-1]
-        # make sure we haven't visited this word
-        if current_word not in visited:
+        # make sure we havent visited this word
+        if current_word not in visted:
             # add to visited
-            visited.add(current_word)
-            # check if this word is our target
+            visted.add(current_word)
+            # check if this current_word is our target
             if current_word == end_word:
                 return path
             # otherwise, find all neighbors and create new paths
@@ -72,6 +145,12 @@ def find_path(begin_word, end_word):
                 words_to_visit.enqueue(path_copy)
 
 
-begin_word = 'hit'
-end_word = 'cog'
+begin_word = "hit"
+end_word = "cog"
 print(find_path(begin_word, end_word))
+# return: ['hit', 'hot', 'cot', 'cog']
+
+begin_word = "sail"
+end_word = "boat"
+print(find_path(begin_word, end_word))
+# ['sail', 'bail', 'boil', 'boll', 'bolt', 'boat']
