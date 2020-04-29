6ee113c019d9e3e0d06e684e340742ae4eb215bd
MattGThomas <mgthomas0616@gmail.com>
2020-04-29 18:37:01 -0500

    working on social

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,21 +1,22 @@
 # from graph import Graph
+# from ../graph/util import Stack
+from util import Stack
 
+# class Stack():
+#     def __init__(self):
+#         self.stack = []
 
-class Stack():
-    def __init__(self):
-        self.stack = []
+#     def push(self, value):
+#         self.stack.append(value)
 
-    def push(self, value):
-        self.stack.append(value)
+#     def pop(self):
+#         if self.size() > 0:
+#             return self.stack.pop()
+#         else:
+#             return None
 
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-
-    def size(self):
-        return len(self.stack)
+#     def size(self):
+#         return len(self.stack)
 
 
 class Graph:
