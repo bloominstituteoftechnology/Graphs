c0484c066bc6eaf3c699fef073cf10945c38b2f9
Moses A. Samuel <mosamuel98@gmail.com>
2020-05-12 14:55:34 -0700

    complete earliest ancestor algorithm

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,7 @@
+def earliest_ancestor(ancestors, starting_node, has_parent=False):
+    for parent, child in ancestors:
+        if child == starting_node:
+            # find the parent of the parent
+            return earliest_ancestor(ancestors, parent, True)
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    return starting_node if has_parent == True else -1
