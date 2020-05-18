9bcc155b9124562336ef683bcfdcc36701001248
Code4Blessings <robinwarden@code4blessings.com>
2020-05-18 09:30:01 -0400

    Completed graph with one unfound bug

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,11 +11,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+#map_file = "maps/test_line.txt"
+#map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -27,6 +27,7 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
+walking_directions = {'n': 's', 's': 'n', 'e': 'w','w': 'e'}
 
 # traversal_path = ['n', 'n']
 traversal_path = []
@@ -34,40 +35,36 @@ traversal_path = []
 #Problem:
 #The goal is to move from one room to the next in under 2000 steps (Goal is actually 950), the strategy is to only visit one room going in one direction around the map.  The problem is if we go in 1 direction, how do we backtrack if the player reaches a dead end?
 
-#Strategy:
-#MOVEMENTS!!!!
-#1. Move to the east
-    #Is room unvisited or is there a dead end?
-        #If unvisited, add to uv queue
-        #check for exits
-            #if exit is east, move east, 
-                #dequeue from uv queue
-                #add to visited list
 
-            #if exit is west, move west
-                #Is room unvisited or is there a dead end?
-                #If unvisited, add to uv queue 
-                #If deadend, mark room as visited and dequeue from unvisited
+def dfs_path(room, visited=None):
+    #If visited is none, we're going initialize it to equal set
+    if visited is None:
+        visited = set()
+        explored_path = []
+        #If the room has not been visited, add to visited
+        visited.add(room.id)
+        #For each room, we want to check for exits
+        for exit in room.get_exits():
+            next_room = room.get_room_in_direction(exit)
+            if next_room.id not in visited:
+                #Now that we have visited our first room, we can carry onto the next room with recursion and determine the path. 
+                path_has_no_dead_ends = dfs_path(next_room, visited)
+                if path_has_no_dead_ends:
+                    #Update the path with each visit--check for exits, and this time, include walking directions.
+                    current_path = [exit] + path_has_no_dead_ends + [walking_directions[exit]]
+                else:
+                    #If path does have dead ends, backtrack.  Leave out the path that has no dead ends and show the walking directions with just an exit (no other rooms present).
+                    current_path = [exit, walking_directions[exit]]
+                #The explored path will now include the explored path with the current path, in either case
+                explored_path = explored_path + current_path
 
-            #if exit is south, move south
-                #Is room unvisited or is there a dead end?
-                #If unvisited, add to uv queue
-                #If deadend, mark room as visited and dequeue from unvisited
+        return explored_path
 
-            #if exit is north, move north
-                #Is room unvisited or is there a dead end?
-                #If unvisited, add to uv queue
-                #If deadend, mark room as visited and dequeue from unvisited
-                #If deadend, mark room as visited and dequeue
+#Now we assign the dfs function to the traversal path with the player in the current room
+traversal_path = dfs_path(player.current_room)
+            
 
-#5. Now does len(room_graph) == len(visited) 
-    #If false, repeat process
-    # If true, traversal path complete 
-     
-
-
-
-# TRAVERSAL TEST
+# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -83,38 +80,7 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-# while True:
-#       cmds = input("-> ").lower().split(" ")
-#        if cmds[0] in ["n", "s", "e", "w"]:
-#             player.travel(cmds[0], True)
-#         elif cmds[0] == "q":
-#             break
-#         else:
-#             print("I did not understand that command.")
-
-
-# if n < limit:
-    #     room_list = list(world.print_rooms())
-    #     i = 0
-    #     complete = False
-
-    #     while i < len(room_list) and not complete:
-    #         #If visited is none, we're going initialize it to equal set
-    #         if visited is None:
-    #             visited = set()
-    #             traversal_path.append(n)
-    #     #If the room has not been visited, add to visited
-    #         if player not in visited:
-    #             visited.add(player.current_room)
-    #             Room.get_exits()
-    #             complete = adventure_moves(n+1, traversal_path, room_list[i],limit)
-    #         i + 1
-
-
-
-    
-
+print("")
+# print traversal path
+# print(f"traversal-path: {traversal_path}")
+print("")
