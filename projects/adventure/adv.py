a053ac9bd8e70a5ce3c45a006e1ac37a48e6b06b
Code4Blessings <robinwarden@code4blessings.com>
2020-05-18 10:04:02 -0400

    Fixed bug

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -14,7 +14,7 @@ world = World()
 #map_file = "maps/test_line.txt"
 #map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -40,25 +40,25 @@ def dfs_path(room, visited=None):
     #If visited is none, we're going initialize it to equal set
     if visited is None:
         visited = set()
-        explored_path = []
+    explored_path = []
         #If the room has not been visited, add to visited
-        visited.add(room.id)
+    visited.add(room.id)
         #For each room, we want to check for exits
-        for exit in room.get_exits():
-            next_room = room.get_room_in_direction(exit)
-            if next_room.id not in visited:
-                #Now that we have visited our first room, we can carry onto the next room with recursion and determine the path. 
-                path_has_no_dead_ends = dfs_path(next_room, visited)
-                if path_has_no_dead_ends:
-                    #Update the path with each visit--check for exits, and this time, include walking directions.
-                    current_path = [exit] + path_has_no_dead_ends + [walking_directions[exit]]
-                else:
+    for exit in room.get_exits():
+        next_room = room.get_room_in_direction(exit)
+        if next_room.id not in visited:
+            #Now that we have visited our first room, we can carry onto the next room with recursion and determine the path. 
+            path_has_no_dead_ends = dfs_path(next_room, visited)
+            if path_has_no_dead_ends:
+                 #Update the path with each visit--check for exits, and this time, include walking directions.
+                current_path = [exit] + path_has_no_dead_ends + [walking_directions[exit]]
+            else:
                     #If path does have dead ends, backtrack.  Leave out the path that has no dead ends and show the walking directions with just an exit (no other rooms present).
-                    current_path = [exit, walking_directions[exit]]
+                current_path = [exit, walking_directions[exit]]
                 #The explored path will now include the explored path with the current path, in either case
-                explored_path = explored_path + current_path
+            explored_path = explored_path + current_path
 
-        return explored_path
+    return explored_path
 
 #Now we assign the dfs function to the traversal path with the player in the current room
 traversal_path = dfs_path(player.current_room)
@@ -84,3 +84,16 @@ print("")
 # print traversal path
 # print(f"traversal-path: {traversal_path}")
 print("")
+
+#######
+# UNCOMMENT TO WALK AROUND
+#######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
