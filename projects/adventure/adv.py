44abb7cc89bcf7866b2094d750e223ed4e33c5d8
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-06-12 15:43:11 -0700

    finished mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -13,10 +13,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 #map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+#map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -27,108 +27,118 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-print("travel", player.travel('n',True))
-print("travel1", player.current_room.id)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-player_graph = {}
+
 
 # , create a graph
 # traverse the graph using dft
 # keep track of the direction and the node you visited
 
 visited = set()
-s = Stack()
-s.push(player)  # [player class, ]
+# s = Stack()
+# s.push(player.current_room.id)  
+room_visit_path = []
 
-print("start", player.current_room)
-visited_new = set()
+visited = set()
+test =[]
+def dft(player, direction=None):
+    room_id = player.current_room.id
+    room_visit_path.append(room_id)
+        
+    if room_id not in visited:
+        
+        visited.add(room_id)
+        
+        room = room_graph[room_id]
+        exits = player.current_room.get_exits()
 
-while s.size() > 0:
-    player = s.pop()
-    print("current room", player.current_room.id)
-#     # get the directions to go to from the current room
+        for exit in exits: # [n, s, w, e]
+            
+            new_room = player.current_room.get_room_in_direction(exit)
+            new_player = Player(new_room)
 
-    valid_directions = player.current_room.get_exits()
+            if new_room.id not in visited:
+                #recursion
+                dft(new_player,exit)
+        
+                room_visit_path.append(room_id)
 
-    if player.current_room.id not in visited:
-        visited.add(player.current_room.id)
-        directions_to_go_to = {}
-        for direction in valid_directions:
-            directions_to_go_to[direction] = '?'
-    # {0: n: ?}
-    # # add the directions to move and the room id to the player_graph {n: ?}
-        player_graph[player.current_room.id] = directions_to_go_to
-    # {0:{n:1, s, e, w}, 1:{n:?, s:0}}
+                
+           
+              
+               
+                # if direction == 'n':
+                #     traversal_path.append('s')
+                   
 
-    player_direction = player_graph[player.current_room.id]
+                # elif direction=='s':
+                #     traversal_path.append('n')
+                #     test.append((room_id, 'n'))
+                # elif direction=='e':
+                #     traversal_path.append('w')
+                    
+                # elif direction=='w':
+                #     traversal_path.append('e')
+                
+                # print("traversal", room_visit_path)
+                # print("traversal", traversal_path)
+            
+def take_a_walk(player):
+    dft(player)             
+    for i in range(len(room_visit_path)-1):
+        current_room =  room_visit_path[i]
+        next_room = room_visit_path[i+1]
 
-    # {n: 1, s: ?, e: ?, w: ?}
+        for key, value in room_graph[current_room][1].items():
+            if value == next_room:
+                traversal_path.append(key)
+        
 
-    unexplored_routes = [
-        key for key in player_direction if player_direction[key] == '?']
-    #['n', 's']
+take_a_walk(player)
 
-    if len(unexplored_routes) > 0:
-        key = random.choice(unexplored_routes)
-        # key = 'n'
-        print(
-            f'key for room {player.current_room.id} is {key} with value {player_direction[key]}')
-        if player_direction[key] == '?':
-
-            player_direction[key] = player.current_room.get_room_in_direction(
-                key).id
-            traversal_path.append(key)
-            # [n,]
-
-            room = player.current_room.get_room_in_direction(key)
-            new_player = Player(room)  # player class (1) which is n
-
-            s.push(new_player)
-            # [player-room 1]
-            # {0: n:1, s, e , w}
-    else:
-        visited_bfs = set()
-
-        q = Queue()
-
-        q.enqueue(player)
-        path = [player.current_room.id]
-
-        while q.size() > 0:
-            path = q.dequeue()
-            player = path[-1]
+print('roomvisited', room_visit_path)
+# while s.size() > 0:
+#     current_room_id = s.pop()
+#     print("id", current_room_id)
+#     rooms_travelled.append(current_room_id)
+    
+#     if current_room_id not in visited:
+#         visited.add(current_room_id)
+#         room = room_graph[current_room_id]
+#         print("room", room)
+#         exits = random.
+#         neighbor = random.choice(room[1][]
+            
+#             s.push(room[1][neighbor])
            
 
-            if player not in visited_bfs:
-                visited_bfs.add(player)
-                player_direction = player_graph[player] # {n:1,s:5,e:7, w:4}
-                
-                
-                unexplored_routes =[key for key in player_direction if player_direction[key] == '?']
-                if len(unexplored_routes) > 0:
-                    s.push(player)
-                    break
 
-                neighbors = player_graph(player.current_room.id) #{n:1,e:7,w:3,s:5}
-                # for key in neighbors:
-                #     new_path.append(key, neighbors[key])
-                #     new_path = list(path)
-                #     new_path.append(neighbors[key])
+           
         
-                    #   room = player.current_room.get_room_in_direction(key)
-                    #   new_player = Player(room)
-                    # q.enqueue(new_path)
+    
+            
+            
 
 
-print("visited", visited)
-print("traversal", traversal_path)
-print(player_graph)
+
+            
 
 
-player.current_room.get_room_in_direction(key).id
 
+# visited_new = set()
+
+# while s.size() > 0:
+#     player = s.pop()
+
+# #     # get the directions to go to from the current room
+
+#     valid_directions = player.current_room.get_exits()
+    
+#     for direction in valid_directions:
+
+#         if room_graph[player[1] 
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -159,3 +169,56 @@ while True:
         break
     else:
         print("I did not understand that command.")
+
+
+
+
+print("diretions", valid_directions)
+#     for direction in valid_directions:
+
+#     if player.current_room.id not in visited:
+#         visited.add(player.current_room.id)
+#         directions_to_go_to = {}
+#         for direction in valid_directions:
+#             directions_to_go_to[direction] = '?'
+#     # {0: n: ?}
+#     # # add the directions to move and the room id to the player_graph {n: ?}
+#         player_graph[player.current_room.id] = directions_to_go_to
+#     # {0:{n:1, s, e, w}, 1:{n:?, s:0}}
+
+#     player_direction = player_graph[player.current_room.id]
+
+#     # {n: 1, s: ?, e: ?, w: ?}
+
+#     unexplored_routes = [
+#         key for key in player_direction if player_direction[key] == '?']
+#     #['n', 's']
+
+#     if len(unexplored_routes) > 0:
+#         key = random.choice(unexplored_routes)
+#         # key = 'n'
+#         print(
+#             f'key for room {player.current_room.id} is {key} with value {player_direction[key]}')
+#         if player_direction[key] == '?':
+
+#             player_direction[key] = player.current_room.get_room_in_direction(
+#                 key).id
+#             traversal_path.append(key)
+#             # [n,]
+
+#             room = player.current_room.get_room_in_direction(key)
+#             new_player = Player(room)  # player class (1) which is n
+
+#             s.push(new_player)
+#             # [player-room 1]
+#             # {0: n:1, s, e , w}
+  
+        
+
+
+# # print("visited", visited)
+# print("traversal", traversal_path)
+# print(player_graph)
+
+
+# player.current_room.get_room_in_direction(key).id
