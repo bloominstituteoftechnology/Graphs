e9ebd2a2acae512f7e09b510c446dde46400928d
Konstadinos Angelis <angeliskonstadinos@gmail.com>
2020-06-15 10:35:38 -0700

    fin

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from collections import deque
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -13,8 +13,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,83 +28,36 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path_for_test = []
-traversal_path = []
 
-reversal_dict = {
-    'n': 's',
-    's': 'n',
-    'w': 'e',
-    'e': 'w'
-}
+path = Stack()
 
-visited = set([player.current_room.id])
+path.push(0)
 
-log = {}
-log[player.current_room.id] = {}
-for exits in player.current_room.get_exits():
-    log[player.current_room.id].update({exits : '?'})
+visited = set()
 
-while len(visited) != len(room_graph):
-    checkForChange = False
+while len(visited) < len(room_graph):
+    nextMove = Stack()
 
-    for direction in player.current_room.get_exits():
-        if log[player.current_room.id][direction] == '?':
-            checkForChange = True
-            #Save our current room
-            saved_room = player.current_room.id
+    curr = path.stack[-1]
 
-            #Move rooms
-            player.travel(direction)
+    visited.add(curr)
 
-            #Add to visited, and path.
-            visited.add(player.current_room.id)
-            traversal_path_for_test.append(direction)
-            traversal_path.append(reversal_dict[direction])
+    future_moves = room_graph[curr][1]
 
-            #Update our previous room's log
-            log[saved_room][direction] = player.current_room.id
+    for move, next_room in future_moves.items():
+        if next_room not in visited:
+            nextMove.push(next_room)
 
-            #Make a new log for this room
-            if player.current_room.id not in log:
-                log[player.current_room.id] = {}
-                for e in player.current_room.get_exits():
-                    log[player.current_room.id].update({e : '?'})
-            log[player.current_room.id][reversal_dict[direction]] = saved_room
+    if nextMove.size() > 0:
+        room = nextMove.stack[0]
+        path.push(room)
+    else:
+        room = path.stack[-2]
+        path.pop()
 
-            break
-
-    if player.current_room.get_exits() == list(traversal_path[-1]):
-        queue = deque()
-        queue.append(player.current_room.id)
-
-        vis = []
-
-        while len(queue) > 0:
-            curr = queue.popleft()
-
-            if curr not in vis:
-                vis.append(curr)
-                
-                for adj in log[curr]:
-                    if log[curr][adj] == '?':
-                        while player.current_room.id != curr:
-                            checkForChange = True
-                            print('room', player.current_room.id)
-                            print('trav', traversal_path)
-                            print('eehh', log[player.current_room.id])
-                            print('total_path', traversal_path_for_test)
-                            backwards = traversal_path.pop(-1)
-                            player.travel(backwards)
-                            traversal_path_for_test.append(backwards)
-                        break
-
-                    queue.append(log[curr][adj])
-    if checkForChange == False:
-        break
-
-    print(log)
-    
-
+    for move, next_room in future_moves.items():
+        if next_room ==  room:
+            traversal_path_for_test.append(move)
 
 # TRAVERSAL TEST
 visited_rooms = set()
