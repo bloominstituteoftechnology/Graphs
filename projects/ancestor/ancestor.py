3029d5cf5d8eb02a8bce5b753de700ce85a9541b
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-14 16:29:27 -0400

    some tests succeeded in graph. starting work on ancestor.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,7 @@
+from util import Stack, Queue
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    queue = Queue()
+
+    for item in ancestors:
+        
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,42 +13,82 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+        else:
+            raise IndexError("Cannot add vertex_id a second time")
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        elif v1 not in self.vertices and v2 in self.vertices:
+            self.vertices[v1] = {v2}
+        elif v1 in self.vertices and v2 not in self.vertices:
+            raise IndexError("v2 not a valid vertice")
+        else:
+            raise IndexError("Neither v1 nor v2 are valid vertices")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            raise IndexError("vertex_id is not a valid vertex")
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        queue = Queue()
+        visited = set()
+        queue.enqueue(starting_vertex)
+
+        while queue.size() > 0:
+            vertex = queue.dequeue()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                for next_vertice in self.get_neighbors(vertex):
+                    queue.enqueue(next_vertice)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        stack = Stack()
+        visited = set()
+        stack.push(starting_vertex)
 
-    def dft_recursive(self, starting_vertex):
+        while stack.size() > 0:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                print(vertex)
+                for next_vertex in self.get_neighbors(vertex):
+                    stack.push(next_vertex)
+
+    def dft_recursive(self, starting_vertex, visited = set()):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # visited = set()
+        if starting_vertex not in visited:
+            visited.add(starting_vertex)
+            print(starting_vertex)
+        else:
+            return
+        for vertex in self.get_neighbors(starting_vertex):
+                return self.dft_recursive(vertex, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -56,7 +96,26 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        queue = Queue()
+        queue.enqueue(starting_vertex)
+
+        visited = set()
+
+        while queue.size() > 0:
+            path = queue.dequeue()
+            end_vertex = path[-1]
+
+            if end_vertex not in visited:
+                if end_vertex == destination_vertex:
+                    return path
+                visited.add(end_vertex)
+
+                for next_vertex in self.get_neighbors(v):
+                    new_path = list(path)
+                    new_path.append(next_vertex)
+                    queue.enqueue(new_path)
+        return None
+
 
     def dfs(self, starting_vertex, destination_vertex):
         """
