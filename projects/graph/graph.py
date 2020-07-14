db1061d4c073cc1dc8bc979c001a5c8c357ca936
Hector <hectoremil21@hotmail.com>
2020-07-14 12:26:30 -0400

    Implement add_vertex() and add_edge() in graph.py

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,13 +13,15 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
