ef448355d5fd9de44ab6947840204252271d7790
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-07-15 18:35:20 -0400

    passing Tues tests

--- projects/ancestor/ancestor_2.py
+++ projects/ancestor/ancestor_2.py
@@ -29,37 +29,27 @@ def earliest_ancestor(ancestors, starting_node):
     for connection in ancestors:
         graph.add_edge(connection[1], connection[0])
 
-    print("----------------------------------------------------")
-    print(graph.vertices)
-    ## search dictionary/graph for all possible paths to earliest ancestor
-
     ## empty set to store the paths (set of lists)
     paths = []
     visited_paths = []
 
     visited_paths.append([starting_node])
-    ## if starting node is stored as a key
+
     while len(visited_paths) > 0:
         current_path = visited_paths.pop(0)
-        current_node = current_path[-1]
-        print(current_node)
-        if current_node in graph.vertices:
-            for parent in graph.vertices[current_node]:
-                ## append each value to copy of starting node as a new_path
-                new_path = []
-                new_path.append(current_path)
-                ## append each new_path to the paths set() & end of list
+        starting_node = current_path[-1]
+
+        if starting_node in graph.vertices:
+            for parent in graph.vertices[starting_node]:
+                new_path = current_path.copy()
                 new_path.append(parent)
                 paths.append(new_path)
                 visited_paths.append(new_path)
-                # current_node = visited_paths.pop(0)[-1]
-            
 
-    print(paths)
     if len(paths) == 0:
-        return -1
+        return -1    
     longest = max([len(i) for i in paths])
-    plist = [] 
+    plist = []
     for path in paths:
         if len(path) == longest:
             plist.append(path)
@@ -68,21 +58,12 @@ def earliest_ancestor(ancestors, starting_node):
     for path in plist:
         oldest.append(path[-1])
     
-    return max(oldest)
+    return min(oldest)    
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
 
-
-
-
-    ## reset each as a current_node and traverse accordingly
-    ## search each new value - key/value pair
-
-    
-    ## if current path not in set
-    ## add to set
-    ## get max length list
-    ## if more than one
-        ## compare parents - take smaller one
-    ## return parent
