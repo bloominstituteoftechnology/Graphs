45db34d826f3dffb77c1ad1f901c17aa97035ab4
Jon Bash <jonbash@protonmail.com>
2020-07-16 16:04:29 -0700

    write alternate solution

--- projects/adventure/traverse.py
+++ projects/adventure/traverse.py
@@ -11,6 +11,57 @@ __opposites = {
 }
 
 
+def get_room_from_path(starting_room, path):
+    room = starting_room
+    for direction in path:
+        room = room.get_room_in_direction(direction)
+    return room
+
+def to_nearest_unexplored(starting_room, graph):
+    explored = set()
+    queue = Queue()
+    queue.enqueue([])
+
+    while queue.size() != 0:
+        path = queue.dequeue()
+        room = get_room_from_path(starting_room, path)
+        exits = room.get_exits()
+
+        for direction in exits:
+            new_path = path.copy()
+            new_path.append(direction)
+            if graph[room.id][direction] == '?':
+                return new_path
+            if (room, direction) not in explored:
+                queue.enqueue(new_path)
+            explored.add((room, direction))
+
+def add_to_graph_if_needed(room, graph):
+    adjacent_rooms = graph.get(room.id)
+    if adjacent_rooms is None:
+        adjacent_rooms = {direction:'?' for direction in room.get_exits()}
+        graph[room.id] = adjacent_rooms
+
+def traverse2(player):
+    graph = {}
+    path = []
+    room = player.current_room
+    add_to_graph_if_needed(room, graph)
+
+    while True:
+        exits = room.get_exits()
+        path_to_unexplored = to_nearest_unexplored(room, graph)
+        if path_to_unexplored is None or len(path_to_unexplored) == 0:
+            return path
+        for direction in path_to_unexplored:
+            new_room = room.get_room_in_direction(direction)
+            path.append(direction)
+            graph[room.id][direction] = new_room.id
+            add_to_graph_if_needed(new_room, graph)
+            graph[new_room.id][__opposites[direction]] = room.id
+            room = new_room
+
+
 def traverse(player):
     graph = {}
     breadcrumbs = Stack()
