f7f594766fb23eff4b8d4fceea743918b03e7b61
Hector <hectoremil21@hotmail.com>
2020-07-15 21:25:01 -0400

    Implement ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,8 +1,41 @@
 from projects.graph.graph import Graph
 def earliest_ancestor(ancestors, starting_node):
     graph = Graph()
+
+    # Populate graph
     for pair in ancestors:
-        a = pair[0]
-        b = pair[1]
         graph.add_edge(pair[0], pair[1])
-    print(graph.vertices)
+
+    # Extract vertices, for easier code comprehension
+    stuff = graph.vertices
+
+    # Find all current parents
+    parents = [x for x in stuff.keys() if starting_node in stuff[x]]
+
+    if len(parents):
+        best = (parents[0], 0)
+        parents.reverse()
+        for parent in parents:
+            finished = False
+            current = parent
+            count = 1
+            while not finished:
+                for key in stuff.keys():
+                    if current in stuff[key]:
+                        current = key
+                        count += 1
+                        break
+                    else:
+                        finished = True
+
+            # Handle same distance
+            if count > best[1]: best = (current, count)
+            elif count is best[1]:
+                if current < best[0]:
+                    best = (current, count)
+        return best[0]
+
+    # If none are found, return -1 outright
+    else:
+        return -1
+
