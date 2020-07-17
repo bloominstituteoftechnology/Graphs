ce3d098964702611b508a632fac8ad1f34008aa8
Mark Gerrior <github@gerrior.com>
2020-07-16 21:45:02 -0700

    Intial idea around traversal_graph flush out

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -32,11 +32,15 @@ traversal_path = []
 # My Code Goes Here
 traversal_graph = {}
 
-traversal_graph[player.current_room.id] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
-player.travel('n', False)
-traversal_path.append('n')
+traversal_graph[player.current_room.id] = {'n': player.current_room.n_to, 's': player.current_room.s_to, 'w': player.current_room.w_to, 'e': player.current_room.e_to}
+direction_of_travel = 'n'
+prior_room_id = player.current_room.id
+player.travel(direction_of_travel, False)
+traversal_path.append(direction_of_travel)
 
-traversal_graph[player.current_room.id] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+traversal_graph[player.current_room.id] = {'n': player.current_room.n_to, 's': player.current_room.s_to, 'w': player.current_room.w_to, 'e': player.current_room.e_to}
+traversal_graph[prior_room_id][direction_of_travel] = player.current_room.id
+traversal_graph[player.current_room.id]['s'] = prior_room_id
 player.travel('n', False)
 traversal_path.append('n')
 
