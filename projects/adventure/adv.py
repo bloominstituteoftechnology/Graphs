1a2a2691c4a60f54cb94efdba91bc67a638d4276
maybemichael <maybemichael@icloud.com>
2020-07-17 10:52:11 -0700

    Implemented a recursive solution but wasn't able to improve my number of moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -18,6 +18,28 @@ map_file = "maps/main_maze.txt"
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 
+# room_graph = {
+#   0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}],
+#   1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}],
+#   2: [(3, 7), {'s': 1}],
+#   3: [(4, 5), {'w': 0, 'e': 4}],
+#   4: [(5, 5), {'w': 3}],
+#   5: [(3, 4), {'n': 0, 's': 6}],
+#   6: [(3, 3), {'n': 5, 'w': 11}],
+#   7: [(2, 5), {'w': 8, 'e': 0}],
+#   8: [(1, 5), {'e': 7}],
+#   9: [(1, 4), {'n': 8, 's': 10}],
+#   10: [(1, 3), {'n': 9, 'e': 11}],
+#   11: [(2, 3), {'w': 10, 'e': 6}],
+#   12: [(4, 6), {'w': 1, 'e': 13}],
+#   13: [(5, 6), {'w': 12, 'n': 14}],
+#   14: [(5, 7), {'s': 13}],
+#   15: [(2, 6), {'e': 1, 'w': 16}],
+#   16: [(1, 6), {'n': 17, 'e': 15}],
+#   17: [(1, 7), {'s': 16}]
+# }
+
+
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -36,7 +58,6 @@ def starting_room():
     for way_out in exits:
         direction_dict[way_out] = "?"
     maze_graph[room.id] = direction_dict
-    print(maze_graph)
 
 def explore(direction):
     direction_dict = {}
@@ -74,7 +95,6 @@ def closest_unexplored_path(room_id):
                 if room is "?":
                     shortest.pop(0)
                     path = [item[1] for item in shortest]
-                    print(f"Players Current Room: {player.current_room.id}")
                     return path
     return []
 
@@ -86,20 +106,19 @@ def complete_maze(start):
         direction = stack.pop()
         room = player.current_room
         next_room = room.get_room_in_direction(direction)
-        print(f"Players Current Room: {player.current_room.id}")
+        # print(f"Players Current Room: {player.current_room.id}")
         exits = set(room.get_exits())
         if next_room:
             explore(direction)
-            print(f"Players Current Room: {player.current_room.id}")
+            # print(f"Players Current Room: {player.current_room.id}")
             stack.append(direction)
         if not stack:
             for way_out in exits:
                 if maze_graph[room.id][way_out] is "?":
                     explore(way_out)
-                    print(f"Players Current Room: {player.current_room.id}")
+                    # print(f"Players Current Room: {player.current_room.id}")
                     stack.append(way_out)
                     break
-        if not stack:
             if len(maze_graph.keys()) is len(room_graph.keys()):
                 return 
             path_to_unexplored = closest_unexplored_path(player.current_room.id)
@@ -108,18 +127,35 @@ def complete_maze(start):
                 if i is len(path_to_unexplored) - 1:
                     stack.appendleft(path_to_unexplored[i])
     print(f"Length of maze graph: {len(maze_graph.keys())}")
-    
-                    
-            
-                
 
+# Recursive Solution
 
-        
-
+def complete_maze2(direction):
+    if len(maze_graph) is 0:
+        starting_room()
+    current = player.current_room
+    next_room = player.current_room.get_room_in_direction(direction)
+    if len(maze_graph.keys()) is len(room_graph.keys()):
+        return
+    if next_room:
+        explore(direction)
+        print(f"Players Current Room: {player.current_room.id}")
+        return complete_maze2(direction)
+    if not next_room:
+        exits = current.get_exits()
+        for way_out in exits:
+            if maze_graph[current.id][way_out] is "?":
+                explore(way_out)
+                return complete_maze2(way_out)
+        path_to_unexplored = closest_unexplored_path(player.current_room.id) 
+        for i in range(len(path_to_unexplored)):
+            explore(path_to_unexplored[i]) 
+            if i is len(path_to_unexplored) - 1:
+                return complete_maze2(path_to_unexplored[i])
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-complete_maze("n")
+complete_maze2("w")
 
 
 # TRAVERSAL TEST
