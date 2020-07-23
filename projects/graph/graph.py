35f299e6820cb201d42b42307edf2e81dbcc7752
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-22 20:10:32 -0700

    Expand comments.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -6,7 +6,8 @@ from util import Stack, Queue  # These may come in handy
 
 class Graph:
     """
-    Represent a graph as a dictionary of vertices mapping labels to edges.
+    Represent a directed graph as a dictionary of vertices mapping labels to
+    sets of outgoing edges.
     """
 
     def __init__(self):
@@ -26,7 +27,7 @@ class Graph:
 
     def get_neighbors(self, vertex_id):
         """
-        Get all neighbors (edges) of a vertex.
+        Get all neighbors (outgoing edges) of a vertex.
         """
         return self.vertices[vertex_id]
 
@@ -35,15 +36,20 @@ class Graph:
         Print each vertex in breadth-first order beginning from
         starting_vertex.
         """
-        printed = set()
-        to_print = Queue()
+        printed = set()     # Nodes already printed.
+        to_print = Queue()  # Nodes to print, in FIFO order for BFT.
 
         to_print.enqueue(starting_vertex)
         while to_print.size() > 0:
             vertex = to_print.dequeue()
+
+            # The print queue can contain duplicate nodes, so we need to check
+            # that a node hasn't already been printed before outputting it.
             if vertex not in printed:
                 print(vertex)
                 printed.add(vertex)
+
+            # Append unvisited neighbors to the queue for future printing.
             for edge in self.vertices[vertex]:
                 if edge not in printed:
                     to_print.enqueue(edge)
@@ -52,15 +58,20 @@ class Graph:
         """
         Print each vertex in depth-first order beginning from starting_vertex.
         """
-        printed = set()
-        to_print = Stack()
+        printed = set()     # Nodes already printed.
+        to_print = Stack()  # Nodes to print, in LIFO order for DFT.
 
         to_print.push(starting_vertex)
         while to_print.size() > 0:
             vertex = to_print.pop()
+
+            # The print stack can contain duplicate nodes, so we need to check
+            # that a node hasn't already been printed before outputting it.
             if vertex not in printed:
                 print(vertex)
                 printed.add(vertex)
+
+            # Push unvisited neighbors to the stack for future printing.
             for edge in self.vertices[vertex]:
                 if edge not in printed:
                     to_print.push(edge)
@@ -70,12 +81,15 @@ class Graph:
         Print each vertex in depth-first order beginning from starting_vertex,
         using recursion.
         """
+        # Special case for initial function call.
         if printed is None:
             printed = set()
 
+        # Print the current vertex.
         print(starting_vertex)
         printed.add(starting_vertex)
 
+        # Recursively print a DFT of each previously unvisited neighbor.
         for edge in self.vertices[starting_vertex]:
             if edge not in printed:
                 self.dft_recursive(edge, printed)
@@ -85,72 +99,111 @@ class Graph:
         Return a list containing the shortest path from starting_vertex to
         destination_vertex in breath-first order.
         """
-        visited = {}
-        to_search = Queue()
+        visited = {}         # Maps visited node to the previous node in path.
+        to_search = Queue()  # Tuples of (node, previous node) to be searched.
 
         to_search.enqueue((starting_vertex, None))
         while to_search.size() > 0:
             (vertex, prev) = to_search.dequeue()
+
+            # Only unvisited nodes still need to be searched.
             if vertex not in visited:
+
+                # Add vertex -> previous vertex entry to dictionary of visited
+                # nodes.
                 visited[vertex] = prev
+
+                # If the destination vertex has been found, return the path by
+                # which it was reached.
                 if vertex == destination_vertex:
                     step = vertex
                     path = []
+                    # Work backwards node-by-node to reconstruct the path.
                     while step is not None:
                         path.append(step)
                         step = visited[step]
+                    # Reverse to return in order from start to destination.
                     return path[::-1]
 
-            for edge in self.vertices[vertex]:
-                if edge not in visited:
-                    to_search.enqueue((edge, vertex))
+                # Otherwise, continue searching. Add unvisited neighbors to the
+                # queue, with the current node as the previous step.
+                for edge in self.vertices[vertex]:
+                    if edge not in visited:
+                        to_search.enqueue((edge, vertex))
 
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from starting_vertex to
         destination_vertex in depth-first order.
         """
-        visited = {}
-        to_search = Stack()
+        visited = {}         # Maps visited node to the previous node in path.
+        to_search = Stack()  # Tuples of (node, previous node) to be searched.
+
         to_search.push((starting_vertex, None))
         while to_search.size() > 0:
             (vertex, prev) = to_search.pop()
+
+            # Only unvisited nodes still need to be searched.
             if vertex not in visited:
+
+                # Add vertex -> previous vertex entry to dictionary of visited
+                # nodes.
                 visited[vertex] = prev
+
+                # If the destination vertex has been found, return the path by
+                # which it was reached.
                 if vertex == destination_vertex:
                     step = vertex
                     path = []
+                    # Work backwards node-by-node to reconstruct the path.
                     while step is not None:
                         path.append(step)
                         step = visited[step]
+                    # Reverse to return in order from start to destination.
                     return path[::-1]
-            for edge in self.vertices[vertex]:
-                if edge not in visited:
-                    to_search.push((edge, vertex))
+
+                # Otherwise, continue searching. Add unvisited neighbors to the
+                # stack, with the current node as the previous step.
+                for edge in self.vertices[vertex]:
+                    if edge not in visited:
+                        to_search.push((edge, vertex))
 
     def dfs_recursive(self,
                       starting_vertex,
                       destination_vertex,
-                      visited=[],
-                      deadend=[]):
+                      visited=None,
+                      deadend=None):
         """
         Return a list containing a path from starting_vertex to
         destination_vertex in depth-first order, using recursion.
         """
+        # Special case for initial function call.
         if visited is None:
-            visited = []
+            visited = []  # The path so far.
+            deadend = []  # Nodes searched completely but unsuccessfully.
 
+        # Base case: destination vertex reached.
         if starting_vertex == destination_vertex:
             return visited + [starting_vertex]
+
+        # Recursive case: continue searching.
         else:
             visited.append(starting_vertex)
+
+            # Recursively search each unvisited neighbor in turn.
             for edge in self.vertices[starting_vertex]:
                 if edge not in visited and edge not in deadend:
                     path = self.dfs_recursive(edge,
                                               destination_vertex,
-                                              visited)
+                                              visited,
+                                              deadend)
+                    # Stop and return at the first successful search, if any.
                     if path is not None:
                         return path
+
+            # If no path to destination found through any neighbor of the
+            # current node, step back to the parent function call after
+            # crossing the current node off the list.
             visited.remove(starting_vertex)
             deadend.append(starting_vertex)
 
@@ -218,6 +271,7 @@ if __name__ == '__main__':
     '''
     print('BFS')
     print(graph.bfs(1, 6))
+    print(f'If not in graph: {graph.bfs(1, 9)}\n')
 
     '''
     Valid DFS paths:
@@ -226,5 +280,7 @@ if __name__ == '__main__':
     '''
     print('DFS')
     print(graph.dfs(1, 6))
+    print(f'If not in graph: {graph.dfs(1, 9)}\n')
     print('DFS recursive')
     print(graph.dfs_recursive(1, 6))
+    print(f'If not in graph: {graph.dfs_recursive(1, 9)}')
