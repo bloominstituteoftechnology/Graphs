aaf0c56128531629b041e33dcac255673eda73ee
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-07-26 20:02:43 -0700

    Fix 'total friendships' bug, update comments.

--- projects/social/social.py
+++ projects/social/social.py
@@ -3,6 +3,9 @@ from queue import SimpleQueue
 
 
 class User:
+    """
+    Represents a user of a friendship-based social networking site.
+    """
     def __init__(self, name):
         self.name = name
 
@@ -14,6 +17,9 @@ class User:
 
 
 class SocialGraph:
+    """
+    Represents a friendship-based social networking site.
+    """
     def __init__(self):
         self.last_id = 0
         self.users = {}
@@ -24,10 +30,10 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            raise ValueError("WARNING: You cannot be friends with yourself.")
         elif (friend_id in self.friendships[user_id] or
               user_id in self.friendships[friend_id]):
-            print("WARNING: Friendship already exists")
+            raise ValueError("WARNING: Friendship already exists.")
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
@@ -52,47 +58,73 @@ class SocialGraph:
         The number of users must be greater than the average number of
         friendships.
         """
-        # Reset graph
+        # Check argument validity.
+        if avg_friendships > num_users - 1:
+            raise ValueError("Number of users must be greater than the average"
+                             "number of friendships.")
+
+        # Reset graph.
         self.last_id = 0
         self.users = {}
         self.friendships = {}
 
-        # Add users
+        # Load name list.
         with open('first-names.txt') as file:
             names = file.read().split()
 
+        # Add users, assigning a random name to each. (Duplicate names are
+        # possible.)
         for i in range(num_users):
             self.add_user(random.choice(names))
 
-        # Create friendships
+        # Create friendships.
         for i in range(num_users * avg_friendships // 2):
             successful = False
             while not successful:
                 user1 = random.choice(list(self.users.keys()))
                 user2 = random.choice(list(self.users.keys()))
-                if user1 != user2:
-                    if ((user2 not in self.friendships[user1] and
-                         user1 not in self.friendships[user2])):
-                        self.add_friendship(user1, user2)
-                        successful = True
+                try:
+                    self.add_friendship(user1, user2)
+                except ValueError:
+                    pass
+                else:
+                    successful = True
 
     def get_total_users(self):
+        """
+        Returns the total number of users in the network.
+        """
         return len(self.users)
 
     def get_total_friendships(self):
+        """
+        Returns the total number of friendships in the network. (Remember that
+        each friendship is listed twice, once in each direction.)
+        """
         return sum([len(friend_list) for friend_list in
-                    self.friendships.values()])
+                    self.friendships.values()]) / 2
 
     def get_average_friendships(self):
+        """
+        Returns the mean number of friendships held users in the network.
+        """
         num_users = self.get_total_users()
         total_friendships = self.get_total_friendships()
-        return total_friendships / num_users
+        return 2 * total_friendships / num_users
 
     def get_min_friendships(self):
+        """
+        Returns the number of friendships participated in by the user with the
+        fewest friends.
+        """
         return min([len(friend_list) for friend_list in
                     self.friendships.values()])
 
     def get_max_friendships(self):
+        """
+        Returns the number of friendships participated in by the user with the
+        most friends.
+        """
         return max([len(friend_list) for friend_list in
                     self.friendships.values()])
 
@@ -107,6 +139,7 @@ class SocialGraph:
         """
         visited = {}
 
+        # Use a queue for breadth-first traversal.
         to_visit = SimpleQueue()
         to_visit.put((user_id, None))
 
@@ -115,12 +148,19 @@ class SocialGraph:
 
             if vertex not in visited:
                 if vertex == user_id:
+                    # Self node is always reachable.
                     visited[vertex] = [user_id]
                 elif prev == user_id:
+                    # Immediate friends are directly reachable from the self.
                     visited[vertex] = [user_id]
                 else:
+                    # The shortest path to friends of friends is the shortest
+                    # path to the previous node through which they are first
+                    # reached, plus that previous node.
                     visited[vertex] = visited[prev] + [prev]
 
+            # Add the next outward friends-of-friends circle to the queue for
+            # traversal.
             for edge in self.friendships[vertex]:
                 if edge not in visited:
                     to_visit.put((edge, vertex))
