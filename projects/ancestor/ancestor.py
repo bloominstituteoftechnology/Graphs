57678b541e62cb9de840e628826d773bb239f66a
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2020-08-02 13:53:09 -0500

    changes to ancestor code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,40 +1,73 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
 def earliest_ancestor(ancestors, starting_node):
-	ancestor, depth = spirit_medium(ancestors, starting_node)
-	return ancestor if depth >= 0 else -1
+    # transform the input into a graph
+    graph = Graph()
+    for pair in ancestors: # (parent, child)
+        parent = pair[0]
+        child = pair[1]
+        graph.add_vertex(parent)
+        graph.add_vertex(child)
+        # build the edge as well
+        graph.add_edge(child, parent)
+    # Do a BFS
+    visited_neighbors = set()
+    neighbors_to_visit = Queue()
+    neighbors_to_visit.enqueue(
+        {
+            'vertex': starting_node, 
+            'path_so_far': [starting_node]
+        }
+    )
+    max_path_len = 1
+    earliest_ancestor = -1 # we have not found an earliest ancestor
+    while neighbors_to_visit.size() > 0:
+        vertex_and_path = neighbors_to_visit.dequeue()
+        current_vertex = vertex_and_path['vertex']
+        current_path = vertex_and_path['path_so_far']
+        # check if visited
+        if (current_vertex not in visited_neighbors):
+            # add to visited
+            visited_neighbors.add(current_vertex)
+            # Check if this current path is the longest we have seen
+            # if it is, update the longest path seen variable
+            # also, if its the same length as the longest one we have seen, if the ancestor has smaller ID, use it
+            if ((len(current_path) >= max_path_len and current_vertex < earliest_ancestor) 
+            or (len(current_path) > max_path_len)):
+                earliest_ancestor = current_vertex
+                max_path_len = len(current_path)
 
-
-def spirit_medium(ancestors, starting_node, parents=None):
-    # need a place to store 
-	if parents is None:
-		parents = {}
-		for parent, child in ancestors:
-			if child not in parents:
-				parents[child] = {parent}
-			else:
-				parents[child].add(parent)
-		print(parents)
-	# add a base case for when there are no parents
-	if starting_node not in parents:
-		return starting_node, -1
-
-	deepest_node = None
-	deepest_depth = 0
-	for parent in parents[starting_node]:
-		# no parents? we want to take the deepest
-		if deepest_depth is 0:
-			if deepest_node is None or parent < deepest_node:
-				deepest_node = parent
-
-		# keep going until we find the deepest ancestor
-		node, depth = spirit_medium(ancestors, parent, parents=parents)
-		if node is not None:
-			if depth > deepest_depth:
-				deepest_depth = depth
-				deepest_node = node
-
-			# If the ancestors are the same depth, get the deeper one
-			elif depth == deepest_depth:
-				if node < deepest_node:
-					deepest_node = node
-
-	return deepest_node, deepest_depth + 1
+            for neighbor in graph.vertices[current_vertex]: 
+                path_copy = list(current_path)
+                path_copy.append(neighbor)
+                neighbors_to_visit.enqueue({
+                    'vertex': neighbor,
+                    'path_so_far': path_copy
+                })
+    return earliest_ancestor
