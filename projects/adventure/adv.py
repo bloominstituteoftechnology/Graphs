0e759a0d5741a3ac364f99142c19b63d4186ad90
Mayankho <yankho@DESKTOP-NNENQO4.localdomain>
2020-08-04 18:38:07 -0400

    Adv

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -30,6 +30,29 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+visited =  {}
+reverse path = [] 
+o = {"n" : "s", "e": "w", "s":"n", "w": "e" } 
+
+visited[player.current_room.id] = player.current_room.get_exits()
+
+while len(visited) > len(room_graph):
+    if player.current_room.id not in visited: 
+        visited[player.current_room.id] = player.current_room.get_exits()
+        prev = revers_path[-1]
+        visited[player.currrent_room.id].remove(prev)
+    if len(visited[player.current_room.id]) == 0: 
+        prev = reverse_path[-1]
+        reverse_path.pop()
+        traversal_path.append(prev)
+        player.travel(prev)
+    else:
+        d = visited[player.current_room.id][-1]
+        visited[player.current_room.id].pop()
+        traversal_path.append(d) 
+        reverse_path.append(o[d])
+        player.travel(d)
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
