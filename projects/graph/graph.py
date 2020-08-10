e95a84e38ec432fbe5ee756e8da13c6f15cc5556
Ramses Gasque <cedro.gasque@wsu.edu>
2020-08-10 16:28:35 -0700

    Revert "Revert "part 1""
    This reverts commit 70443a0eecaa82ca6675ffb2907eba60d4cd405c.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,19 +13,19 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
