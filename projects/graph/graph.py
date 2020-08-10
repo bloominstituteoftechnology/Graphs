69944186c4c23e186ecec2a5bedbdb9a24e0df44
Nick <ptnguyen1901@gmail.com>
2020-08-10 16:06:51 -0400

    bft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,6 +3,7 @@ Simple graph implementation
 """
 from util import Stack, Queue
 
+
 # These may come in handy
 
 class Graph:
@@ -12,32 +13,46 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-       self.vertices[vertex_id] = set() # this will hold edges
+        self.vertices[vertex_id] = set()  # this will hold edges
 
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
 
-            self.vertices[v1].add(v2) # there's an edge from v1 to v2
+            self.vertices[v1].add(v2)  # there's an edge from v1 to v2
         else:
             raise IndexError("nonexistent vert")
 
-
     def get_neighbors(self, vertex_id):
-
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        # Create an empty queue
+        q = Queue()
+
+        # Create a set to store the visited nodes
+        visited = set()
+
+        # Init: enqueue the starting node
+        q.enqueue(starting_vertex)
+
+        # While the queue isn't empty
+        while q.size() > 0:
+
+            # Dequeue the first item
+            v = q.dequeue()
+            # If it's not been visited:
+            if v not in visited:
+                # Mark as visited (i.e add to the visited set)
+                visited.add(v)
+
+                # Do something with the node
+                print(f"Visited {v}")
+                # Add all neighbors to the queue
+                for next_vert in self.get_neighbors(v):
+                    q.enqueue(next_vert)
 
     def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+
 
     def dft_recursive(self, starting_vertex):
         """
