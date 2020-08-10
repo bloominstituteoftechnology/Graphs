0d1ca655965f0bc7ada0efdd4d711fd86c5fd312
Mark Poggi <markp@mac.com>
2020-08-10 10:58:27 -0700

    Completed graph traversal, graph search and ancestor exercises

--- projects/graph/util.py
+++ projects/graph/util.py
@@ -1,5 +1,11 @@
 
 # Note: This Queue class is sub-optimal. Why?
+
+# we're using an array for the Queue which means we need to pop from the front, the remaining values need to
+# move over 1 at a time.  This is O(n) complexity.
+# this should be O(1) or constant.  That's what is important about a queue.
+# Needs to be a linked list (circular linked list?) / dictionary. 
+
 class Queue():
     def __init__(self):
         self.queue = []
