b5311fa11d5b34f26341c2a09092bd2880dd06e0
Wesley Mountford <goe_horus@hotmail.com>
2020-08-12 14:13:20 -0500

    Partially did monday's assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,42 @@
+from projects.graph.graph import Graph
+from projects.graph.util import Queue
+
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for parent, child in ancestors:
+        if parent not in graph.vertices:
+            graph.add_vertex(parent)
+        if child not in graph.vertices:
+            graph.add_vertex(child)
+        graph.add_edge(child, parent)
+
+    #print(graph.vertices)
+
+    if starting_node not in graph.vertices:
+        return -1
+
+    queue = Queue()
+    queue.enqueue([starting_node])
+
+    max_path = 1
+    earliest = -1
+
+    while queue.size() > 0:
+        print(queue.queue)
+
+        path = queue.dequeue()
+
+        value = path[-1]
+        if (len(path) >= max_path and value < earliest) or (len(path) > max_path):
+            earliest = value
+            max_path = len(path)
+
+        for next_item in graph.vertices[value]:
+            copy = list(path)
+            copy.append(next_item)
+            queue.enqueue(copy)
+
+    return earliest
