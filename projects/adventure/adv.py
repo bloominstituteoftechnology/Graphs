5936200ee1a34901af81982cf3c9146ebbae62ce
David Horstman <davidzilla27@gmail.com>
2020-08-13 00:00:32 -0700

    Pass traversal test for cross-shaped room

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,8 +10,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -29,6 +29,26 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+
+def path_to_next_unvisited_room(starting_room, visited):
+    queue = [[starting_room]]
+    inner_visited = set()
+    while len(queue) > 0:
+        current_path = queue.pop(0)
+        current_room = current_path[-1][-1]
+        if current_room.id not in visited:
+            return current_path[1:]
+        inner_visited.add(current_room.id)
+        for direction in current_room.get_exits():
+            room_in_direction = current_room.get_room_in_direction(direction)
+            if room_in_direction.id not in inner_visited:
+                queue.append([*current_path,
+                              (current_room.id,
+                               direction,
+                               room_in_direction)
+                              ])
+
+
 # Go in the first listed direction as far as you can
 # If all adjacent rooms have been explored, identify the nearest unexplored room and go there
 reverse_direction = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
@@ -42,31 +62,29 @@ rooms_to_visit = [
 ]
 
 while len(visited) < len(room_graph):
+    # Extract info and update traversal path and visited graph
     prev_room, direction_moved, current_room = rooms_to_visit.pop(0)
     traversal_path.append(direction_moved)
     visited[prev_room][direction_moved] = current_room.id
+
+    # If we're in a room for the first time
     if current_room.id not in visited:
         visited[current_room.id] = {x: '?' for x in current_room.get_exits()}
         visited[current_room.id][reverse_direction[direction_moved]] = prev_room
-        unvisited_directions = [dir_ for dir_,val in visited[current_room.id].items() if val == '?']
+        unvisited_directions = [
+            dir_ for dir_, val in visited[current_room.id].items() if val == '?']
+        # If this room isn't a dead-end, queue up a random unvisited room next
         if len(unvisited_directions) > 0:
-            next_direction = random.sample(unvisited_directions,1)[0]
-            rooms_to_visit.append((current_room.id, next_direction, current_room.get_room_in_direction(next_direction)))
+            next_direction = random.sample(unvisited_directions, 1)[0]
+            rooms_to_visit.append(
+                (current_room.id, next_direction, current_room.get_room_in_direction(next_direction)))
+        # If we're at a dead-end, find the path to the next room and add it to the queue
         else:
-            break
-
-
-# queue = [world.starting_room]
-# while len(queue) > 0:
-#     current_room = queue.pop(0)
-#     if current_room.id in visited:
-#         continue
-#     visited[current_room.id] = {x:'?' for x in current_room.get_exits()}
-#     for direction in current_room.get_exits():
-#         next_room = current_room.get_room_in_direction(direction)
-#         if next_room is not None:
-#             queue.append(next_room)
-#             visited[current_room.id][direction] = next_room.id
+            next_path = path_to_next_unvisited_room(
+                (prev_room, direction_moved, current_room),
+                visited)
+            if next_path:
+                rooms_to_visit.extend(next_path)
 
 
 # print(visited)
