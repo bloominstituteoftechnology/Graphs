6b3bb25e38789988c3557d54907f91cdf3467e9c
Rachele Edwards <dot.com1472@gmail.com>
2020-08-14 11:17:45 -0600

    made a while loop for empty list and doesn't work--get same error

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -61,6 +61,16 @@ while len(visit) < len(room_graph) -1:
         # because we know that direction leads to the room we were just in
         visit[current_room].remove[last]
 
+    #Step 1/2 -- for when list is empty and need to revert back into the room you just visited
+    # means we've been in that room before, now we're moving backwards until we reach a room that hasn't been visited
+    while len(visit) < 1:
+        #create a variable to remove last item from path so we can go that way
+        go_back = go_back_if_needed[-1]
+        #add direction into the traversal path
+        traversal_path.append(go_back)
+        #make the reverted move
+        player.travel(go_back)
+
 
     #Step two (after each move we need to mark it):
     #Create a variable for the first direction in current room--the move we just made
