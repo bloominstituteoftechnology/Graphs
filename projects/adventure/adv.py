917ad24eb409feeebffb54af246052733abaae2d
Rachele Edwards <dot.com1472@gmail.com>
2020-08-14 10:58:12 -0600

    coded pseudo but not allowing me to pop first direction...debugging

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -13,7 +13,7 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
@@ -31,28 +31,45 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 # traversal_path is the list of results/moves to go through the rooms
+traversal_path = []
 # Reverse directions so that each room can be the equivalent the opposites direction room move
-# path is a list to keep track of where to go when we need to reverse our steps
+rev_directions = {"n": "s", "e": "w", "s": "n", "w": "e"}
+# create a path that is a list to keep track of where to go when we need to reverse our steps
+go_back_if_needed = []
 # visit dictionary to have rooms as key and directions as value
+visit = {}
+#DRYing code aka being lazy XD
+current_room = player.current_room.id
+neighbors = player.current_room.get_exits()
 
 # First add players current room to visited, directions will be the exits (‘neighbors’)
+visit[current_room] = neighbors
 
 # REPL — while visit is still less than the graph of rooms -1, we’ll do all the steps
 #when len of visit is the length of graph, it will end the loop
-
-    # Step one, check if that current room’s id is in visit
+while len(visit) < len(room_graph) -1:
+    # Step one, check if that current room is in visit
     # If not, 
+    if current_room not in visit:
         # add to visit
+        visit[current_room] = neighbors
         # Create a variable for the last room/direction in the path
+        last = go_back_if_needed[-1]
         # Remove the last direction from visited — this will remove direction from visited
         # because we know that direction leads to the room we were just in
+        visit[current_room].remove[last]
 
 
 #Step two (after each move we need to mark it):
     #Create a variable for the first direction in current room--the move we just made
+    move_made = visit[current_room].pop(0)
     #Add that variable to traversal path to track where we've gone
+    traversal_path.append(move_made)
     #add the rooms opposite direction to path (next room to go to) -- so that we can go back if needed
+    go_back_if_needed.append(rev_directions[move_made])
     #let player move through directions
+    player.travel(move_made)
+
 
 
 
