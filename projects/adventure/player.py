f3185b2671689796384a3c614a84b4ab15ac7c43
john <jgreenberg@runbox.com>
2020-08-14 13:54:56 -0400

    sprint

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -7,5 +7,7 @@ class Player:
             self.current_room = next_room
             if (show_rooms):
                 next_room.print_room_description(self)
+            return True
         else:
             print("You cannot move in that direction.")
+            return False
