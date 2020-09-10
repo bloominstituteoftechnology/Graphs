ca8bef86d73702169f9439b59877bffb90ab4bfa
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-09-09 23:31:39 -0400

    Working on Day1

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,26 +13,27 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set ()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        return self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        
 
     def dft(self, starting_vertex):
         """
