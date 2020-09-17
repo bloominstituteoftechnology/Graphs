4dbeda6c811a22cae3984c14087e2d3b56c1082b
David Wright <davidtwright89@gmail.com>
2020-09-16 22:29:06 -0400

    Part 2: Implement Breadth-First Traversal

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,14 +1,14 @@
-"""
-Simple graph implementation
-"""
 from util import Stack, Queue  # These may come in handy
+from collections import deque
 
 class Graph:
 
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
+    def __repr__(self):
+        return str(self.vertices)
+    
     def add_vertex(self, vertex_id):
         if vertex_id not in self.vertices:
             self.vertices[vertex_id] = set()
@@ -21,11 +21,16 @@ class Graph:
         return self.vertices[vertex_id] if vertex_id in self.vertices else None
 
     def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        visited = set()
+        queue = deque()
+        queue.append(starting_vertex)
+        while len(queue) > 0:
+            currNode = queue.popleft()
+            if currNode not in visited:
+                visited.add(currNode)
+                print(currNode)
+                for neighbor in self.get_neighbors(currNode):
+                    queue.append(neighbor)
 
     def dft(self, starting_vertex):
         """
