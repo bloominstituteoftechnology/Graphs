143e9bb447b3f95332f76d3400c53d731f9b472a
Sal B Amer <iosdevsalman@gmail.com>
2020-09-19 05:32:15 -0700

    added graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -8,9 +8,11 @@ def earliest_ancestor(ancestors, starting_node):
         graph.add_vertex(vertex_1)
         graph.add.add_vertex(vertex_2)
 
-        //add edge to both vetrices
+        # add edge to both vetrices
         for vertex_1, vertex_2 in ancestors:
             graph.add_edge(vertex_1,vertex_2)
 
         target_vertex = None
-        
+
+        # find longest path
+        longest_path = 1
