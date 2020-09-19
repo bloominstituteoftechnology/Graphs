20cd04d2f134f1cf3cd7c91815de1ea814c49cce
Sal B Amer <iosdevsalman@gmail.com>
2020-09-19 05:28:49 -0700

    setup earliest Ancestors Day 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,16 @@
 
+from graph import Graph
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    for vertex_1, vertex_2 in ancestors:
+        graph.add_vertex(vertex_1)
+        graph.add.add_vertex(vertex_2)
+
+        //add edge to both vetrices
+        for vertex_1, vertex_2 in ancestors:
+            graph.add_edge(vertex_1,vertex_2)
+
+        target_vertex = None
+        
