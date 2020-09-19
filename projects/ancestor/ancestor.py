fe319f32600d045e70a33fdc55870dc696c824e9
Sal B Amer <iosdevsalman@gmail.com>
2020-09-19 05:43:47 -0700

    Update ancestor.py
    fixed DFS error after test

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -6,30 +6,27 @@ def earliest_ancestor(ancestors, starting_node):
 
     for vertex_1, vertex_2 in ancestors:
         graph.add_vertex(vertex_1)
-        graph.add.add_vertex(vertex_2)
+        graph.add_vertex(vertex_2)
 
-        # add edge to both vetrices
-        for vertex_1, vertex_2 in ancestors:
-            graph.add_edge(vertex_1,vertex_2)
+    for vertex_1, vertex_2 in ancestors:
+        graph.add_edge(vertex_1, vertex_2)
 
-        target_vertex = None
+    target_vertex = None
 
-        # find longest path
-        longest_path = 1
+    longest_path = 1
 
-        for vertex in graph.vertices:
+    for vertex in graph.vertices:
 
-            #starting vertex and node are destination
-            #import DFS from graph helper
-            path = graph.dfs(vertex, starting_node)
+        path = graph.dfs(vertex, starting_node)
 
-            if path:
-                print(path)
+        if path:
+            print(path)
 
-                if len(path) > longest_path:
-                    longest_path = len(path)
-                    target_vertex = vertex
-            elif not path and longest_path == 1:
-                target_vertex = -1
+            if len(path) > longest_path:
+                longest_path = len(path)
+                target_vertex = vertex
 
-        return target_vertex
+        elif not path and longest_path == 1:
+            target_vertex = -1
+
+    return target_vertex
