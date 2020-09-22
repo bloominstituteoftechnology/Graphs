14c9b546f623b52bd30cd11d54f11c286212339b
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-09-21 23:31:33 -0400

    Finished Day3 assignment

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,9 @@
+import random 
+import sys
+sys.path.append('../graph')
+
+from util import Stack, Queue
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -45,8 +51,25 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(0, num_users):
+            self.add_user(f'User{i+1}')
 
         # Create friendships
+        #generate all the possible friendship
+        possible_friendship = []
+        #generate all possible  firend for every single user
+        for user_id in self.users:
+            for friend_id in range(user_id +1, self.last_id +1):
+                possible_friendship.append((user_id, friend_id))
+        #randomly select x friendship
+        # x= num_users * avg_friendship //2
+        #shuffle the array and pick x friendship from the front of it
+        random.shuffle(possible_friendship)
+        num_friendship =  num_users * avg_friendships //2
+        for i in range(0, num_friendship):
+            friendship = possible_friendship[i]
+            self.add_friendship(friendship[0], friendship[1])
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -57,8 +80,28 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
+        #do the Bft for shortes path    
+        #create a queue
+        queue = Queue()
+        #create dic for visited vertex and path
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        #enqueue the queue with the starting user_id as a path
+        queue.enqueue([user_id])
+        #while queue is not empty
+        while queue.size()>0:
+            #dequeue the current path
+            current_path = queue.dequeue()
+            #get the current vertex from end of the path
+            current_vertex = current_path[-1]
+            if current_vertex not in visited:
+                visited[current_vertex] = current_path
+                #queue up all the neighbours as path
+                for neighbour in self.friendships[current_vertex]:
+                    new_path = current_path.copy()
+                    new_path.append(neighbour)
+                    queue.enqueue(new_path)
+            
+        
         return visited
 
 
