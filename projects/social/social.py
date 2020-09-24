4c136192d6c2d3d86fd5f764320d7d4efe6f12ec
David Wright <davidtwright89@gmail.com>
2020-09-23 22:15:57 -0400

    Added a more efficient populate_graph_linear() method to replace the populate_graph() method which was O(n^2)

--- projects/social/social.py
+++ projects/social/social.py
@@ -70,6 +70,40 @@ class SocialGraph:
             friendship = possible_friendships[i]
             self.add_friendship(friendship[0], friendship[1])
 
+    def populate_graph_linear(self, num_users, avg_friendships):
+        # Keep randomly making friendships until we've made the right amount
+        # Randomly select two vertices to become friends
+        # if it's a success, then increment number of friendships made
+        # else try again
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
+        for i in range(0, num_users):
+            self.add_user(f"User {i}")
+        
+        target_friendships = num_users * avg_friendships
+        total_friendships = 0
+        collisions = 0
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+            if self.add_friendship_linear(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
+        print(f"collisions: {collisions}")
+
+    def add_friendship_linear(self, user_id, friend_id):
+        if user_id == friend_id:
+            return False
+        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+            return False
+        else:
+            self.friendships[user_id].add(friend_id)
+            self.friendships[friend_id].add(user_id)
+            return True
+
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
@@ -148,7 +182,8 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(1000, 5)
+    # sg.populate_graph(1000, 5)
+    sg.populate_graph_linear(1000, 5)
     # print(sg.friendships)
     # connections = sg.get_all_social_paths(1)
     # print(connections)
