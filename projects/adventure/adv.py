6f68c3db2f36f313e4f767743bbafacb4aad0e6b
Thomas Huynh <t_huynh927@yahoo.com>
2020-09-23 19:49:52 -0700

    working on optimized adv

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -50,19 +50,38 @@ def dfs(visited, room, path, d):
 # dfs(visited, player.current_room, traversal_path, "")
 
 visited2 = {}
+stack = [player.current_room]
 
-def dfs2(visited, room, path, d):
-    if d:
-        path.append(d)
-    if room.name not in visited:
-        visited[room.name] = set()
-    for exit in room.get_exits():
-        room_in_dir = room.get_room_in_direction(exit)
-        if room_in_dir.name not in visited:
-            dfs2(visited, room_in_dir, path, exit)
-            path.append(opposite_dir[exit])
-
-
+def dfs2(path, iter=0):
+    d = ""
+    while stack:
+        if iter == 15:
+            return
+        curr_room = stack[-1]
+        if curr_room.name not in visited2:
+            visited2[curr_room.name] = set()
+        if d:
+            visited2[curr_room.name].add(opposite_dir[d])
+            path.append(d)
+        new_rooms = []
+        for exit in curr_room.get_exits():
+            room_in_dir = curr_room.get_room_in_direction(exit)
+            # print(room_in_dir.name, exit)
+            if room_in_dir.name not in visited2 or exit not in visited2[curr_room.name]:
+                d = exit
+                visited2[curr_room.name].add(d)
+                # print(d)
+                new_rooms.append(room_in_dir)
+                break
+                # path.append(opposite_dir[exit])
+        if new_rooms: 
+            stack.extend(new_rooms)
+        else:
+            stack.pop()
+        print(curr_room.name)
+        # print(visited2)
+        iter += 1
+dfs2(traversal_path)
 
 # player.travel('n', True)
 
