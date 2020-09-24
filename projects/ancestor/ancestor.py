b6bf85021db82ee6b861a3e5a82d03852d5b05d3
Joshua Rutkowski <54906829+joshrutkowski@users.noreply.github.com>
2020-09-24 14:37:31 -0400

    Update social.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -15,24 +15,29 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex_id):
         if vertex_id not in self.vertices:
             self.vertices[vertex_id] = set()
+
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
         else:
             raise IndexError("That vertex does not exist!")
+
     def get_neighbors(self, vertex_id):
         return self.vertices[vertex_id]
 
 
 def earliest_ancestor(ancestors, starting_node):
     ancestors_graph = Graph()
+
     for pair in ancestors:
         ancestors_graph.add_vertex(pair[0])
         ancestors_graph.add_vertex(pair[1])
         ancestors_graph.add_edge(pair[1], pair[0]) #bfs storing the path of how you get to the first instance of the earliest ancestor
+
     queue = Queue()
     queue.enqueue([starting_node])
     max_path_length = 1
