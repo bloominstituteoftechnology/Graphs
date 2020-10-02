bd5378c866e01230b94bc614c5321577db4e01f0
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-10-01 22:17:34 -0400

    Done with 1000 steps

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -20,9 +20,9 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 #map_file = "maps/test_line.txt"
 #map_file = "maps/test_cross.txt"
-map_file = "maps/test_loop.txt"
+#map_file = "maps/test_loop.txt"
 #map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -45,9 +45,6 @@ opposit_dic = {'n': 's',
 
 traversal_path = []
 
-#create a visited room dic
-
-#do bfs for unexplored room
 def bfs(current_room):
     """
     BFS for the unexplored room, then 
@@ -55,45 +52,18 @@ def bfs(current_room):
     """
     #add the visited room
     visited = set() 
-    # player.current_room = current_room
-    # current_dir = player.current_room.get_exits()
-    # exits = current_dir[-1]
-    # #get room in that direction
-    # new_room = player.current_room
-
-    #print('new_room', new_room.id)
-    #player.travel(current_dir)
-    #traversal_path.append(current_dir)
     #rooms to check
     q =[]
-    #visited_room=set()
-    # explored_room = []
-    # explored_dir=[]
-    # room_dir = []
-   
-    #print('bfs room', player.current_room.get_exits())
-    #enqueue with player current room exits direction
-    #empty list is the path to the list
     q.append((current_room, []))
     count = 0
     #create a visited vertex
     while len(q)>0:
         #dequeue the current room exist
         (room, path) = q.pop(0)
-        # current_dir = current_exit[-1]
-        # print('bfsssssssssss',current_dir)
-        # print('room_id', new_room.id)
-        # print(explored_room)
         if room in visited :
             continue
         else:
             visited.add(room)
-            #explored_dir.append(current_dir)
-            #track_room.append((new_room.id,current_dir))
-            #new_room = new_room.get_room_in_direction(current_dir)
-            #explored_room.append(new_room.id)
-            #room_dir.append(current_dir)
-            #print('expl ', explored_room)
         for  direction in visited_room[room]:
             if visited_room[room][direction] == '?':
                 return [path, direction]
@@ -104,7 +74,6 @@ def bfs(current_room):
                 q.append((next_room, update_path))
     return None
 import random
-#print('choiccccccccccccc', unexplored_dir)
 def dft(unexplored_dir):
     #create an empty  stack and add the starting room exists directions
     stack = Stack()
@@ -114,57 +83,37 @@ def dft(unexplored_dir):
     while stack.size() >0:
         #pop the current room exits direction
         current_exit = stack.pop()
-        print(current_exit)
-        #print(visited_room)
         move_dir =current_exit[-1]
         # if this direction is not explored
-        print('key_error', move_dir)
-        #print(visited_room[player.current_room.id])
-        if  visited_room[player.current_room.id][move_dir] =='?':
+        if move_dir not in visited_room[player.current_room.id]:
+             continue
+        elif  visited_room[player.current_room.id][move_dir] =='?':
             previous_room = player.current_room.id
-            #print(previous_room)
             #move player in that direction
             player.travel(move_dir)
             # store the movement in the traversal path
             traversal_path.append(move_dir)
-            #print('temp vis', visited_room)
             # update the unexplored direction in the dictionary
             visited_room[previous_room][move_dir] = player.current_room.id
-            #print(player.current_room.id)
             opposite_value = opposit_dic[move_dir]
             if player.current_room.id not in visited_room:
             #if visited_room[player.current_room.id]
                 visited_room[player.current_room.id] = {opposite_value:previous_room}
             else:
                 visited_room[player.current_room.id][opposite_value]= previous_room
-
-            #print(visited_room)
             # get all the neighbour room direction
-            print('current room', player.current_room.id)
-            
-            #print(visited_room[player.current_room.id])
             for direction in player.current_room.get_exits():
                 if direction not in visited_room[player.current_room.id]:
                     visited_room[player.current_room.id][direction]='?'
-                    #print('second room', player.current_room.id)
-                new_dir = current_exit.copy()
+                new_dir = []
                 new_dir.append(direction)
                 stack.push(new_dir)
-            #count =+1
-            #print(visited_room)
-   # print(player.current_room.get_exits())
-    #player.travel(player.current_room.get_exits())
-    print('*************************')
     unexplored_dir = bfs(player.current_room.id)
     print('unexplored_dir',unexplored_dir)
-    
-    #print('bfs return',bfs())
     if unexplored_dir !=None:
         for direction in unexplored_dir[0]:
             player.travel(direction)
             traversal_path.append(direction)
-            #print('bfs result ', unexplored_dir[1][-1])
-        #print('player current room', player.current_room.id)
         dft([unexplored_dir[1]])
 
 starting_dir = random.choice(player.current_room.get_exits())
@@ -172,11 +121,9 @@ starting_dir = random.choice(player.current_room.get_exits())
 visited_room ={player.current_room.id :{}}
 for direction in player.current_room.get_exits():
     visited_room[player.current_room.id][direction] ='?'
-#print(unexplored_dir)
+
 dft([starting_dir])
-# print(traversal_path)           
-# print(visited_room)
-        
+
 
 
 # Fill this out with directions to walk
