51503a5aed29f105d9a2fe11d178394d328678e8
Your name <a.m.verdine@gmail.com>
2020-10-15 00:43:55 -0400

    Part 6: Implement DFS

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -74,12 +74,20 @@ class Graph:
                     queue.enqueue({'current_vertex': neighbor, 'path': new_path})
 
     def dfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-        """
-        pass  # TODO
+        stack = Stack()
+        stack.push([starting_vertex])
+        visited = set()
+        while stack.size() > 0:
+            currPath = stack.pop()
+            currNode = currPath[-1]
+            if currNode == destination_vertex:
+                return currPath
+            if currNode not in visited:
+                visited.add(currNode)
+                for neighbor in self.get_neighbors(currNode):
+                    newPath = list(currPath)
+                    newPath.append(neighbor)
+                    stack.push(newPath)
 
     def dfs_recursive(self, starting_vertex, destination_vertex):
         """
