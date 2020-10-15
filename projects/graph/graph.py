e5c584223a198d11a7d0ff0d53b68e8fc652e7b5
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-10-14 19:59:25 -0500

    set up adding vertices edges and checking neighbors

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,19 +13,23 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else: 
+            print("Error vertex not found")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id] if vertex_id in self.vertices else None
 
     def bft(self, starting_vertex):
         """
