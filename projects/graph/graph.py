73cf83b3df0b62bb80519b7b5a87f0e047840507
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-10-14 20:04:04 -0500

    dft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -56,7 +56,16 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = set()
+        stack = Stack()
+        stack.push(starting_vertex)
+        while stack.size() > 0:
+            currNode = stack.pop()
+            if currNode not in visited:
+                visited.add(currNode)
+                print(currNode)
+                for neighbor in self.get_neighbors(currNode):
+                    stack.push(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
