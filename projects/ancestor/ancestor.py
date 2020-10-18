0d3849bb5bc9e1ad931728465882f3a7319c1be4
Your name <a.m.verdine@gmail.com>
2020-10-18 13:07:46 -0400

    Add Queue class and class methods

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.ppend(value)
+
+    def dequeue(self):
+        if self.size > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
