6e5a505883cada25c056cbb8984c34c8c9f8ecbd
Your name <a.m.verdine@gmail.com>
2020-10-18 23:26:42 -0400

    Create Graph // Perform BFS

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -3,10 +3,10 @@ class Queue():
         self.queue = []
 
     def enqueue(self, value):
-        self.queue.ppend(value)
+        self.queue.append(value)
 
     def dequeue(self):
-        if self.size > 0:
+        if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
@@ -17,4 +17,34 @@ class Queue():
 
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    parents_for_child = {}
+
+    # Build graph where IDs = vertices & relationships = edges
+    for relationship in ancestors:
+        parent = relationship[0]
+        child = relationship[1]
+        if child not in parents_for_child:
+            parents_for_child[child] = set()
+        parents_for_child[child].add(parent)
+
+    earliest_ancestor = -1
+
+    # Run BFS n graph and return last ancestor found
+    queue = Queue()
+    queue.enqueue(starting_node)
+    while queue.size() > 0:
+        curr_vertex = queue.dequeue()
+        if curr_vertex in parents_for_child:
+            parent_with_lowest_ID = None
+            for parent in parents_for_child[curr_vertex]:
+                if parent_with_lowest_ID is None:
+                    parent_with_lowest_ID = parent
+                elif parent < parent_with_lowest_ID:
+                    parent_with_lowest_ID = parent
+                queue.enqueue(parent)
+            if parent_with_lowest_ID is not None:
+                earliest_ancestor = parent_with_lowest_ID
+
+
+    return earliest_ancestor
+
