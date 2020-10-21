b4853ee62526a9ff7bb0f14c4682a352e4477c5d
Ariunbold <ariun1118@gmail.com>
2020-10-20 20:37:46 -0400

    Populate graph method is done

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,12 +1,15 @@
+import random
 class User:
     def __init__(self, name):
         self.name = name
-
+    def __repr__(self):
+        return f'User({repr(self.name)})'
 class SocialGraph:
     def __init__(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
+  
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -43,10 +46,21 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
+        
         # Add users
-
+        for i in range(num_users):
+            self.add_user(f'{i}')
         # Create friendships
+        possible_friendships = []
+        for user in self.users:
+            for friend in range(user + 1, self.last_id + 1):
+                possible_friendships.append((user, friend))
+
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +73,7 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        
         return visited
 
 
@@ -66,5 +81,6 @@ if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    print(sg.users)
+    # connections = sg.get_all_social_paths(1)
+    # print(connections)
