d8a6e165184d29c454c8c125da824ac5da890280
vy3191 <venkyafhs@gmail.com>
2020-10-20 21:27:11 -0400

    Modified practice1.py and practice2.py files

---
+++ practice2.py
@@ -0,0 +1,13 @@
+""" Write a function that takes a 2D binary array and returns the number of 1 islands.
+An island consists of 1s that are connected to the north, south, east or west. For example:
+islands = [
+  [0, 1, 0, 1, 0],
+  [1, 1, 0, 1, 1],
+  [0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0],
+  [1, 1, 0, 0, 0]]
+island_counter(islands) # returns 4
+"""
+# Solution: Turn this into a graph..what would be a vertex?
+def island_counter(islands):
+  pass
--- projects/ancestor/ancestor1.py
+++ projects/ancestor/ancestor1.py
@@ -6,6 +6,15 @@
 
 def earliest_ancestor(ancestors, starting_node):
     #Turn the ancestors list into an adjacency list
+    adjacency_list = {}    
+    for ancestor_pair in ancestors:
+      # add both vertices to adjacency_list
+      if ancestor_pair[0] not in adjacency_list:
+        adjacency_list[ancestor_pair[0]] = set()
+      if ancestor_pair[1] not in adjacency_list:
+        adjacency_list[ancestor_pair[1]] = set()
+      # add the edge between the two vertices
+      adjacency_list[ancestor_pair[1]].add(ancestor_pair[0])
 
     # create a queue
     queue = [ [starting_node] ]
@@ -17,12 +26,10 @@ def earliest_ancestor(ancestors, starting_node):
       #get the current vertex our of the path
       current_path = queue.pop(0)
       current_vertex = current_path[-1]
-
       # if the vertex has not been visited
       if current_vertex not in visited:
         #add the vertex to the visited set
         visited.add(current_vertex)
-
         # explore the neighbors
         # add the neighbor vertices to the queue(make sure to build the new paths)
         for neighbor in adjacency_list[current_vertex]:
