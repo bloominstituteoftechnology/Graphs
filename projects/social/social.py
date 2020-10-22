0e667d99ebf38817ac4d355c589739e2eb4454cf
Samuel Kraus <53798819+Skraus5628@users.noreply.github.com>
2020-10-22 18:11:20 -0500

    Update social.py

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,18 +1,14 @@
 from collections import deque
 import random
-import math 
-
-
+import math
 class User:
     def __init__(self, name):
         self.name = name
-
 class SocialGraph:
     def __init__(self):
         self.last_id = 0
         self.users = {} # {1: User("1"), 2: User("2"), ...}
-        self.friendships = {} # {1: {2, 3, 4}, 2: {1}, 2:{1}, 4:{1}}
-
+        self.friendships = {} # {1: {2, 3, 4}, 2: {1}, 3: {1}, 4: {1}}
     def add_friendship(self, user_id, friend_id):
         """
         Creates a bi-directional friendship
@@ -24,7 +20,6 @@ class SocialGraph:
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
-
     def add_user(self, name):
         """
         Create a new user with a sequential integer ID
@@ -32,65 +27,121 @@ class SocialGraph:
         self.last_id += 1  # automatically increment the ID to assign the new user
         self.users[self.last_id] = User(name) # {1: User("mari")}
         self.friendships[self.last_id] = set() # {1: {}}
-
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
-
         # Add users
-
         for i in range(num_users):
-            self.add_user(f"User{i}")
-
+            self.add_user(f"User {i}")
         # Create friendships
-        #Generate all poss. friendships and put into array
-        # 3 users (0 1 2)
-        #[(0,1), (0,2), (1,2)]
+        # Generate all the possible friendships and put them into an array
+        # 3 users (0, 1, 2)
+        # [(0, 1), (0, 2), (1, 2)]
         possible_friendships = []
         for user_id in self.users:
-            #prevent duplicate friendhips, create from user_id + 1
+            # To prevent duplicate friendship, create from user_id + 1
             for friend_id in range(user_id + 1, self.last_id + 1):
                 possible_friendships.append((user_id, friend_id))
-
-
-        # Shuffle friends array
-        #[(1,2), (0,1), (0,2)]
+        # Shuffle the friendship array
+        # [(1, 2), (0, 1), (0, 2)]
         random.shuffle(possible_friendships)
-
-        #Take first num_users * avg_friendhips / 2 and that will be friendships for the graph
+        # Take the first num_users * avg_friendships / 2 and that will be the friendships for that graph
         for i in range(math.floor(num_users * avg_friendships / 2)):
             friendship = possible_friendships[i]
             self.add_friendship(friendship[0], friendship[1])
-
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        queue = deque()
+        visited[user_id] = [user_id]
+        for friend_id in self.friendships[user_id]:
+            queue.append((friend_id, [friend_id]))
+        
+        while len(queue) > 0:
+            currNode = queue.popleft()
+            curr_friend_id = currNode[0]
+            curr_path = currNode[1]
+            if curr_friend_id not in visited:
+                visited[curr_friend_id] = curr_path
+                for new_friend_id in self.friendships[curr_friend_id]:
+                    if new_friend_id not in visited:
+                        new_path = list(curr_path) + [new_friend_id]
+                        queue.append((new_friend_id, new_path))
+
         return visited
 
 
+    def count_direct_friends(self, friend_id):
+        if friend_id not in self.friendships:
+            print(f"WARNING: No user exists with friend_id: {friend_id}")
+            return -1
+        return len(self.friendships[friend_id])
+
+    def count_extended_network(self, friend_id):
+        if friend_id not in self.friendships:
+            print(f"WARNING: No user exists with friend_id: {friend_id}")
+            return -1
+        return len(self.get_all_social_paths(friend_id))
+
+    def percent_of_total_users_in_extended_network(self, friend_id):
+        if friend_id not in self.friendships:
+            print(f"WARNING: No user exists with friend_id: {friend_id}")
+            return -1
+        return (self.count_extended_network(friend_id) / len(self.users)) * 100
+
+    def avg_degree_of_separation_in_extended_network(self, friend_id):
+        if friend_id not in self.friendships:
+            print(f"WARNING: No user exists with friend_id: {friend_id}")
+            return -1
+        degrees_of_separation = []
+        extended_network = self.get_all_social_paths
+        degrees_of_separation = [len(p) for p in self.get_all_social_paths(friend_id).values()]
+        return sum(degrees_of_separation) / len(degrees_of_separation)
+
+    def print_stats(self):
+        direct_friends_count_list = []
+        extended_network_count_list = []
+        avg_degree_of_separation_in_extended_network_list = []
+
+        for friend_id in self.users.keys():
+            direct_friends_count_list.append(self.count_direct_friends(friend_id))
+            extended_network_count_list.append(self.count_extended_network(friend_id))
+            avg_degree_of_separation_in_extended_network_list.append(self.avg_degree_of_separation_in_extended_network(friend_id))
+
+        total_avg_direct_friends = self.average(direct_friends_count_list)
+        total_avg_extended_network = self.average(extended_network_count_list)
+        total_avg_percent_of_total_users_in_extended_network = (total_avg_extended_network / len(self.users)) * 100
+        total_avg_degree_of_separation_in_extended_network = self.average(avg_degree_of_separation_in_extended_network_list)
+
+        print(f"""
+        Total number of users in social network: {len(self.users)}
+        Average number of friends per user: {total_avg_direct_friends}
+        Average size of a user's extended network: {total_avg_extended_network}
+        Percentage of total users in a user's extended network: {total_avg_percent_of_total_users_in_extended_network}%
+        Average degrees of separation in a user's extended network: {total_avg_degree_of_separation_in_extended_network}
+        """)
+
+    def average(self, list_of_values):
+        return sum(list_of_values) / len(list_of_values)
+
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
-    print(connections)
+    sg.populate_graph(1000, 5)
+    #print(sg.friendships)
+    #connections = sg.get_all_social_paths(1)
+    #print(connections)
+    sg.print_stats()
