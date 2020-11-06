1890e15a4912f4a8a29bb1f6c55b5fe778f4fbec
emilme <emilme@sir.mle>
2020-11-05 23:24:05 -0800

    Sprint attempt one...recursive approach

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -35,6 +35,73 @@ traversal_path = []
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+############################################################################################################################################################################################################################
+roomDirections = {}
+rooms = {}
+opposite = {'n':'s', 's':'n', 'e':'w', 'w': 'e'}
+#less wordy
+currRoomExits = player.current_room.get_exits()  
+curRoomId =   player.current_room.id 
+#adds starting room to rooms
+for i in currRoomExits:
+    roomDirections[i] = player.current_room.get_room_in_direction(i).id   
+rooms[player.current_room.id] = roomDirections
+  
+def visitRoom():
+    currRoomExits = player.current_room.get_exits()  
+    curRoomId =   player.current_room.id 
+    roomDirections = {}
+    travelDirection = 0
+    print('starting room',curRoomId)
+    if len(rooms) != 20:
+
+        if curRoomId not in rooms:
+            #adds to roomDirections
+            for direction in currRoomExits:
+                roomDirections[direction] = player.current_room.get_room_in_direction(direction).id
+
+            rooms[curRoomId] = roomDirections
+            visited_rooms.add(curRoomId)
+            rooms[curRoomId][opposite[traversal_path[-1]]] = 'origin'
+
+
+        if len(rooms[curRoomId]) != 1:
+            while currRoomExits[travelDirection] not in rooms[curRoomId] or rooms[curRoomId][currRoomExits[travelDirection]] == 'origin':
+                travelDirection = travelDirection +1
+
+            
+            #makes sure the road you want to travel to has not been traveled in this direction and is not an origin road so you don't cut yourself off
+            if currRoomExits[travelDirection] in rooms[curRoomId] and rooms[curRoomId][currRoomExits[travelDirection]] != 'origin':
+                #before you travel make sure you remove this road from currentRoom so you can't travel it again
+                rooms[curRoomId].pop(currRoomExits[travelDirection])
+
+                player.travel(currRoomExits[travelDirection])
+                traversal_path.append(currRoomExits[travelDirection])
+                visitRoom()
+
+
+        if len(rooms[curRoomId]) == 1:
+
+            #one lenth is one that means all that is left is the origin so just go back
+            if rooms[curRoomId][currRoomExits[travelDirection]] != 'origin':
+                while currRoomExits[travelDirection] not in rooms[curRoomId]:
+                    travelDirection +=1
+
+            rooms[curRoomId].pop(currRoomExits[travelDirection])
+
+            player.travel(currRoomExits[travelDirection])
+            traversal_path.append(currRoomExits[travelDirection])
+
+
+            visitRoom()
+    print(len(traversal_path), len(visited_rooms),'lenoftraversalpath............................................................................................................................................')
+
+    print('EndingRooms',  rooms)
+
+
+
+visitRoom()
+print(len(traversal_path))
 
 for move in traversal_path:
     player.travel(move)
@@ -47,16 +114,65 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
+'''
 #######
 # UNCOMMENT TO WALK AROUND
 #######
+roomDirections = {}
+rooms = {}
+opposite = {'n':'s', 's':'n', 'e':'w', 'w': 'e'}
+
+#prints current room basic info
 player.current_room.print_room_description(player)
+
+#adds starting room to rooms
+if player.current_room.id not in rooms:
+    for i in player.current_room.get_exits():
+        roomDirections[i] = player.current_room.get_room_in_direction(i).id   
+    rooms[player.current_room.id] = roomDirections
+
 while True:
+    #resets roomDirections
+    roomDirections = {}
+
+    #takes input direction      (not important)
     cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
+
+
+    if cmds[0] in ["n", "s", "e", "w"] and cmds[0] in rooms[player.current_room.id]:
+        #travels
+        rooms[player.current_room.id].pop(cmds[0])
+
         player.travel(cmds[0], True)
+
+
+        #addes to rooms
+        if player.current_room.id not in rooms:
+            #adds to roomDirections
+            for i in player.current_room.get_exits():
+                roomDirections[i] = player.current_room.get_room_in_direction(i).id
+
+            rooms[player.current_room.id] = roomDirections
+
+            rooms[player.current_room.id][opposite[cmds[0]]] = 'origin'
+
+
+
+    #quits game                (not important)
     elif cmds[0] == "q":
         break
+    #make sure its nsew        (not important)
     else:
         print("I did not understand that command.")
+
+
+
+    #TEST PRINTS
+    print('current',roomDirections)
+    for i in rooms:
+        print(i, rooms[i])
+    print(player.current_room.get_exits()[0])  
+'''
+
+
+
