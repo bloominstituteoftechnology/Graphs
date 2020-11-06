ec7676ebf110465beffc508fba96b44d1b50c942
emilme <emilme@sir.mle>
2020-11-06 08:04:23 -0800

    adv.py passing with 999 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -29,79 +29,98 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-
-
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-############################################################################################################################################################################################################################
+#MY_CODE###############################################################################################################################################################################################################
+'''
+roomDirections....keeps track of visited rooms and their none traveld directions
+
+rooms.............temporarily holds a given nodes directions
+
+opposite..........dictionary used to get the oposite of a given direction
+'''
 roomDirections = {}
 rooms = {}
 opposite = {'n':'s', 's':'n', 'e':'w', 'w': 'e'}
-#less wordy
+
+#less wordy 
 currRoomExits = player.current_room.get_exits()  
 curRoomId =   player.current_room.id 
-#adds starting room to rooms
+
+#adds starting room to rooms dictionary
 for i in currRoomExits:
     roomDirections[i] = player.current_room.get_room_in_direction(i).id   
 rooms[player.current_room.id] = roomDirections
-  
-def visitRoom():
+
+#until all 500 rooms have been visited
+while len(rooms) != 500:
+    '''
+    bug... it was traveling twice in one loop and I was tired 
+    so this was just the quickest solution at the time. It's fine.
+    '''
+    bug = 0
+
+    #updates less wordy varibles
     currRoomExits = player.current_room.get_exits()  
-    curRoomId =   player.current_room.id 
+    curRoomId =   player.current_room.id
+
+    #resets roomDirections and travel direction for new room 
     roomDirections = {}
     travelDirection = 0
-    print('starting room',curRoomId)
-    if len(rooms) != 20:
 
-        if curRoomId not in rooms:
-            #adds to roomDirections
-            for direction in currRoomExits:
-                roomDirections[direction] = player.current_room.get_room_in_direction(direction).id
+    if curRoomId not in rooms:
+        #adds current rooms original directions to roomDirections
+        for direction in currRoomExits:
+            roomDirections[direction] = player.current_room.get_room_in_direction(direction).id
 
-            rooms[curRoomId] = roomDirections
-            visited_rooms.add(curRoomId)
-            rooms[curRoomId][opposite[traversal_path[-1]]] = 'origin'
+        #adds roomDirections to it's room in rooms
+        rooms[curRoomId] = roomDirections
 
-
-        if len(rooms[curRoomId]) != 1:
-            while currRoomExits[travelDirection] not in rooms[curRoomId] or rooms[curRoomId][currRoomExits[travelDirection]] == 'origin':
-                travelDirection = travelDirection +1
-
-            
-            #makes sure the road you want to travel to has not been traveled in this direction and is not an origin road so you don't cut yourself off
-            if currRoomExits[travelDirection] in rooms[curRoomId] and rooms[curRoomId][currRoomExits[travelDirection]] != 'origin':
-                #before you travel make sure you remove this road from currentRoom so you can't travel it again
-                rooms[curRoomId].pop(currRoomExits[travelDirection])
-
-                player.travel(currRoomExits[travelDirection])
-                traversal_path.append(currRoomExits[travelDirection])
-                visitRoom()
+        #sets the opposite direction of the last known direction on the current node to 'origin'
+        rooms[curRoomId][opposite[traversal_path[-1]]] = 'origin'
 
 
-        if len(rooms[curRoomId]) == 1:
+    #if there is more than one direction
+    if len(rooms[curRoomId]) != 1:
+        #if you've already traveled the road or it's your way back then try the next travel direction
+        while currRoomExits[travelDirection] not in rooms[curRoomId] or rooms[curRoomId][currRoomExits[travelDirection]] == 'origin':
+            '''
+            travelDirection...a number used to access the original directions list of the current node.
+            if rooms says you can't travel a road you just up the number to change directions.
+            '''
+            travelDirection = travelDirection +1
 
-            #one lenth is one that means all that is left is the origin so just go back
-            if rooms[curRoomId][currRoomExits[travelDirection]] != 'origin':
-                while currRoomExits[travelDirection] not in rooms[curRoomId]:
-                    travelDirection +=1
-
+        
+        #makes sure the road you want to travel to has not been traveled in this direction and is not an origin road so you don't cut yourself off
+        if currRoomExits[travelDirection] in rooms[curRoomId] and rooms[curRoomId][currRoomExits[travelDirection]] != 'origin':
+            #before you travel remove this road from currentRoom so you can't travel it again
             rooms[curRoomId].pop(currRoomExits[travelDirection])
 
+            #traveling...
             player.travel(currRoomExits[travelDirection])
             traversal_path.append(currRoomExits[travelDirection])
 
+            #here that bug from earlier lol this let's us know traveling has already happened this loop
+            bug += 1
 
-            visitRoom()
-    print(len(traversal_path), len(visited_rooms),'lenoftraversalpath............................................................................................................................................')
+    #This checks if all that's left is to go back then it goes back to the last known node with more than one exit
+    if len(rooms[curRoomId]) == 1 and bug == 0:
 
-    print('EndingRooms',  rooms)
+        #same princible as line 88
+        while currRoomExits[travelDirection] not in rooms[curRoomId]:
+            travelDirection +=1
 
+        #traveling...
+        rooms[curRoomId].pop(currRoomExits[travelDirection])
+        player.travel(currRoomExits[travelDirection])
+        traversal_path.append(currRoomExits[travelDirection])
 
-
-visitRoom()
-print(len(traversal_path))
+#resets for test
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
+######################################################################################################################################################################################################################
 
 for move in traversal_path:
     player.travel(move)
@@ -112,7 +131,7 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
+    print(traversal_path)
 
 '''
 #######
@@ -131,6 +150,8 @@ if player.current_room.id not in rooms:
         roomDirections[i] = player.current_room.get_room_in_direction(i).id   
     rooms[player.current_room.id] = roomDirections
 
+
+#original testing code####################################################################
 while True:
     #resets roomDirections
     roomDirections = {}
