0eb075768812ffc30a35bff380ac1c473eb4321e
TheNativeDev <thenativedev@outlook.com>
2020-11-17 23:22:13 -0800

    social complete

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+from util import Queue
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -32,39 +37,66 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
-        # Reset graph
+        # Reset
         self.last_id = 0
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
-
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
         # Add users
-
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id+1):
+                possible_friendships.append((user_id, friend_id))
         # Create friendships
+        random.shuffle(possible_friendships)
+        # Create n friendship where n = avg_friendships * num_users // 2
+        # avg_friendships = total_friendships / num_users
+        # total_friendships = avg_friendships * num_users
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        # a dictionary, keys are equal to established connections and the value is a list of how to get there
+        queue = Queue()
+
+        queue.enqueue([user_id])
+        while queue.size() > 0:
+            current_path = queue.dequeue()
+            current_node = current_path[-1]
+            if current_node not in visited.keys():
+                visited[current_node] = current_path
+            for friend_id in self.friendships[current_node]:
+                new_path = [*current_path, friend_id]
+                if friend_id not in visited.keys():
+                    queue.enqueue(new_path)
+                elif len(new_path) < len(visited[friend_id]):
+                    visited[friend_id] = new_path
+
         return visited
 
+
+test_example = {1: {8, 10, 5}, 2: {10, 5, 7}, 3: {4}, 4: {9, 3}, 5: {
+    8, 1, 2}, 6: {10}, 7: {2}, 8: {1, 5}, 9: {4}, 10: {1, 2, 6}}
 
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
     print(sg.friendships)
+    # sg.friendships = {1: {8, 10, 5}, 2: {10, 5, 7}, 3: {4}, 4: {9, 3}, 5: {8, 1, 2}, 6: {10}, 7: {2}, 8: {1, 5}, 9: {4}, 10: {1, 2, 6}}
     connections = sg.get_all_social_paths(1)
     print(connections)
