b7649e63523df996eee9868485a91b5ff2ba492e
Veto Ramirez <vetorobert3@gmail.com>
2020-11-17 19:05:34 -0800

    Did ancestors cide from Mari

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,21 +1,23 @@
-from collections import deque
+"""
+Understand:
+Make a function that takes any given number in a dataset and returns the earliest known ancestor.
 
-def earliest_ancestor(ancestors, starting_node):
-    """
-    Understand:
-    Make a function that takes any given number in a dataset and returns the earliest known ancestor.
+Plan:
 
-    Plan:
-
-    1. Translate into graph terminoloy
+1. Translate into graph terminoloy
     vertex = individual 
     edge = connection of parent and child
     weight = None
 
-    2. Build the graph
-    We will need to do a dfs to traverse the graph all the way down to the earliest (lowest) ancestor (number). We will stop once we find the earliest ancestor which is why we use a DFS.
+2. Build the graph
+We will need to do a dfs to traverse the graph all the way down to the earliest (lowest) ancestor (number). We will stop once we find the earliest ancestor which is why we use a DFS.
 
-    """
+"""
+
+from collections import deque
+from collections import defaultdict
+
+def earliest_ancestor(ancestors, starting_node):
     class Stack():
         def __init__(self):
             self.stack = []
@@ -29,11 +31,11 @@ def earliest_ancestor(ancestors, starting_node):
         def size(self):
             return len(self.stack)
 
-    ancestors = {}
+    # ancestors = {}
 
-    visited = set()
-    stack = deque()
-    stack.append([starting_node])
+    # visited = set()
+    # stack = deque()
+    # stack.append([starting_node])
 
     # while len(stack) > 0:
     #     currPath = stack.pop()
@@ -47,12 +49,44 @@ def earliest_ancestor(ancestors, starting_node):
     #             newPath.append(neighbor)
     #             stack.append(newPath)
 
+    # while len(stack) > 0:
+    #     currPath = stack.pop()
+    #     currNode = currPath[-1]
+    #     if currNode not in visited:
+    #         visited.add(currNode)
+    #         for individual in ancestors[currNode]:
+    #             newPath = list(currPath)
+    #             newPath.append(individual)
+    #             stack.append(newPath)
+
+    # Mari's code...
+
+    graph = createGraph(ancestors)
+
+    earliestAncestor = ((starting_node, 0))
+    stack = deque()
+    stack.append((starting_node, 0))
+    visited = set()
+
     while len(stack) > 0:
-        currPath = stack.pop()
-        currNode = currPath[-1]
-        if currNode not in visited:
-            visited.add(currNode)
-            for individual in ancestors[currNode]:
-                newPath = list(currPath)
-                newPath.append(individual)
-                stack.append(newPath)
+        curr = stack.pop()
+        currNode, distance = curr[0], curr[1]
+        visited.add(curr)
+
+        if currNode not in graph:
+            if distance > earliestAncestor[1]:
+                earliestAncestor = curr
+            elif distance == earliestAncestor[1] and currNode < earliestAncestor[0]:
+                earliestAncestor = curr
+        else:
+            for ancestor in graph[currNode]:
+                if ancestor not in visited:
+                    stack.append((ancestor, distance + 1))
+    return earliestAncestor[0] if earliestAncestor[0] != starting_node else - 1
+
+def createGraph(edges):
+    graph = defaultdict(set)
+    for edge in edges:
+        ancestor, child = edge[0], edge[1]
+        graph[child].add(ancestor)
+    return graph
