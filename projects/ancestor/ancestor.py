0aaa90c23dd0ad88af3651e072f3fd7ac8ff7009
scrunts23 <scrunts23@gmail.com>
2020-11-19 17:09:13 -0600

    social and ancestor completed

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,67 @@
 
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        return self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    dict_ancestor = {}
+    stack = Stack()
+    visited = set()
+    longest_path = []
+
+    for i, x in enumerate(ancestors):
+        dict_ancestor[i] = x
+
+    for i, v in dict_ancestor.items():
+        if v[1] is starting_node:
+            stack.push([i])
+
+
+    size = stack.size()
+
+    if stack.size() > 0:
+        while size > 0:
+            # print(dict_ancestor)
+            # print(stack.stack)
+            current_node = stack.pop()
+            single_node = current_node[-1]
+            size = stack.size()
+
+            if single_node not in visited:
+                # print("sn not in visited")
+                visited.add(single_node)
+                parent = dict_ancestor[single_node][0]
+                # print(parent)
+
+                for i, v in dict_ancestor.items():
+                    if v[1] is parent:
+                        new_path = current_node[:]
+                        new_path.append(i)
+                        stack.push(new_path)
+                        # print("new_path:", new_path)
+
+                        if len(new_path) > len(longest_path):
+                            longest_path = new_path
+                    elif i == len(dict_ancestor) - 1:
+                        longest_path = current_node
+
+
+                size = stack.size()
+
+        final_answer = (dict_ancestor[longest_path[-1]][0])
+        return final_answer 
+    else:
+        return -1
--- projects/social/social.py
+++ projects/social/social.py
@@ -1,70 +1,108 @@
+import random
+import queue
+
 class User:
     def __init__(self, name):
         self.name = name
 
 class SocialGraph:
     def __init__(self):
-        self.last_id = 0
+        self.lastID = 0
         self.users = {}
         self.friendships = {}
 
-    def add_friendship(self, user_id, friend_id):
+    def addFriendship(self, userID, friendID):
         """
         Creates a bi-directional friendship
         """
-        if user_id == friend_id:
+        if userID == friendID:
             print("WARNING: You cannot be friends with yourself")
-        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+        elif friendID in self.friendships[userID] or userID in self.friendships[friendID]:
             print("WARNING: Friendship already exists")
         else:
-            self.friendships[user_id].add(friend_id)
-            self.friendships[friend_id].add(user_id)
+            self.friendships[userID].add(friendID)
+            self.friendships[friendID].add(userID)
 
-    def add_user(self, name):
+    def addUser(self, name):
         """
         Create a new user with a sequential integer ID
         """
-        self.last_id += 1  # automatically increment the ID to assign the new user
-        self.users[self.last_id] = User(name)
-        self.friendships[self.last_id] = set()
+        self.lastID += 1  # automatically increment the ID to assign the new user
+        self.users[self.lastID] = User(name)
+        self.friendships[self.lastID] = set()
 
-    def populate_graph(self, num_users, avg_friendships):
+    def populateGraph(self, numUsers, avgFriendships):
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
-        self.last_id = 0
+        self.lastID = 0
         self.users = {}
         self.friendships = {}
         # !!!! IMPLEMENT ME
 
-        # Add users
+        for i in range(0, numUsers):
+            self.addUser(i)
+
+        total_friendships = numUsers * avgFriendships
+        call_add_friendships = total_friendships // 2
+
+        user_ids = range(1, numUsers + 1)
+
+        friends = []
+        for user in user_ids:
+            for i in range(user + 1, numUsers + 1):
+                friends.append((user, i))
+
+        # print("friends",friends)
+        random.shuffle(friends)
+
+        # print("shuffle_friends", friends)
+        friends_to_make = friends[:call_add_friendships]
+
+        for f in friends_to_make:
+            self.addFriendship(f[0], f[1])
+
 
         # Create friendships
 
-    def get_all_social_paths(self, user_id):
+    def getAllSocialPaths(self, userID):
         """
-        Takes a user's user_id as an argument
-
+        Takes a user's userID as an argument
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
+
+        q = queue.Queue()
         visited = {}  # Note that this is a dictionary, not a set
+        q.put([userID])
         # !!!! IMPLEMENT ME
+
+        while not q.empty():
+            current_path = q.get()
+            user_or_friend = current_path[-1]
+
+            if user_or_friend not in visited:
+                visited[user_or_friend] = current_path
+
+                edges = self.friendships[user_or_friend]
+
+                for e in edges:
+                    path_copy = current_path[:]
+                    path_copy.append(e)
+                    q.put(path_copy)
+
         return visited
 
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populateGraph(10, 2)
     print(sg.friendships)
-    connections = sg.get_all_social_paths(1)
+    connections = sg.getAllSocialPaths(1)
     print(connections)
