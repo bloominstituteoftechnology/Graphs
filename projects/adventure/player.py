bd28c7d5994bde252b59e5ea6114feef36c72897
Claudia Maciel <clc_80@yahoo.com>
2020-11-19 20:54:01 -0800

    cleaned up code

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -1,6 +1,7 @@
 class Player:
     def __init__(self, starting_room):
         self.current_room = starting_room
+        
     def travel(self, direction, show_rooms = False):
         next_room = self.current_room.get_room_in_direction(direction)
         if next_room is not None:
