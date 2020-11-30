8248995f38ef9f004eef2b3f7bdccc409edf810d
kmk028 <kmk028@users.noreply.github.com>
2020-11-29 22:02:28 -0800

    adventure game completed

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,13 +1,16 @@
 from room import Room
 from player import Player
 from world import World
-
+from collections import deque
 import random
 from ast import literal_eval
 
 # Load world
 world = World()
 
+#construct a  traversal graph
+#do dft for finding all the possible  room player can move
+#do bfs for finding unexplored direction
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
@@ -24,12 +27,82 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
+player.current_room = world.starting_room
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+backtrack = {
+    'n': 's',
+    'e': 'w',
+    'w': 'e',
+    's': 'n'}
 
+def bfs(curr_room):
+    visited = set()
+    queue = deque()
+    queue.append((curr_room,[]))
+
+    while len(queue)>0:
+        (room,path) = queue.popleft()
+        if room in visited:
+            continue
+        visited.add(room)
+        for direction in visited_room[room]:
+            if visited_room[room][direction] == '?':
+                return [path,direction]
+            else:
+                newPath = path.copy()
+                newPath.append(direction)
+                next_room = visited_room[room][direction]
+                queue.append((next_room,newPath))
+
+def dft(unexplored_diection):
+    stack = deque()
+    stack.append(unexplored_diection)
+
+    while len(stack)>0:
+        curr_exit = stack.pop()
+        move_direction = curr_exit[-1]
+        if move_direction not in visited_room[player.current_room.id]:
+            continue
+        elif visited_room[player.current_room.id][move_direction] == '?':
+            prev_room = player.current_room.id
+            player.travel(move_direction)
+            traversal_path.append(move_direction)
+
+            visited_room[prev_room][move_direction] = player.current_room.id
+            opposite_val = backtrack[move_direction]
+
+            if player.current_room.id not in visited_room:
+                visited_room[player.current_room.id] = {opposite_val:prev_room}
+            else:
+                visited_room[player.current_room.id][opposite_val] = prev_room
+
+            for direction in player.current_room.get_exits():
+                if direction not in visited_room[player.current_room.id]:
+                    visited_room[player.current_room.id][direction] = '?'
+                    new_dir = []
+                    new_dir.append(direction)
+                    stack.append(new_dir)
+
+    unexplored_diection = bfs(player.current_room.id)
+
+    if unexplored_diection != None:
+        for direction in unexplored_diection[0]:
+            player.travel(direction)
+            traversal_path.append(direction)
+        dft([unexplored_diection[1]])
+
+
+starting_dir = random.choice(player.current_room.get_exits())
+
+visited_room = {player.current_room.id:{}}
+for direction in player.current_room.get_exits():
+    visited_room[player.current_room.id][direction] = '?'
+
+dft(starting_dir)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -46,17 +119,15 @@ else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# #######
+# # UNCOMMENT TO WALK AROUND
+# #######
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/social/social.py
+++ projects/social/social.py
@@ -74,7 +74,7 @@ class SocialGraph:
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        
         return visited
 
 
