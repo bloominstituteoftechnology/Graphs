421b7380c38a53deedfa35f80b6082d2f5187057
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-03-31 15:35:03 -0400

    holy fucking christ

--- src/traversal/bidirectional_search.py
+++ src/traversal/bidirectional_search.py
@@ -1,14 +1,118 @@
 from collections import deque
 
 
+# Bi-Directional Search
+# Operates by running two BFS simultaneously, one from the starting node and one from the destination node
+# Each iteration, we'll take one step forward and one step back
+# Once the paths overlap, we know we've hit a short path
+#
+# Queue for FORWARD --- represents the next nodes to visit in the starting_node's BST
+# Queue for BACKWARD -- represents the next nodes to visit in the destination_node's BST
+#
+#
+# https://www.cs.princeton.edu/courses/archive/spr06/cos423/Handouts/EPP%20shortest%20path%20algorithms.pdf
+
+
 def bidirectional_search(graph, start, destination):
+    # print(f"\n\n--------------------\nRUNNING FUNCTION---args: start={start}, dest={destination}")
+
+    if start == destination:
+        return [start]
+
     q1 = deque()
     q2 = deque()
     q1.append(start)
     q2.append(destination)
     v1 = set()
     v2 = set()
-    v1.add(start)
+    visited = set()
+    path_forward = deque()
+    path_backward = deque()
+
+    while q1 or q2:
+        step_f = q1.popleft()
+        step_b = q2.popleft()
+        iter_msg = f"""
+ITER --- 
+    step_f={step_f} 
+    step_b={step_b}
+    
+    v1={v1}
+    v2={v2}
+    
+    path_f={path_forward}
+    path_b={path_backward} 
+"""
+        print(iter_msg)
+
+        try:
+            b_is_last_f = path_forward[-1] == step_b
+        except IndexError:
+            b_is_last_f = False
+        try:
+            f_is_last_b = path_backward[0] == step_f
+        except IndexError:
+            f_is_last_b = False
+        f_is_b = step_f == step_b
+
+        tie_f = step_f in v2
+        tie_b =  step_b in v1
+
+        if f_is_b:
+            if f_is_last_b and b_is_last_f:
+                return list(path_forward + path_backward)
+            elif not (f_is_last_b or b_is_last_f):
+                path_forward.append(step_f)
+                return list(path_forward + path_backward)
+            else:
+                path_forward.append(step_f)
+
+        if f_is_last_b and b_is_last_f:
+            q1.clear()
+            q2.clear()
+            return list(path_forward + path_backward)
+        if not f_is_last_b and not b_is_last_f:
+            if tie_f:
+                path_forward.append(step_f)
+                return list(path_forward + path_backward)
+            if tie_b:
+                path_forward.append(step_b)
+                return list(path_forward + path_backward)
+
+            path_forward.append(step_f)
+            path_backward.appendleft(step_b)
+        elif not b_is_last_f:
+            return list(path_forward + path_backward)
+        elif not f_is_last_b:
+            return list(path_forward + path_backward)
+
+        for connection in graph[step_f]:
+            if connection not in v1:
+                v1.add(connection)
+                visited.add(connection)
+                q1.append(connection)
+
+        for connection in graph[step_b]:
+            if connection not in v2:
+                v2.add(connection)
+                visited.add(connection)
+                q2.append(connection)
+
+
+def bidirectional_search_printy(graph, start, destination):
+    print(f"\n\n--------------------\nRUNNING FUNCTION---args: start={start}, dest={destination}")
+
+    if start == destination:
+        return [start]
+
+    q1 = deque()
+    q2 = deque()
+    q1.append(start)
+    q2.append(destination)
+    v1 = set()
+    v2 = set()
+    visited = set()
+    # v1.add(start)
     path_forward = deque()
     path_backward = deque()
 
@@ -19,26 +123,85 @@ def bidirectional_search(graph, start, destination):
             print(f"--------")
         print(f"\tq1: {q1}\n\tq2: {q2}")
         print(f"\tv1: {v1}\n\tv2: {v2}")
+        print(f"\tvisited: {visited}")
         print(f"\tforward: {path_forward}\n\tbackward: {path_backward}")
 
     while q1 or q2:
-        n1 = q1.popleft()
-        n2 = q2.popleft()
+        step_f = q1.popleft()
+        step_b = q2.popleft()
 
-        if n1 in v2 or n2 in v1:  # path found
+        print_all(f"\nSTART ITER --- step_f={step_f}, step_b={step_b}")
+        print()
+
+        if step_f in v2 and step_b in v1:
+            print(f"step_f in v2 --- {step_f} in {v2}")
+            print(f"step_b in v1 --- {step_b} in {v1}")
+            x_f = path_forward.copy()
+            x_b = path_backward.copy()
+            print(f"for={x_f}, bac={x_b}")
+        elif step_b in v1:
+            x_f = path_forward.copy()
+            x_b = path_backward.copy()
+            print(f"step_b in v1 --- {step_b} in {v1}")
+            print(f"for={x_f}, bac={x_b}")
+        elif step_f in v2:
+            x_f = path_forward.copy()
+            x_b = path_backward.copy()
+            print(f"step_f in v2 --- {step_f} in {v2}")
+            print(f"for={x_f}, bac={x_b}")
+        else:
+            print("neither step in either visited")
+
+        # if n1 in v2 or n2 in v1:  # path found
+        # print(f"if {step_f} in {v2} or {step_b} in {v1} --- {step_f in v2 or step_b in v1}")
+        # print(f"result would be {list(path_forward + path_backward)}")
+        # print(f"if {step_f} in {visited} or {step_b} in {visited} --- {step_f in visited or step_b in visited}")
+        # if step_b in visited and step_f in visited:
+        #     x_f = path_forward.copy()
+        #     x_f.append(step_f)
+        #     x_b = path_backward.copy()
+        #     x_b.appendleft(step_b)
+        #     x_r = list(set(x_f + x_b))
+        #     print(f"BOTH in vis == {x_r}")
+        #     return x_r
+        # elif step_f in visited:
+        #     x = path_forward.copy()
+        #     x.append(step_f)
+        #     x_r = list(x + path_backward)
+        #     print(f"step_f in vis == {x_r}")
+        # elif step_b in visited:
+        #     x = path_backward.copy()
+        #     x.appendleft(step_b)
+        #     x_r = list(path_forward + x)
+        #     print(f"step_b in vis == {x_r}")
+
+        if step_f in v2 or step_b in v1:
             combined = list(path_forward + path_backward)
-            return combined if combined else [start]
+            print_all(f"*** step_f={step_f}, step_b={step_b}, combined={combined}")
+            res = combined
+            print(f"\n\nENDING FUNCTION-----returned: {res}")
+            return res
 
-        for connection in graph[n1]:
+        for connection in graph[step_f]:
             if connection not in v1:
                 v1.add(connection)
+                visited.add(connection)
                 q1.append(connection)
 
-        for connection in graph[n2]:
+        for connection in graph[step_b]:
             if connection not in v2:
                 v2.add(connection)
+                visited.add(connection)
                 q2.append(connection)
 
-        path_forward.append(n1)
-        if n2 != n1:
-            path_backward.appendleft(n2)
+        path_forward.append(step_f)
+        # if step_b != step_f:
+        path_backward.appendleft(step_b)
+
+        print_all(f"\nEND ITER")
+        # visited.add(n1)
+        # visited.add(n2)
+        #
+        # print_all(f"\nBEFORE --- step_f={step_f}, step_b={step_b}")
+        # print_all(f"AFTER")
+        # print_all(f"forward_q={q1}\nbackward_q={q2}")
--- src/traversal/test_bidirectional_search.py
+++ src/traversal/test_bidirectional_search.py
@@ -2,7 +2,7 @@ import unittest
 from bidirectional_search import *
 
 
-class MyTestCase(unittest.TestCase):
+class TestCaseOne(unittest.TestCase):
     def setUp(self) -> None:
         self.graph = {
             0: {1, 2},
@@ -46,12 +46,26 @@ class MyTestCase(unittest.TestCase):
         self.assertIn(actual, self.zero_to[end], fail_msg)
 
     def test_valid_zero_to_self(self):
-        print("TESTING ZERO TO 0")
         self.make_tester_zero_to(0)
 
     def test_valid_zero_to_one(self):
-        print("TESTING ZERO TO ONE")
         self.make_tester_zero_to(1)
+
+    def test_valid_zero_to_two(self):
+        self.make_tester_zero_to(2)
+
+    def test_valid_zero_to_three(self):
+        self.make_tester_zero_to(3)
+
+    def test_valid_zero_to_four(self):
+        self.make_tester_zero_to(4)
+
+    # @unittest.skip
+    def test_valid_zero_to_five(self):
+        self.make_tester_zero_to(5)
+
+    def test_zero_to_six(self):
+        self.make_tester_zero_to(6)
 
 
 if __name__ == '__main__':
